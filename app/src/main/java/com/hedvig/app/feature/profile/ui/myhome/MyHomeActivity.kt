02738758573ac7e43c055c4f508fa66e255f75f5
Oscar Nylander <oscar@hedvig.com>
2019-10-14 13:42:23 +0200

    Various fixes to My Home-screen

--- app/src/main/java/com/hedvig/app/feature/profile/ui/myhome/MyHomeActivity.kt
+++ app/src/main/java/com/hedvig/app/feature/profile/ui/myhome/MyHomeActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
-import androidx.lifecycle.Observer
+import com.hedvig.android.owldroid.graphql.ProfileQuery
 import com.hedvig.android.owldroid.type.InsuranceType
 import com.hedvig.app.BaseActivity
 import com.hedvig.app.R
 import com.hedvig.app.feature.profile.ui.ProfileViewModel
 import com.hedvig.app.util.extensions.compatColor
 import com.hedvig.app.util.extensions.compatSetTint
+import com.hedvig.app.util.extensions.observe
 import com.hedvig.app.util.extensions.setupLargeTitle
 import com.hedvig.app.util.extensions.view.remove
 import com.hedvig.app.util.extensions.view.show
@@ -37,64 +38,97 @@ class MyHomeActivity : BaseActivity() {
             ChangeHomeInfoDialog().show(supportFragmentManager, ChangeHomeInfoDialog.TAG)
         }
 
-        loadData()
+        profileViewModel.data.observe(lifecycleOwner = this) { profileData ->
+            profileData?.insurance?.let { bind(it) }
+        }
     }
 
-    private fun loadData() {
-        profileViewModel.data.observe(this, Observer { profileData ->
-            loadingSpinner.remove()
-            sphereContainer.show()
-
-            profileData?.insurance?.let { insuranceData ->
-                sphereText.text = insuranceData.address
-                postalNumber.text = insuranceData.postalNumber
-                insuranceType.text =
-                    when (insuranceData.type) {
-                        InsuranceType.BRF -> resources.getString(R.string.PROFILE_MY_HOME_INSURANCE_TYPE_BRF)
-                        InsuranceType.STUDENT_BRF -> resources.getString(R.string.PROFILE_MY_HOME_INSURANCE_TYPE_BRF)
-                        InsuranceType.RENT -> resources.getString(R.string.PROFILE_MY_HOME_INSURANCE_TYPE_RENT)
-                        InsuranceType.STUDENT_RENT -> resources.getString(R.string.PROFILE_MY_HOME_INSURANCE_TYPE_RENT)
-                        else -> ""
-                    }
-                livingSpace.text = interpolateTextKey(
-                    resources.getString(R.string.PROFILE_MY_HOME_SQUARE_METER_POSTFIX),
-                    "SQUARE_METER" to insuranceData.livingSpace.toString()
-                )
-                infoContainer.show()
-                bindOrHide(profileData.insurance.ancillaryArea?.toString(), ancillaryAreaLabel, ancillaryArea)
-                bindOrHide(profileData.insurance.yearOfConstruction?.toString(), yearOfConstructionLabel, yearOfConstruction)
-                bindOrHide(profileData.insurance.numberOfBathrooms?.toString(), bathroomsLabel, bathrooms)
-                bindOrHide(profileData.insurance.isSubleted?.let {
-                    if (it) resources.getString(R.string.HOUSE_INFO_SUBLETED_TRUE) else resources.getString(R.string.HOUSE_INFO_SUBLETED_FALSE)
-                }, subletedLabel, subleted)
-
-                profileData.insurance.extraBuildings?.let { extraBuildings ->
-                    additionalBuildingsTitle.show()
-
-                    extraBuildings.forEach { extraBuilding ->
-                        val row = LayoutInflater
-                            .from(additionalBuildingsContainer.context)
-                            .inflate(R.layout.additional_buildings_row, additionalBuildingsContainer, false)
-                        row.title.text = extraBuilding.displayName
-
-                        var bodyText = interpolateTextKey(
-                            resources.getString(R.string.HOUSE_INFO_BOYTA_SQUAREMETERS),
-                            "HOUSE_INFO_AMOUNT_BOYTA" to extraBuilding.area
-                        )
-                        if (extraBuilding.isHasWaterConnected) {
-                            bodyText += ", " + resources.getString(R.string.HOUSE_INFO_CONNECTED_WATER)
-                        }
-                        row.body.text = bodyText
-                        additionalBuildingsContainer.addView(row)
-                    }
-
-                    additionalBuildingsContainer.show()
-                } ?: run {
-                    additionalBuildingsTitle.remove()
-                    additionalBuildingsContainer.remove()
-                }
+    private fun bind(data: ProfileQuery.Insurance) {
+        loadingSpinner.remove()
+        sphereContainer.show()
+        sphereText.text = data.address
+        address.text = data.address
+        postalNumber.text = data.postalNumber
+        insuranceType.text =
+            when (data.type) {
+                InsuranceType.BRF -> resources.getString(R.string.PROFILE_MY_HOME_INSURANCE_TYPE_BRF)
+                InsuranceType.STUDENT_BRF -> resources.getString(R.string.PROFILE_MY_HOME_INSURANCE_TYPE_BRF)
+                InsuranceType.RENT -> resources.getString(R.string.PROFILE_MY_HOME_INSURANCE_TYPE_RENT)
+                InsuranceType.STUDENT_RENT -> resources.getString(R.string.PROFILE_MY_HOME_INSURANCE_TYPE_RENT)
+                InsuranceType.HOUSE -> getString(R.string.MY_HOME_INSURANCE_TYPE_HOUSE)
+                else -> ""
             }
-        })
+        livingSpace.text = interpolateTextKey(
+            resources.getString(R.string.PROFILE_MY_HOME_SQUARE_METER_POSTFIX),
+            "SQUARE_METER" to data.livingSpace.toString()
+        )
+        bindHouseData(data)
+        infoContainer.show()
+
+        data.extraBuildings?.let { extraBuildings ->
+            bindExtraBuildings(extraBuildings)
+        } ?: run {
+            removeExtraBuildingsViews()
+        }
+    }
+
+    private fun bindHouseData(data: ProfileQuery.Insurance) {
+        bindOrHide(
+            data.ancillaryArea?.toString(),
+            ancillaryAreaLabel,
+            ancillaryArea
+        )
+        bindOrHide(
+            data.yearOfConstruction?.toString(),
+            yearOfConstructionLabel,
+            yearOfConstruction
+        )
+        bindOrHide(
+            data.numberOfBathrooms?.toString(),
+            bathroomsLabel,
+            bathrooms
+        )
+        bindOrHide(data.isSubleted?.let {
+            if (it) resources.getString(R.string.HOUSE_INFO_SUBLETED_TRUE) else resources.getString(
+                R.string.HOUSE_INFO_SUBLETED_FALSE
+            )
+        }, subletedLabel, subleted)
+    }
+
+    private fun bindExtraBuildings(extraBuildings: List<ProfileQuery.ExtraBuilding>) {
+        if (extraBuildings.isEmpty()) {
+            removeExtraBuildingsViews()
+            return
+        }
+        additionalBuildingsTitle.show()
+
+        extraBuildings.forEach { extraBuilding ->
+            val row = LayoutInflater
+                .from(additionalBuildingsContainer.context)
+                .inflate(
+                    R.layout.additional_buildings_row,
+                    additionalBuildingsContainer,
+                    false
+                )
+            row.title.text = extraBuilding.displayName
+
+            var bodyText = interpolateTextKey(
+                resources.getString(R.string.HOUSE_INFO_BOYTA_SQUAREMETERS),
+                "HOUSE_INFO_AMOUNT_BOYTA" to extraBuilding.area
+            )
+            if (extraBuilding.isHasWaterConnected) {
+                bodyText += ", " + resources.getString(R.string.HOUSE_INFO_CONNECTED_WATER)
+            }
+            row.body.text = bodyText
+            additionalBuildingsContainer.addView(row)
+        }
+
+        additionalBuildingsContainer.show()
+    }
+
+    private fun removeExtraBuildingsViews() {
+        additionalBuildingsTitle.remove()
+        additionalBuildingsContainer.remove()
     }
 
     private fun bindOrHide(text: String?, label: View, body: TextView) {
