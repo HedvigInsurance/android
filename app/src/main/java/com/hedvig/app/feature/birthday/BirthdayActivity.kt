4a1eb3795c2b78b932f099631121b39c00db2bbe
Mathias Ohrstrom <MathiasOhrstrom@gmail.com>
2020-01-20 10:43:45 +0100

    changed name on LottieAnimationView

--- app/src/main/java/com/hedvig/app/feature/birthday/BirthdayActivity.kt
+++ app/src/main/java/com/hedvig/app/feature/birthday/BirthdayActivity.kt
@@ -9,7 +9,6 @@ import com.hedvig.app.feature.loggedin.ui.LoggedInActivity
 import com.hedvig.app.util.extensions.view.hide
 import com.hedvig.app.util.extensions.view.show
 import kotlinx.android.synthetic.main.activity_birthday.*
-import timber.log.Timber
 
 
 class BirthdayActivity : AppCompatActivity() {
@@ -26,9 +25,9 @@ class BirthdayActivity : AppCompatActivity() {
         fromText.hide()
 
         //Birthday animation
-        confetti_animation_view.addAnimatorListener(object : Animator.AnimatorListener {
+        confetti_animation.addAnimatorListener(object : Animator.AnimatorListener {
             override fun onAnimationEnd(animation: Animator) {
-                confetti_animation_view.hide()
+                confetti_animation.hide()
                 startActivity(Intent(applicationContext, LoggedInActivity::class.java))
             }
 
@@ -48,8 +47,8 @@ class BirthdayActivity : AppCompatActivity() {
 
         animateText()
 
-        confetti_animation_view.setMaxFrame(animationEndFrame)
-        confetti_animation_view.playAnimation()
+        confetti_animation.setMaxFrame(animationEndFrame)
+        confetti_animation.playAnimation()
     }
 
     private fun animateText() {
