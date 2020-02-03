86cd804de7091be7aa79de0e1cfc9dae9f91562e
Mathias Ohrstrom <MathiasOhrstrom@gmail.com>
2020-02-03 16:49:53 +0100

    done with coverage sheet

---
+++ app/src/main/java/com/hedvig/app/feature/keygear/CoverageInfoAdapter.kt
@@ -0,0 +1,49 @@
+package com.hedvig.app.feature.keygear
+
+import android.graphics.drawable.PictureDrawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.hedvig.app.R
+import com.hedvig.app.util.svg.GlideRequest
+import kotlinx.android.synthetic.main.coverage_info_layout.view.*
+
+class CoverageInfoAdapter(
+    private val data: List<KeyGearItemCoverageBox>,
+    private val requestBuilder: GlideRequest<PictureDrawable>
+) :
+    RecyclerView.Adapter<CoverageInfoAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.coverage_info_layout,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val bulletPoint = data[position]
+        holder.apply {
+
+            requestBuilder.load(bulletPoint.icon.iconByTheme(bulletPointIcon.context))
+                .into(bulletPointIcon)
+
+            bulletPointTitle.text = bulletPoint.title
+            bulletPointDescription.text = bulletPoint.description
+        }
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val bulletPointIcon: ImageView = itemView.bulletPointIcon
+        val bulletPointTitle: TextView = itemView.bulletPointTitle
+        val bulletPointDescription: TextView = itemView.bulletPointDescription
+    }
+}
--- app/src/main/java/com/hedvig/app/feature/keygear/KeyGearCoverageBottomSheet.kt
+++ app/src/main/java/com/hedvig/app/feature/keygear/KeyGearCoverageBottomSheet.kt
@@ -2,18 +2,32 @@ package com.hedvig.app.feature.keygear
 
 import android.app.Dialog
 import android.os.Bundle
+import android.os.Parcelable
 import com.hedvig.app.R
 import com.hedvig.app.ui.fragment.RoundedBottomSheetDialogFragment
+import com.hedvig.app.util.apollo.ThemedIconUrls
+import com.hedvig.app.util.svg.buildRequestBuilder
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.dialog_key_gear_coverage.*
 
 class KeyGearCoverageBottomSheet : RoundedBottomSheetDialogFragment() {
+
+    private val requestBuilder by lazy { buildRequestBuilder() }
 
     override fun getTheme() = R.style.NoTitleBottomSheetDialogTheme
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = super.onCreateDialog(savedInstanceState)
-
         dialog.setContentView(R.layout.dialog_key_gear_coverage)
 
+        requestBuilder.load(mock.icon.iconByTheme(requireContext()))
+            .into(dialog.icon)
+
+        dialog.title.text = mock.title
+        dialog.description.text = mock.description
+
+        dialog.bulletPointsRecyclerView.adapter =
+            CoverageInfoAdapter(mock.boxes, requestBuilder)
 
         return dialog
     }
@@ -26,3 +40,77 @@ class KeyGearCoverageBottomSheet : RoundedBottomSheetDialogFragment() {
         }
     }
 }
+
+@Parcelize
+data class KeyGearItemCoverage(
+    val id: String,
+    val icon: ThemedIconUrls,
+    val title: String,
+    val description: String,
+    val boxes: List<KeyGearItemCoverageBox>
+) : Parcelable
+
+@Parcelize
+data class KeyGearItemCoverageBox(
+    val id: String,
+    val icon: ThemedIconUrls,
+    val title: String,
+    val description: String
+) : Parcelable
+
+val mock = KeyGearItemCoverage(
+    "123",
+    ThemedIconUrls(
+        "https://image.flaticon.com/icons/svg/2210/2210794.svg",
+        "https://image.flaticon.com/icons/svg/2210/2210794.svg"
+    ),
+    "hallå",
+    "Vi försöker reparera i första hand, men om din mobiltelefon skulle behöva ersättas helt (ex. om den blivit stulen) ersätts du med 70% av vad du köpte den för.",
+    listOf(
+        KeyGearItemCoverageBox(
+            "123",
+            ThemedIconUrls(
+                "https://image.flaticon.com/icons/svg/2210/2210794.svg",
+                "https://image.flaticon.com/icons/svg/2210/2210794.svg"
+            ),
+            "Ersättning",
+            "Hedvig täcker hela kostnaden för en reparation minus självrisk (1 500 kr) eller det uppskattade värdet på telefonen minus självrisk om den inte går att reparera"
+        ),
+        KeyGearItemCoverageBox(
+            "123",
+            ThemedIconUrls(
+                "https://image.flaticon.com/icons/svg/2210/2210794.svg",
+                "https://image.flaticon.com/icons/svg/2210/2210794.svg"
+            ),
+            "Ersättning",
+            "Hedvig täcker hela kostnaden för en reparation minus självrisk (1 500 kr) eller det uppskattade värdet på telefonen minus självrisk om den inte går att reparera"
+        ),
+        KeyGearItemCoverageBox(
+            "123",
+            ThemedIconUrls(
+                "https://image.flaticon.com/icons/svg/2210/2210794.svg",
+                "https://image.flaticon.com/icons/svg/2210/2210794.svg"
+            ),
+            "Ersättning",
+            "Hedvig täcker hela kostnaden för en reparation minus självrisk (1 500 kr) eller det uppskattade värdet på telefonen minus självrisk om den inte går att reparera"
+        ),
+        KeyGearItemCoverageBox(
+            "123",
+            ThemedIconUrls(
+                "https://image.flaticon.com/icons/svg/2210/2210794.svg",
+                "https://image.flaticon.com/icons/svg/2210/2210794.svg"
+            ),
+            "Ersättning",
+            "Hedvig täcker hela kostnaden för en reparation minus självrisk (1 500 kr) eller det uppskattade värdet på telefonen minus självrisk om den inte går att reparera"
+        ),
+        KeyGearItemCoverageBox(
+            "123",
+            ThemedIconUrls(
+                "https://image.flaticon.com/icons/svg/2210/2210794.svg",
+                "https://image.flaticon.com/icons/svg/2210/2210794.svg"
+            ),
+            "Ersättning",
+            "Hedvig täcker hela kostnaden för en reparation minus självrisk (1 500 kr) eller det uppskattade värdet på telefonen minus självrisk om den inte går att reparera"
+        )
+    )
+)
