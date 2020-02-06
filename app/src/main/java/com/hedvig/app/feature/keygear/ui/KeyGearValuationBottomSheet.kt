46f4aafe84612b1d5472943348fd1404d14eff0e
Mathias Ohrstrom <MathiasOhrstrom@gmail.com>
2020-02-06 16:11:53 +0100

    gets selected date

--- app/src/main/java/com/hedvig/app/feature/keygear/ui/KeyGearValuationBottomSheet.kt
+++ app/src/main/java/com/hedvig/app/feature/keygear/ui/KeyGearValuationBottomSheet.kt
@@ -3,20 +3,39 @@ package com.hedvig.app.feature.keygear.ui
 import android.app.Dialog
 import android.os.Bundle
 import com.hedvig.app.R
+import com.hedvig.app.feature.keygear.ui.itemdetail.KeyGearItemDetailViewModel
 import com.hedvig.app.feature.keygear.ui.itemdetail.PurchaseDateYearMonthPicker
 import com.hedvig.app.ui.fragment.RoundedBottomSheetDialogFragment
+import com.hedvig.app.util.extensions.observe
 import kotlinx.android.synthetic.main.dialog_key_gear_valuation.*
+import org.koin.android.viewmodel.ext.android.sharedViewModel
+import timber.log.Timber
+import java.text.DateFormatSymbols
 
 class KeyGearValuationBottomSheet : RoundedBottomSheetDialogFragment() {
 
     override fun getTheme() = R.style.NoTitleBottomSheetDialogTheme
+
+    private val model: KeyGearItemDetailViewModel by sharedViewModel()
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = super.onCreateDialog(savedInstanceState)
         dialog.setContentView(R.layout.dialog_key_gear_valuation)
 
         dialog.dateInput.setOnClickListener {
-            fragmentManager?.let { it1 -> PurchaseDateYearMonthPicker.newInstance("title").show(it1, PurchaseDateYearMonthPicker.TAG) }
+            fragmentManager?.let { fragmentManager ->
+                PurchaseDateYearMonthPicker.newInstance("title")
+                    .show(fragmentManager, PurchaseDateYearMonthPicker.TAG)
+            }
+        }
+
+        model.purchaseDate.observe(this) { yearMonth ->
+            yearMonth?.let {
+                // FIXME crashes when january is picked
+                Timber.d("value ${yearMonth.month.value}")
+                dialog.dateInput.text =
+                    "${DateFormatSymbols().months[yearMonth.month.value]} ${yearMonth.year}"
+            }
         }
 
 
