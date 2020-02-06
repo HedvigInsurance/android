4da44cbe28a9e54f0543d293e0d346cc9e83d4f7
Oscar Nylander <oscar@hedvig.com>
2020-02-06 15:36:52 +0100

    Add a toolbar with a nice scroll effect

--- app/src/main/java/com/hedvig/app/feature/keygear/ui/itemdetail/KeyGearItemDetailActivity.kt
+++ app/src/main/java/com/hedvig/app/feature/keygear/ui/itemdetail/KeyGearItemDetailActivity.kt
@@ -2,11 +2,18 @@ package com.hedvig.app.feature.keygear.ui.itemdetail
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.core.view.doOnNextLayout
+import androidx.core.view.updatePadding
+import androidx.core.widget.NestedScrollView
 import com.hedvig.app.BaseActivity
 import com.hedvig.app.R
+import com.hedvig.app.util.boundedColorLerp
+import com.hedvig.app.util.extensions.compatColor
+import com.hedvig.app.util.extensions.compatDrawable
 import com.hedvig.app.util.extensions.view.useEdgeToEdge
+import dev.chrisbanes.insetter.doOnApplyWindowInsets
 import kotlinx.android.synthetic.main.activity_key_gear_item_detail.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -20,6 +27,30 @@ class KeyGearItemDetailActivity : BaseActivity(R.layout.activity_key_gear_item_d
         supportPostponeEnterTransition()
 
         root.useEdgeToEdge()
+        toolbar.doOnApplyWindowInsets { view, insets, initialState ->
+            view.updatePadding(top = initialState.paddings.top + insets.systemWindowInsetTop)
+        }
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        toolbar.navigationIcon = compatDrawable(R.drawable.ic_back)
+        scrollView.setOnScrollChangeListener { _: NestedScrollView?, _: Int, scrollY: Int, _: Int, _: Int ->
+            val positionInSpan =
+                scrollY - (photos.height - (toolbar.height * 2.0f))
+            val percentage = positionInSpan / toolbar.height
+
+            // Avoid some unnecessary background color updates
+            if (percentage < -1 || percentage > 2) {
+                return@setOnScrollChangeListener
+            }
+
+            toolbar.setBackgroundColor(
+                boundedColorLerp(
+                    Color.TRANSPARENT,
+                    compatColor(R.color.background_elevation_1),
+                    percentage
+                )
+            )
+        }
 
         photos.adapter = PhotosAdapter(
             listOf(
