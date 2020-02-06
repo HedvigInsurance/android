a84cd99374266d1db0bd264463105cadd0c26530
Mathias Ohrstrom <MathiasOhrstrom@gmail.com>
2020-02-06 11:52:53 +0100

    returns null if there is an error saving the image and showing a toast message to the user

--- app/src/main/java/com/hedvig/app/feature/keygear/ui/ReceiptActivity.kt
+++ app/src/main/java/com/hedvig/app/feature/keygear/ui/ReceiptActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Environment
 import android.os.Parcelable
+import android.widget.Toast
 import androidx.core.content.FileProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
@@ -13,9 +14,11 @@ import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 import com.hedvig.app.BaseActivity
 import com.hedvig.app.R
+import com.hedvig.app.util.extensions.makeToast
 import com.hedvig.app.util.extensions.view.setHapticClickListener
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.activity_receipt.*
+import timber.log.Timber
 import java.io.File
 import java.io.FileOutputStream
 
@@ -46,31 +49,35 @@ class ReceiptActivity : BaseActivity(R.layout.activity_receipt) {
                         resource: Bitmap,
                         transition: Transition<in Bitmap>?
                     ) {
-                        val filePath = saveImage(resource, "receipt")
+                        saveImage(resource, "receipt")?.let { filePath ->
 
-                        val sendIntent = Intent().apply {
+                            val sendIntent = Intent().apply {
 
-                            action = Intent.ACTION_SEND
-                            type = "image/jpg"
+                                action = Intent.ACTION_SEND
+                                type = "image/jpg"
 
-                            this.data = FileProvider.getUriForFile(
-                                applicationContext,
-                                getString(R.string.file_provider_authority),
-                                File(filePath)
-                            )
-
-                            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-                            putExtra(
-                                Intent.EXTRA_STREAM,
-                                FileProvider.getUriForFile(
+                                this.data = FileProvider.getUriForFile(
                                     applicationContext,
                                     getString(R.string.file_provider_authority),
                                     File(filePath)
                                 )
-                            )
+
+                                flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                                putExtra(
+                                    Intent.EXTRA_STREAM,
+                                    FileProvider.getUriForFile(
+                                        applicationContext,
+                                        getString(R.string.file_provider_authority),
+                                        File(filePath)
+                                    )
+                                )
+                            }
+                            val shareIntent = Intent.createChooser(sendIntent, null)
+                            startActivity(shareIntent)
+                        } ?: run {
+                            makeToast("Error sharing image", Toast.LENGTH_SHORT)
                         }
-                        val shareIntent = Intent.createChooser(sendIntent, null)
-                        startActivity(shareIntent)
+
                     }
                 })
 
@@ -81,7 +88,7 @@ class ReceiptActivity : BaseActivity(R.layout.activity_receipt) {
             .into(receipt)
     }
 
-    private fun saveImage(finalBitmap: Bitmap, image_name: String): String {
+    private fun saveImage(finalBitmap: Bitmap, image_name: String): String? {
         val root = getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString()
         val myDir = File(root)
         myDir.mkdirs()
@@ -99,7 +106,8 @@ class ReceiptActivity : BaseActivity(R.layout.activity_receipt) {
             file.absolutePath
         } catch (e: Exception) {
             e.printStackTrace()
-            ""
+            Timber.e("Error saving image")
+            null
         }
     }
 }
