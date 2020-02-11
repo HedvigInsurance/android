7a538a02144ac64ba18f71fd4945e5a966695a60
Oscar Nylander <oscar@hedvig.com>
2020-02-11 11:05:47 +0100

    Illustration/text for Key Gear Tab Screen

---
+++ app/src/main/java/com/hedvig/app/feature/keygear/ui/tab/KeyGearItemsDiffCallback.kt
@@ -0,0 +1,19 @@
+package com.hedvig.app.feature.keygear.ui.tab
+
+import androidx.recyclerview.widget.DiffUtil
+import com.hedvig.android.owldroid.graphql.KeyGearItemsQuery
+
+class KeyGearItemsDiffCallback(
+    private val old: List<KeyGearItemsQuery.KeyGearItemsSimple>,
+    private val new: List<KeyGearItemsQuery.KeyGearItemsSimple>
+) : DiffUtil.Callback() {
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+        old[oldItemPosition].fragments.keyGearItemFragment.id == new[newItemPosition].fragments.keyGearItemFragment.id
+
+    override fun getOldListSize() = old.size
+
+    override fun getNewListSize() = new.size
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+        old[oldItemPosition] == new[newItemPosition]
+}
