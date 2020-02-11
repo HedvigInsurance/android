84f84d24131fad28e52d3dfa223529392a87cda6
Oscar Nylander <oscar@hedvig.com>
2020-02-11 13:22:41 +0100

    Markdown support!
    That was easy. Thanks markwon

--- app/src/main/java/com/hedvig/app/util/extensions/TextViewExtensions.kt
+++ app/src/main/java/com/hedvig/app/util/extensions/TextViewExtensions.kt
@@ -2,11 +2,16 @@ package com.hedvig.app.util.extensions
 
 import android.graphics.Paint
 import android.widget.TextView
+import io.noties.markwon.Markwon
 
 fun TextView.setStrikethrough(strikethrough: Boolean) {
-    if (strikethrough) {
-        paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+    paintFlags = if (strikethrough) {
+        paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
     } else {
-        paintFlags = paintFlags and Paint.STRIKE_THRU_TEXT_FLAG
+        paintFlags and Paint.STRIKE_THRU_TEXT_FLAG
     }
+}
+
+fun TextView.setMarkdownText(text: String) {
+    Markwon.create(context).setMarkdown(this, text)
 }
