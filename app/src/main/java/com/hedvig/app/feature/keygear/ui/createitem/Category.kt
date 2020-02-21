0d812a1f18a8a1b29743b8e83384ff1f48be6453
Oscar Nylander <oscar@hedvig.com>
2020-02-21 15:59:06 +0100

    LAAAAAST MIIIILE

--- app/src/main/java/com/hedvig/app/feature/keygear/ui/createitem/Category.kt
+++ app/src/main/java/com/hedvig/app/feature/keygear/ui/createitem/Category.kt
@@ -19,8 +19,9 @@ val KeyGearItemCategory.label: Int
             KeyGearItemCategory.TV -> R.string.ITEM_TYPE_TV
             KeyGearItemCategory.JEWELRY -> R.string.ITEM_TYPE_JEWELRY
             KeyGearItemCategory.WATCH -> R.string.ITEM_TYPE_WATCH
+            KeyGearItemCategory.BIKE -> R.string.ITEM_TYPE_BIKE
             KeyGearItemCategory.SMART_WATCH -> R.string.ITEM_TYPE_WATCH //TODO get correct translation
-            else -> 0 // Null string resource - does not cause a crash, simply does nothing
+            else -> 0 // Null string resource - causes a crash, use with caution
         }
 
 @get:DrawableRes
@@ -31,7 +32,8 @@ val KeyGearItemCategory?.illustration: Int
             KeyGearItemCategory.PHONE -> R.drawable.illustration_phone
             KeyGearItemCategory.TV -> R.drawable.illustration_tv
             KeyGearItemCategory.JEWELRY -> R.drawable.illustration_jewelry
-            KeyGearItemCategory.WATCH -> R.drawable.illustration_jewelry //TODO get illustration
-            KeyGearItemCategory.SMART_WATCH -> R.drawable.illustration_jewelry //TODO get illustration
+            KeyGearItemCategory.WATCH -> R.drawable.illustration_clock
+            KeyGearItemCategory.BIKE -> R.drawable.illustration_bicycle
+            KeyGearItemCategory.SMART_WATCH -> R.drawable.illustration_clock
             else -> 0
         }
