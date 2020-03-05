0764c452072576b76b5fd9c02688d2648d7af481
Mathias Öhrström <MathiasOhrstrom@gmail.com>
2020-03-05 09:50:09 +0100

    loggs out when market is changed

--- app/src/main/java/com/hedvig/app/feature/settings/SettingsActivity.kt
+++ app/src/main/java/com/hedvig/app/feature/settings/SettingsActivity.kt
@@ -10,11 +10,21 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.firebase.iid.FirebaseInstanceId
 import com.hedvig.app.BaseActivity
 import com.hedvig.app.R
+import com.hedvig.app.feature.chat.viewmodel.UserViewModel
+import com.hedvig.app.service.LoginStatusService
+import com.hedvig.app.util.extensions.setAuthenticationToken
+import com.hedvig.app.util.extensions.setIsLoggedIn
+import com.hedvig.app.util.extensions.showAlert
+import com.hedvig.app.util.extensions.storeBoolean
+import com.hedvig.app.util.extensions.triggerRestartActivity
 import kotlinx.android.synthetic.main.activity_settings.*
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 class SettingsActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -28,6 +38,7 @@ class SettingsActivity : BaseActivity() {
     }
 
     class PreferenceFragment : PreferenceFragmentCompat() {
+        private val userViewModel: UserViewModel by sharedViewModel()
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.preferences, rootKey)
 
@@ -42,6 +53,33 @@ class SettingsActivity : BaseActivity() {
                             .from(v)
                             .apply()
 
+                    }
+                    true
+                }
+            }
+
+            val marketPreference = findPreference<ListPreference>(SETTINGS_MARKET)
+            marketPreference?.let { mp ->
+                val oldValue = mp.value
+                mp.setOnPreferenceChangeListener { preference, newValue ->
+                    if (oldValue != newValue) {
+                        //TODO real text for dialog
+                        requireContext().showAlert(
+                            R.string.alert_title,
+                            R.string.alert_message,
+                            positiveAction = {
+                                userViewModel.logout {
+                                    requireContext().storeBoolean(
+                                        LoginStatusService.IS_VIEWING_OFFER,
+                                        false
+                                    )
+                                    requireContext().setAuthenticationToken(null)
+                                    requireContext().setIsLoggedIn(false)
+                                    FirebaseInstanceId.getInstance().deleteInstanceId()
+                                    requireActivity().triggerRestartActivity()
+                                }
+                            }
+                        )
                     }
                     true
                 }
@@ -89,6 +127,7 @@ class SettingsActivity : BaseActivity() {
         const val SETTING_THEME = "theme"
         const val SETTING_LANGUAGE = "language"
         const val SETTING_NOTIFICATIONS = "notifications"
+        const val SETTINGS_MARKET = "market"
         fun newInstance(context: Context) = Intent(context, SettingsActivity::class.java)
     }
 }
