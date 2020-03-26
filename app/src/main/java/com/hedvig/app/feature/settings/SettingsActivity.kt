c34bfa2573d121f4195aa6dbbb06d94594d29d1d
Mathias Öhrström <MathiasOhrstrom@gmail.com>
2020-03-26 09:34:42 +0100

    removed todo :)

--- app/src/main/java/com/hedvig/app/feature/settings/SettingsActivity.kt
+++ app/src/main/java/com/hedvig/app/feature/settings/SettingsActivity.kt
@@ -71,7 +71,6 @@ class SettingsActivity : BaseActivity() {
                 val oldValue = mp.value
                 mp.setOnPreferenceChangeListener { _, newValue ->
                     if (oldValue != newValue) {
-                        //TODO real text for dialog
                         requireContext().showAlert(
                             R.string.SETTINGS_ALERT_CHANGE_MARKET_TITLE,
                             R.string.SETTINGS_ALERT_CHANGE_MARKET_TEXT,
