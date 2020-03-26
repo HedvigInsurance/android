c037ef8df7b87137af8f8beb9bc7b46e37494ad8
Oscar Nylander <oscar@hedvig.com>
2020-03-26 15:40:46 +0100

    Fix

--- app/src/main/java/com/hedvig/app/util/apollo/GraphQLUtils.kt
+++ app/src/main/java/com/hedvig/app/util/apollo/GraphQLUtils.kt
@@ -6,9 +6,12 @@ import com.hedvig.app.feature.settings.Language
 import com.hedvig.app.getLocale
 
 fun defaultLocale(context: Context) =
-    when (getLocale(Language.fromSettings(context)?.apply(context) ?: context).language) {
-        "en" -> com.hedvig.android.owldroid.type.Locale.EN_SE
-        else -> com.hedvig.android.owldroid.type.Locale.SV_SE
+    when (getLocale(Language.fromSettings(context)?.apply(context) ?: context).toString()) {
+        "en_NO" -> Locale.EN_NO
+        "nb_NO" -> Locale.NB_NO
+        "sv_SE" -> Locale.SV_SE
+        "en_SE" -> Locale.EN_SE
+        else -> Locale.EN_SE
     }
 
 fun Locale.toLocaleString() = when (this) {
