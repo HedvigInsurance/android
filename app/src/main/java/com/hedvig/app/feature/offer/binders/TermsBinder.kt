7e723a324ae744da687fb84f4ebcde8ee82c45e6
Oscar Nylander <oscar@hedvig.com>
2020-04-08 18:28:42 +0200

    Assorted offer fixes

--- app/src/main/java/com/hedvig/app/feature/offer/binders/TermsBinder.kt
+++ app/src/main/java/com/hedvig/app/feature/offer/binders/TermsBinder.kt
@@ -1,22 +1,24 @@
 package com.hedvig.app.feature.offer.binders
 
-import android.view.LayoutInflater
 import android.widget.LinearLayout
 import com.hedvig.android.owldroid.graphql.OfferQuery
+import com.hedvig.app.BASE_MARGIN_DOUBLE
 import com.hedvig.app.feature.dashboard.ui.contractcoverage.InsurableLimitsAdapter
 import com.hedvig.app.feature.offer.OfferTracker
 import com.hedvig.app.feature.offer.TermsAdapter
+import com.hedvig.app.ui.decoration.GridSpacingItemDecoration
 import kotlinx.android.synthetic.main.offer_terms_area.view.*
 
 class TermsBinder(
     private val root: LinearLayout,
-    private val tracker: OfferTracker
+    tracker: OfferTracker
 ) {
-
     private var previousData: OfferQuery.Data? = null
 
-    private val layoutInflater: LayoutInflater by lazy {
-        LayoutInflater.from(root.context)
+    init {
+        root.insurableLimits.adapter = InsurableLimitsAdapter()
+        root.insurableLimits.addItemDecoration((GridSpacingItemDecoration(BASE_MARGIN_DOUBLE)))
+        root.termsRecyclerView.adapter = TermsAdapter(tracker)
     }
 
     fun bind(data: OfferQuery.Data) = root.apply {
@@ -25,13 +27,11 @@ class TermsBinder(
         }
         val quote = data.lastQuoteOfMember.asCompleteQuote
 
-        insurableLimits.adapter = InsurableLimitsAdapter()
         quote?.insurableLimits?.let { limits ->
             (insurableLimits.adapter as InsurableLimitsAdapter).items =
                 limits.map { it.fragments.insurableLimitsFragment }
         }
 
-        termsRecyclerView.adapter = TermsAdapter(tracker)
         quote?.insuranceTerms?.let { terms ->
             (termsRecyclerView.adapter as TermsAdapter).items = terms
         }
