12ebf7e97f74924b7d06ef5a5e02760f24c38334
Mathias Öhrström <MathiasOhrstrom@gmail.com>
2020-04-15 10:50:08 +0200

    Apollo 1.4.4

--- app/src/main/java/com/hedvig/app/feature/profile/ui/ProfileViewModelImpl.kt
+++ app/src/main/java/com/hedvig/app/feature/profile/ui/ProfileViewModelImpl.kt
@@ -22,6 +22,7 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class ProfileViewModelImpl(
