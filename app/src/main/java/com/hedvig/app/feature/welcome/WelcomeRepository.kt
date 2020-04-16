7182d6d5e5c7eb57d90749aded75d5248241515e
Mathias Öhrström <MathiasOhrstrom@gmail.com>
2020-04-16 10:09:36 +0200

    welcome

--- app/src/main/java/com/hedvig/app/feature/welcome/WelcomeRepository.kt
+++ app/src/main/java/com/hedvig/app/feature/welcome/WelcomeRepository.kt
@@ -1,23 +1,21 @@
 package com.hedvig.app.feature.welcome
 
 import android.content.Context
-import com.apollographql.apollo.rx2.Rx2Apollo
+import com.apollographql.apollo.api.Response
 import com.hedvig.android.owldroid.graphql.WelcomeQuery
 import com.hedvig.app.ApolloClientWrapper
 import com.hedvig.app.util.apollo.defaultLocale
-import io.reactivex.Observable
+import com.hedvig.app.util.apollo.toDeferred
 
 class WelcomeRepository(
     private val apolloClientWrapper: ApolloClientWrapper,
     private val context: Context
 ) {
-    fun fetchWelcomeScreens(): Observable<WelcomeQuery.Data?> {
+    suspend fun fetchWelcomeScreens(): Response<WelcomeQuery.Data> {
         val welcomeQuery = WelcomeQuery(
             locale = defaultLocale(context)
         )
 
-        return Rx2Apollo
-            .from(apolloClientWrapper.apolloClient.query(welcomeQuery))
-            .map { it.data() }
+        return apolloClientWrapper.apolloClient.query(welcomeQuery).toDeferred().await()
     }
 }
--- app/src/main/java/com/hedvig/app/feature/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/hedvig/app/feature/welcome/WelcomeViewModel.kt
@@ -2,11 +2,12 @@ package com.hedvig.app.feature.welcome
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hedvig.app.feature.dismissiblepager.DismissiblePagerPage
 import com.hedvig.app.util.apollo.ThemedIconUrls
 import e
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.rxkotlin.plusAssign
+import kotlinx.coroutines.launch
 
 class WelcomeViewModel(
     private val welcomeRepository: WelcomeRepository
@@ -22,19 +23,22 @@ class WelcomeViewModel(
     }
 
     fun fetch() {
-        disposables += welcomeRepository
-            .fetchWelcomeScreens()
-            .subscribe({
-                it?.let { response ->
-                    data.postValue(
-                        response.welcome.map { page ->
-                            DismissiblePagerPage(
-                                ThemedIconUrls.from(page.illustration.variants.fragments.iconVariantsFragment),
-                                page.title,
-                                page.paragraph
-                            )
-                        })
-                } ?: e { "No welcome data" }
-            }, { e(it) })
+        viewModelScope.launch {
+            val response = runCatching { welcomeRepository.fetchWelcomeScreens() }
+            if (response.isFailure) {
+                response.exceptionOrNull()?.let { e(it) }
+                return@launch
+            }
+            response.getOrNull()?.data()?.let { response ->
+                data.postValue(
+                    response.welcome.map { page ->
+                        DismissiblePagerPage(
+                            ThemedIconUrls.from(page.illustration.variants.fragments.iconVariantsFragment),
+                            page.title,
+                            page.paragraph
+                        )
+                    })
+            }
+        }
     }
 }
