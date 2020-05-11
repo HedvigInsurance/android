e4c3a650a27f88e518b72210153a7e565c924d0d
Oscar Nylander <oscar@hedvig.com>
2020-05-11 11:21:31 +0200

    Less than or equals-support

--- app/src/main/java/com/hedvig/app/feature/embark/EmbarkViewModel.kt
+++ app/src/main/java/com/hedvig/app/feature/embark/EmbarkViewModel.kt
@@ -98,6 +98,14 @@ abstract class EmbarkViewModel : ViewModel() {
                     return binaryExpression.text
                 }
             }
+            if (binaryExpression.binaryType == EmbarkExpressionTypeBinary.LESS_THAN_OR_EQUALS) {
+                val storedAsInt = store[binaryExpression.key]?.toIntOrNull() ?: return null
+                val valueAsInt = binaryExpression.value.toIntOrNull() ?: return null
+
+                if (storedAsInt <= valueAsInt) {
+                    return binaryExpression.text
+                }
+            }
             return null
         }
         return null
