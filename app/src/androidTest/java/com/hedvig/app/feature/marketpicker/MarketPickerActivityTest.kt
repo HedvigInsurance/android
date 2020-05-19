76c903f1c317ae1f289ce01f923baf367eb46495
Mathias Öhrström <MathiasOhrstrom@gmail.com>
2020-05-19 10:55:25 +0200

    language test

--- app/src/androidTest/java/com/hedvig/app/feature/marketpicker/MarketPickerActivityTest.kt
+++ app/src/androidTest/java/com/hedvig/app/feature/marketpicker/MarketPickerActivityTest.kt
@@ -1,15 +1,21 @@
 package com.hedvig.app.feature.marketpicker
 
 import android.content.Intent
+import androidx.preference.PreferenceManager
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.agoda.kakao.screen.Screen.Companion.onScreen
 import com.apollographql.apollo.api.toJson
 import com.hedvig.android.owldroid.graphql.GeoQuery
 import com.hedvig.app.R
 import com.hedvig.app.feature.marketpicker.screens.MarketPickerScreen
+import com.hedvig.app.feature.settings.Language
+import com.hedvig.app.feature.settings.SettingsActivity
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -51,16 +57,6 @@ class MarketPickerActivityTest {
                         }
                     }
                 }
-                languageRecyclerView {
-                    firstChild<MarketPickerScreen.LanguageItem> {
-                        languageText { R.string.swedish }
-                    }
-                    lastChild<MarketPickerScreen.LanguageItem> {
-                        languageText {
-                            hasText(R.string.english_swedish)
-                        }
-                    }
-                }
             }
         }
     }
@@ -69,12 +65,28 @@ class MarketPickerActivityTest {
     fun checkSaveButton() {
         activityRule.launchActivity(Intent())
 
+        val pref =
+            PreferenceManager.getDefaultSharedPreferences(InstrumentationRegistry.getInstrumentation().targetContext)
+
         onScreen<MarketPickerScreen> {
             save {
                 isDisabled()
             }
+            marketRecyclerView {
+                childWith<MarketPickerScreen.MarketItem> {
+                    withDescendant {
+                        withText(R.string.sweden)
+                    }
+                } perform {
+                    click()
+                }
+            }
             languageRecyclerView {
-                firstChild<MarketPickerScreen.LanguageItem> {
+                childWith<MarketPickerScreen.LanguageItem> {
+                    withDescendant {
+                        withText(R.string.swedish)
+                    }
+                } perform {
                     click()
                 }
             }
@@ -82,12 +94,18 @@ class MarketPickerActivityTest {
                 click()
             }
         }
+        val market = pref.getString(Market.MARKET_SHARED_PREF, null)
+        val language = pref.getString(SettingsActivity.SETTING_LANGUAGE, null)
+
+        Assert.assertThat(market, CoreMatchers.containsString(MARKET))
+        Assert.assertThat(language, CoreMatchers.containsString(Language.SETTING_SV_SE))
     }
 
     companion object {
+        private const val MARKET = "SE"
         private val DATA = GeoQuery.Data(
             geo = GeoQuery.Geo(
-                countryISOCode = "SE"
+                countryISOCode = MARKET
             )
         )
     }
