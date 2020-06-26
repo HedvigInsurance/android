0a78b85c4ffa2e9cebe32c2c9bf67ee2c933242b
Oscar Nylander <oscar@hedvig.com>
2020-06-26 14:13:30 +0200

    Move test to `ReferralsDataBuilder`

--- app/src/androidTest/java/com/hedvig/app/feature/referrals/tab/ReferralTabCodeSnackbarTest.kt
+++ app/src/androidTest/java/com/hedvig/app/feature/referrals/tab/ReferralTabCodeSnackbarTest.kt
@@ -10,8 +10,6 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.agoda.kakao.screen.Screen
 import com.apollographql.apollo.api.toJson
-import com.hedvig.android.owldroid.fragment.CostFragment
-import com.hedvig.android.owldroid.fragment.MonetaryAmountFragment
 import com.hedvig.android.owldroid.graphql.LoggedInQuery
 import com.hedvig.android.owldroid.graphql.ReferralsQuery
 import com.hedvig.app.ApolloClientWrapper
@@ -19,6 +17,7 @@ import com.hedvig.app.feature.loggedin.ui.LoggedInActivity
 import com.hedvig.app.feature.loggedin.ui.LoggedInTabs
 import com.hedvig.app.feature.referrals.ReferralScreen
 import com.hedvig.app.feature.referrals.builders.LoggedInDataBuilder
+import com.hedvig.app.feature.referrals.builders.ReferralsDataBuilder
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -57,7 +56,7 @@ class ReferralTabCodeSnackbarTest : KoinTest {
                     }
 
                     if (body.contains(ReferralsQuery.OPERATION_NAME.name())) {
-                        return MockResponse().setBody(REFERRALS_DATA.toJson())
+                        return MockResponse().setBody(ReferralsDataBuilder().build().toJson())
                     }
 
                     return MockResponse()
@@ -96,89 +95,5 @@ class ReferralTabCodeSnackbarTest : KoinTest {
 
             assertThat(clipboardContent).isEqualTo("TEST123")
         }
-    }
-
-    companion object {
-        private val REFERRALS_DATA = ReferralsQuery.Data(
-            insuranceCost = ReferralsQuery.InsuranceCost(
-                fragments = ReferralsQuery.InsuranceCost.Fragments(
-                    CostFragment(
-                        monthlyDiscount = CostFragment.MonthlyDiscount(
-                            fragments = CostFragment.MonthlyDiscount.Fragments(
-                                MonetaryAmountFragment(
-                                    amount = "0.00",
-                                    currency = "SEK"
-                                )
-                            )
-                        ),
-                        monthlyNet = CostFragment.MonthlyNet(
-                            fragments = CostFragment.MonthlyNet.Fragments(
-                                MonetaryAmountFragment(
-                                    amount = "349.00",
-                                    currency = "SEK"
-                                )
-                            )
-                        ),
-                        monthlyGross = CostFragment.MonthlyGross(
-                            fragments = CostFragment.MonthlyGross.Fragments(
-                                MonetaryAmountFragment(
-                                    amount = "349.00",
-                                    currency = "SEK"
-                                )
-                            )
-                        )
-                    )
-                )
-            ),
-            referralInformation = ReferralsQuery.ReferralInformation(
-                campaign = ReferralsQuery.Campaign(
-                    code = "TEST123",
-                    incentive = ReferralsQuery.Incentive(
-                        asMonthlyCostDeduction = ReferralsQuery.AsMonthlyCostDeduction(
-                            amount = ReferralsQuery.Amount(
-                                fragments = ReferralsQuery.Amount.Fragments(
-                                    MonetaryAmountFragment(
-                                        amount = "10.00",
-                                        currency = "SEK"
-                                    )
-                                )
-                            )
-                        )
-                    )
-                ),
-                costReducedIndefiniteDiscount = ReferralsQuery.CostReducedIndefiniteDiscount(
-                    fragments = ReferralsQuery.CostReducedIndefiniteDiscount.Fragments(
-                        CostFragment(
-                            monthlyDiscount = CostFragment.MonthlyDiscount(
-                                fragments = CostFragment.MonthlyDiscount.Fragments(
-                                    MonetaryAmountFragment(
-                                        amount = "0.00",
-                                        currency = "SEK"
-                                    )
-                                )
-                            ),
-                            monthlyNet = CostFragment.MonthlyNet(
-                                fragments = CostFragment.MonthlyNet.Fragments(
-                                    MonetaryAmountFragment(
-                                        amount = "349.00",
-                                        currency = "SEK"
-                                    )
-                                )
-                            ),
-                            monthlyGross = CostFragment.MonthlyGross(
-                                fragments = CostFragment.MonthlyGross.Fragments(
-                                    MonetaryAmountFragment(
-                                        amount = "349.00",
-                                        currency = "SEK"
-                                    )
-                                )
-                            )
-                        )
-                    )
-                ),
-                referredBy = null,
-                invitations = emptyList()
-            )
-        )
     }
 }
