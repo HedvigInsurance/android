e5e8ede3404a5b4861c850f33ce05f515a298247
Oscar Nylander <oscar@hedvig.com>
2020-07-14 13:08:54 +0200

    Insets and toolbar-related fixes for edit code screen
    Also, tweak margins to feel a bit better.

--- app/src/main/java/com/hedvig/app/feature/referrals/ui/editcode/ReferralsEditCodeActivity.kt
+++ app/src/main/java/com/hedvig/app/feature/referrals/ui/editcode/ReferralsEditCodeActivity.kt
@@ -5,9 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.doOnLayout
+import androidx.core.view.updatePadding
 import androidx.lifecycle.observe
 import com.hedvig.app.BaseActivity
 import com.hedvig.app.R
+import dev.chrisbanes.insetter.doOnApplyWindowInsets
+import dev.chrisbanes.insetter.setEdgeToEdgeSystemUiFlags
 import e
 import kotlinx.android.synthetic.main.activity_referrals_edit_code.*
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -17,7 +21,24 @@ class ReferralsEditCodeActivity : BaseActivity(R.layout.activity_referrals_edit_
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        root.setEdgeToEdgeSystemUiFlags(true)
+
+        toolbar.doOnLayout { applyInsets(it.height) }
+
+        toolbar.doOnApplyWindowInsets { view, insets, initialState ->
+            view.updatePadding(top = initialState.paddings.top + insets.systemWindowInsetTop)
+            applyInsets(view.height)
+        }
+
+        scrollView.doOnApplyWindowInsets { view, insets, initialState ->
+            view.updatePadding(bottom = initialState.paddings.bottom + insets.systemWindowInsetBottom)
+        }
+
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
 
         val currentCode = intent.getStringExtra(CODE)
 
@@ -46,6 +67,10 @@ class ReferralsEditCodeActivity : BaseActivity(R.layout.activity_referrals_edit_
             true
         }
         else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun applyInsets(toolbarHeight: Int) {
+        scrollView.updatePadding(top = toolbarHeight)
     }
 
     companion object {
