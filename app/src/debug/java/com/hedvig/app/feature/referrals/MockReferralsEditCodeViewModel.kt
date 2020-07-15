f5b3d72e5a722861bb1495579c50fc840bcd53f2
Oscar Nylander <oscar@hedvig.com>
2020-07-15 10:13:39 +0200

    Show an error when user submits code but it is already taken

--- app/src/debug/java/com/hedvig/app/feature/referrals/MockReferralsEditCodeViewModel.kt
+++ app/src/debug/java/com/hedvig/app/feature/referrals/MockReferralsEditCodeViewModel.kt
@@ -2,7 +2,9 @@ package com.hedvig.app.feature.referrals
 
 import android.os.Handler
 import com.hedvig.app.feature.referrals.ui.editcode.ReferralsEditCodeViewModel
+import com.hedvig.app.testdata.feature.referrals.EDIT_CODE_DATA_ALREADY_TAKEN
 import com.hedvig.app.testdata.feature.referrals.EDIT_CODE_DATA_SUCCESS
+import com.hedvig.app.testdata.feature.referrals.builders.EditCodeDataBuilder
 
 class MockReferralsEditCodeViewModel : ReferralsEditCodeViewModel() {
     override fun changeCode(newCode: String) {
@@ -12,11 +14,20 @@ class MockReferralsEditCodeViewModel : ReferralsEditCodeViewModel() {
                 return@postDelayed
             }
 
-            _data.postValue(Result.success(EDIT_CODE_DATA_SUCCESS))
+            _data.postValue(
+                Result.success(
+                    when (variant) {
+                        EditCodeDataBuilder.ResultVariant.SUCCESS -> EDIT_CODE_DATA_SUCCESS
+                        EditCodeDataBuilder.ResultVariant.ALREADY_TAKEN -> EDIT_CODE_DATA_ALREADY_TAKEN
+                        else -> throw Error("No mock provided for this variant")
+                    }
+                )
+            )
         }, 1000)
     }
 
     companion object {
         var shouldSucceed = false
+        var variant = EditCodeDataBuilder.ResultVariant.SUCCESS
     }
 }
