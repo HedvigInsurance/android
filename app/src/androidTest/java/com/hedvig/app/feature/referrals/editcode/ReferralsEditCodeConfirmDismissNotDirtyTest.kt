d5b169c0ee0aa0f32a9a1d33cb1680706ba907b0
Oscar Nylander <oscar@hedvig.com>
2020-07-17 10:04:25 +0200

    Ensure that pressing back when form is not dirty results in leaving the screen

---
+++ app/src/androidTest/java/com/hedvig/app/feature/referrals/editcode/ReferralsEditCodeConfirmDismissNotDirtyTest.kt
@@ -0,0 +1,33 @@
+package com.hedvig.app.feature.referrals.editcode
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.hedvig.app.feature.referrals.ui.editcode.ReferralsEditCodeActivity
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ReferralsEditCodeConfirmDismissNotDirtyTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(ReferralsEditCodeActivity::class.java, false, false)
+
+    @Test
+    fun shouldNotShowConfirmDismissWhenFormIsNotDirty() {
+        activityRule.launchActivity(
+            ReferralsEditCodeActivity.newInstance(
+                ApplicationProvider.getApplicationContext(),
+                "TEST123"
+            )
+        )
+
+        Espresso.pressBackUnconditionally()
+
+        assertTrue(activityRule.activity.isFinishing)
+    }
+}
+
