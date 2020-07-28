55d9cae679587da86aa819230d21635f5ccb69af
Oscar Nylander <oscar@hedvig.com>
2020-07-28 11:55:16 +0200

    Modernize tests

--- app/src/androidTest/java/com/hedvig/app/feature/offer/NoExistingInsuranceTest.kt
+++ app/src/androidTest/java/com/hedvig/app/feature/offer/NoExistingInsuranceTest.kt
@@ -4,53 +4,44 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.agoda.kakao.screen.Screen.Companion.onScreen
 import com.hedvig.android.owldroid.graphql.OfferQuery
-import com.hedvig.app.ApolloClientWrapper
 import com.hedvig.app.R
 import com.hedvig.app.feature.offer.ui.OfferActivity
 import com.hedvig.app.testdata.feature.offer.OFFER_DATA_SWEDISH_APARTMENT
-import com.hedvig.app.util.apolloMockServer
-import org.junit.Before
+import com.hedvig.app.util.ApolloCacheClearRule
+import com.hedvig.app.util.ApolloMockServerRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.core.KoinComponent
-import org.koin.core.inject
 
 @RunWith(AndroidJUnit4::class)
-class NoExistingInsuranceTest : KoinComponent {
-    private val apolloClientWrapper: ApolloClientWrapper by inject()
+class NoExistingInsuranceTest {
 
     @get:Rule
     val activityRule = ActivityTestRule(OfferActivity::class.java, false, false)
 
-    @Before
-    fun setup() {
-        apolloClientWrapper
-            .apolloClient
-            .clearNormalizedCache()
-    }
+    @get:Rule
+    val mockServerRule = ApolloMockServerRule(
+        OfferQuery.OPERATION_NAME to { OFFER_DATA_SWEDISH_APARTMENT }
+    )
+
+    @get:Rule
+    val apolloCacheClearRule = ApolloCacheClearRule()
 
     @Test
     fun shouldNotShowSwitcherSectionWhenUserHasNoExistingInsurance() {
-        apolloMockServer(
-            OfferQuery.OPERATION_NAME to { OFFER_DATA_SWEDISH_APARTMENT }
-        ).use { webServer ->
-            webServer.start(8080)
+        activityRule.launchActivity(null)
 
-            activityRule.launchActivity(null)
-
-            onScreen<OfferScreen> {
-                scroll {
-                    hasSize(6)
-                    childAt<OfferScreen.HeaderItem>(0) {
-                        startDate {
-                            hasText(R.string.START_DATE_TODAY)
-                            click()
-                        }
+        onScreen<OfferScreen> {
+            scroll {
+                hasSize(6)
+                childAt<OfferScreen.HeaderItem>(0) {
+                    startDate {
+                        hasText(R.string.START_DATE_TODAY)
+                        click()
                     }
                 }
-                autoSetDate { hasText(R.string.ACTIVATE_TODAY_BTN) }
             }
+            autoSetDate { hasText(R.string.ACTIVATE_TODAY_BTN) }
         }
     }
 }
