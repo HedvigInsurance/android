f48742f4fe2958f063558491159b16ea22c46df9
Oscar Nylander <oscar@hedvig.com>
2020-08-19 16:44:03 +0200

    Extract `CommonClaimBuilder` to its own file

---
+++ testdata/src/main/java/com/hedvig/app/testdata/feature/home/builders/CommonClaimBuilder.kt
@@ -0,0 +1,52 @@
+package com.hedvig.app.testdata.feature.home.builders
+
+import com.hedvig.android.owldroid.fragment.IconVariantsFragment
+import com.hedvig.android.owldroid.graphql.HomeQuery
+import com.hedvig.android.owldroid.type.HedvigColor
+
+data class CommonClaimBuilder(
+    val title: String = "Example",
+    val variant: Variant = Variant.TITLE_AND_BULLET_POINTS
+) {
+    fun build() = HomeQuery.CommonClaim(
+        title = title,
+        icon = HomeQuery.Icon(
+            variants = HomeQuery.Variants(
+                fragments = HomeQuery.Variants.Fragments(
+                    IconVariantsFragment(
+                        dark = IconVariantsFragment.Dark(
+                            svgUrl = "/app-content-service/warning_dark.svg"
+                        ),
+                        light = IconVariantsFragment.Light(
+                            svgUrl = "/app-content-service/warning.svg"
+                        )
+                    )
+                )
+            )
+        ),
+        layout = HomeQuery.Layout(
+            asTitleAndBulletPoints = if (variant == Variant.TITLE_AND_BULLET_POINTS) {
+                HomeQuery.AsTitleAndBulletPoints(
+                    bulletPoints = emptyList(),
+                    buttonTitle = "",
+                    color = HedvigColor.BLACK,
+                    title = title
+                )
+            } else {
+                null
+            },
+            asEmergency = if (variant == Variant.EMERGENCY) {
+                HomeQuery.AsEmergency(
+                    color = HedvigColor.BLACK
+                )
+            } else {
+                null
+            }
+        )
+    )
+
+    enum class Variant {
+        EMERGENCY,
+        TITLE_AND_BULLET_POINTS
+    }
+}
