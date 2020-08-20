9d262cd8ee42a74e4e18106c6ed65f6b2432d9d9
Mathias Öhrström <MathiasOhrstrom@gmail.com>
2020-08-20 09:29:41 +0200

    test name change

--- app/src/androidTest/java/com/hedvig/app/feature/loggedin/NavBarKeyGearEnabledTest.kt
+++ app/src/androidTest/java/com/hedvig/app/feature/loggedin/NavBarKeyGearEnabledTest.kt
@@ -16,7 +16,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class NavBarNoReferralTest {
+class NavBarKeyGearEnabledTest {
     @get:Rule
     val activityRule = ActivityTestRule(LoggedInActivity::class.java, false, false)
 
