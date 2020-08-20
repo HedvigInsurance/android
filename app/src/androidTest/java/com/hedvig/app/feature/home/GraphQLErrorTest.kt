783b9be36b1d74eac0ebf6cbd2bc3907ecf46350
Mathias Öhrström <MathiasOhrstrom@gmail.com>
2020-08-20 11:37:38 +0200

    all tests are now working

--- app/src/androidTest/java/com/hedvig/app/feature/home/GraphQLErrorTest.kt
+++ app/src/androidTest/java/com/hedvig/app/feature/home/GraphQLErrorTest.kt
@@ -9,7 +9,7 @@ import com.hedvig.android.owldroid.graphql.LoggedInQuery
 import com.hedvig.app.R
 import com.hedvig.app.feature.loggedin.ui.LoggedInActivity
 import com.hedvig.app.testdata.feature.home.HOME_DATA_PENDING
-import com.hedvig.app.testdata.feature.referrals.LOGGED_IN_DATA_WITH_KEY_GEAR_FEATURE_ENABLED
+import com.hedvig.app.testdata.feature.referrals.LOGGED_IN_DATA_WITH_REFERRALS_ENABLED
 import com.hedvig.app.util.ApolloCacheClearRule
 import com.hedvig.app.util.ApolloMockServerRule
 import com.hedvig.app.util.apolloResponse
@@ -29,7 +29,7 @@ class GraphQLErrorTest {
     val mockServerRule = ApolloMockServerRule(
         LoggedInQuery.QUERY_DOCUMENT to apolloResponse {
             success(
-                LOGGED_IN_DATA_WITH_KEY_GEAR_FEATURE_ENABLED
+                LOGGED_IN_DATA_WITH_REFERRALS_ENABLED
             )
         },
         HomeQuery.QUERY_DOCUMENT to apolloResponse {
--- app/src/androidTest/java/com/hedvig/app/feature/home/TerminatedTest.kt
+++ app/src/androidTest/java/com/hedvig/app/feature/home/TerminatedTest.kt
@@ -11,7 +11,7 @@ import com.hedvig.android.owldroid.graphql.LoggedInQuery
 import com.hedvig.app.R
 import com.hedvig.app.feature.loggedin.ui.LoggedInActivity
 import com.hedvig.app.testdata.feature.home.HOME_DATA_TERMINATED
-import com.hedvig.app.testdata.feature.referrals.LOGGED_IN_DATA_WITH_KEY_GEAR_FEATURE_ENABLED
+import com.hedvig.app.testdata.feature.referrals.LOGGED_IN_DATA_WITH_REFERRALS_ENABLED
 import com.hedvig.app.util.ApolloCacheClearRule
 import com.hedvig.app.util.ApolloMockServerRule
 import com.hedvig.app.util.apolloResponse
@@ -29,7 +29,7 @@ class TerminatedTest {
     val mockServerRule = ApolloMockServerRule(
         LoggedInQuery.QUERY_DOCUMENT to apolloResponse {
             success(
-                LOGGED_IN_DATA_WITH_KEY_GEAR_FEATURE_ENABLED
+                LOGGED_IN_DATA_WITH_REFERRALS_ENABLED
             )
         },
         HomeQuery.QUERY_DOCUMENT to apolloResponse { success(HOME_DATA_TERMINATED) }
