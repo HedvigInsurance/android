84f6f271d46ebafff2607ed9a2dcbc790c2071c8
Oscar Nylander <oscar@hedvig.com>
2020-08-26 16:28:36 +0200

    This file sucks

--- app/src/main/java/com/hedvig/app/feature/profile/ui/ProfileMenuRow.kt
+++ app/src/main/java/com/hedvig/app/feature/profile/ui/ProfileMenuRow.kt
@@ -1,15 +1,13 @@
 package com.hedvig.app.feature.profile.ui
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.Gravity
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.hedvig.app.R
-import com.hedvig.app.util.extensions.compatDrawable
 import com.hedvig.app.util.extensions.view.remove
 import com.hedvig.app.util.extensions.view.show
-import kotlinx.android.synthetic.main.profile_row.view.*
 
 class ProfileMenuRow : ConstraintLayout {
     private var attributeSet: AttributeSet? = null
@@ -40,42 +38,37 @@ class ProfileMenuRow : ConstraintLayout {
         setupDynamicContent()
     }
 
-    var icon: Drawable? = null
-        set(value) {
-            field = value
-            //icon.setImageDrawable(field)
-        }
     var name: CharSequence? = null
         set(value) {
             field = value
-            title.text = field
+            findViewById<TextView>(R.id.title).text = field
         }
     var description: CharSequence? = null
         set(value) {
             field = value
-            caption.text = field
-            caption.show()
-            title.gravity = Gravity.NO_GRAVITY
+            findViewById<TextView>(R.id.caption).apply {
+                text = field
+                show()
+            }
+            findViewById<TextView>(R.id.title).gravity = Gravity.NO_GRAVITY
         }
 
     private fun setupDynamicContent() {
         val attributes =
             context.obtainStyledAttributes(attributeSet, R.styleable.ProfileMenuRow, defStyle, 0)
 
-        val iconResourceId = attributes.getResourceId(R.styleable.ProfileMenuRow_iconImage, -1)
-        if (iconResourceId != -1) icon = context.compatDrawable(iconResourceId)
         name = attributes.getText(R.styleable.ProfileMenuRow_name)
 
         val description = attributes.getText(R.styleable.ProfileMenuRow_description)
         description?.let { d ->
-            caption.text = d
+            findViewById<TextView>(R.id.caption).text = d
         } ?: makeSingleLine()
 
         attributes.recycle()
     }
 
     private fun makeSingleLine() {
-        caption.remove()
-        title.gravity = Gravity.CENTER_VERTICAL
+        findViewById<TextView>(R.id.caption).remove()
+        findViewById<TextView>(R.id.title).gravity = Gravity.CENTER_VERTICAL
     }
 }
