0ceadeb07228813522dff86278e0c6505af07a85
Oscar Nylander <oscar@hedvig.com>
2020-10-06 10:57:23 +0200

    Add two regression-preventing tests

---
+++ app/src/androidTest/java/com/hedvig/app/feature/adyen/ConnectPayinNorwayTest.kt
@@ -0,0 +1,59 @@
+package com.hedvig.app.feature.adyen
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.intent.Intents.times
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.agoda.kakao.screen.Screen
+import com.hedvig.android.owldroid.graphql.HomeQuery
+import com.hedvig.android.owldroid.graphql.LoggedInQuery
+import com.hedvig.android.owldroid.graphql.PayinStatusQuery
+import com.hedvig.app.R
+import com.hedvig.app.feature.home.screens.HomeTabScreen
+import com.hedvig.app.feature.loggedin.ui.LoggedInActivity
+import com.hedvig.app.testdata.feature.home.HOME_DATA_ACTIVE
+import com.hedvig.app.testdata.feature.payment.PAYIN_STATUS_DATA_NEEDS_SETUP
+import com.hedvig.app.testdata.feature.referrals.LOGGED_IN_DATA_WITH_REFERRALS_ENABLED
+import com.hedvig.app.util.ApolloCacheClearRule
+import com.hedvig.app.util.ApolloMockServerRule
+import com.hedvig.app.util.apolloResponse
+import com.hedvig.app.util.stubExternalIntents
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ConnectPayinNorwayTest {
+    @get:Rule
+    val activityRule = IntentsTestRule(LoggedInActivity::class.java, false, false)
+
+    @get:Rule
+    val mockServerRule = ApolloMockServerRule(
+        LoggedInQuery.QUERY_DOCUMENT to apolloResponse {
+            success(
+                LOGGED_IN_DATA_WITH_REFERRALS_ENABLED
+            )
+        },
+        HomeQuery.QUERY_DOCUMENT to apolloResponse { success(HOME_DATA_ACTIVE) },
+        PayinStatusQuery.QUERY_DOCUMENT to apolloResponse { success(PAYIN_STATUS_DATA_NEEDS_SETUP) }
+    )
+
+    @get:Rule
+    val apolloCacheClearRule = ApolloCacheClearRule()
+
+    @Test
+    fun shouldOpenConnectPayinAdyen() {
+        activityRule.launchActivity(LoggedInActivity.newInstance(ApplicationProvider.getApplicationContext()))
+        Screen.onScreen<HomeTabScreen> {
+            recycler {
+                childAt<HomeTabScreen.InfoCardItem>(3) {
+                    action {
+                        click()
+                    }
+                    connectPayin { intended() }
+                }
+            }
+        }
+    }
+}
