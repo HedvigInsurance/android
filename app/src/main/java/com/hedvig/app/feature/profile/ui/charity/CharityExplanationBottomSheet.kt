1336191731d5c7db95f78449773c86ef3e9805e2
Oscar Nylander <oscar@hedvig.com>
2020-11-05 16:20:23 +0100

    WIP replace text keys
    This is now on hold due to some lokalise-things

--- app/src/main/java/com/hedvig/app/feature/profile/ui/charity/CharityExplanationBottomSheet.kt
+++ app/src/main/java/com/hedvig/app/feature/profile/ui/charity/CharityExplanationBottomSheet.kt
@@ -6,13 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.hedvig.app.R
+import com.hedvig.app.databinding.BottomSheetCharityExplanationBinding
+import com.hedvig.app.util.extensions.setMarkdownText
+import com.hedvig.app.util.extensions.viewBinding
 
 class CharityExplanationBottomSheet : BottomSheetDialogFragment() {
+    private val binding by viewBinding(BottomSheetCharityExplanationBinding::bind)
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.bottom_sheet_charity_explanation, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.body.setMarkdownText(getString(R.string.PROFILE_MY_CHARITY_INFO_BODY))
+    }
 
     companion object {
         const val TAG = "charity_explanation_bottom_sheet"
