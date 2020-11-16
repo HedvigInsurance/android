28e4504a383126bcc0a4bcb727d3d0369a3af705
Mathias Öhrström <mathias.ohrstrom@gmail.com>
2020-11-16 10:45:05 +0100

    actionbar

--- app/src/main/java/com/hedvig/app/feature/onbarding/ChoosePlanActivity.kt
+++ app/src/main/java/com/hedvig/app/feature/onbarding/ChoosePlanActivity.kt
@@ -3,10 +3,13 @@ package com.hedvig.app.feature.onbarding
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.core.view.updatePaddingRelative
 import com.hedvig.app.BaseActivity
 import com.hedvig.app.R
 import com.hedvig.app.databinding.ActivityChoosePlanBinding
+import com.hedvig.app.util.extensions.makeToast
 import com.hedvig.app.util.extensions.viewBinding
 import dev.chrisbanes.insetter.doOnApplyWindowInsets
 import dev.chrisbanes.insetter.setEdgeToEdgeSystemUiFlags
@@ -23,7 +26,24 @@ class ChoosePlanActivity : BaseActivity(R.layout.activity_choose_plan) {
             }
             setSupportActionBar(toolbar)
             supportActionBar?.title = "TODO Choose Plan"
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
 
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.choose_plan_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.moreOptions -> {
+            makeToast("TODO")
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
         }
     }
 
