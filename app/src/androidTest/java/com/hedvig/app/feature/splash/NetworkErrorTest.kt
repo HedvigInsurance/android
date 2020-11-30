acb659be130422934767cc4d9e7895fa79a40cbf
Oscar Nylander <oscar@hedvig.com>
2020-11-30 16:52:57 +0100

    Try, try again

--- app/src/androidTest/java/com/hedvig/app/feature/splash/NetworkErrorTest.kt
+++ app/src/androidTest/java/com/hedvig/app/feature/splash/NetworkErrorTest.kt
@@ -1,14 +1,19 @@
 package com.hedvig.app.feature.splash
 
-import androidx.test.rule.ActivityTestRule
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import com.agoda.kakao.intent.KIntent
+import com.agoda.kakao.screen.Screen
+import com.agoda.kakao.screen.Screen.Companion.onScreen
 import com.hedvig.android.owldroid.graphql.ContractStatusQuery
 import com.hedvig.app.SplashActivity
+import com.hedvig.app.feature.marketing.ui.MarketingActivity
 import com.hedvig.app.util.ApolloCacheClearRule
 import com.hedvig.app.util.ApolloMockServerRule
 import com.hedvig.app.util.apolloResponse
 import com.hedvig.app.util.context
 import com.hedvig.app.util.extensions.isLoggedIn
 import com.hedvig.app.util.extensions.setIsLoggedIn
+import com.hedvig.app.util.stub
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.After
 import org.junit.Before
@@ -17,7 +22,7 @@ import org.junit.Test
 
 class NetworkErrorTest : TestCase() {
     @get:Rule
-    val activityRule = ActivityTestRule(SplashActivity::class.java, false, false)
+    val activityRule = IntentsTestRule(SplashActivity::class.java, false, false)
 
     @get:Rule
     val mockServerRule = ApolloMockServerRule(
@@ -38,10 +43,17 @@ class NetworkErrorTest : TestCase() {
     @Test
     fun shouldNotCrashOnNetworkError() = run {
         activityRule.launchActivity(null)
+        onScreen<SplashScreen> {
+            marketing { stub() }
+        }
     }
 
     @After
     fun teardown() {
         context().setIsLoggedIn(previousLoginStatus)
+    }
+
+    class SplashScreen : Screen<SplashScreen>() {
+        val marketing = KIntent { hasComponent(MarketingActivity::class.java.name) }
     }
 }
