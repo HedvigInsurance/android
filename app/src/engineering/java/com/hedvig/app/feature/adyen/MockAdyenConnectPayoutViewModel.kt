d3bc6f6f6382d9e28e6ea3260bb463238f1973c4
Oscar Nylander <oscar@hedvig.com>
2020-12-07 14:41:07 +0100

    Integrate with adyen to connect payout

---
+++ app/src/engineering/java/com/hedvig/app/feature/adyen/MockAdyenConnectPayoutViewModel.kt
@@ -0,0 +1,39 @@
+package com.hedvig.app.feature.adyen
+
+import com.adyen.checkout.base.model.PaymentMethodsApiResponse
+import com.hedvig.app.feature.adyen.payout.AdyenConnectPayoutViewModel
+import org.json.JSONObject
+
+class MockAdyenConnectPayoutViewModel:AdyenConnectPayoutViewModel() {
+    init {
+        _payoutMethods.postValue(PaymentMethodsApiResponse.SERIALIZER.deserialize(JSONObject(
+            """
+                {
+                  "paymentMethods": [
+                    {
+                      "name": "Trustly",
+                      "supportsRecurring": true,
+                      "type": "trustly"
+                    }
+                  ],
+                  "groups": [
+                    {
+                      "name": "Credit Card",
+                      "types": [
+                        "visa",
+                        "mc",
+                        "amex",
+                        "amex_applepay",
+                        "diners",
+                        "discover",
+                        "maestro",
+                        "mc_applepay",
+                        "visa_applepay"
+                      ]
+                    }
+                  ]
+                }
+            """.trimIndent()
+        )))
+    }
+}
