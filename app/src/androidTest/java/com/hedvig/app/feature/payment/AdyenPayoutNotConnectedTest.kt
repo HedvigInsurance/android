f775dc4931a18a32b3ce8969db00241f8253e173
Oscar Nylander <oscar@hedvig.com>
2020-12-10 15:58:19 +0100

    Move redeem discount

--- app/src/androidTest/java/com/hedvig/app/feature/payment/AdyenPayoutNotConnectedTest.kt
+++ app/src/androidTest/java/com/hedvig/app/feature/payment/AdyenPayoutNotConnectedTest.kt
@@ -53,7 +53,7 @@ class AdyenPayoutNotConnectedTest : TestCase() {
         onScreen<PaymentScreen> {
             adyenConnectPayout { stub() }
             recycler {
-                childAt<PaymentScreen.Link>(3) {
+                childAt<PaymentScreen.Link>(4) {
                     click()
                 }
             }
