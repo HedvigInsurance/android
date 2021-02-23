ebe576c9041f94747879c21f069f577dea470479
hedvigoscar <39644124+hedvigoscar@users.noreply.github.com>
2021-02-23 13:23:41 +0100

    Update app/src/androidTest/java/com/hedvig/app/feature/marketpicker/screens/MarketSelectedScreen.kt

--- app/src/androidTest/java/com/hedvig/app/feature/marketpicker/screens/MarketSelectedScreen.kt
+++ app/src/androidTest/java/com/hedvig/app/feature/marketpicker/screens/MarketSelectedScreen.kt
@@ -11,5 +11,4 @@ object MarketSelectedScreen : KScreen<MarketSelectedScreen>() {
     override val viewClass = MarketSelectedFragment::class.java
 
     val loginButton = KButton { withId(R.id.logIn) }
-
 }
