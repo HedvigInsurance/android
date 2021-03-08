f6680486590abe0dded55b03f9431e2d53654449
Hugo Källström <hugo@hedvig.com>
2021-03-08 10:18:28 +0100

    Remove unused val

--- app/src/androidTest/java/com/hedvig/app/feature/embark/screens/MoreOptionsScreen.kt
+++ app/src/androidTest/java/com/hedvig/app/feature/embark/screens/MoreOptionsScreen.kt
@@ -3,7 +3,6 @@ package com.hedvig.app.feature.embark.screens
 import android.view.View
 import com.agoda.kakao.recycler.KRecyclerItem
 import com.agoda.kakao.recycler.KRecyclerView
-import com.agoda.kakao.text.KButton
 import com.agoda.kakao.text.KTextView
 import com.hedvig.app.R
 import com.hedvig.app.feature.embark.ui.MoreOptionsActivity
@@ -23,6 +22,4 @@ object MoreOptionsScreen : KScreen<MoreOptionsScreen>() {
     class Row(parent: Matcher<View>) : KRecyclerItem<Row>(parent) {
         val info = KTextView(parent) { withId(R.id.info) }
     }
-
-    val loginButton = KButton { withId(R.id.logIn) }
 }
