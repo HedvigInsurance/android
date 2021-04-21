617543946e0f025817a10c99dbbe8fddf9a72ede
Hugo <hugo@hedvig.com>
2021-04-21 15:11:39 +0200

    APP-372: Start embark story when pressing continue from moving flow intro screen (#519)

--- testdata/src/main/java/com/hedvig/app/testdata/feature/changeaddress/builders/SelfChangeEligibilityBuilder.kt
+++ testdata/src/main/java/com/hedvig/app/testdata/feature/changeaddress/builders/SelfChangeEligibilityBuilder.kt
@@ -1,13 +1,12 @@
 package com.hedvig.app.testdata.feature.changeaddress.builders
 
 import com.hedvig.android.owldroid.graphql.SelfChangeEligibilityQuery
-import com.hedvig.android.owldroid.type.SelfChangeBlocker
 
 class SelfChangeEligibilityBuilder(
-    private val blockers: List<SelfChangeBlocker> = emptyList()
+    val embarkStoryId: String? = null
 ) {
 
     fun build() = SelfChangeEligibilityQuery.SelfChangeEligibility(
-        blockers = blockers
+        embarkStoryId = embarkStoryId
     )
 }
