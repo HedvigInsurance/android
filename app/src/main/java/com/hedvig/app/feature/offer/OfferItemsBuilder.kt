03d7d0dec6396677313b9893886b03b8971c4c53
Hugo <hugo@hedvig.com>
2021-05-11 09:54:46 +0200

    Offer view state (#542)
    * Observe view state to decouple network model from view
    
    * Show general error message if no message is supplied in view state error
    
    * Fix PR comments

---
+++ app/src/main/java/com/hedvig/app/feature/offer/OfferItemsBuilder.kt
@@ -0,0 +1,24 @@
+package com.hedvig.app.feature.offer
+
+import com.hedvig.android.owldroid.graphql.OfferQuery
+import com.hedvig.app.feature.offer.ui.OfferModel
+
+object OfferItemsBuilder {
+    fun createItems(data: OfferQuery.Data): List<OfferModel> {
+        return listOfNotNull(
+            OfferModel.Header(data),
+            OfferModel.Info,
+            OfferModel.Facts(data),
+            OfferModel.Perils(data),
+            OfferModel.Terms(data),
+            data.lastQuoteOfMember.asCompleteQuote?.currentInsurer?.let { currentInsurer ->
+                if (currentInsurer.switchable == true) {
+                    OfferModel.Switcher(currentInsurer.displayName)
+                } else {
+                    null
+                }
+            },
+            OfferModel.Footer
+        )
+    }
+}
