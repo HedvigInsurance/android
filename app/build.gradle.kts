f131a4dcd6b92399f097e22bdad7e7bb07280481
Oscar Nylander <oscar@hedvig.com>
2021-05-27 10:23:43 +0200

    Add compiler argument and bump compose to latest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
     kotlinOptions {
         jvmTarget = "1.8"
+        freeCompilerArgs += "-Xallow-jvm-ir-dependencies"
     }
     composeOptions {
         kotlinCompilerVersion = CommonVersions.kotlin
--- buildSrc/src/main/kotlin/CommonVersions.kt
+++ buildSrc/src/main/kotlin/CommonVersions.kt
@@ -1,5 +1,5 @@
 object CommonVersions {
     const val kotlin = "1.4.32"
     const val apollo = "2.5.6"
-    const val compose = "1.0.0-beta06"
+    const val compose = "1.0.0-beta07"
 }
