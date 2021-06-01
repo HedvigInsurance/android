2ece0a6e1c24cfa4d80dec5934d23c6d3e6eff2c
Oscar Nylander <oscar@hedvig.com>
2021-06-01 11:27:02 +0200

    Prevent rare lifecycle-related crash in honesty pledge bottom sheet

--- app/src/main/java/com/hedvig/app/util/extensions/FragmentExtensions.kt
+++ app/src/main/java/com/hedvig/app/util/extensions/FragmentExtensions.kt
@@ -2,12 +2,12 @@ package com.hedvig.app.util.extensions
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.lifecycleScope
 
 val Fragment.screenWidth: Int
     get() = requireActivity().screenWidth
 
 val Fragment.viewLifecycleScope
-    get() = viewLifecycleOwner.lifecycle.coroutineScope
+    get() = viewLifecycleOwner.lifecycleScope
 
 fun FragmentTransaction.addToBackStack() = addToBackStack(null)
