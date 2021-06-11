905cd370b4940bad6b0bfca368ce2f55676cdbc7
hedvigoscar <39644124+hedvigoscar@users.noreply.github.com>
2021-06-11 14:10:00 +0200

    APP-7: Address Change, but in Android-Views (#565)

--- app/src/main/java/com/hedvig/app/util/extensions/ViewBindingExtensions.kt
+++ app/src/main/java/com/hedvig/app/util/extensions/ViewBindingExtensions.kt
@@ -3,15 +3,8 @@ package com.hedvig.app.util.extensions
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
-import com.hedvig.app.util.safeLet
-import kotlin.properties.ReadOnlyProperty
-import kotlin.reflect.KProperty
 
 inline fun <T : ViewBinding> AppCompatActivity.viewBinding(crossinline binder: (View) -> T) =
     lazy(LazyThreadSafetyMode.NONE) {
@@ -27,4 +20,3 @@ inline fun <T : ViewBinding> ViewGroup.viewBinding(crossinline binder: (View) ->
     lazy(LazyThreadSafetyMode.NONE) {
         binder(this)
     }
-
