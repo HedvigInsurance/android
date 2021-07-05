e57888eb112a61f11008e55b2b3bf45a90c22f3d
Hugo Källström <hugo@hedvig.com>
2021-07-05 09:39:10 +0200

    Set sign button text and icon depending on SignMethod

--- app/src/main/java/com/hedvig/app/feature/offer/ui/OfferAdapter.kt
+++ app/src/main/java/com/hedvig/app/feature/offer/ui/OfferAdapter.kt
@@ -3,7 +3,6 @@ package com.hedvig.app.feature.offer.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.core.view.doOnNextLayout
 import androidx.core.view.updatePaddingRelative
 import androidx.fragment.app.FragmentManager
