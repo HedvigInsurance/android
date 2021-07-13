a229149149539ac0e692d2374cba1dea38ec63dd
Hugo Källström <hugo@hedvig.com>
2021-07-13 13:07:19 +0200

    Inject dependency

--- app/src/main/java/com/hedvig/app/ApplicationModule.kt
+++ app/src/main/java/com/hedvig/app/ApplicationModule.kt
@@ -415,7 +415,7 @@ val changeDateBottomSheetModule = module {
 }
 
 val checkoutModule = module {
-    viewModel { (ids: List<String>) -> CheckoutViewModel(ids, get(), get(), get()) }
+    viewModel { (ids: List<String>) -> CheckoutViewModel(ids, get(), get(), get(), get()) }
 }
 
 val serviceModule = module {
