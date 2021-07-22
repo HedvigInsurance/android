72fbc5022dbe85995d0d6992b65474b250cd886a
Hugo <hugo@hedvig.com>
2021-07-22 11:12:52 +0200

    APP-658: Add tests for offer start date and fix logic (#632)

--- testdata/src/main/java/com/hedvig/app/testdata/feature/offer/builders/ConcurrentInceptionBuilder.kt
+++ testdata/src/main/java/com/hedvig/app/testdata/feature/offer/builders/ConcurrentInceptionBuilder.kt
@@ -6,8 +6,8 @@ import java.time.LocalDate
 
 class ConcurrentInceptionBuilder(
     val quoteIds: List<String> = listOf("ea656f5f-40b2-4953-85d9-752b33e69e38"),
-    val startDate: LocalDate = LocalDate.now(),
-    val currentInsurer: OfferQuery.CurrentInsurer1 = OfferQuery.CurrentInsurer1(
+    val startDate: LocalDate? = LocalDate.now(),
+    val currentInsurer: OfferQuery.CurrentInsurer1? = OfferQuery.CurrentInsurer1(
         fragments = OfferQuery.CurrentInsurer1.Fragments(
             currentInsurerFragment = CurrentInsurerFragment(
                 id = "currentinsurerid",
