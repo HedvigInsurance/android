55263d16e1dbf8f3564b455877813aab93905377
hedvigoscar <39644124+hedvigoscar@users.noreply.github.com>
2021-07-26 11:51:57 +0200

    APP-720: Add missing house contract detail items (#643)
    * Add missing house contract detail info
    
    This code is not great, but it shouldn't be long-lived either.
    
    * Bonus: Remove unused code
    
    * Bonus: Fix APP-712

--- app/src/main/java/com/hedvig/app/feature/insurance/ui/detail/documents/DocumentsFragment.kt
+++ app/src/main/java/com/hedvig/app/feature/insurance/ui/detail/documents/DocumentsFragment.kt
@@ -7,18 +7,14 @@ import com.hedvig.app.R
 import com.hedvig.app.databinding.ContractDetailDocumentsFragmentBinding
 import com.hedvig.app.feature.documents.DocumentAdapter
 import com.hedvig.app.feature.insurance.ui.detail.ContractDetailViewModel
-import com.hedvig.app.feature.settings.MarketManager
 import com.hedvig.app.util.extensions.view.updatePadding
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import dev.chrisbanes.insetter.doOnApplyWindowInsets
-import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class DocumentsFragment : Fragment(R.layout.contract_detail_documents_fragment) {
     private val binding by viewBinding(ContractDetailDocumentsFragmentBinding::bind)
     private val model: ContractDetailViewModel by sharedViewModel()
-
-    private val marketManager: MarketManager by inject()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.root.apply {
