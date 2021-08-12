a8db400461454feddd8e0c0296ba75616afeaace
hedvigoscar <39644124+hedvigoscar@users.noreply.github.com>
2021-08-12 11:02:33 +0200

    Adyen Fixes (#677)

--- app/src/main/java/com/hedvig/app/feature/adyen/payin/AdyenPayinDropInService.kt
+++ app/src/main/java/com/hedvig/app/feature/adyen/payin/AdyenPayinDropInService.kt
@@ -1,12 +1,14 @@
 package com.hedvig.app.feature.adyen.payin
 
+import com.adyen.checkout.components.ActionComponentData
+import com.adyen.checkout.components.PaymentComponentState
 import com.adyen.checkout.dropin.service.DropInService
 import com.adyen.checkout.dropin.service.DropInServiceResult
 import com.hedvig.app.feature.adyen.AdyenRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 import org.json.JSONObject
 import org.koin.android.ext.android.inject
 import kotlin.coroutines.CoroutineContext
@@ -23,44 +25,61 @@ class AdyenPayinDropInService : DropInService(), CoroutineScope {
         // coroutineJob.cancel() // Cannot cancel this job due to https://github.com/Adyen/adyen-android/issues/447
     }
 
-    override fun makeDetailsCall(actionComponentJson: JSONObject) = runBlocking(coroutineContext) {
-        val response = runCatching {
-            adyenRepository
-                .submitAdditionalPaymentDetails(actionComponentJson)
+    override fun onDetailsCallRequested(actionComponentData: ActionComponentData, actionComponentJson: JSONObject) {
+        launch(coroutineContext) {
+            val response = runCatching {
+                adyenRepository
+                    .submitAdditionalPaymentDetails(actionComponentJson)
+            }
+
+            val result = response.getOrNull()?.data?.submitAdditionalPaymentDetails
+
+            if (result == null) {
+                sendResult(DropInServiceResult.Error("Error"))
+                return@launch
+            }
+
+            result.asAdditionalPaymentsDetailsResponseAction?.action?.let { action ->
+                sendResult(DropInServiceResult.Action(action))
+                return@launch
+            }
+
+            result.asAdditionalPaymentsDetailsResponseFinished?.resultCode?.let { resultCode ->
+                sendResult(DropInServiceResult.Finished(resultCode))
+                return@launch
+            }
+
+            sendResult(DropInServiceResult.Error("Unknown error"))
         }
-
-        val result = response.getOrNull()?.data?.submitAdditionalPaymentDetails
-            ?: return@runBlocking DropInServiceResult.Error("Error")
-
-        result.asAdditionalPaymentsDetailsResponseAction?.action?.let { action ->
-            return@runBlocking DropInServiceResult.Action(action)
-        }
-
-        result.asAdditionalPaymentsDetailsResponseFinished?.resultCode?.let { resultCode ->
-            return@runBlocking DropInServiceResult.Finished(resultCode)
-        }
-
-        DropInServiceResult.Error("Unknown error")
     }
 
-    override fun makePaymentsCall(paymentComponentJson: JSONObject) =
-        runBlocking(coroutineContext) {
+    override fun onPaymentsCallRequested(
+        paymentComponentState: PaymentComponentState<*>,
+        paymentComponentJson: JSONObject
+    ) {
+        launch(coroutineContext) {
             val response = runCatching {
                 adyenRepository
                     .tokenizePaymentDetails(paymentComponentJson)
             }
 
             val result = response.getOrNull()?.data?.tokenizePaymentDetails
-                ?: return@runBlocking DropInServiceResult.Error("Error")
+            if (result == null) {
+                sendResult(DropInServiceResult.Error("Error"))
+                return@launch
+            }
 
             result.asTokenizationResponseAction?.action?.let { action ->
-                return@runBlocking DropInServiceResult.Action(action)
+                sendResult(DropInServiceResult.Action(action))
+                return@launch
             }
 
             result.asTokenizationResponseFinished?.resultCode?.let { resultCode ->
-                return@runBlocking DropInServiceResult.Finished(resultCode)
+                sendResult(DropInServiceResult.Finished(resultCode))
+                return@launch
             }
 
-            DropInServiceResult.Error("Unknown error")
+            sendResult(DropInServiceResult.Error("Unknown error"))
         }
+    }
 }
