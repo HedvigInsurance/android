b067b40a3704fcd22dba4ee96355d6fa30790505
Hugo Källström <hugo@hedvig.com>
2021-08-13 08:45:40 +0200

    Bump version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         applicationId = "com.hedvig"
 
         versionCode = 43
-        versionName = "6.0.2"
+        versionName = "6.0.3"
 
         vectorDrawables.useSupportLibrary = true
 
