0234e68160a30dd9c616bf73948b9dee5b3cde45
StylianosGakis <stylianos.gakis98@gmail.com>
2021-09-13 13:30:52 +0200

    Add cross selling result as a screen in engineering mode

---
+++ app/src/engineering/java/com/hedvig/app/feature/crossselling/CrossSellingResultMockActivity.kt
@@ -0,0 +1,42 @@
+package com.hedvig.app.feature.crossselling
+
+import com.hedvig.app.GenericDevelopmentAdapter
+import com.hedvig.app.MockActivity
+import com.hedvig.app.feature.crossselling.ui.CrossSellingResult
+import com.hedvig.app.feature.crossselling.ui.CrossSellingResultActivity
+import com.hedvig.app.genericDevelopmentAdapter
+import java.time.LocalDate
+import org.koin.core.module.Module
+
+class CrossSellingResultMockActivity : MockActivity() {
+    override val original: List<Module> = listOf()
+    override val mocks: List<Module> = listOf()
+
+    override fun adapter(): GenericDevelopmentAdapter = genericDevelopmentAdapter {
+        header("Cross-Sell result screens")
+        clickableItem("Successful Accident cross-sell starting before today") {
+            startActivity(
+                CrossSellingResultActivity.newInstance(
+                    this@CrossSellingResultMockActivity,
+                    CrossSellingResult.Success(LocalDate.now().minusDays(1)),
+                )
+            )
+        }
+        clickableItem("Successful Accident cross-sell starting in two days") {
+            startActivity(
+                CrossSellingResultActivity.newInstance(
+                    this@CrossSellingResultMockActivity,
+                    CrossSellingResult.Success(LocalDate.now().plusDays(2)),
+                )
+            )
+        }
+        clickableItem("Failed") {
+            startActivity(
+                CrossSellingResultActivity.newInstance(
+                    this@CrossSellingResultMockActivity,
+                    CrossSellingResult.Error
+                )
+            )
+        }
+    }
+}
