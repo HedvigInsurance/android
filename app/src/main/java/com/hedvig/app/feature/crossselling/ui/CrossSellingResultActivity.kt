1b3221d0ced7d9ada876c288a92ec98dc7b48de7
stylianosgakis <stelios.gakis@yahoo.com>
2021-09-14 11:32:30 +0200

    Small tweaks after review

--- app/src/main/java/com/hedvig/app/feature/crossselling/ui/CrossSellingResultActivity.kt
+++ app/src/main/java/com/hedvig/app/feature/crossselling/ui/CrossSellingResultActivity.kt
@@ -10,19 +10,20 @@ class CrossSellingResultActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val crossSellingResult: CrossSellingResult =
-            intent.getParcelableExtra(CROSS_SELLING_RESULT) ?: CrossSellingResult.Error
+        val crossSellingResult = intent.getParcelableExtra<CrossSellingResult>(CROSS_SELLING_RESULT)
+            ?: throw IllegalArgumentException(
+                "Programmer error: CROSS_SELLING_RESULT not provided to ${this.javaClass.name}"
+            )
         setContent {
             CrossSellingResultScreen(crossSellingResult)
         }
     }
 
     companion object {
-        fun newInstance(context: Context, crossSellingResult: CrossSellingResult): Intent {
-            return Intent(context, CrossSellingResultActivity::class.java).apply {
+        fun newInstance(context: Context, crossSellingResult: CrossSellingResult): Intent =
+            Intent(context, CrossSellingResultActivity::class.java).apply {
                 putExtra(CROSS_SELLING_RESULT, crossSellingResult)
             }
-        }
 
         private const val CROSS_SELLING_RESULT = "CROSS_SELLING_RESULT"
     }
