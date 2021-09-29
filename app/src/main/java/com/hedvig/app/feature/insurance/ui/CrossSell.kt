c4c924e4f836c190b250d8d7e53a4b195f76fae5
Hugo <hugo@hedvig.com>
2021-09-29 14:23:24 +0200

    Cross sell card click listener (#723)
    * Add click listener to cross sell card
    
    * Fix formatting

--- app/src/main/java/com/hedvig/app/feature/insurance/ui/CrossSell.kt
+++ app/src/main/java/com/hedvig/app/feature/insurance/ui/CrossSell.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -70,7 +71,11 @@ fun CrossSell(
             ),
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable(
+                    onClick = onCtaClick
+                ),
         )
         Box(
             contentAlignment = Alignment.BottomCenter,
