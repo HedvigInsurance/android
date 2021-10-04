a9356390a0a96ae613ee071e7124ccc72dae8ab6
Hugo <hugo@hedvig.com>
2021-10-04 10:24:14 +0200

    Remove showkase preview annotations in order for release build to compile (#728)
    Co-authored-by: Oscar Nylander <oscar@hedvig.com>

--- app/src/main/java/com/hedvig/app/feature/crossselling/ui/CrossSellingResultScreen.kt
+++ app/src/main/java/com/hedvig/app/feature/crossselling/ui/CrossSellingResultScreen.kt
@@ -1,6 +1,5 @@
 package com.hedvig.app.feature.crossselling.ui
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -24,7 +23,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import com.airbnb.android.showkase.annotation.ShowkaseComposable
 import com.hedvig.app.R
 import com.hedvig.app.ui.compose.composables.buttons.LargeContainedButton
 import com.hedvig.app.ui.compose.composables.buttons.LargeContainedTextButton
@@ -187,28 +185,6 @@ private fun ButtonsSection(
 )
 @Composable
 fun CrossSellingResultScreenPreview(
-    @PreviewParameter(ActivityResultProvider::class) crossSellingResult: CrossSellingResult,
-) {
-    HedvigTheme {
-        CrossSellingResultScreen(
-            crossSellingResult,
-            Clock.systemDefaultZone(),
-            DateTimeFormatter.ISO_LOCAL_DATE,
-            {},
-            {}
-        )
-    }
-}
-
-@ShowkaseComposable(skip = true)
-@Preview(
-    showSystemUi = true,
-    name = "Accident Result • Dark",
-    group = "Cross Sell result",
-    uiMode = UI_MODE_NIGHT_YES,
-)
-@Composable
-fun CrossSellingResultScreenPreviewDark(
     @PreviewParameter(ActivityResultProvider::class) crossSellingResult: CrossSellingResult,
 ) {
     HedvigTheme {
