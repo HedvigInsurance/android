e8cdfccf28f96c5b1bc61782719c5b06f9613442
stylianosgakis <stelios.gakis@yahoo.com>
2021-10-07 19:54:15 +0200

    Split NotificationSubheading and Subheading as completely separate items

--- app/src/main/java/com/hedvig/app/feature/insurance/ui/NotificationSubheading.kt
+++ app/src/main/java/com/hedvig/app/feature/insurance/ui/NotificationSubheading.kt
@@ -1,10 +1,16 @@
 package com.hedvig.app.feature.insurance.ui
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,15 +19,26 @@ import com.hedvig.app.ui.compose.theme.HedvigTheme
 
 @Composable
 fun NotificationSubheading(text: String) {
-    Subheading(
-        text = text,
-        contentBeforeText = {
-            Canvas(Modifier.size(8.dp)) {
-                drawCircle(Color.Red)
-            }
-            Spacer(modifier = Modifier.width(8.dp))
-        },
-    )
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = 16.dp,
+                top = 48.dp,
+                end = 16.dp,
+                bottom = 8.dp,
+            ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Canvas(Modifier.size(8.dp)) {
+            drawCircle(Color.Red)
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = text,
+            style = MaterialTheme.typography.h6,
+        )
+    }
 }
 
 @Preview(
--- app/src/main/java/com/hedvig/app/feature/insurance/ui/Subheading.kt
+++ app/src/main/java/com/hedvig/app/feature/insurance/ui/Subheading.kt
@@ -1,12 +1,10 @@
 package com.hedvig.app.feature.insurance.ui
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,9 +13,10 @@ import com.hedvig.app.ui.compose.theme.HedvigTheme
 @Composable
 fun Subheading(
     text: String,
-    contentBeforeText: (@Composable () -> Unit)? = null,
 ) {
-    Row(
+    Text(
+        text = text,
+        style = MaterialTheme.typography.h6,
         modifier = Modifier
             .fillMaxWidth()
             .padding(
@@ -26,14 +25,7 @@ fun Subheading(
                 end = 16.dp,
                 bottom = 8.dp,
             ),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        contentBeforeText?.invoke()
-        Text(
-            text = text,
-            style = MaterialTheme.typography.h6,
-        )
-    }
+    )
 }
 
 @Preview(
