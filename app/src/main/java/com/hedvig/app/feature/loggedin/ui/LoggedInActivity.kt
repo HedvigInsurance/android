f2893ac1ea36fa1fcd41c3f5275ed55073be0a8d
stylianosgakis <stelios.gakis@yahoo.com>
2021-10-15 15:28:03 +0200

    Change val name to better clarify it's type

--- app/src/main/java/com/hedvig/app/feature/loggedin/ui/LoggedInActivity.kt
+++ app/src/main/java/com/hedvig/app/feature/loggedin/ui/LoggedInActivity.kt
@@ -119,25 +119,25 @@ class LoggedInActivity : BaseActivity(R.layout.activity_logged_in) {
 
             bottomNavigation.itemIconTintList = null
             bottomNavigation.setOnItemSelectedListener { menuItem ->
-                val id = LoggedInTabs.fromId(menuItem.itemId)
-                if (id == null) {
+                val selectedTab = LoggedInTabs.fromId(menuItem.itemId)
+                if (selectedTab == null) {
                     e { "Programmer error: Invalid menu item chosen" }
                     return@setOnItemSelectedListener false
                 }
 
-                if (id == lastSelectedTab) {
+                if (selectedTab == lastSelectedTab) {
                     return@setOnItemSelectedListener false
                 }
                 supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.tabContent, id.fragment)
+                    .replace(R.id.tabContent, selectedTab.fragment)
                     .commitNowAllowingStateLoss()
 
                 setupToolBar()
-                animateGradient(id)
-                lastSelectedTab = id
-                loggedInViewModel.onTabVisited(id)
-                loggedInTracker.tabVisited(id)
+                animateGradient(selectedTab)
+                lastSelectedTab = selectedTab
+                loggedInViewModel.onTabVisited(selectedTab)
+                loggedInTracker.tabVisited(selectedTab)
                 true
             }
 
