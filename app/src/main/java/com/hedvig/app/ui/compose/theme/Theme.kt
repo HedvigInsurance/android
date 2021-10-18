5206aa2eb9f2838589acb8c350372d5a8c13dfcb
stylianosgakis <stelios.gakis@yahoo.com>
2021-10-19 00:13:28 +0200

    Pass language preference as a composition local

--- app/src/main/java/com/hedvig/app/ui/compose/theme/Theme.kt
+++ app/src/main/java/com/hedvig/app/ui/compose/theme/Theme.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalContext
 import com.google.android.material.composethemeadapter.MdcTheme
+import com.hedvig.app.feature.settings.Language
 import com.hedvig.app.feature.settings.Market
 import com.hedvig.app.feature.settings.MarketManager
 import org.koin.java.KoinJavaComponent.getKoin
@@ -13,10 +15,18 @@ val LocalMarket: ProvidableCompositionLocal<Market?> = staticCompositionLocalOf 
     error("CompositionLocal LocalMarket not present")
 }
 
+val LocalLanguage: ProvidableCompositionLocal<Language> = staticCompositionLocalOf {
+    error("CompositionLocal LocalMarket not present")
+}
+
 @Composable
 fun HedvigTheme(content: @Composable () -> Unit) {
     val marketManager = getKoin().get<MarketManager>()
-    CompositionLocalProvider(LocalMarket provides marketManager.market) {
+    val language = Language.fromSettings(LocalContext.current, marketManager.market)
+    CompositionLocalProvider(
+        LocalMarket provides marketManager.market,
+        LocalLanguage provides language,
+    ) {
         MdcTheme(setDefaultFontFamily = true, content = content)
     }
 }
