3127d27bae323266d95df75a84e01c51a8ca550d
stylianosgakis <stelios.gakis@yahoo.com>
2021-10-18 17:43:44 +0200

    Fix corner radius for the button inside the cross-sell card

--- app/src/main/java/com/hedvig/app/feature/insurance/ui/CrossSell.kt
+++ app/src/main/java/com/hedvig/app/feature/insurance/ui/CrossSell.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
@@ -106,6 +107,7 @@ fun CrossSell(
             ) {
                 Button(
                     onClick = { onClick(data.callToAction) },
+                    shape = RoundedCornerShape(6.dp),
                     colors = ButtonDefaults.buttonColors(
                         backgroundColor = whiteHighEmphasis,
                         contentColor = hedvigBlack,
