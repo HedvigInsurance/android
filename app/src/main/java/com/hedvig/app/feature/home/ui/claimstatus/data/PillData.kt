fe698aa8fdc624f7edf72a37086fde66b6486be0
stylianosgakis <stylianos@hedvig.com>
2021-10-29 14:33:52 +0200

    Remove DisplayableText workaround
    This also removes the need to pass market and language through a
    CompositionLocal
    Allow the composables to simply receive the string that they should
    display without any need for them to manipulate the data

--- app/src/main/java/com/hedvig/app/feature/home/ui/claimstatus/data/PillData.kt
+++ app/src/main/java/com/hedvig/app/feature/home/ui/claimstatus/data/PillData.kt
@@ -1,15 +1,17 @@
 package com.hedvig.app.feature.home.ui.claimstatus.data
 
+import android.content.res.Resources
 import com.hedvig.android.owldroid.graphql.HomeQuery
 import com.hedvig.android.owldroid.type.ClaimOutcome
 import com.hedvig.android.owldroid.type.ClaimStatus
 import com.hedvig.app.R
+import com.hedvig.app.util.apollo.format
 import com.hedvig.app.util.apollo.toMonetaryAmount
 import com.hedvig.app.util.compose.DarkAndLightColor
-import com.hedvig.app.util.compose.DisplayableText
+import java.util.Locale
 
 data class PillData(
-    val displayableText: DisplayableText,
+    val text: String,
     val type: PillType
 ) {
 
@@ -19,77 +21,92 @@ data class PillData(
     }
 
     companion object {
-        fun fromClaimStatus(homeQueryClaim: HomeQuery.Claim): List<PillData> {
-            return when (homeQueryClaim.status) {
-                ClaimStatus.SUBMITTED -> listOf(
-                    PillData(
-                        DisplayableText(R.string.home_claim_card_pill_claim),
-                        PillType.Outlined,
-                    ),
-                )
-                ClaimStatus.BEING_HANDLED -> listOf(
-                    PillData(
-                        DisplayableText(R.string.home_claim_card_pill_claim),
-                        PillType.Outlined,
-                    ),
-                )
-                ClaimStatus.CLOSED -> {
-                    when (homeQueryClaim.outcome) {
-                        ClaimOutcome.PAID -> {
-                            mutableListOf<PillData>().apply {
+        fun fromClaimStatus(
+            homeQueryClaim: HomeQuery.Claim,
+            resources: Resources,
+            locale: Locale
+        ): List<PillData> {
+            return listOfPillData(homeQueryClaim, resources, locale)
+                .map { pillData: PillData ->
+                    pillData.copy(text = pillData.text.uppercase(locale))
+                }
+        }
+
+        private fun listOfPillData(
+            homeQueryClaim: HomeQuery.Claim,
+            resources: Resources,
+            locale: Locale
+        ): List<PillData> = when (homeQueryClaim.status) {
+            ClaimStatus.SUBMITTED -> listOf(
+                PillData(
+                    resources.getString(R.string.home_claim_card_pill_claim),
+                    PillType.Outlined,
+                ),
+            )
+            ClaimStatus.BEING_HANDLED -> listOf(
+                PillData(
+                    resources.getString(R.string.home_claim_card_pill_claim),
+                    PillType.Outlined,
+                ),
+            )
+            ClaimStatus.CLOSED -> {
+                when (homeQueryClaim.outcome) {
+                    ClaimOutcome.PAID -> {
+                        mutableListOf<PillData>().apply {
+                            add(
+                                PillData(
+                                    resources.getString(R.string.claim_decision_paid),
+                                    PillType.Contained(DarkAndLightColor.primary()),
+                                ),
+                            )
+                            homeQueryClaim.payout?.let { payout: HomeQuery.Payout ->
                                 add(
                                     PillData(
-                                        DisplayableText(R.string.claim_decision_paid),
-                                        PillType.Contained(DarkAndLightColor.primary()),
-                                    ),
-                                )
-                                homeQueryClaim.payout?.let { payout: HomeQuery.Payout ->
-                                    add(
-                                        PillData(
-                                            DisplayableText(payout.fragments.monetaryAmountFragment.toMonetaryAmount()),
-                                            PillType.Contained(ClaimStatusColors.Pill.paid)
-                                        )
+                                        payout.fragments.monetaryAmountFragment
+                                            .toMonetaryAmount()
+                                            .format(locale, 0),
+                                        PillType.Contained(ClaimStatusColors.Pill.paid)
                                     )
-                                }
-                            }.toList()
-                        }
-                        ClaimOutcome.NOT_COMPENSATED -> listOf(
-                            PillData(
-                                DisplayableText(R.string.claim_decision_not_compensated),
-                                PillType.Contained(DarkAndLightColor.primary())
-                            )
-                        )
-                        ClaimOutcome.NOT_COVERED -> listOf(
-                            PillData(
-                                DisplayableText(R.string.claim_decision_not_covered),
-                                PillType.Contained(DarkAndLightColor.primary())
-                            )
-                        )
-                        else -> listOf(
-                            PillData(
-                                DisplayableText(R.string.home_claim_card_pill_claim),
-                                PillType.Outlined,
-                            )
-                        )
+                                )
+                            }
+                        }.toList()
                     }
+                    ClaimOutcome.NOT_COMPENSATED -> listOf(
+                        PillData(
+                            resources.getString(R.string.claim_decision_not_compensated),
+                            PillType.Contained(DarkAndLightColor.primary())
+                        )
+                    )
+                    ClaimOutcome.NOT_COVERED -> listOf(
+                        PillData(
+                            resources.getString(R.string.claim_decision_not_covered),
+                            PillType.Contained(DarkAndLightColor.primary())
+                        )
+                    )
+                    else -> listOf(
+                        PillData(
+                            resources.getString(R.string.home_claim_card_pill_claim),
+                            PillType.Outlined,
+                        )
+                    )
                 }
-                ClaimStatus.REOPENED -> listOf(
-                    PillData(
-                        DisplayableText(R.string.home_claim_card_pill_reopened),
-                        PillType.Contained(ClaimStatusColors.Pill.reopened)
-                    ),
-                    PillData(
-                        DisplayableText(R.string.home_claim_card_pill_claim),
-                        PillType.Outlined
-                    ),
-                )
-                else -> listOf(
-                    PillData(
-                        DisplayableText(R.string.home_claim_card_pill_claim),
-                        PillType.Outlined,
-                    ),
-                )
             }
+            ClaimStatus.REOPENED -> listOf(
+                PillData(
+                    resources.getString(R.string.home_claim_card_pill_reopened),
+                    PillType.Contained(ClaimStatusColors.Pill.reopened)
+                ),
+                PillData(
+                    resources.getString(R.string.home_claim_card_pill_claim),
+                    PillType.Outlined
+                ),
+            )
+            else -> listOf(
+                PillData(
+                    resources.getString(R.string.home_claim_card_pill_claim),
+                    PillType.Outlined,
+                ),
+            )
         }
     }
 }
--- app/src/main/java/com/hedvig/app/feature/home/ui/claimstatus/data/RelatedContractTypeData.kt
+++ app/src/main/java/com/hedvig/app/feature/home/ui/claimstatus/data/RelatedContractTypeData.kt
@@ -1,13 +1,12 @@
 package com.hedvig.app.feature.home.ui.claimstatus.data
 
 import com.hedvig.android.owldroid.graphql.HomeQuery
-import com.hedvig.app.util.compose.DisplayableText
 
-data class RelatedContractTypeData(val displayableText: DisplayableText) {
+data class RelatedContractTypeData(val text: String) {
     companion object {
         fun fromClaimStatus(homeQueryClaim: HomeQuery.Claim): RelatedContractTypeData {
             val relatedContract = homeQueryClaim.contract?.displayName ?: String()
-            return RelatedContractTypeData(DisplayableText(relatedContract))
+            return RelatedContractTypeData(relatedContract)
         }
     }
 }
