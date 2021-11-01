49f51feb2ad2064a0346853d7e944d5ded71bfe1
Oscar Nylander <oscar@hedvig.com>
2021-11-01 16:15:08 +0100

    Delete unused resources
    By doing this, we enable removing translations from Lokalise.

--- app/src/main/java/com/hedvig/app/feature/ratings/RatingsTracker.kt
+++ app/src/main/java/com/hedvig/app/feature/ratings/RatingsTracker.kt
@@ -5,6 +5,5 @@ import com.hedvig.app.feature.tracking.TrackingFacade
 class RatingsTracker(
     private val trackingFacade: TrackingFacade
 ) {
-    fun rate() = trackingFacade.track("RATINGS_DIALOG_BODY_RATE_YES")
-    fun doNotRate() = trackingFacade.track("RATINGS_DIALOG_BODY_RATE_NO")
+    fun rate() = trackingFacade.track("rate_app_completed")
 }
