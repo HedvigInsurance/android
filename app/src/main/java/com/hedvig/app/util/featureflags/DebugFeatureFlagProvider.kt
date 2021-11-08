2b9f0f604bc6051486fbe882b0a4a1f1d1b509d4
hedvigoscar <39644124+hedvigoscar@users.noreply.github.com>
2021-11-08 15:12:50 +0100

    Hide Claims Status behind a feature flag (#797)

--- app/src/main/java/com/hedvig/app/util/featureflags/DebugFeatureFlagProvider.kt
+++ app/src/main/java/com/hedvig/app/util/featureflags/DebugFeatureFlagProvider.kt
@@ -14,6 +14,7 @@ class DebugFeatureFlagProvider(
             Feature.MOVING_FLOW -> marketManager.market == Market.SE
             Feature.INSURELY_EMBARK -> true
             Feature.EMBARK_CLAIMS -> true
+            Feature.CLAIMS_STATUS -> true
         }
     }
 
