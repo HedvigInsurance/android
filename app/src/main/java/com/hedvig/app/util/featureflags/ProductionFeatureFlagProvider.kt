fb6a6c96cbf650f01bb2182d21d2ebc350568f6d
Hugo <hugo@hedvig.com>
2021-11-09 11:22:52 +0100

    Enable embark claims flow and claims status for production (#801)
    * Enable embark claims flow for production
    
    * Enable claims status for production

--- app/src/main/java/com/hedvig/app/util/featureflags/ProductionFeatureFlagProvider.kt
+++ app/src/main/java/com/hedvig/app/util/featureflags/ProductionFeatureFlagProvider.kt
@@ -14,8 +14,8 @@ class ProductionFeatureFlagProvider(
         return when (feature) {
             Feature.MOVING_FLOW -> marketManager.market == Market.SE
             Feature.INSURELY_EMBARK -> false
-            Feature.EMBARK_CLAIMS -> false
-            Feature.CLAIMS_STATUS -> false
+            Feature.EMBARK_CLAIMS -> true
+            Feature.CLAIMS_STATUS -> true
         }
     }
 
