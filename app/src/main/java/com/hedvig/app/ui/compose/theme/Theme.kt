e3654bf2b97d88fcedac2638a8a0162784649f24
stylianosgakis <stylianos@hedvig.com>
2021-11-10 18:32:31 +0100

    Make Coil use the proper ImageLoader inside Compose
    Temporary solution which breaks previews

--- app/src/main/java/com/hedvig/app/ui/compose/theme/Theme.kt
+++ app/src/main/java/com/hedvig/app/ui/compose/theme/Theme.kt
@@ -1,12 +1,19 @@
 package com.hedvig.app.ui.compose.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import coil.ImageLoader
+import coil.compose.LocalImageLoader
 import com.google.accompanist.insets.ProvideWindowInsets
 import com.google.android.material.composethemeadapter.MdcTheme
+import org.koin.core.context.GlobalContext.get
 
 @Composable
 fun HedvigTheme(content: @Composable () -> Unit) {
-    ProvideWindowInsets {
-        MdcTheme(setDefaultFontFamily = true, content = content)
+    val imageLoader = get().get<ImageLoader>()
+    CompositionLocalProvider(LocalImageLoader provides imageLoader) {
+        ProvideWindowInsets {
+            MdcTheme(setDefaultFontFamily = true, content = content)
+        }
     }
 }
