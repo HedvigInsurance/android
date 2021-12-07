c45f0c3a8a1ea5437076ee4a38d2640547dd3129
stylianosgakis <stylianos@hedvig.com>
2021-12-07 17:15:08 +0100

    Update DTO -> DataCollectionStatus
    Context:
    https://hedviginsurance.slack.com/archives/C02EXK554A2/p1638435677001900

--- app/src/main/java/com/hedvig/app/feature/offer/usecase/insurelydatacollection/DataCollectionResult.kt
+++ app/src/main/java/com/hedvig/app/feature/offer/usecase/insurelydatacollection/DataCollectionResult.kt
@@ -24,24 +24,21 @@ data class DataCollectionResult(
         companion object {
             fun fromDto(dto: com.hedvig.android.owldroid.type.DataCollectionStatus): DataCollectionStatus {
                 return when (dto) {
-                    // todo check what running actually means?
-                    com.hedvig.android.owldroid.type.DataCollectionStatus.RUNNING -> IN_PROGRESS
-                    com.hedvig.android.owldroid.type.DataCollectionStatus.COLLECTING -> IN_PROGRESS
-                    // todo should show loading while not knowing? 🤔
-                    com.hedvig.android.owldroid.type.DataCollectionStatus.UNKNOWN__ -> IN_PROGRESS
-
+                    com.hedvig.android.owldroid.type.DataCollectionStatus.COLLECTING,
+                    com.hedvig.android.owldroid.type.DataCollectionStatus.RUNNING,
                     com.hedvig.android.owldroid.type.DataCollectionStatus.LOGIN,
-                    com.hedvig.android.owldroid.type.DataCollectionStatus.USER_INPUT,
-                    com.hedvig.android.owldroid.type.DataCollectionStatus.WAITING_FOR_AUTHENTICATION,
-                    -> IN_PROGRESS // todo should never reach these options while waiting for auth?
+                    com.hedvig.android.owldroid.type.DataCollectionStatus.UNKNOWN__,
+                    -> IN_PROGRESS
 
-                    // todo what is partial? Should be considered complete?
                     com.hedvig.android.owldroid.type.DataCollectionStatus.COMPLETED_PARTIAL,
                     com.hedvig.android.owldroid.type.DataCollectionStatus.COMPLETED,
                     com.hedvig.android.owldroid.type.DataCollectionStatus.COMPLETED_EMPTY,
                     -> COMPLETE
 
-                    com.hedvig.android.owldroid.type.DataCollectionStatus.FAILED -> FAILED
+                    com.hedvig.android.owldroid.type.DataCollectionStatus.WAITING_FOR_AUTHENTICATION,
+                    com.hedvig.android.owldroid.type.DataCollectionStatus.USER_INPUT,
+                    com.hedvig.android.owldroid.type.DataCollectionStatus.FAILED,
+                    -> FAILED
                 }
             }
         }
