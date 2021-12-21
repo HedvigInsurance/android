fd961021a0c691ccbc9b4a5cbacfca22200f56a2
stylianosgakis <stylianos@hedvig.com>
2021-12-21 19:14:03 +0200

    More UI work on claim details screen

--- app/src/main/java/com/hedvig/app/feature/claimdetail/ui/ClaimDetailCard.kt
+++ app/src/main/java/com/hedvig/app/feature/claimdetail/ui/ClaimDetailCard.kt
@@ -1,26 +1,22 @@
 package com.hedvig.app.feature.claimdetail.ui
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,6 +24,7 @@ import com.hedvig.app.R
 import com.hedvig.app.feature.claimdetail.model.ClaimDetailsData
 import com.hedvig.app.feature.home.ui.claimstatus.composables.ClaimProgress
 import com.hedvig.app.feature.home.ui.claimstatus.data.ClaimProgressData
+import com.hedvig.app.ui.compose.composables.ChatIcon
 import com.hedvig.app.ui.compose.theme.HedvigTheme
 import com.hedvig.app.util.compose.preview.previewData
 
@@ -39,56 +36,69 @@ fun ClaimDetailCard(
 ) {
     Card(modifier = modifier) {
         Column {
-            Spacer(Modifier.height(16.dp))
-            ClaimProgress(
-                claimProgressData = data.progress,
-                modifier = Modifier.padding(horizontal = 16.dp)
+            TopSection(
+                progressData = data.progress,
+                statusParagraph = data.statusParagraph,
+                modifier = Modifier.padding(
+                    start = 16.dp,
+                    top = 16.dp,
+                    end = 16.dp,
+                    bottom = 20.dp,
+                )
             )
-            Spacer(Modifier.height(24.dp))
-            Text(
-                text = data.statusParagraph,
-                modifier = Modifier.padding(horizontal = 16.dp),
-                style = MaterialTheme.typography.subtitle1,
-            )
-            Spacer(Modifier.height(24.dp))
             Divider()
-            Spacer(Modifier.height(16.dp))
-            Row(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Column {
-                    Text(
-                        text = stringResource(R.string.claim_status_contact_generic_subtitle),
-                        style = MaterialTheme.typography.caption,
-                    )
-                    Spacer(Modifier.height(4.dp))
-                    Text(
-                        text = stringResource(R.string.claim_status_contact_generic_title),
-                        style = MaterialTheme.typography.body1,
-                    )
-                }
-                IconButton(onClick = onChatClick) {
-                    Surface(
-                        modifier = Modifier.size(40.dp),
-                        shape = MaterialTheme.shapes.medium,
-                        color = MaterialTheme.colors.background,
-                    ) {
-                        Image(
-                            painter = painterResource(R.drawable.ic_chat_black),
-                            contentDescription = stringResource(
-                                R.string.claim_status_detail_chat_button_description
-                            ),
-                            contentScale = ContentScale.Inside,
-                        )
-                    }
-                }
-            }
-            Spacer(Modifier.height(16.dp))
+            BottomSection(
+                onChatClick = onChatClick,
+                modifier = Modifier.padding(16.dp)
+            )
         }
+    }
+}
+
+@Composable
+private fun TopSection(
+    progressData: List<ClaimProgressData>,
+    statusParagraph: String,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        ClaimProgress(
+            claimProgressData = progressData,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        Spacer(Modifier.height(24.dp))
+        Text(
+            text = statusParagraph,
+            modifier = Modifier.padding(horizontal = 16.dp),
+            style = MaterialTheme.typography.subtitle1,
+        )
+    }
+}
+
+@Composable
+private fun BottomSection(
+    onChatClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+            Text(
+                text = stringResource(R.string.claim_status_contact_generic_subtitle),
+                style = MaterialTheme.typography.caption,
+            )
+            Text(
+                text = stringResource(R.string.claim_status_contact_generic_title),
+                style = MaterialTheme.typography.body1,
+            )
+        }
+        ChatIcon(
+            onClick = onChatClick,
+            contentDescription = stringResource(R.string.claim_status_detail_chat_button_description),
+        )
     }
 }
 
---
+++ app/src/main/java/com/hedvig/app/ui/compose/composables/ChatIcon.kt
@@ -0,0 +1,97 @@
+package com.hedvig.app.ui.compose.composables
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hedvig.app.R
+import com.hedvig.app.ui.compose.theme.HedvigTheme
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ChatIcon(
+    onClick: () -> Unit,
+    contentDescription: String?,
+    showRedDot: Boolean = true,
+) {
+    Surface(
+        color = MaterialTheme.colors.background,
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.size(40.dp),
+        onClick = onClick
+    ) {
+        val paddingAroundRedDot = 2
+        Layout(
+            modifier = Modifier.size(40.dp),
+            content = {
+                Image(
+                    painterResource(R.drawable.ic_chat_on_background),
+                    contentDescription = contentDescription,
+                    modifier = Modifier.size(24.dp)
+                )
+                if (showRedDot) {
+                    Box(
+                        modifier = Modifier
+                            .size(12.dp)
+                            .clip(CircleShape)
+                            .background(MaterialTheme.colors.background)
+                            .padding(paddingAroundRedDot.dp)
+                            .clip(CircleShape)
+                            .background(MaterialTheme.colors.error),
+                        content = {},
+                    )
+                }
+            },
+        ) { measurables, initialConstraints ->
+            val constraints = initialConstraints.copy(minWidth = 0, minHeight = 0)
+            val image = measurables[0].measure(constraints)
+            val circle = measurables.getOrNull(1)?.measure(constraints)
+
+            val maxWidth = constraints.maxWidth
+            val maxHeight = constraints.maxHeight
+
+            layout(maxWidth, maxHeight) {
+                val imageX = (maxWidth - image.width) / 2
+                val imageY = (maxHeight - image.height) / 2
+                image.place(
+                    x = imageX,
+                    y = imageY,
+                )
+                circle?.place(
+                    x = imageX + image.width - circle.width + paddingAroundRedDot.dp.roundToPx(),
+                    y = imageY - paddingAroundRedDot.dp.roundToPx(),
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ChatIconPreview() {
+    HedvigTheme {
+        Surface(
+            color = MaterialTheme.colors.surface,
+        ) {
+            Column() {
+                ChatIcon({}, null,)
+                ChatIcon({}, null, false)
+            }
+        }
+    }
+}
