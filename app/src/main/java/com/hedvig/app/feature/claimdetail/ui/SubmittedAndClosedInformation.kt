4b700769935c900cbe10c7f8819f9e8d5bdd9daf
stylianosgakis <stylianos@hedvig.com>
2021-12-23 17:21:31 +0200

    Make time inside SubmittedAndClosedInformation update live

--- app/src/main/java/com/hedvig/app/feature/claimdetail/ui/SubmittedAndClosedInformation.kt
+++ app/src/main/java/com/hedvig/app/feature/claimdetail/ui/SubmittedAndClosedInformation.kt
@@ -1,23 +1,31 @@
 package com.hedvig.app.feature.claimdetail.ui
 
-import android.text.format.DateUtils
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hedvig.app.R
+import com.hedvig.app.ui.compose.theme.HedvigTheme
+import com.hedvig.app.util.HedvigDateUtils
+import com.hedvig.app.util.compose.currentTimeAsState
+import java.time.Duration
 import java.time.Instant
 import java.util.Locale
+import java.util.TimeZone
 
 @Composable
 fun SubmittedAndClosedInformation(
@@ -25,15 +33,17 @@ fun SubmittedAndClosedInformation(
     closedAt: Instant?,
     locale: Locale,
 ) {
-    val context = LocalContext.current
-    val submittedText = remember(submittedAt) {
-        DateUtils.getRelativeTimeSpanString(context, submittedAt.toEpochMilli()).toString()
+    val now by currentTimeAsState(updateIntervalInSeconds = 1L)
+    val submittedText by remember {
+        derivedStateOf { HedvigDateUtils.getRelativeTimeSpanString(submittedAt, now) }
     }
-    val closedText = remember(closedAt) {
-        if (closedAt == null) {
-            "—"
-        } else {
-            DateUtils.getRelativeTimeSpanString(context, closedAt.toEpochMilli()).toString()
+    val closedText by remember {
+        derivedStateOf {
+            if (closedAt == null) {
+                "—"
+            } else {
+                HedvigDateUtils.getRelativeTimeSpanString(closedAt, now)
+            }
         }
     }
     Row {
@@ -70,5 +80,28 @@ private fun SubmittedAndClosedInformationColumn(
             text = bottomText,
             style = MaterialTheme.typography.body1,
         )
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(locale = "sv")
+@Preview(locale = "nn")
+@Preview(locale = "da")
+@Preview(locale = "fr")
+@Preview(locale = "el")
+@Composable
+fun SubmittedAndClosedInformationPreview() {
+    TimeZone.getDefault().id
+    HedvigTheme {
+        Surface(
+            color = MaterialTheme.colors.background,
+        ) {
+            SubmittedAndClosedInformation(
+                submittedAt = Instant.now().minus(Duration.ofDays(10)),
+                closedAt = Instant.now().minus(Duration.ofSeconds(30)),
+                Locale.ENGLISH,
+            )
+        }
     }
 }
