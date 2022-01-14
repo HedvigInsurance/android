3969069b740e83f374bb059d6f0001d77bfb1dbb
stylianosgakis <stylianos@hedvig.com>
2022-01-14 12:27:09 +0200

    Move FakeAudioWaves into their own file

--- app/src/main/java/com/hedvig/app/feature/claimdetail/ui/FakeWaveAudioPlayerCard.kt
+++ app/src/main/java/com/hedvig/app/feature/claimdetail/ui/FakeWaveAudioPlayerCard.kt
@@ -7,21 +7,15 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -32,20 +26,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerInputChange
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import com.google.android.material.math.MathUtils.lerp
 import com.hedvig.app.R
 import com.hedvig.app.service.audioplayer.AudioPlayerState
 import com.hedvig.app.service.audioplayer.AudioPlayerState.Ready.ReadyState
@@ -53,9 +43,6 @@ import com.hedvig.app.ui.compose.theme.HedvigTheme
 import com.hedvig.app.ui.compose.theme.onWarning
 import com.hedvig.app.ui.compose.theme.warning
 import com.hedvig.app.util.ProgressPercentage
-import kotlin.math.abs
-import kotlin.math.absoluteValue
-import kotlin.random.Random
 
 fun interface WaveInteraction {
     /**
@@ -76,11 +63,11 @@ fun FakeWaveAudioPlayerCard(
     modifier: Modifier = Modifier,
 ) {
     val color by animateColorAsState(
-        cardColor(audioPlayerState),
+        cardColorForState(audioPlayerState),
         animationSpec = tween(500)
     )
     val contentColor by animateColorAsState(
-        cardContentColor(audioPlayerState),
+        cardContentColorForState(audioPlayerState),
         animationSpec = tween(500)
     )
     Surface(
@@ -100,14 +87,14 @@ fun FakeWaveAudioPlayerCard(
 }
 
 @Composable
-private fun cardColor(audioPlayerState: AudioPlayerState): Color = when {
+private fun cardColorForState(audioPlayerState: AudioPlayerState): Color = when {
     audioPlayerState.isFailed -> MaterialTheme.colors.warning
     isSystemInDarkTheme() -> MaterialTheme.colors.secondary
     else -> colorResource(R.color.lavender_200)
 }
 
 @Composable
-private fun cardContentColor(audioPlayerState: AudioPlayerState): Color = when {
+private fun cardContentColorForState(audioPlayerState: AudioPlayerState): Color = when {
     audioPlayerState.isFailed -> MaterialTheme.colors.onWarning
     isSystemInDarkTheme() -> colorResource(R.color.lavender_200)
     else -> MaterialTheme.colors.secondary
@@ -167,7 +154,7 @@ private fun FakeWaveAudioPlayerCardContent(
 
 @Composable
 private fun AudioPlayerActionOrLoadingIcon(
-    audioPlayerState: AudioPlayerState, // AudioPlayerState.Perparing | AudioPlayerState.Ready
+    audioPlayerState: AudioPlayerState, // AudioPlayerState.Preparing | AudioPlayerState.Ready
     startPlaying: () -> Unit,
     pause: () -> Unit,
     modifier: Modifier = Modifier,
@@ -206,98 +193,6 @@ private fun AudioPlayerActionOrLoadingIcon(
             is AudioPlayerState.Failed -> throw IllegalArgumentException("Impossible")
         }
     }
-}
-
-private const val numberOfWaves = 50
-private const val waveWidthPercentOfSpaceAvailable = 0.5f
-
-@Composable
-private fun FakeAudioWaves(
-    progressPercentage: ProgressPercentage,
-    playedColor: Color,
-    notPlayedColor: Color,
-    waveInteraction: WaveInteraction,
-    modifier: Modifier = Modifier,
-) {
-    BoxWithConstraints(modifier) {
-        val updatedWaveInteraction by rememberUpdatedState(waveInteraction)
-        val waveWidth = remember(maxWidth) {
-            (maxWidth / numberOfWaves.toFloat()) * waveWidthPercentOfSpaceAvailable
-        }
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .pointerInput(Unit) {
-                    detectTapGestures { offset ->
-                        updatedWaveInteraction.onInteraction(
-                            ProgressPercentage.of(current = offset.x.toDp(), target = maxWidth)
-                        )
-                    }
-                }
-                .pointerInput(Unit) {
-                    detectHorizontalDragGestures { change: PointerInputChange, dragAmount: Float ->
-                        // Do not trigger on minuscule movements
-                        if (dragAmount.absoluteValue < 1f) return@detectHorizontalDragGestures
-                        updatedWaveInteraction.onInteraction(
-                            ProgressPercentage.of(current = change.position.x.toDp(), target = maxWidth)
-                        )
-                    }
-                }
-        ) {
-            repeat(numberOfWaves) { waveIndex ->
-                FakeAudioWave(
-                    progressPercentage = progressPercentage,
-                    numberOfWaves = numberOfWaves,
-                    waveIndex = waveIndex,
-                    playedColor = playedColor,
-                    notPlayedColor = notPlayedColor,
-                    modifier = Modifier.width(waveWidth)
-                )
-            }
-        }
-    }
-}
-
-private const val minWaveHeightFraction = 0.1f
-private const val maxWaveHeightFractionForSideWaves = 0.1f
-private const val maxWaveHeightFraction = 1.0f
-
-@Composable
-private fun FakeAudioWave(
-    progressPercentage: ProgressPercentage,
-    @Suppress("SameParameterValue")
-    numberOfWaves: Int,
-    waveIndex: Int,
-    playedColor: Color,
-    notPlayedColor: Color,
-    modifier: Modifier = Modifier,
-) {
-    val height = remember(waveIndex, numberOfWaves) {
-        val wavePosition = waveIndex + 1
-        val centerPoint = numberOfWaves / 2
-        val distanceFromCenterPoint = abs(centerPoint - wavePosition)
-        val percentageToCenterPoint = ((centerPoint - distanceFromCenterPoint).toFloat() / centerPoint)
-        val maxHeightFraction = lerp(
-            maxWaveHeightFractionForSideWaves,
-            maxWaveHeightFraction,
-            percentageToCenterPoint
-        )
-        if (maxHeightFraction <= minWaveHeightFraction) {
-            maxHeightFraction
-        } else {
-            Random.nextDouble(minWaveHeightFraction.toDouble(), maxHeightFraction.toDouble()).toFloat()
-        }
-    }
-    val hasPlayedThisWave = remember(progressPercentage, numberOfWaves, waveIndex) {
-        progressPercentage.value * numberOfWaves > waveIndex
-    }
-    Surface(
-        shape = CircleShape,
-        color = if (hasPlayedThisWave) playedColor else notPlayedColor,
-        modifier = modifier.fillMaxHeight(fraction = height),
-    ) {}
 }
 
 @Preview("Interactive Preview")
