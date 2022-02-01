a6bb21d99022403b26bfca750cc26ee8444befd0
Oscar Nylander <oscar@hedvig.com>
2022-02-01 11:10:26 +0100

    Migrate old engineering tracking tooling to hAnalytics

--- app/src/engineering/java/com/hedvig/app/feature/tracking/DebugLogTrackerSink.kt
+++ app/src/engineering/java/com/hedvig/app/feature/tracking/DebugLogTrackerSink.kt
@@ -1,31 +1,15 @@
 package com.hedvig.app.feature.tracking
 
-import com.adyen.checkout.core.model.getStringOrNull
-import com.hedvig.app.ScreenTracker
+import com.hedvig.app.util.toJsonObject
+import com.hedvig.hanalytics.HAnalyticsEvent
 import d
-import org.json.JSONObject
 
 /**
  * Sink to assist in seeing where in the app you are navigating while testing and which events are triggered
  * Use by opening AS Logcat on "Debug" mode with the filter "DebugLogTrackerSink"
  */
-class DebugLogTrackerSink : TrackerSink {
-    override fun track(eventName: String, properties: JSONObject?) {
-        d {
-            if (eventName == ScreenTracker.SCREEN_VIEW) {
-                properties?.getStringOrNull(ScreenTracker.SCREEN_NAME)?.let { screenName ->
-                    return@d "Navigated to: $screenName"
-                }
-            }
-            "Track $eventName, properties: $properties"
-        }
-    }
-
-    override fun identify(id: String) {
-        // Not relevant to see in logcat
-    }
-
-    override fun setProperty(name: String, value: String) {
-        // Not relevant to see in logcat
+class DebugLogTrackerSink : HAnalyticsSink {
+    override fun send(event: HAnalyticsEvent) {
+        d { "Track ${event.name}, properties: ${event.properties.toJsonObject().toString(2)}" }
     }
 }
