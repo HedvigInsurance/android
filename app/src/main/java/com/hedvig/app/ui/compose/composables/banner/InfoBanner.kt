b524d764c61cb5bab54762e73122b91dd3dc3055
Hugo Linder <hugo@hedvig.com>
2022-02-02 15:52:58 +0100

    Tweak banner design

--- app/src/main/java/com/hedvig/app/ui/compose/composables/banner/InfoBanner.kt
+++ app/src/main/java/com/hedvig/app/ui/compose/composables/banner/InfoBanner.kt
@@ -1,5 +1,6 @@
 package com.hedvig.app.ui.compose.composables.banner
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,11 +16,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hedvig.app.R
 import com.hedvig.app.ui.compose.theme.HedvigTheme
+import com.hedvig.app.ui.compose.theme.hedvigBlack12percent
 
 @Composable
 fun InfoBanner(
     onClick: () -> Unit,
-    text: String
+    text: String,
+    modifier: Modifier = Modifier
 ) {
     val backgroundColor = if (isSystemInDarkTheme()) {
         colorResource(R.color.lavender_400)
@@ -28,16 +31,17 @@ fun InfoBanner(
     }
 
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .clickable { onClick() },
         color = backgroundColor,
+        border = BorderStroke(1.dp, hedvigBlack12percent)
     ) {
         Text(
             modifier = Modifier.padding(16.dp),
             text = text,
-            color = contentColorFor(backgroundColor),
-            style = MaterialTheme.typography.caption,
+            color = contentColorFor(backgroundColor).copy(alpha = 0.5f),
+            style = MaterialTheme.typography.subtitle2,
         )
     }
 }
