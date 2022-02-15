79ee9c28f2fdc3ba1dcb312249b1a3502ae21036
Hugo Linder <hugo@hedvig.com>
2022-02-15 16:36:36 +0100

    Let moving flow be enabled/disabled by feature manager

--- app/src/main/java/com/hedvig/app/util/featureflags/DebugFeatureFlagProvider.kt
+++ app/src/main/java/com/hedvig/app/util/featureflags/DebugFeatureFlagProvider.kt
@@ -3,21 +3,18 @@ package com.hedvig.app.util.featureflags
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
-import com.hedvig.app.feature.settings.Market
-import com.hedvig.app.feature.settings.MarketManager
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 
 class DebugFeatureFlagProvider(
-    private val marketManager: MarketManager,
     private val dataStore: DataStore<Preferences>,
 ) : FeatureFlagProvider {
 
     override val priority = DEBUG_PRIORITY
 
     override fun isFeatureEnabled(feature: Feature) = when (feature) {
-        Feature.MOVING_FLOW -> marketManager.market == Market.SE || marketManager.market == Market.NO
+        Feature.MOVING_FLOW -> isEnabled(Feature.MOVING_FLOW)
         Feature.FRANCE_MARKET -> isEnabled(Feature.FRANCE_MARKET)
         Feature.ADDRESS_AUTO_COMPLETE -> isEnabled(Feature.ADDRESS_AUTO_COMPLETE)
         Feature.REFERRAL_CAMPAIGN -> isEnabled(Feature.REFERRAL_CAMPAIGN)
