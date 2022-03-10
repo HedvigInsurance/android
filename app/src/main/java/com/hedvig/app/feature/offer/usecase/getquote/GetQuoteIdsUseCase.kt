f0619de9c5eda4a95d1c4da5a2b4664fcaefabdc
stylianosgakis <stylianos@hedvig.com>
2022-03-10 14:39:15 +0100

    Fix crash on startup on a weird app state

--- app/src/main/java/com/hedvig/app/feature/offer/usecase/getquote/GetQuoteIdsUseCase.kt
+++ app/src/main/java/com/hedvig/app/feature/offer/usecase/getquote/GetQuoteIdsUseCase.kt
@@ -1,8 +1,8 @@
 package com.hedvig.app.feature.offer.usecase.getquote
 
 import arrow.core.Either
+import arrow.core.left
 import arrow.core.nonEmptyListOf
-import arrow.core.right
 import com.apollographql.apollo.ApolloClient
 import com.hedvig.app.feature.offer.OfferRepository
 import com.hedvig.app.util.featureflags.Feature
@@ -39,6 +39,6 @@ class GetQuoteIdsUseCase(
     }
 
     private fun getIdsFromLastQuoteOfMember(): Either<Error, QuoteIds> {
-        return QuoteIds(emptyList()).right()
+        return Error(null).left()
     }
 }
