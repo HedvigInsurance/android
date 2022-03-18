9ab64423995887bb46373aba3bb33b2fb2359897
stylianosgakis <stylianos@hedvig.com>
2022-03-18 13:22:43 +0100

    Make test mock use coroutines

--- app/src/androidTest/java/com/hedvig/app/util/FeatureFlagRule.kt
+++ app/src/androidTest/java/com/hedvig/app/util/FeatureFlagRule.kt
@@ -3,7 +3,7 @@ package com.hedvig.app.util
 import com.hedvig.app.featureManagerModule
 import com.hedvig.app.util.featureflags.Feature
 import com.hedvig.app.util.featureflags.FeatureManager
-import io.mockk.every
+import io.mockk.coEvery
 import io.mockk.mockk
 import org.junit.rules.ExternalResource
 import org.koin.core.context.loadKoinModules
@@ -11,12 +11,12 @@ import org.koin.core.context.unloadKoinModules
 import org.koin.dsl.module
 
 class FeatureFlagRule(
-    vararg flags: Pair<Feature, Boolean>
+    vararg flags: Pair<Feature, Boolean>,
 ) : ExternalResource() {
     private val mockModule = module {
         single {
             val mock = mockk<FeatureManager>()
-            flags.forEach { every { mock.isFeatureEnabled(it.first) } returns it.second }
+            flags.forEach { coEvery { mock.isFeatureEnabled(it.first) } returns it.second }
             mock
         }
     }
