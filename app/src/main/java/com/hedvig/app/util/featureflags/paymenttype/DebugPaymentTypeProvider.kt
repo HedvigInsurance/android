00fc22283a40ce584c7c5c35db3ac0f52e876b9d
stylianosgakis <stylianos@hedvig.com>
2022-03-22 19:54:36 +0100

    Add local and hanalytics providers for more experiments

---
+++ app/src/main/java/com/hedvig/app/util/featureflags/paymenttype/DebugPaymentTypeProvider.kt
@@ -0,0 +1,20 @@
+package com.hedvig.app.util.featureflags.paymenttype
+
+import com.hedvig.app.feature.settings.Market
+import com.hedvig.app.feature.settings.MarketManager
+import com.hedvig.hanalytics.PaymentType
+
+class DebugPaymentTypeProvider(
+    private val marketManager: MarketManager,
+) : PaymentTypeProvider {
+    // todo actually check what this needs to be in our dev environment
+    override suspend fun getPaymentType(): PaymentType {
+        return when (marketManager.market) {
+            Market.SE -> PaymentType.ADYEN
+            Market.NO -> PaymentType.TRUSTLY
+            Market.DK -> PaymentType.TRUSTLY
+            Market.FR -> throw IllegalArgumentException()
+            null -> PaymentType.TRUSTLY
+        }
+    }
+}
