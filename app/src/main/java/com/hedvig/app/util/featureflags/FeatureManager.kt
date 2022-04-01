4cd14b39c541252f37b2f5c81d066dee4da879f2
stylianosgakis <stylianos@hedvig.com>
2022-04-01 15:26:32 +0200

    Expose invalidateExperiments function to FeatureManager

--- app/src/main/java/com/hedvig/app/util/featureflags/FeatureManager.kt
+++ app/src/main/java/com/hedvig/app/util/featureflags/FeatureManager.kt
@@ -4,13 +4,21 @@ import com.hedvig.app.util.featureflags.flags.FeatureFlagProvider
 import com.hedvig.app.util.featureflags.loginmethod.LoginMethodProvider
 import com.hedvig.app.util.featureflags.paymenttype.PaymentTypeProvider
 
-interface FeatureManager : FeatureFlagProvider, LoginMethodProvider, PaymentTypeProvider
+interface FeatureManager : FeatureFlagProvider, LoginMethodProvider, PaymentTypeProvider {
+    suspend fun invalidateExperiments()
+}
 
 class FeatureManagerImpl(
     private val featureFlagProvider: FeatureFlagProvider,
     private val loginMethodProvider: LoginMethodProvider,
     private val paymentTypeProvider: PaymentTypeProvider,
+    private val clearHAnalyticsExperimentsCacheUseCase: ClearHAnalyticsExperimentsCacheUseCase,
 ) : FeatureManager,
     FeatureFlagProvider by featureFlagProvider,
     LoginMethodProvider by loginMethodProvider,
-    PaymentTypeProvider by paymentTypeProvider
+    PaymentTypeProvider by paymentTypeProvider {
+
+    override suspend fun invalidateExperiments() {
+        clearHAnalyticsExperimentsCacheUseCase.invoke()
+    }
+}
