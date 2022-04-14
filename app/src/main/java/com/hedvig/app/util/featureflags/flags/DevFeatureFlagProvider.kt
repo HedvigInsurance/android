b19da52885848935b53d12ae3cddde8d3d776ba0
stylianosgakis <stylianos@hedvig.com>
2022-04-14 14:58:42 +0200

    Use `sort lines` action on Features enum
    Since this file will only get bigger, might as well keep the features
    sorted alphabetically. Luckily IntelliJ provides a build-in support to
    do that for us.

--- app/src/main/java/com/hedvig/app/util/featureflags/flags/DevFeatureFlagProvider.kt
+++ app/src/main/java/com/hedvig/app/util/featureflags/flags/DevFeatureFlagProvider.kt
@@ -8,13 +8,13 @@ class DevFeatureFlagProvider(
 ) : FeatureFlagProvider {
 
     override suspend fun isFeatureEnabled(feature: Feature) = when (feature) {
+        Feature.CONNECT_PAYMENT_AT_SIGN -> marketManager.market == Market.NO || marketManager.market == Market.DK
         Feature.EXTERNAL_DATA_COLLECTION -> marketManager.market == Market.SE
         Feature.FRANCE_MARKET -> true
         Feature.KEY_GEAR -> false
         Feature.MOVING_FLOW -> true
-        Feature.CONNECT_PAYMENT_AT_SIGN -> marketManager.market == Market.NO || marketManager.market == Market.DK
-        Feature.UPDATE_NECESSARY -> false
-        Feature.REFERRAL_CAMPAIGN -> false
         Feature.QUOTE_CART -> false
+        Feature.REFERRAL_CAMPAIGN -> false
+        Feature.UPDATE_NECESSARY -> false
     }
 }
