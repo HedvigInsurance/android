76af03df57b86fb00c2d0236b11002956e48625d
Oscar Nylander <oscar@hedvig.com>
2022-04-25 10:10:17 +0200

    Update hAnalytics and remove TODO

--- app/src/main/java/com/hedvig/app/util/featureflags/flags/HAnalyticsFeatureFlagProvider.kt
+++ app/src/main/java/com/hedvig/app/util/featureflags/flags/HAnalyticsFeatureFlagProvider.kt
@@ -16,7 +16,7 @@ class HAnalyticsFeatureFlagProvider(
         Feature.MOVING_FLOW -> hAnalytics.movingFlow()
         Feature.QUOTE_CART -> hAnalytics.useQuoteCart()
         Feature.REFERRAL_CAMPAIGN -> hAnalytics.foreverFebruaryCampaign()
-        Feature.SHOW_CHARITY -> TODO("hAnalytics.qasa() or whatever the name will be")
+        Feature.SHOW_CHARITY -> hAnalytics.showCharity()
         Feature.UPDATE_NECESSARY -> hAnalytics.updateNecessary()
     }
 }
