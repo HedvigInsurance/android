683031baa34dff8dd19249e0c0d5d3cf5a7a2fda
stylianosgakis <stylianos@hedvig.com>
2022-04-25 19:08:37 +0300

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         applicationId = "com.hedvig"
 
         versionCode = 43
-        versionName = "6.5.5"
+        versionName = "6.5.6"
 
         vectorDrawables.useSupportLibrary = true
 
