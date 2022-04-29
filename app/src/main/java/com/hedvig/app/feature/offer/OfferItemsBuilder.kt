7487698d443bc63e9213e684e09b2fcab85436d5
stylianosgakis <stylianos@hedvig.com>
2022-04-29 17:57:18 +0300

    Add VariantHeader to go with the VariantButtons

--- app/src/main/java/com/hedvig/app/feature/offer/OfferItemsBuilder.kt
+++ app/src/main/java/com/hedvig/app/feature/offer/OfferItemsBuilder.kt
@@ -47,17 +47,20 @@ object OfferItemsBuilder {
             ),
         )
 
-        offerModel.variants.takeIf { it.size > 1 }?.map {
-            add(
-                OfferItems.VariantButton(
-                    id = it.id,
-                    title = it.title,
-                    subTitle = it.tag,
-                    price = it.bundle.cost.finalPremium,
-                    isSelected = it.id == quoteBundleVariant.id,
-                    onVariantSelected = onVariantSelected
+        if (offerModel.variants.isNotEmpty()) {
+            add(OfferItems.VariantHeader)
+            offerModel.variants.map {
+                add(
+                    OfferItems.VariantButton(
+                        id = it.id,
+                        title = it.title,
+                        subTitle = it.tag,
+                        price = it.bundle.cost.finalPremium,
+                        isSelected = it.id == quoteBundleVariant.id,
+                        onVariantSelected = onVariantSelected
+                    )
                 )
-            )
+            }
         }
 
         if (externalProvider != null) {
