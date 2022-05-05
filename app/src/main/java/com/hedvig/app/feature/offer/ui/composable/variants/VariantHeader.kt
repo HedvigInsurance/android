247221dd13abedd95481b2093c78e19e7668509c
stylianosgakis <stylianos@hedvig.com>
2022-05-05 12:23:43 +0300

    Add string resource

--- app/src/main/java/com/hedvig/app/feature/offer/ui/composable/variants/VariantHeader.kt
+++ app/src/main/java/com/hedvig/app/feature/offer/ui/composable/variants/VariantHeader.kt
@@ -7,14 +7,16 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hedvig.app.R
 import com.hedvig.app.ui.compose.theme.HedvigTheme
 
 @Composable
 fun VariantHeader() {
     Text(
-        text = "Choose coverage",
+        text = stringResource(id = R.string.offer_screen_varianted_offers_header),
         style = MaterialTheme.typography.h6,
         modifier = Modifier
             .padding(horizontal = 16.dp)
