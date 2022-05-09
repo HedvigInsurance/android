049a41e99b80fe1e8bd0a431f62bcd972506e9e8
stylianosgakis <stylianos@hedvig.com>
2022-05-09 18:03:09 +0300

    Simply build the SelfChangeEligibilityResult without an `either`

--- app/src/main/java/com/hedvig/app/feature/home/ui/changeaddress/GetAddressChangeStoryIdUseCase.kt
+++ app/src/main/java/com/hedvig/app/feature/home/ui/changeaddress/GetAddressChangeStoryIdUseCase.kt
@@ -3,11 +3,13 @@ package com.hedvig.app.feature.home.ui.changeaddress
 import arrow.core.Either
 import arrow.core.computations.either
 import arrow.core.computations.ensureNotNull
+import arrow.core.getOrHandle
 import arrow.core.identity
 import com.apollographql.apollo.ApolloClient
 import com.hedvig.android.owldroid.graphql.ActiveContractBundlesQuery
 import com.hedvig.app.feature.embark.QUOTE_CART_ID_KEY
 import com.hedvig.app.feature.embark.quotecart.CreateQuoteCartUseCase
+import com.hedvig.app.util.ErrorMessage
 import com.hedvig.app.util.apollo.safeQuery
 
 class GetAddressChangeStoryIdUseCase(
@@ -16,44 +18,29 @@ class GetAddressChangeStoryIdUseCase(
 ) {
 
     suspend fun invoke(): SelfChangeEligibilityResult {
-        return either<SelfChangeFailureType, SelfChangeEligibilityResult.Eligible> {
-            val activeContractBundlesQueryData = apolloClient.query(ActiveContractBundlesQuery())
-                .safeQuery()
-                .toEither(SelfChangeFailureType::Error)
-                .bind()
-            val storyId =
-                activeContractBundlesQueryData.activeContractBundles.firstOrNull()?.angelStories?.addressChangeV2
-            ensureNotNull(storyId) { SelfChangeFailureType.Blocked }
-            val storyIdWithQuoteCartId = addQuoteCartId(storyId).bind()
-            SelfChangeEligibilityResult.Eligible(storyIdWithQuoteCartId)
-        }.fold(SelfChangeFailureType::toSelfChangeEligibilityResult, ::identity)
+        val activeContractBundlesQueryData = apolloClient.query(ActiveContractBundlesQuery())
+            .safeQuery()
+            .toEither()
+            .getOrHandle { errorQueryResult ->
+                return SelfChangeEligibilityResult.Error(errorQueryResult.message)
+            }
+
+        val storyId = activeContractBundlesQueryData.activeContractBundles.firstOrNull()?.angelStories?.addressChangeV2
+            ?: return SelfChangeEligibilityResult.Blocked
+        val storyIdWithQuoteCartId = addQuoteCartId(storyId).getOrHandle { errorMessage ->
+            return SelfChangeEligibilityResult.Error(errorMessage.message)
+        }
+        return SelfChangeEligibilityResult.Eligible(storyIdWithQuoteCartId)
     }
 
-    private suspend fun addQuoteCartId(storyId: String): Either<SelfChangeFailureType.Error, String> {
-        return createQuoteCartUseCase.invoke()
-            .bimap(
-                { errorMessage -> SelfChangeFailureType.Error(errorMessage.message) },
-                { quoteCartId -> appendQuoteCartId(storyId, quoteCartId.id) }
-            )
+    private suspend fun addQuoteCartId(storyId: String): Either<ErrorMessage, String> {
+        return createQuoteCartUseCase.invoke().map { quoteCartId -> appendQuoteCartId(storyId, quoteCartId.id) }
     }
 
     sealed class SelfChangeEligibilityResult {
         data class Eligible(val embarkStoryId: String) : SelfChangeEligibilityResult()
         object Blocked : SelfChangeEligibilityResult()
         data class Error(val message: String?) : SelfChangeEligibilityResult()
-    }
-
-    private sealed interface SelfChangeFailureType {
-        fun toSelfChangeEligibilityResult(): SelfChangeEligibilityResult {
-            return when (this) {
-                Blocked -> SelfChangeEligibilityResult.Blocked
-                is Error -> SelfChangeEligibilityResult.Error(this.message)
-            }
-        }
-
-        object Blocked : SelfChangeFailureType
-
-        data class Error(val message: String?) : SelfChangeFailureType
     }
 }
 
