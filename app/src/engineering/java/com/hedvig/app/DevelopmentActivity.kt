eeba37eb4988bed6881c675a76c515aba3bed0f9
stylianosgakis <stylianos@hedvig.com>
2022-05-12 14:18:08 +0300

    Fix a couple of mishaps after cherry pick.
    Now there is no meaningful diff of this state head and APP-1671: Check
    offer checkout status (#993) (88d940177c5c9a542eb350a83e7bc98978488c49)
    Which was the head before this whole reverting shenanigans.
    
    Should merge this after the release branch is cut out

--- app/src/engineering/java/com/hedvig/app/DevelopmentActivity.kt
+++ app/src/engineering/java/com/hedvig/app/DevelopmentActivity.kt
@@ -15,7 +15,6 @@ import com.hedvig.app.feature.genericauth.GenericAuthMockActivity
 import com.hedvig.app.feature.home.HomeMockActivity
 import com.hedvig.app.feature.loggedin.LoggedInMockActivity
 import com.hedvig.app.feature.loggedin.ui.LoggedInActivity
-import com.hedvig.app.feature.onboarding.OnboardingMockActivity
 import com.hedvig.app.feature.referrals.ReferralsMockActivity
 import com.hedvig.app.feature.sunsetting.ForceUpgradeActivity
 import com.hedvig.app.feature.tracking.TrackingLogActivity
@@ -67,9 +66,6 @@ class DevelopmentActivity : AppCompatActivity(R.layout.activity_development) {
                     },
                     DevelopmentScreenAdapter.DevelopmentScreenItem.Row("Home") {
                         startActivity(Intent(this, HomeMockActivity::class.java))
-                    },
-                    DevelopmentScreenAdapter.DevelopmentScreenItem.Row("Insurance") {
-                        startActivity(Intent(this, InsuranceMockActivity::class.java))
                     },
                     DevelopmentScreenAdapter.DevelopmentScreenItem.Row("Logged in") {
                         startActivity(Intent(this, LoggedInMockActivity::class.java))
