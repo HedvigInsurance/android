36e5e17696fc952ad784168cd086adda6a9bd790
stylianosgakis <stylianos@hedvig.com>
2022-05-16 21:33:21 +0300

    Migrate apollo config inside build.gradle.kts

--- apollo/build.gradle.kts
+++ apollo/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.apollographql.apollo3.compiler.MODELS_COMPAT
+
 plugins {
     id("com.android.library")
     id("kotlin-android")
@@ -5,27 +7,48 @@ plugins {
 }
 
 apollo {
-    generateKotlinModels.set(true)
-    customTypeMapping.set(
-        mapOf(
-            "URL" to "kotlin.String",
-            "LocalDate" to "java.time.LocalDate",
-            "Upload" to "com.apollographql.apollo3.api.FileUpload",
-            "PaymentMethodsResponse" to "com.adyen.checkout.components.model.PaymentMethodsApiResponse",
-            "CheckoutPaymentsAction" to "kotlin.String",
-            "CheckoutPaymentAction" to "kotlin.String",
-            "JSONString" to "org.json.JSONObject",
-            "Instant" to "java.time.Instant",
+    service("giraffe") {
+        introspection {
+            endpointUrl.set("https://graphql.dev.hedvigit.com/graphql")
+            schemaFile.set(file("src/main/graphql/com/hedvig/android/owldroid/schema.graphqls"))
+        }
+        schemaFile.set(file("src/main/graphql/com/hedvig/android/owldroid/schema.graphqls"))
+        srcDir(file("src/main/graphql/com/hedvig/android/owldroid/graphql"))
+
+        packageName.set("com.hedvig.android.owldroid.type")
+        codegenModels.set(MODELS_COMPAT)
+
+        // Test builders setup
+        generateKotlinModels.set(true)
+        generateTestBuilders.set(true)
+        testDirConnection {
+            // Make test builders available to main (not just test or androidTest) to be used by our mock data
+            connectToAndroidSourceSet("main")
+        }
+
+        // https://www.apollographql.com/docs/android/advanced/operation-variables/#make-nullable-variables-non-optional
+        generateOptionalOperationVariables.set(false)
+        customTypeMapping.set(
+            mapOf(
+                "URL" to "kotlin.String",
+                "LocalDate" to "java.time.LocalDate",
+                "Upload" to "com.apollographql.apollo3.api.FileUpload",
+                "PaymentMethodsResponse" to "com.adyen.checkout.components.model.PaymentMethodsApiResponse",
+                "CheckoutPaymentsAction" to "kotlin.String",
+                "CheckoutPaymentAction" to "kotlin.String",
+                "JSONString" to "org.json.JSONObject",
+                "Instant" to "java.time.Instant",
+            )
         )
-    )
-    sealedClassesForEnumsMatching.set(
-        listOf(
-            "TypeOfContract",
-            "CrossSellType",
-            "ClaimStatus",
-            "EmbarkExternalRedirectLocation",
+        sealedClassesForEnumsMatching.set(
+            listOf(
+                "TypeOfContract",
+                "CrossSellType",
+                "ClaimStatus",
+                "EmbarkExternalRedirectLocation",
+            )
         )
-    )
+    }
 }
 
 android {
