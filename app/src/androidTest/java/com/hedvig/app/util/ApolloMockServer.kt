72d10d212feb198fad31013d89f786aea9780899
stylianosgakis <stylianos@hedvig.com>
2022-05-20 14:45:55 +0300

    Replace MergingResponseWriter with Opration.Data.toJsonString()

--- app/src/androidTest/java/com/hedvig/app/util/ApolloMockServer.kt
+++ app/src/androidTest/java/com/hedvig/app/util/ApolloMockServer.kt
@@ -1,7 +1,9 @@
 package com.hedvig.app.util
 
+import com.apollographql.apollo3.api.CustomScalarAdapters
 import com.apollographql.apollo3.api.Operation
-import com.apollographql.apollo3.api.ScalarTypeAdapters
+import com.apollographql.apollo3.api.json.buildJsonString
+import com.apollographql.apollo3.api.toJson
 import com.hedvig.app.CUSTOM_SCALAR_ADAPTERS
 import com.hedvig.app.TestApplication
 import okhttp3.WebSocket
@@ -10,14 +12,28 @@ import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.RecordedRequest
-import okio.IOException
 import org.json.JSONObject
 import org.junit.rules.ExternalResource
 
-typealias ApolloResultProvider = (JSONObject) -> ApolloMockServerResult
+fun interface ApolloResultProvider {
+    fun provideResult(withVariables: JSONObject): ApolloMockServerResult
+}
 
-fun apolloMockServer(vararg mocks: Pair<String, ApolloResultProvider>) =
-    MockWebServer().apply {
+data class QueryToResultProvider(
+    val queryDocument: String,
+    val apolloResultProvider: ApolloResultProvider,
+) {
+    companion object {
+        fun fromPair(pair: Pair<String, ApolloResultProvider>): QueryToResultProvider {
+            return QueryToResultProvider(pair.first, pair.second)
+        }
+    }
+}
+
+fun apolloMockServer(vararg mocks: Pair<String, ApolloResultProvider>): MockWebServer {
+    @Suppress("NAME_SHADOWING")
+    val mocks: List<QueryToResultProvider> = mocks.map(QueryToResultProvider::fromPair)
+    return MockWebServer().apply {
         dispatcher = object : Dispatcher() {
             override fun dispatch(request: RecordedRequest): MockResponse {
                 if (request.headers.find { it.first == "Upgrade" && it.second == "websocket" } != null) {
@@ -37,10 +53,10 @@ fun apolloMockServer(vararg mocks: Pair<String, ApolloResultProvider>) =
                 }
 
                 val dataProvider =
-                    mocks.firstOrNull { it.first == query }?.second
+                    mocks.firstOrNull { it.queryDocument == query }?.apolloResultProvider
                         ?: return super.peek()
 
-                return when (val result = dataProvider(variables)) {
+                return when (val result = dataProvider.provideResult(withVariables = variables)) {
                     ApolloMockServerResult.InternalServerError -> MockResponse().setResponseCode(500)
                     is ApolloMockServerResult.GraphQLError -> MockResponse().setBody(
                         jsonObjectOf(
@@ -66,11 +82,13 @@ fun apolloMockServer(vararg mocks: Pair<String, ApolloResultProvider>) =
                 })
         }
     }
+}
 
 inline fun apolloResponse(
     crossinline build: ApolloMockServerResponseBuilder.() -> ApolloMockServerResult,
-): (JSONObject) -> ApolloMockServerResult =
-    { vars -> build(ApolloMockServerResponseBuilder(vars)) }
+): ApolloResultProvider = ApolloResultProvider { vars ->
+    build(ApolloMockServerResponseBuilder(vars))
+}
 
 sealed class ApolloMockServerResult {
     object InternalServerError : ApolloMockServerResult()
@@ -91,12 +109,13 @@ class ApolloMockServerResponseBuilder(
     fun graphQLError(vararg errors: JSONObject) =
         ApolloMockServerResult.GraphQLError(errors.toList())
 
-    fun success(data: Operation.Data) =
-        ApolloMockServerResult.GraphQLResponse(
-            data.toJson(
-                scalarTypeAdapters = CUSTOM_SCALAR_ADAPTERS
+    fun success(operationData: Operation.Data): ApolloMockServerResult.GraphQLResponse {
+        return ApolloMockServerResult.GraphQLResponse(
+            operationData.toJsonString(
+                customScalarAdapters = CUSTOM_SCALAR_ADAPTERS,
             )
         )
+    }
 
     fun success(data: JSONObject) =
         ApolloMockServerResult.GraphQLResponse(jsonObjectOf("data" to data).toString())
@@ -116,16 +135,9 @@ class ApolloMockServerRule(
     }
 }
 
-fun Operation.Data.toJson(
-    indent: String = "",
-    scalarTypeAdapters: ScalarTypeAdapters = ScalarTypeAdapters.DEFAULT,
-): String {
-    return try {
-        MergingResponseWriter(scalarTypeAdapters).let { writer ->
-            marshaller().marshal(writer)
-            writer.toJson(indent)
-        }
-    } catch (e: IOException) {
-        throw IllegalStateException(e)
+// todo replace with com.apollographql.apollo3.api.toJsonString when it's stable
+fun Operation.Data.toJsonString(customScalarAdapters: CustomScalarAdapters = CustomScalarAdapters.Empty): String {
+    return buildJsonString {
+        this@toJsonString.toJson(jsonWriter = this@buildJsonString, customScalarAdapters = customScalarAdapters)
     }
 }
