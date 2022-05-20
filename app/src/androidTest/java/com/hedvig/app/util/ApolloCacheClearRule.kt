1399f4a9d17ecead80de208d5fa147eea069b71e
stylianosgakis <stylianos@hedvig.com>
2022-05-20 14:45:38 +0300

    Migrate clearing normalized cache

--- app/src/androidTest/java/com/hedvig/app/util/ApolloCacheClearRule.kt
+++ app/src/androidTest/java/com/hedvig/app/util/ApolloCacheClearRule.kt
@@ -1,6 +1,7 @@
 package com.hedvig.app.util
 
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.cache.normalized.apolloStore
 import org.junit.rules.ExternalResource
 import org.koin.test.KoinTest
 import org.koin.test.inject
@@ -8,7 +9,6 @@ import org.koin.test.inject
 class ApolloCacheClearRule : ExternalResource(), KoinTest {
     private val apolloClient: ApolloClient by inject()
     override fun before() {
-        apolloClient
-            .clearNormalizedCache()
+        apolloClient.apolloStore.clearAll()
     }
 }
