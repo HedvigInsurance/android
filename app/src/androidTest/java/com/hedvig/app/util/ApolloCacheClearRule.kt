a17fa5f9c201d308a5369e76b2e56e4612b92c36
stylianosgakis <stylianos@hedvig.com>
2022-05-28 14:12:20 +0300

    Move get<apolloClient>() on `before()` to ensure getting latest instance

--- app/src/androidTest/java/com/hedvig/app/util/ApolloCacheClearRule.kt
+++ app/src/androidTest/java/com/hedvig/app/util/ApolloCacheClearRule.kt
@@ -4,11 +4,11 @@ import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.cache.normalized.apolloStore
 import org.junit.rules.ExternalResource
 import org.koin.test.KoinTest
-import org.koin.test.inject
+import org.koin.test.get
 
 class ApolloCacheClearRule : ExternalResource(), KoinTest {
-    private val apolloClient: ApolloClient by inject()
     override fun before() {
+        val apolloClient: ApolloClient = get()
         apolloClient.apolloStore.clearAll()
     }
 }
