8c9049a55da1edbb4d81ba86e1814d6c1c8c8e12
stylianosgakis <stylianos@hedvig.com>
2022-05-31 12:58:21 +0300

    Add sonatype to resolve the apollo gradle plugin
    From my understanding:
    It's not enough for allProjects to be configured that way since we want
    access to sonatype in the root build.gradle since the apollo gradle
    plugin resides there.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,16 +19,6 @@ subprojects {
 }
 
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        mavenLocal()
-        gradlePluginPortal()
-        maven {
-            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-        }
-    }
-
     dependencies {
         classpath(libs.android.gradlePlugin)
         classpath(libs.kotlin.gradlePlugin)
