793288ce654afeb4d4dfe52c7394197ca0ce65ab
stylianosgakis <stylianos@hedvig.com>
2022-06-01 14:03:15 +0300

    Use typed typename for ContractStatus

--- testdata/src/main/java/com/hedvig/app/testdata/common/ContractStatus.kt
+++ testdata/src/main/java/com/hedvig/app/testdata/common/ContractStatus.kt
@@ -1,5 +1,13 @@
 package com.hedvig.app.testdata.common
 
+import com.hedvig.android.owldroid.graphql.type.ActiveInFutureAndTerminatedInFutureStatus
+import com.hedvig.android.owldroid.graphql.type.ActiveInFutureStatus
+import com.hedvig.android.owldroid.graphql.type.ActiveStatus
+import com.hedvig.android.owldroid.graphql.type.PendingStatus
+import com.hedvig.android.owldroid.graphql.type.TerminatedInFutureStatus
+import com.hedvig.android.owldroid.graphql.type.TerminatedStatus
+import com.hedvig.android.owldroid.graphql.type.TerminatedTodayStatus
+
 enum class ContractStatus {
     PENDING,
     ACTIVE_IN_FUTURE,
@@ -12,12 +20,12 @@ enum class ContractStatus {
 
     val typename: String
         get() = when (this) {
-            PENDING -> "PendingStatus"
-            ACTIVE_IN_FUTURE, ACTIVE_IN_FUTURE_INVALID -> "ActiveInFutureStatus"
-            ACTIVE_IN_FUTURE_AND_TERMINATED_IN_FUTURE -> "ActiveInFutureAndTerminatedInFutureStatus"
-            ACTIVE -> "ActiveStatus"
-            TERMINATED_TODAY -> "TerminatedTodayStatus"
-            TERMINATED -> "TerminatedStatus"
-            TERMINATED_IN_FUTURE -> "TerminatedInFutureStatus"
+            PENDING -> PendingStatus.type.name
+            ACTIVE_IN_FUTURE, ACTIVE_IN_FUTURE_INVALID -> ActiveInFutureStatus.type.name
+            ACTIVE_IN_FUTURE_AND_TERMINATED_IN_FUTURE -> ActiveInFutureAndTerminatedInFutureStatus.type.name
+            ACTIVE -> ActiveStatus.type.name
+            TERMINATED_TODAY -> TerminatedTodayStatus.type.name
+            TERMINATED -> TerminatedStatus.type.name
+            TERMINATED_IN_FUTURE -> TerminatedInFutureStatus.type.name
         }
 }
