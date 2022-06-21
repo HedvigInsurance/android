d8be2638786c4bdc3950e168f798b376fbf1a6ee
stylianosgakis <stylianos@hedvig.com>
2022-06-21 16:52:48 +0300

    Rename to just runApolloTest for brevity

--- app/src/test/java/com/hedvig/app/apollo/ApolloUtils.kt
+++ app/src/test/java/com/hedvig/app/apollo/ApolloUtils.kt
@@ -1,12 +1,28 @@
 package com.hedvig.app.apollo
 
+import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.Adapter
 import com.apollographql.apollo3.api.CustomScalarAdapters
 import com.apollographql.apollo3.api.Operation
 import com.apollographql.apollo3.api.json.buildJsonString
 import com.apollographql.apollo3.api.toJson
 import com.apollographql.apollo3.api.toJsonString
+import com.apollographql.apollo3.mockserver.MockServer
 import com.hedvig.app.util.apollo.adapter.CUSTOM_SCALAR_ADAPTERS
+import kotlinx.coroutines.test.TestScope
+
+fun runApolloTest(block: suspend TestScope.(MockServer, ApolloClient) -> Unit) {
+    return kotlinx.coroutines.test.runTest {
+        val mockServer = MockServer()
+        val apolloClient = ApolloClient.Builder()
+            .customScalarAdapters(CUSTOM_SCALAR_ADAPTERS)
+            .serverUrl(mockServer.url())
+            .build()
+        block(mockServer, apolloClient)
+        apolloClient.close()
+        mockServer.stop()
+    }
+}
 
 fun Operation.Data.toJsonStringWithData(
     customScalarAdapters: CustomScalarAdapters = CUSTOM_SCALAR_ADAPTERS,
