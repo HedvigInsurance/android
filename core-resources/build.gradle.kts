4ea5aecd5ec6f10face18fedb960bb68bd1f9377
stylianosgakis <stylianos@hedvig.com>
2022-07-22 13:36:36 +0300

    Introduce core-resources to make use of lokalise gradle plugin
    More information in core-resources/README.md

---
+++ core-resources/build.gradle.kts
@@ -0,0 +1,16 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+  id("hedvig.android.library")
+  id("hedvig.android.lokalise")
+}
+
+val lokaliseProperties = Properties()
+lokaliseProperties.load(FileInputStream(rootProject.file("lokalise.properties")))
+
+lokalise {
+  lokaliseProjectId.set(lokaliseProperties.getProperty("id"))
+  lokaliseToken.set(lokaliseProperties.getProperty("token"))
+  outputDirectory.set(file("src/main/res"))
+}
