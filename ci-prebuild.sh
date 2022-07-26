b5ae994204293b819ffae3c47549e5c685809984
stylianosgakis <stylianos@hedvig.com>
2022-07-26 11:59:44 +0200

    More Ci fix attempts

--- ci-prebuild.sh
+++ ci-prebuild.sh
@@ -23,7 +23,7 @@ cat <<EOT > app/src/debug/res/values/shake.xml
 </resources>
 EOT
 
-./gradlew :apollo:downloadGiraffeApolloSchemaFromIntrospection :core-resources:downloadStrings licenseReleaseReport
+./gradlew :apollo:downloadGiraffeApolloSchemaFromIntrospection
 ./gradlew :core-resources:downloadStrings
 ./gradlew licenseReleaseReport
 
