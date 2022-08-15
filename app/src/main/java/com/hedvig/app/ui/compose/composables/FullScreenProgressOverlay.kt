3ae2e9449ee71fd86ef1aaa96cbb7cc359359ad0
stylianosgakis <stylianos@hedvig.com>
2022-08-15 13:30:58 +0200

    Use `graphicsLayer` to reduce recompositions

--- app/src/main/java/com/hedvig/app/ui/compose/composables/FullScreenProgressOverlay.kt
+++ app/src/main/java/com/hedvig/app/ui/compose/composables/FullScreenProgressOverlay.kt
@@ -12,8 +12,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -21,7 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,13 +55,14 @@ fun FullScreenProgressOverlay(show: Boolean) {
         Icon(
           painter = painterResource(R.drawable.ic_hedvig_h_progress),
           modifier = Modifier
-            .rotate(angle)
+            .graphicsLayer {
+              rotationZ = angle
+            }
             .animateEnterExit(
               enter = fadeIn(animationSpec = tween(200, delayMillis = 700)),
               exit = fadeOut(animationSpec = tween(200)),
             )
-            .width(32.dp)
-            .height(32.dp),
+            .size(32.dp),
           contentDescription = null,
         )
       }
