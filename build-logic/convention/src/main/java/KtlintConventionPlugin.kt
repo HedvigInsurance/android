8e1fbd4c4e7ee810e3451459681617fa1b96da83
stylianosgakis <stylianos@hedvig.com>
2022-08-30 16:58:56 +0200

    Use kotlinter for applying ktlint

--- build-logic/convention/src/main/java/KtlintConventionPlugin.kt
+++ build-logic/convention/src/main/java/KtlintConventionPlugin.kt
@@ -4,37 +4,40 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.the
 import org.gradle.kotlin.dsl.withType
-import org.jlleitschuh.gradle.ktlint.KtlintExtension
-import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+import org.jmailen.gradle.kotlinter.KotlinterExtension
+import org.jmailen.gradle.kotlinter.support.ReporterType
+import org.jmailen.gradle.kotlinter.tasks.FormatTask
+import org.jmailen.gradle.kotlinter.tasks.LintTask
 
 class KtlintConventionPlugin : Plugin<Project> {
   override fun apply(target: Project) {
     with(target) {
       val libs = the<LibrariesForLibs>()
       with(pluginManager) {
-        apply(libs.plugins.ktlint.get().pluginId)
+        apply(libs.plugins.kotlinter.get().pluginId)
       }
 
-      extensions.configure<KtlintExtension> {
-        version.set(libs.versions.ktlint.get())
-        enableExperimentalRules.set(true)
-        additionalEditorconfigFile.set(rootProject.file(".editorconfig"))
-        outputToConsole.set(true)
-        filter {
-          exclude("**/generated/**")
-          include("**/src/**/*.kt")
-          include("**/src/**/*.kts")
-          include("**/build.gradle.kts")
-        }
-        reporters {
-          reporter(ReporterType.CHECKSTYLE)
-        }
+      extensions.configure<KotlinterExtension> {
+        ignoreFailures = false
+        @Suppress("MISSING_DEPENDENCY_SUPERCLASS")
+        reporters = arrayOf(ReporterType.checkstyle.name)
       }
 
-      tasks.withType<org.jlleitschuh.gradle.ktlint.tasks.GenerateReportsTask>().configureEach {
-        reportsOutputDirectory.set(
-          rootProject.layout.buildDirectory.dir("ktlint-report-in-checkstyle-format"),
-        )
+      tasks.withType<LintTask>().configureEach {
+        @Suppress("MISSING_DEPENDENCY_SUPERCLASS")
+        exclude { it.file.path.contains("generated/") }
+      }
+      tasks.withType<FormatTask>().configureEach {
+        @Suppress("MISSING_DEPENDENCY_SUPERCLASS")
+        exclude { it.file.path.contains("generated/") }
+      }
+
+      tasks.register("ktlintCheck") {
+        dependsOn(tasks.withType<LintTask>())
+      }
+
+      tasks.register("ktlintFormat") {
+        dependsOn(tasks.withType<FormatTask>())
       }
     }
   }
