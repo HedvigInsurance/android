25a118574f24122224f1afc98f599272df9e9a40
stylianosgakis <stylianos@hedvig.com>
2022-10-27 19:42:13 +0200

    Update odyssey entry point

--- app/src/main/kotlin/com/hedvig/app/feature/claims/ui/ClaimsFlowActivity.kt
+++ app/src/main/kotlin/com/hedvig/app/feature/claims/ui/ClaimsFlowActivity.kt
@@ -5,19 +5,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.hedvig.android.language.LanguageService
 import com.hedvig.app.R
 import com.hedvig.app.authenticate.AuthenticationTokenService
 import com.hedvig.app.util.extensions.startChat
-import com.hedvig.common.nav.OdysseyNavigationComposable
 import com.hedvig.common.remote.actions.CHAT_URL
 import com.hedvig.common.remote.actions.CLOSE_URL
-import com.hedvig.common.ui.OdysseyTheme
+import com.hedvig.common.ui.OdysseyRoot
 import org.koin.android.ext.android.inject
 
 class ClaimsFlowActivity : ComponentActivity() {
 
   private val authenticationTokenService: AuthenticationTokenService by inject()
   private val odysseyUrl get() = getString(R.string.ODYSSEY_URL)
+  private val languageService: LanguageService by inject()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -25,21 +26,18 @@ class ClaimsFlowActivity : ComponentActivity() {
     requireNotNull(token)
 
     setContent {
-      OdysseyTheme {
-        OdysseyNavigationComposable(
-          apiUrl = odysseyUrl,
-          authorizationToken = token,
-          context = this,
-          navigationRootUrl = ROOT_URL,
-          onExternalNavigation = { url ->
-            when (url) {
-              CLOSE_URL -> finish()
-              CHAT_URL -> startChat()
-            }
-          },
-          onExit = { finish() },
-        )
-      }
+      OdysseyRoot(
+        apiUrl = odysseyUrl,
+        authorizationToken = token,
+        locale = languageService.getLocale().toString(),
+        initialUrl = ROOT_URL,
+        onExternalNavigation = { url ->
+          when (url) {
+            CLOSE_URL -> finish()
+            CHAT_URL -> startChat()
+          }
+        },
+      )
     }
   }
 
