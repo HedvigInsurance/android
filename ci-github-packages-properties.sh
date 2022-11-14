4705dd02dc5d866af0dce962f6143815c100cbd0
stylianosgakis <stylianos@hedvig.com>
2022-11-14 10:00:48 +0100

    Fix using wrong value for wrong key

--- ci-github-packages-properties.sh
+++ ci-github-packages-properties.sh
@@ -2,5 +2,5 @@
 set +uex
 
 echo "" >> ~/.gradle/gradle.properties
-echo "odysseyUsername=${HEDVIG_GITHUB_PACKAGES_TOKEN}" >> ~/.gradle/gradle.properties
-echo "odysseyPassword=${HEDVIG_GITHUB_PACKAGES_USER}" >> ~/.gradle/gradle.properties
+echo "odysseyUsername=${HEDVIG_GITHUB_PACKAGES_USER}" >> ~/.gradle/gradle.properties
+echo "odysseyPassword=${HEDVIG_GITHUB_PACKAGES_TOKEN}" >> ~/.gradle/gradle.properties
