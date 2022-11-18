ae76e2aa78d5f907955066de3f654bafa83b492a
Hugo <hugo@hedvig.com>
2022-11-18 14:24:29 +0100

    CLX-166: Pass itemtype from phone common claim card (#1304)
    * Pass itemtype from phone common claim card
    
    * Add value class for item type

--- feature-odyssey/build.gradle.kts
+++ feature-odyssey/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   id("hedvig.android.library")
   id("hedvig.android.library.compose")
   id("hedvig.android.ktlint")
+  id("kotlin-parcelize")
 }
 
 dependencies {
--- feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/ClaimsFlowActivity.kt
+++ feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/ClaimsFlowActivity.kt
@@ -3,6 +3,7 @@ package com.hedvig.android.odyssey
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import coil.ImageLoader
@@ -11,7 +12,10 @@ import com.hedvig.android.language.LanguageService
 import com.hedvig.android.navigation.Navigator
 import com.hedvig.common.remote.actions.CHAT_URL
 import com.hedvig.common.remote.actions.CLOSE_URL
+import com.hedvig.common.remote.scopes.ScopeValues
+import com.hedvig.common.remote.scopes.keys.InitialDataScopeValueKey
 import com.hedvig.common.ui.OdysseyRoot
+import kotlinx.parcelize.Parcelize
 import org.koin.android.ext.android.inject
 
 class ClaimsFlowActivity : ComponentActivity() {
@@ -26,7 +30,14 @@ class ClaimsFlowActivity : ComponentActivity() {
     val token = authenticationTokenService.authenticationToken
     requireNotNull(token)
     val odysseyUrl = intent.getStringExtra(ODYSSEY_URL_KEY) ?: error("ODYSSEY_URL_KEY needs to be passed in")
+    val itemType = intent.getParcelableExtra<ItemType>(EXTRA_ITEM_TYPE)?.name
+
     val locale = languageService.getLocale().toString()
+
+    val scopeValues = ScopeValues()
+    if (itemType != null) {
+      scopeValues.setValue(InitialDataScopeValueKey, mapOf("itemType" to itemType))
+    }
 
     setContent {
       OdysseyRoot(
@@ -36,6 +47,7 @@ class ClaimsFlowActivity : ComponentActivity() {
         imageLoader = imageLoader,
         initialUrl = ROOT_URL,
         onExternalNavigation = ::onExternalNavigation,
+        scopeValues = scopeValues,
       )
     }
   }
@@ -53,13 +65,20 @@ class ClaimsFlowActivity : ComponentActivity() {
   companion object {
     private const val ROOT_URL = "/automation-claim"
     private const val ODYSSEY_URL_KEY = "com.hedvig.android.odyssey.ODYSSEY_URL_KEY"
+    private const val EXTRA_ITEM_TYPE = "EXTRA_ITEM_TYPE"
 
     fun newInstance(
       context: Context,
       odysseyUrl: String,
+      itemType: ItemType? = null
     ): Intent {
       return Intent(context, ClaimsFlowActivity::class.java)
         .putExtra(ODYSSEY_URL_KEY, odysseyUrl)
+        .putExtra(EXTRA_ITEM_TYPE, itemType)
     }
   }
+
+  @Parcelize
+  @JvmInline
+  value class ItemType(val name: String) : Parcelable
 }
