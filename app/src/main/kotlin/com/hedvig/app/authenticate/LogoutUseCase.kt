a8997f5445e9664af083041fb49ee0a90496be1f
stylianosgakis <stylianos@hedvig.com>
2022-11-27 19:29:58 +0100

    Use logoutUseCase in more places
    Prefer instead of doing the individual actions manually in random
    places. When we want to log out, we want all of the LogoutUseCase.kt
    actions to happen, so prefer that.
    
    Launch each action in ApplicationScope as they should all happen without
     depending on each other. If one fails for whatever reason, the rest
     should continue normally.

--- app/src/main/kotlin/com/hedvig/app/authenticate/LogoutUseCase.kt
+++ app/src/main/kotlin/com/hedvig/app/authenticate/LogoutUseCase.kt
@@ -1,15 +1,17 @@
 package com.hedvig.app.authenticate
 
 import com.apollographql.apollo3.ApolloClient
-import com.hedvig.android.apollo.OperationResult
 import com.hedvig.android.auth.AuthenticationTokenService
 import com.hedvig.android.auth.LoginStatusService
+import com.hedvig.android.core.common.ApplicationScope
 import com.hedvig.android.hanalytics.featureflags.FeatureManager
 import com.hedvig.android.market.MarketManager
 import com.hedvig.app.feature.chat.data.ChatEventStore
 import com.hedvig.app.feature.chat.data.UserRepository
 import com.hedvig.app.service.push.PushTokenManager
 import com.hedvig.app.util.apollo.reconnectSubscriptions
+import com.hedvig.hanalytics.HAnalytics
+import kotlinx.coroutines.launch
 
 class LogoutUseCase(
   private val pushTokenManager: PushTokenManager,
@@ -20,37 +22,21 @@ class LogoutUseCase(
   private val authenticationTokenService: AuthenticationTokenService,
   private val chatEventStore: ChatEventStore,
   private val featureManager: FeatureManager,
+  private val hAnalytics: HAnalytics,
+  private val applicationScope: ApplicationScope,
 ) {
 
-  sealed class LogoutResult {
-    object Success : LogoutResult()
-    data class Error(val message: String?) : LogoutResult()
-  }
-
-  suspend fun invoke(): LogoutResult = when (val result = userRepository.logout()) {
-    is OperationResult.Error -> LogoutResult.Error(result.message)
-    is OperationResult.Success -> {
-      clearLoginStatus()
-      clearMarket()
-      clearAuthenticationToken()
-      runCatching { pushTokenManager.refreshToken() }
-      apolloClient.reconnectSubscriptions()
-      featureManager.invalidateExperiments()
-      chatEventStore.resetChatClosedCounter()
-      LogoutResult.Success
+  fun invoke() {
+    applicationScope.launch { hAnalytics.loggedOut() }
+    applicationScope.launch { userRepository.logout() }
+    applicationScope.launch {
+      loginStatusService.isLoggedIn = false
+      authenticationTokenService.authenticationToken = null
     }
-  }
-
-  private fun clearAuthenticationToken() {
-    authenticationTokenService.authenticationToken = null
-  }
-
-  private fun clearMarket() {
-    marketManager.market = null
-  }
-
-  private fun clearLoginStatus() {
-    loginStatusService.isLoggedIn = false
-    authenticationTokenService.authenticationToken = null
+    applicationScope.launch { marketManager.market = null }
+    applicationScope.launch { pushTokenManager.refreshToken() }
+    applicationScope.launch { featureManager.invalidateExperiments() }
+    applicationScope.launch { chatEventStore.resetChatClosedCounter() }
+    applicationScope.launch { apolloClient.reconnectSubscriptions() }
   }
 }
