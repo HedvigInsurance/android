e2de01e47b6f74fcd0c8561434f2307c133c4a61
Hugo Linder <hugo@hedvig.com>
2022-11-30 10:06:29 +0100

    Fix PR comments

--- auth/src/main/kotlin/com/hedvig/android/auth/network/ResendOtpResponse.kt
+++ auth/src/main/kotlin/com/hedvig/android/auth/network/ResendOtpResponse.kt
@@ -5,7 +5,7 @@ import okhttp3.Response
 
 fun toResendOtpResult(response: Response): ResendOtpResult {
   val responseBody = response.body?.string()
-  return if (response.isSuccessful && responseBody != null) {
+  return if (response.isSuccessful) {
     ResendOtpResult.Success
   } else {
     ResendOtpResult.Error(message = responseBody ?: "Unknown error")
