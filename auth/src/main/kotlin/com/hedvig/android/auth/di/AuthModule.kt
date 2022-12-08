8b283e25db2f3fd451a745c52cf05fa0bac68504
stylianosgakis <stylianos@hedvig.com>
2022-12-08 18:35:04 +0100

    Remove redundant DI providing SharedPreferencesLoginStatusService

--- auth/src/main/kotlin/com/hedvig/android/auth/di/AuthModule.kt
+++ auth/src/main/kotlin/com/hedvig/android/auth/di/AuthModule.kt
@@ -4,10 +4,8 @@ import com.hedvig.android.auth.AuthRepository
 import com.hedvig.android.auth.AuthTokenService
 import com.hedvig.android.auth.AuthTokenServiceImpl
 import com.hedvig.android.auth.AuthenticationTokenService
-import com.hedvig.android.auth.LoginStatusService
 import com.hedvig.android.auth.NetworkAuthRepository
 import com.hedvig.android.auth.SharedPreferencesAuthenticationTokenService
-import com.hedvig.android.auth.SharedPreferencesLoginStatusService
 import com.hedvig.android.auth.interceptor.ExistingAuthTokenAppendingInterceptor
 import org.koin.dsl.module
 
@@ -16,6 +14,5 @@ val authModule = module {
   single<AuthRepository> { NetworkAuthRepository("https://auth.dev.hedvigit.com") }
   single<AuthTokenService> { AuthTokenServiceImpl(get(), get(), get()) }
   single<AuthenticationTokenService> { SharedPreferencesAuthenticationTokenService(get()) }
-  single<LoginStatusService> { SharedPreferencesLoginStatusService(get(), get()) }
   single<ExistingAuthTokenAppendingInterceptor> { ExistingAuthTokenAppendingInterceptor(get()) }
 }
