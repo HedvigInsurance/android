c7e12292944c519ec412c10b3db956beacfa6340
stylianosgakis <stylianos@hedvig.com>
2022-12-08 18:35:27 +0100

    Add replaceWith to ease up migration

--- auth/src/main/kotlin/com/hedvig/android/auth/AuthenticationTokenService.kt
+++ auth/src/main/kotlin/com/hedvig/android/auth/AuthenticationTokenService.kt
@@ -1,6 +1,9 @@
 package com.hedvig.android.auth
 
-@Deprecated("Use AuthTokenService instead")
+@Deprecated(
+  "Use AuthTokenService instead",
+  replaceWith = ReplaceWith("AuthTokenService", "com.hedvig.android.auth.AuthTokenService"),
+)
 interface AuthenticationTokenService {
   var authenticationToken: String?
   var refreshToken: RefreshToken?
