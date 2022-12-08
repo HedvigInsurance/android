4659c799716b9e20ddcf1b7f6a0283805e3863ce
stylianosgakis <stylianos@hedvig.com>
2022-12-08 18:37:49 +0100

    Add another replaceWith to ease migration up

--- auth/src/main/kotlin/com/hedvig/android/auth/LoginStatusService.kt
+++ auth/src/main/kotlin/com/hedvig/android/auth/LoginStatusService.kt
@@ -2,7 +2,10 @@ package com.hedvig.android.auth
 
 import kotlinx.coroutines.flow.Flow
 
-@Deprecated("Use AuthTokenService instead")
+@Deprecated(
+  "Use AuthTokenService instead",
+  replaceWith = ReplaceWith("AuthTokenService", "com.hedvig.android.auth.AuthTokenService"),
+)
 interface LoginStatusService {
   var isLoggedIn: Boolean
   suspend fun getLoginStatus(): LoginStatus
