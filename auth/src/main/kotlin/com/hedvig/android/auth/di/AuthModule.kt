886b3a57454c2fd628cd8b3c6c155c394fcfd9ab
stylianosgakis <stylianos@hedvig.com>
2022-12-09 02:24:14 +0100

    Remove AuthenticationTokenService implementation

--- auth/src/main/kotlin/com/hedvig/android/auth/di/AuthModule.kt
+++ auth/src/main/kotlin/com/hedvig/android/auth/di/AuthModule.kt
@@ -2,14 +2,11 @@ package com.hedvig.android.auth.di
 
 import com.hedvig.android.auth.AuthTokenService
 import com.hedvig.android.auth.AuthTokenServiceImpl
-import com.hedvig.android.auth.AuthenticationTokenService
-import com.hedvig.android.auth.SharedPreferencesAuthenticationTokenService
 import com.hedvig.android.auth.interceptor.ExistingAuthTokenAppendingInterceptor
 import org.koin.dsl.module
 
 @Suppress("RemoveExplicitTypeArguments")
 val authModule = module {
   single<AuthTokenService> { AuthTokenServiceImpl(get(), get(), get()) }
-  single<AuthenticationTokenService> { SharedPreferencesAuthenticationTokenService(get()) }
   single<ExistingAuthTokenAppendingInterceptor> { ExistingAuthTokenAppendingInterceptor(get()) }
 }
