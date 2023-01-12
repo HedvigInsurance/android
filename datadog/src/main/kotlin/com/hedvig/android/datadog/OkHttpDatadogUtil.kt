cb2df9a15032de98c804c658466ae76fd23ee4e7
stylianosgakis <stylianos@hedvig.com>
2023-01-12 16:17:16 +0200

    DatadogInterceptor includes the functionality of TracingInterceptor

--- datadog/src/main/kotlin/com/hedvig/android/datadog/OkHttpDatadogUtil.kt
+++ datadog/src/main/kotlin/com/hedvig/android/datadog/OkHttpDatadogUtil.kt
@@ -2,11 +2,9 @@ package com.hedvig.android.datadog
 
 import com.datadog.android.DatadogEventListener
 import com.datadog.android.DatadogInterceptor
-import com.datadog.android.tracing.TracingInterceptor
 import okhttp3.OkHttpClient
 
 fun OkHttpClient.Builder.addDatadogConfiguration(): OkHttpClient.Builder {
   return addInterceptor(DatadogInterceptor())
-    .addInterceptor(TracingInterceptor())
     .eventListenerFactory(DatadogEventListener.Factory())
 }
