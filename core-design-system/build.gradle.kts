565af1d422921c60940ea9bf45ec7feace63b0b9
stylianosgakis <stylianos@hedvig.com>
2023-01-19 16:09:48 +0200

    Move compose preview related dependencies in convention plugin

--- core-design-system/build.gradle.kts
+++ core-design-system/build.gradle.kts
@@ -15,8 +15,5 @@ dependencies {
   api(libs.accompanist.insetsUi)
   api(libs.androidx.compose.foundation)
   api(libs.androidx.compose.material)
-  api(libs.androidx.compose.uiToolingPreview)
   implementation(libs.androidx.compose.mdcAdapter)
-
-  debugApi(libs.androidx.compose.uiTooling)
 }
