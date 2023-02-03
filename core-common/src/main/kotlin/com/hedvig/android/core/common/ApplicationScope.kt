fa303664fc2a69724b21a0815220f2c438076ef2
stylianosgakis <stylianos@hedvig.com>
2023-02-04 01:57:44 +0200

    Improve ApplicationScope to be overridable in tests
    Old implementation did not allow to go off of the standard `MainScope()`
    CoroutineScope, this lets tests simply use ApplicationScope as a wrapper
    for whatever CoroutineContext is passed in the test

--- core-common/src/main/kotlin/com/hedvig/android/core/common/ApplicationScope.kt
+++ core-common/src/main/kotlin/com/hedvig/android/core/common/ApplicationScope.kt
@@ -6,5 +6,6 @@ import kotlinx.coroutines.MainScope
 /**
  * A [CoroutineScope] which lives for the entire lifecycle of the application. Can be used to launch top-level actions
  * which need to out-live the current CoroutineScope, like even after a ViewModel would be killed.
+ * Defaults to [MainScope] but can be provided with whatever [CoroutineScope] needed if constructed for tests.
  */
-class ApplicationScope : CoroutineScope by MainScope()
+class ApplicationScope(scope: CoroutineScope = MainScope()) : CoroutineScope by scope
