71321f466f3ec00735267e701a7bdba1641b1680
Hugo <hugo@hedvig.com>
2023-03-02 16:12:47 +0100

    Single item price input (#1440)
    * Add price input for single item screen
    
    * Fix focus and input height

--- feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/input/ui/SingleItem.kt
+++ feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/input/ui/SingleItem.kt
@@ -2,21 +2,31 @@ package com.hedvig.android.odyssey.input.ui
 
 import android.app.DatePickerDialog
 import android.widget.DatePicker
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.ImageLoader
 import com.hedvig.android.core.designsystem.component.button.FormRowButton
+import com.hedvig.android.core.designsystem.component.button.LargeContainedButton
 import com.hedvig.android.core.designsystem.component.button.LargeContainedTextButton
 import com.hedvig.android.odyssey.model.ClaimState
 import com.hedvig.android.odyssey.model.Input
@@ -33,11 +43,12 @@ fun SingleItem(
   onDateOfPurchase: (LocalDate) -> Unit,
   onTypeOfDamage: (AutomationClaimInputDTO2.SingleItem.ClaimProblem) -> Unit,
   onModelOption: (AutomationClaimInputDTO2.SingleItem.ItemOptions.ItemModelOption) -> Unit,
-  onPurchasePrice: (MonetaryAmount) -> Unit,
+  onPurchasePrice: (MonetaryAmount?) -> Unit,
   onNext: () -> Unit,
 ) {
-  val openDamagePickerDialog = remember { mutableStateOf(false) }
-  val openModelPickerDialog = remember { mutableStateOf(false) }
+  var openDamagePickerDialog by remember { mutableStateOf(false) }
+  var openModelPickerDialog by remember { mutableStateOf(false) }
+  val focusRequester = remember { FocusRequester() }
 
   val now = LocalDate.now()
   val pickerDialog = DatePickerDialog(
@@ -50,7 +61,7 @@ fun SingleItem(
     now.dayOfMonth,
   )
 
-  if (openDamagePickerDialog.value) {
+  if (openDamagePickerDialog) {
     SingleSelectDialog(
       title = stringResource(R.string.claims_item_screen_type_of_damage_button),
       optionsList = input.problemIds,
@@ -59,10 +70,10 @@ fun SingleItem(
       getImageUrl = { null },
       getId = { it.name },
       imageLoader = imageLoader,
-    ) { openDamagePickerDialog.value = false }
+    ) { openDamagePickerDialog = false }
   }
 
-  if (openModelPickerDialog.value) {
+  if (openModelPickerDialog) {
     SingleSelectDialog(
       title = stringResource(R.string.claims_item_screen_model_button),
       optionsList = input.modelOptions.sortedBy { it.modelName },
@@ -73,26 +84,7 @@ fun SingleItem(
       getImageUrl = { itemModelOption -> itemModelOption.modelImageUrl },
       getId = { itemModelOption -> itemModelOption.modelId },
       imageLoader = imageLoader,
-    ) { openModelPickerDialog.value = false }
-  }
-
-  val message = remember { mutableStateOf("") }
-  val openDialog = remember { mutableStateOf(false) }
-  val editMessage = remember { mutableStateOf("") }
-
-  if (openDialog.value) {
-    PriceInputDialog(
-      title = stringResource(R.string.claims_item_screen_purchase_price_button),
-      message = message,
-      openDialog = openDialog,
-      editMessage = editMessage,
-      onPositiveButtonClicked = {
-        onPurchasePrice(MonetaryAmount(it, "SEK"))
-      },
-      onDismissRequest = {
-        openDialog.value = false
-      },
-    )
+    ) { openModelPickerDialog = false }
   }
 
   Box(
@@ -111,7 +103,7 @@ fun SingleItem(
         mainText = stringResource(R.string.claims_item_screen_model_button),
         secondaryText = selectedModel?.modelName ?: "-",
       ) {
-        openModelPickerDialog.value = true
+        openModelPickerDialog = true
       }
 
       Spacer(modifier = Modifier.padding(top = 12.dp))
@@ -125,11 +117,26 @@ fun SingleItem(
 
       Spacer(modifier = Modifier.padding(top = 12.dp))
 
-      FormRowButton(
-        mainText = stringResource(R.string.claims_item_screen_purchase_price_button),
-        secondaryText = state.item.purchasePrice?.amount ?: "-",
+      LargeContainedButton(
+        onClick = { focusRequester.requestFocus() },
+        colors = ButtonDefaults.buttonColors(
+          containerColor = MaterialTheme.colorScheme.surface,
+          contentColor = MaterialTheme.colorScheme.onSurface,
+        ),
       ) {
-        openDialog.value = true
+        Row(
+          modifier = Modifier.fillMaxWidth(),
+          horizontalArrangement = Arrangement.SpaceBetween,
+          verticalAlignment = Alignment.CenterVertically,
+        ) {
+          Text(text = stringResource(R.string.claims_item_screen_purchase_price_button), maxLines = 1)
+          MonetaryAmountInput(
+            onInput = onPurchasePrice,
+            currency = "SEK",
+            maximumFractionDigits = 0,
+            focusRequester = focusRequester,
+          )
+        }
       }
 
       Spacer(modifier = Modifier.padding(top = 12.dp))
@@ -138,7 +145,7 @@ fun SingleItem(
         mainText = stringResource(R.string.claims_item_screen_type_of_damage_button),
         secondaryText = state.item.problemIds.joinToString { it.getText() },
       ) {
-        openDamagePickerDialog.value = true
+        openDamagePickerDialog = true
       }
     }
 
