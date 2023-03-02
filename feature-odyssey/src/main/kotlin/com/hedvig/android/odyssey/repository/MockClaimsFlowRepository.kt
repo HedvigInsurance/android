01dfdad98012ef19a7f247a4a80b23d1ad3538f9
Hugo <hugo@hedvig.com>
2023-03-02 12:49:13 +0100

    Add more mock data and separate progress for payout (#1437)

--- feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/repository/MockClaimsFlowRepository.kt
+++ feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/repository/MockClaimsFlowRepository.kt
@@ -9,79 +9,34 @@ import java.time.LocalDate
 
 class MockClaimsFlowRepository : ClaimsFlowRepository {
 
-  override suspend fun getOrCreateClaim(commonClaimId: String?): ClaimResult {
-    return ClaimResult.Success(
-      ClaimState(),
-      listOf(
-        Input.Location(
-          listOf(AutomationClaimDTO2.ClaimLocation.AT_HOME, AutomationClaimDTO2.ClaimLocation.ABROAD),
-          selectedLocation = AutomationClaimDTO2.ClaimLocation.ABROAD,
-        ),
-        Input.DateOfOccurrence(LocalDate.now().toString()),
-        Input.AudioRecording(
-          null,
-          null,
-          listOf(
-            AutomationClaimInputDTO2.AudioRecording.AudioRecordingQuestion
-              .CLAIM_QUESTION_WHAT_WHO_TOOK_DAMAGE_OR_NEEDS_REPLACEMENT,
-          ),
-        ),
-      ),
-      resolution = Resolution.None,
-    )
-  }
-
-  override suspend fun updateClaim(claimState: ClaimState, nrOfInputs: Int): ClaimResult {
-    delay(1000)
-    return ClaimResult.Success(
-      claimState = ClaimState(),
-      inputs = listOf(
-        Input.Location(
-          listOf(AutomationClaimDTO2.ClaimLocation.AT_HOME, AutomationClaimDTO2.ClaimLocation.ABROAD),
-          selectedLocation = AutomationClaimDTO2.ClaimLocation.ABROAD,
-        ),
-        Input.DateOfOccurrence(LocalDate.now().toString()),
-        Input.AudioRecording(
-          null,
-          null,
-          listOf(
-            AutomationClaimInputDTO2.AudioRecording.AudioRecordingQuestion
-              .CLAIM_QUESTION_WHAT_WHO_TOOK_DAMAGE_OR_NEEDS_REPLACEMENT,
-          ),
-        ),
-      ),
-      resolution = Resolution.SingleItemPayout(
-        purchasePrice = MonetaryAmount("2000", "SEK"),
-        depreciation = MonetaryAmount("2000", "SEK"),
-        deductible = MonetaryAmount("2000", "SEK"),
-        payoutAmount = MonetaryAmount("2000", "SEK"),
-      ),
-    )
-  }
-
-  override suspend fun getClaim(): ClaimResult {
-    delay(1000)
-    return ClaimResult.Success(
-      ClaimState(),
-      listOf(
-        Input.AudioRecording(
-          null,
-          null,
-          listOf(),
-        ),
-      ),
-      resolution = Resolution.None,
-    )
-  }
-
-  override suspend fun openClaim(amount: MonetaryAmount?) = ClaimResult.Success(
-    claimState = ClaimState(),
-    inputs = listOf(
+  private val mockSuccessResult = ClaimResult.Success(
+    ClaimState(),
+    listOf(
       Input.Location(
         listOf(AutomationClaimDTO2.ClaimLocation.AT_HOME, AutomationClaimDTO2.ClaimLocation.ABROAD),
         selectedLocation = AutomationClaimDTO2.ClaimLocation.ABROAD,
       ),
       Input.DateOfOccurrence(LocalDate.now().toString()),
+      Input.SingleItem(
+        purchasePrice = MonetaryAmount("1000", "SEK"),
+        purchaseDate = "date",
+        problemIds = listOf(
+          AutomationClaimInputDTO2.SingleItem.ClaimProblem.BROKEN,
+          AutomationClaimInputDTO2.SingleItem.ClaimProblem.BROKEN_FRONT,
+          AutomationClaimInputDTO2.SingleItem.ClaimProblem.MISSING,
+        ),
+        selectedProblemIds = listOf(),
+        modelOptions = listOf(
+          AutomationClaimInputDTO2.SingleItem.ItemOptions.ItemModelOption(
+            modelId = "1",
+            modelName = "iphone",
+            modelImageUrl = null,
+            typeId = "phone",
+            brandId = "apple",
+          ),
+        ),
+        selectedModelOptionId = null,
+      ),
       Input.AudioRecording(
         null,
         null,
@@ -91,11 +46,29 @@ class MockClaimsFlowRepository : ClaimsFlowRepository {
         ),
       ),
     ),
-    resolution = Resolution.SingleItemPayout(
-      purchasePrice = MonetaryAmount("2000", "SEK"),
-      depreciation = MonetaryAmount("2000", "SEK"),
-      deductible = MonetaryAmount("2000", "SEK"),
-      payoutAmount = MonetaryAmount("2000", "SEK"),
-    ),
+    resolution = Resolution.None,
   )
+
+  override suspend fun getOrCreateClaim(commonClaimId: String?): ClaimResult {
+    return mockSuccessResult
+  }
+
+  override suspend fun updateClaim(claimState: ClaimState, nrOfInputs: Int): ClaimResult {
+    delay(1000)
+    return mockSuccessResult.copy(
+      resolution = Resolution.SingleItemPayout(
+        purchasePrice = MonetaryAmount("1000", "SEK"),
+        depreciation = MonetaryAmount("1000", "SEK"),
+        deductible = MonetaryAmount("1000", "SEK"),
+        payoutAmount = MonetaryAmount("1000", "SEK"),
+      ),
+    )
+  }
+
+  override suspend fun getClaim(): ClaimResult {
+    delay(1000)
+    return mockSuccessResult
+  }
+
+  override suspend fun openClaim(amount: MonetaryAmount?) = mockSuccessResult
 }
