54ea3a004a286eab9336e250f20775082522c517
stylianosgakis <stylianos@hedvig.com>
2023-03-04 13:17:13 +0200

    Add navigation:navigation-compose-typed module
    To host the functions which target accompanist-navigation instead of
    androidx.navigation. Can also host whatever else API we want to expose
    from typed-navigation instead of going directly to the library functions

---
+++ navigation/navigation-compose-typed/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+  id("hedvig.android.ktlint")
+  id("hedvig.android.library")
+  id("hedvig.android.library.compose")
+  @Suppress("DSL_SCOPE_VIOLATION")
+  alias(libs.plugins.serialization)
+}
+
+android {
+  namespace = "com.hedvig.android.navigation.compose.typed"
+}
+
+dependencies {
+  api(libs.androidx.navigation.common)
+  api(libs.kiwi.navigationCompose)
+  implementation(libs.accompanist.navigationAnimation)
+  implementation(libs.androidx.compose.animation)
+  implementation(libs.androidx.compose.runtime)
+  implementation(libs.kotlinx.serialization.core)
+}
