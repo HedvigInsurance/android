51cb89c21b320cdeccf3dc62d93f373aaa3d8541
stylianosgakis <stylianos@hedvig.com>
2023-03-07 18:27:27 +0200

    Fix wrong URL passed to giraffeClient

--- apollo/giraffe/src/main/kotlin/com/hedvig/android/apollo/giraffe/di/GiraffeModule.kt
+++ apollo/giraffe/src/main/kotlin/com/hedvig/android/apollo/giraffe/di/GiraffeModule.kt
@@ -1,14 +1,16 @@
 package com.hedvig.android.apollo.giraffe.di
 
 import com.apollographql.apollo3.ApolloClient
-import com.hedvig.android.core.common.di.octopusGraphQLUrlQualifier
+import com.hedvig.android.core.common.di.giraffeGraphQLUrlQualifier
+import com.hedvig.android.core.common.di.giraffeGraphQLWebSocketUrlQualifier
 import org.koin.dsl.module
 
 @Suppress("RemoveExplicitTypeArguments")
 val giraffeModule = module {
   single<ApolloClient>(giraffeClient) {
     get<ApolloClient.Builder>().copy()
-      .httpServerUrl(get<String>(octopusGraphQLUrlQualifier))
+      .httpServerUrl(get<String>(giraffeGraphQLUrlQualifier))
+      .webSocketServerUrl(get<String>(giraffeGraphQLWebSocketUrlQualifier))
       .build()
   }
 }
