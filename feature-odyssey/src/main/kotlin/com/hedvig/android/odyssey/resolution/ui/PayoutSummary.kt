e2bfd908195112b9e4528aa91b668750e3e82088
Hugo <hugo@hedvig.com>
2023-03-08 10:29:59 +0100

    Improve phone number input. Align payout summary according to design. (#1455)
    * Improve phone number input. Align payout summary according to design.
    
    * Remove top level surface

--- feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/resolution/ui/PayoutSummary.kt
+++ feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/resolution/ui/PayoutSummary.kt
@@ -1,5 +1,6 @@
 package com.hedvig.android.odyssey.resolution.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,10 +9,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Divider
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -20,6 +23,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.hedvig.android.core.designsystem.component.button.LargeContainedTextButton
+import com.hedvig.android.core.designsystem.preview.HedvigPreview
+import com.hedvig.android.core.designsystem.theme.HedvigTheme
 import com.hedvig.android.odyssey.model.Resolution
 import com.hedvig.odyssey.remote.money.MonetaryAmount
 import hedvig.resources.R
@@ -38,7 +43,7 @@ fun PayoutSummary(
       Spacer(modifier = Modifier.padding(top = 28.dp))
 
       Text(
-        text = stringResource(R.string.claims_payout_summary_title),
+        text = stringResource(R.string.claims_payout_summary_subtitle),
         style = MaterialTheme.typography.h5,
       )
 
@@ -62,18 +67,30 @@ fun PayoutSummary(
 
       Divider(modifier = Modifier.padding(vertical = 16.dp))
 
-      Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+      Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth(),
+      ) {
         Text(text = stringResource(R.string.claims_payout_total))
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-          Text(text = resolution.payoutAmount.amount ?: "")
-        }
+        Text(text = resolution.payoutAmount.amount ?: "", style = MaterialTheme.typography.h6)
       }
 
       Spacer(modifier = Modifier.padding(top = 48.dp))
 
       Text(stringResource(R.string.claims_payout_summary_method), style = MaterialTheme.typography.h5)
 
-      Text(stringResource(R.string.claims_payout_method_autogiro))
+      Spacer(modifier = Modifier.padding(top = 4.dp))
+
+      Surface(
+        shape = RoundedCornerShape(8.dp),
+        color = MaterialTheme.colors.surface,
+        modifier = Modifier
+          .padding(top = 16.dp)
+          .fillMaxWidth(),
+      ) {
+        Text(stringResource(R.string.claims_payout_method_autogiro), modifier = Modifier.padding(16.dp))
+      }
     }
 
     LargeContainedTextButton(
@@ -83,5 +100,23 @@ fun PayoutSummary(
       text = stringResource(R.string.claims_payout_button_label, resolution.payoutAmount.amount ?: "-"),
       modifier = Modifier.align(Alignment.BottomCenter),
     )
+  }
+}
+
+@HedvigPreview
+@Composable
+private fun PreviewPayoutSummary() {
+  HedvigTheme {
+    Surface {
+      PayoutSummary(
+        resolution = Resolution.SingleItemPayout(
+          purchasePrice = MonetaryAmount("1000", "SEK"),
+          depreciation = MonetaryAmount("1000", "SEK"),
+          deductible = MonetaryAmount("1000", "SEK"),
+          payoutAmount = MonetaryAmount("1000", "SEK"),
+        ),
+        onPayout = {},
+      )
+    }
   }
 }
