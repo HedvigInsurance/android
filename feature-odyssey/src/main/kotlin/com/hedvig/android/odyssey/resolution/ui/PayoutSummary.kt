466f37e0de1b3453e631d221c29d5e51fe11a8fc
stylianosgakis <stylianos@hedvig.com>
2023-03-09 13:55:24 +0200

    Remove icon from common claim activity

--- feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/resolution/ui/PayoutSummary.kt
+++ feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/resolution/ui/PayoutSummary.kt
@@ -1,6 +1,5 @@
 package com.hedvig.android.odyssey.resolution.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
