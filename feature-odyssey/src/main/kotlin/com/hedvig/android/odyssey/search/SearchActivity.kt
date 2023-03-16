85b52c78eaf318699f9e4dea9870679b4611a0aa
Hugo Linder <hugo@hedvig.com>
2023-03-16 16:22:46 +0100

    Tweak design and refactor

--- feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/search/SearchActivity.kt
+++ feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/search/SearchActivity.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,11 +19,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.hedvig.android.auth.android.AuthenticatedObserver
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
 import com.hedvig.android.core.ui.appbar.CenterAlignedTopAppBar
 import com.hedvig.android.odyssey.OdysseyClaimsFlowActivity
+import com.hedvig.android.odyssey.search.ui.CommonClaims
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class SearchActivity : ComponentActivity() {
@@ -38,53 +42,49 @@ class SearchActivity : ComponentActivity() {
       val viewModel: SearchViewModel = getViewModel()
       val viewState by viewModel.viewState.collectAsState()
 
-      val commonClaimId = viewState.selectedClaim?.id
-      LaunchedEffect(commonClaimId) {
-        if (commonClaimId != null) {
-          startClaimsFlow(viewModel, commonClaimId)
+      val entryPointId = viewState.selectedClaim?.entryPointId
+      LaunchedEffect(entryPointId) {
+        if (entryPointId != null) {
+          startClaimsFlow(viewModel, entryPointId)
         }
       }
 
       HedvigTheme {
-
         Surface(color = MaterialTheme.colors.background) {
-          Column {
-            CenterAlignedTopAppBar(
-              title = "",
-              onClick = { finish() },
-              backgroundColor = MaterialTheme.colors.background,
-              icon = Icons.Filled.ArrowBack,
-            )
+          Box(modifier = Modifier.fillMaxHeight()) {
+            Column {
+              CenterAlignedTopAppBar(
+                title = "",
+                onClick = { finish() },
+                backgroundColor = MaterialTheme.colors.background,
+                icon = Icons.Filled.ArrowBack,
+              )
 
-            Text(
-              text = getString(hedvig.resources.R.string.home_tab_common_claims_title),
-              style = MaterialTheme.typography.h5,
-              modifier = Modifier.padding(22.dp),
-            )
+              Text(
+                text = "Tell us what happened",
+                style = androidx.compose.material3.MaterialTheme.typography.displaySmall,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(22.dp),
+              )
 
-            Spacer(modifier = Modifier.padding(8.dp))
+              Spacer(modifier = Modifier.padding(8.dp))
 
-            CommonClaims(
-              selectClaim = viewModel::onSelectClaim,
-              commonClaims = viewState.commonClaims,
-              selectOther = {
-                startClaimsFlow(
-                  viewModel = viewModel,
-                  commonClaimId = null,
-                )
-              },
-            )
+              CommonClaims(
+                selectClaim = viewModel::onSelectClaim,
+                commonClaims = viewState.commonClaims,
+              )
+            }
           }
         }
       }
     }
   }
 
-  private fun startClaimsFlow(viewModel: SearchViewModel, commonClaimId: String?) {
+  private fun startClaimsFlow(viewModel: SearchViewModel, entryPointId: String?) {
     val intent = OdysseyClaimsFlowActivity.newInstance(
       context = this,
       odysseyUrl = odysseyUrl,
-      commonClaimId = commonClaimId,
+      entryPointId = entryPointId,
     )
     startActivity(intent)
     viewModel.resetState()
