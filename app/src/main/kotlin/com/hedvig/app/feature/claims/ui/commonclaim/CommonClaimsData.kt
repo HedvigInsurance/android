9c3fffc69a5da7494511b4d6808b82fdfc16e9da
Hugo <hugo@hedvig.com>
2023-04-05 16:24:39 +0200

    Update common claim id and add better error handling (#1509)

--- app/src/main/kotlin/com/hedvig/app/feature/claims/ui/commonclaim/CommonClaimsData.kt
+++ app/src/main/kotlin/com/hedvig/app/feature/claims/ui/commonclaim/CommonClaimsData.kt
@@ -41,10 +41,13 @@ data class CommonClaimsData(
   }
 }
 
-fun CommonClaimsData.isFirstVet() = id == "25" || id == "26"
+fun CommonClaimsData.isFirstVet() = id == "31" || id == "30"
 
-fun getFirstVetIntent(context: Context) = try {
-  context.packageManager.getLaunchIntentForPackage("com.firstvet.firstvet")
-} catch (e: ActivityNotFoundException) {
-  Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.firstvet.firstvet"))
+fun getFirstVetIntent(context: Context): Intent {
+  return context.packageManager.getLaunchIntentForPackage("com.firstvet.firstvet")
+    ?: try {
+      Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.firstvet.firstvet"))
+    } catch (e: ActivityNotFoundException) {
+      Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.firstvet.firstvet"))
+    }
 }
