eddd72b69faa95ef6924e423ae07f7e433011d94
Hugo Linder <hugo@hedvig.com>
2023-04-12 14:11:12 +0200

    Show correct charge amount in profile view

--- testdata/src/main/kotlin/com/hedvig/app/testdata/feature/profile/builders/ProfileDataBuilder.kt
+++ testdata/src/main/kotlin/com/hedvig/app/testdata/feature/profile/builders/ProfileDataBuilder.kt
@@ -4,6 +4,7 @@ import com.hedvig.app.testdata.common.builders.CostBuilder
 import giraffe.ProfileQuery
 import giraffe.fragment.ActivePaymentMethodsFragment
 import giraffe.fragment.CostFragment
+import giraffe.fragment.MonetaryAmountFragment
 import giraffe.type.DirectDebitStatus
 import giraffe.type.StoredCardDetails
 
@@ -30,6 +31,26 @@ data class ProfileDataBuilder(
       freeUntil = null,
       fragments = ProfileQuery.InsuranceCost.Fragments(
         costFragment = cost,
+      ),
+    ),
+    chargeEstimation = ProfileQuery.ChargeEstimation(
+      subscription = ProfileQuery.Subscription(
+        __typename = "",
+        fragments = ProfileQuery.Subscription.Fragments(
+          monetaryAmountFragment = MonetaryAmountFragment("100", "SEK"),
+        ),
+      ),
+      discount = ProfileQuery.Discount(
+        __typename = "",
+        fragments = ProfileQuery.Discount.Fragments(
+          monetaryAmountFragment = MonetaryAmountFragment("100", "SEK"),
+        ),
+      ),
+      charge = ProfileQuery.Charge(
+        __typename = "",
+        fragments = ProfileQuery.Charge.Fragments(
+          monetaryAmountFragment = MonetaryAmountFragment("10", "SEK"),
+        ),
       ),
     ),
     bankAccount = ProfileQuery.BankAccount(directDebitStatus = directDebitStatus),
