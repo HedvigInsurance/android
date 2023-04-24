b9d7d11e70146d20ad9004497aa8b7269fbfe402
Hugo <hugo@hedvig.com>
2023-04-24 11:46:46 +0200

    Remove phone placeholder (#1527)
    * Remove phone placeholder image since it was shown for other claims flows other than phone
    
    * remove import
    
    ---------
    
    Co-authored-by: stylianosgakis <stylianos@hedvig.com>

--- feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/step/summary/ClaimSummaryDestination.kt
+++ feature-odyssey/src/main/kotlin/com/hedvig/android/odyssey/step/summary/ClaimSummaryDestination.kt
@@ -55,7 +55,6 @@ import com.hedvig.android.odyssey.data.ClaimFlowStep
 import com.hedvig.android.odyssey.navigation.ItemProblem
 import com.hedvig.android.odyssey.navigation.LocationOption
 import com.hedvig.android.odyssey.navigation.UiNullableMoney
-import com.hedvig.android.odyssey.step.summary.resources.HedvigDeviceUnknown
 import com.hedvig.android.odyssey.ui.ClaimFlowScaffold
 import com.hedvig.odyssey.compose.getLocale
 import hedvig.resources.R
@@ -181,7 +180,7 @@ private fun ItemIcon(
       ) {
         when (it) {
           is AsyncImagePainter.State.Success -> this@SubcomposeAsyncImage.SubcomposeAsyncImageContent()
-          is AsyncImagePainter.State.Error -> Icon(Icons.Default.HedvigDeviceUnknown, null)
+          is AsyncImagePainter.State.Error -> {}
           AsyncImagePainter.State.Empty,
           is AsyncImagePainter.State.Loading,
           -> {
