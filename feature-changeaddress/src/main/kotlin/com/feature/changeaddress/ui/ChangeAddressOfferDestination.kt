1cb4b9fed7df794557629bcdeaee032930e8ada7
Hugo <hugo@hedvig.com>
2023-05-05 10:33:42 +0200

    Add result destination (#1564)
    * Add offer destination and show quote
    
    * Add result destination

--- feature-changeaddress/src/main/kotlin/com/feature/changeaddress/ui/ChangeAddressOfferDestination.kt
+++ feature-changeaddress/src/main/kotlin/com/feature/changeaddress/ui/ChangeAddressOfferDestination.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
