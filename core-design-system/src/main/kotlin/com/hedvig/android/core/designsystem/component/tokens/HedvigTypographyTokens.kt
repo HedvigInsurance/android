3ba46ee36c4e5a6c7628564e0f85a43176dd81f7
stylianosgakis <stylianos@hedvig.com>
2023-05-09 16:29:26 +0300

    Bring in tokens matching the ones from m3 plus ours
    Colors + Shapes + Typography + tokens for TextField.
    Plus their respective token -> resolved value composable functions.

---
+++ core-design-system/src/main/kotlin/com/hedvig/android/core/designsystem/component/tokens/HedvigTypographyTokens.kt
@@ -0,0 +1,20 @@
+package com.hedvig.android.core.designsystem.component.tokens
+
+@Suppress("unused")
+internal enum class HedvigTypographyKeyTokens {
+  BodyLarge,
+  BodyMedium,
+  BodySmall,
+  DisplayLarge,
+  DisplayMedium,
+  DisplaySmall,
+  HeadlineLarge,
+  HeadlineMedium,
+  HeadlineSmall,
+  LabelLarge,
+  LabelMedium,
+  LabelSmall,
+  TitleLarge,
+  TitleMedium,
+  TitleSmall,
+}
