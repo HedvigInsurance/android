be95c6e663c20059b5a0c813e2c9d7b691674cb6
stylianosgakis <stylianos@hedvig.com>
2023-05-12 11:27:32 +0300

    Add a hedvig Scaffold
    Super similar with the `ClaimFlowScaffold` minus the specific handling
    of the error dialog and the specialized modifier created for that flow,
    while also adding customizable top app bar colors.

--- core-ui/src/main/kotlin/com/hedvig/android/core/ui/scaffold/Scaffold.kt
+++ core-ui/src/main/kotlin/com/hedvig/android/core/ui/scaffold/Scaffold.kt
@@ -1,16 +1,72 @@
 package com.hedvig.android.core.ui.scaffold
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.contentColorFor
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.UiComposable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.util.fastMap
 import androidx.compose.ui.util.fastMaxBy
+import com.hedvig.android.core.ui.appbar.m3.TopAppBarWithBack
+
+/**
+ * A custom scaffold, with a built-in top app bar which is pinned to the top and changes colors according to
+ * [topAppBarColors] on scroll.
+ * Handles all insets by default, leaving the [content] to take place in the available screen.
+ * Gives a [ColumnScope] to the content, and makes the content scrollable by default.
+ */
+@Composable
+fun HedvigScaffold(
+  navigateUp: () -> Unit,
+  modifier: Modifier = Modifier,
+  topAppBarText: String? = null,
+  itemsColumnHorizontalAlignment: Alignment.Horizontal = Alignment.Start,
+  topAppBarColors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(
+    containerColor = androidx.compose.material3.MaterialTheme.colorScheme.background,
+    scrolledContainerColor = androidx.compose.material3.MaterialTheme.colorScheme.surface,
+  ),
+  content: @Composable (ColumnScope.() -> Unit),
+) {
+  Column(modifier.fillMaxSize()) {
+    val topAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+    TopAppBarWithBack(
+      onClick = navigateUp,
+      title = topAppBarText ?: "",
+      colors = topAppBarColors,
+      scrollBehavior = topAppBarScrollBehavior,
+    )
+    Column(
+      horizontalAlignment = itemsColumnHorizontalAlignment,
+      modifier = Modifier
+        .fillMaxSize()
+        .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
+        .verticalScroll(rememberScrollState())
+        .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal)),
+    ) {
+      content()
+      Spacer(Modifier.windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom)))
+    }
+  }
+}
 
 /**
  * Stripped version of [androidx.compose.material.Scaffold] which only contains the bottomBar slot.
