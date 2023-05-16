dbd641a6e85e26ef332dbd6d338d774c4ca273f9
stylianosgakis <stylianos@hedvig.com>
2023-05-16 18:23:51 +0300

    Make more generic HedvigInfoCard
    To be used to get this blue background

--- feature-changeaddress/src/main/kotlin/com/hedvig/android/feature/changeaddress/ui/AddressInfoCard.kt
+++ feature-changeaddress/src/main/kotlin/com/hedvig/android/feature/changeaddress/ui/AddressInfoCard.kt
@@ -1,13 +1,12 @@
 package com.hedvig.android.feature.changeaddress.ui
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,11 +15,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.hedvig.android.core.designsystem.component.card.HedvigCard
-import com.hedvig.android.core.designsystem.material3.infoContainer
+import com.hedvig.android.core.designsystem.component.card.HedvigInfoCard
 import com.hedvig.android.core.designsystem.material3.infoElement
-import com.hedvig.android.core.designsystem.material3.onInfoContainer
-import com.hedvig.android.core.designsystem.material3.squircle
 import com.hedvig.android.core.designsystem.preview.HedvigPreview
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
 
@@ -29,27 +25,21 @@ internal fun AddressInfoCard(
   text: String,
   modifier: Modifier = Modifier,
 ) {
-  HedvigCard(
-    shape = MaterialTheme.shapes.squircle,
-    colors = CardDefaults.outlinedCardColors(
-      containerColor = MaterialTheme.colorScheme.infoContainer,
-      contentColor = MaterialTheme.colorScheme.onInfoContainer,
-    ),
+  HedvigInfoCard(
     modifier = modifier,
+    contentPadding = PaddingValues(12.dp),
   ) {
-    Row(modifier = Modifier.padding(12.dp)) {
-      Icon(
-        imageVector = Icons.Default.Info,
-        contentDescription = "info",
-        modifier = Modifier.padding(top = 2.dp).size(16.dp).padding(1.dp),
-        tint = MaterialTheme.colorScheme.infoElement,
-      )
-      Spacer(modifier = Modifier.padding(start = 8.dp))
-      Text(
-        text = text,
-        style = MaterialTheme.typography.bodyMedium,
-      )
-    }
+    Icon(
+      imageVector = Icons.Default.Info,
+      contentDescription = "info",
+      modifier = Modifier.padding(top = 2.dp).size(16.dp).padding(1.dp),
+      tint = MaterialTheme.colorScheme.infoElement,
+    )
+    Spacer(modifier = Modifier.padding(start = 8.dp))
+    Text(
+      text = text,
+      style = MaterialTheme.typography.bodyMedium,
+    )
   }
 }
 
