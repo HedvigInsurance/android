9f3a07880b9c9bef144db811545498c69a891bb7
Hugo Linder <hugo@hedvig.com>
2023-05-25 13:12:56 +0200

    Fix pr comments

--- app/feature/travel-certificate/src/main/kotlin/com/hedvig/android/feature/travelcertificate/ui/AddCoInsured.kt
+++ app/feature/travel-certificate/src/main/kotlin/com/hedvig/android/feature/travelcertificate/ui/AddCoInsured.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -37,8 +37,8 @@ fun AddCoInsured(
   onAddCoInsured: (CoInsured) -> Unit,
 ) {
 
-  var name by remember { mutableStateOf(coInsured?.name ?: "") }
-  var ssn by remember { mutableStateOf(coInsured?.ssn ?: "") }
+  var name by rememberSaveable { mutableStateOf(coInsured?.name ?: "") }
+  var ssn by rememberSaveable { mutableStateOf(coInsured?.ssn ?: "") }
 
   HedvigScaffold(
     navigateUp = {
