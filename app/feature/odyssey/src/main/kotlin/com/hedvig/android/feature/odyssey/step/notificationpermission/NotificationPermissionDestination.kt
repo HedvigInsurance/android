b51f5d7dda81a6c6a21287f80b0a9b4aafcf5975
stylianosgakis <stylianos@hedvig.com>
2023-06-26 15:10:50 +0200

    Set notification permission to true for preview

--- app/feature/odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/step/notificationpermission/NotificationPermissionDestination.kt
+++ app/feature/odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/step/notificationpermission/NotificationPermissionDestination.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -183,7 +184,8 @@ private fun NotificationPermissionScreen(
 private fun rememberNotificationPermissionState(
   onPermissionResult: (Boolean) -> Unit,
 ): PermissionState {
-  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+  val isPreview = LocalInspectionMode.current
+  return if (!isPreview && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
     rememberPermissionState(Manifest.permission.POST_NOTIFICATIONS, onPermissionResult)
   } else {
     object : PermissionState {
