771cc3c970cf5b23bfcdceaf477198b325bed9c9
stylianosgakis <stylianos@hedvig.com>
2023-06-27 13:03:55 +0200

    Add a convenience preview parameter provider for booleans

---
+++ app/core/ui/src/main/kotlin/com/hedvig/android/core/ui/preview/BooleanCollectionPreviewParameterProvider.kt
@@ -0,0 +1,7 @@
+package com.hedvig.android.core.ui.preview
+
+import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
+
+class BooleanCollectionPreviewParameterProvider : CollectionPreviewParameterProvider<Boolean>(
+  listOf(true, false),
+)
