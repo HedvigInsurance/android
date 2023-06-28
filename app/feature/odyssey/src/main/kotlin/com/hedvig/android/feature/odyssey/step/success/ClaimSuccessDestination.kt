fe5de8506f4de3726d540d2de8d4511d84ade885
stylianosgakis <stylianos@hedvig.com>
2023-06-28 17:47:13 +0200

    Adapt Claim Success to the new design

--- app/feature/odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/step/success/ClaimSuccessDestination.kt
+++ app/feature/odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/step/success/ClaimSuccessDestination.kt
@@ -1,5 +1,7 @@
 package com.hedvig.android.feature.odyssey.step.success
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
@@ -9,8 +11,6 @@ import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -19,11 +19,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.hedvig.android.core.designsystem.component.button.LargeContainedTextButton
-import com.hedvig.android.core.designsystem.component.button.LargeOutlinedButton
-import com.hedvig.android.core.designsystem.component.card.HedvigCard
-import com.hedvig.android.core.designsystem.component.card.HedvigCardElevation
+import com.hedvig.android.core.designsystem.component.button.HedvigContainedButton
+import com.hedvig.android.core.designsystem.component.button.HedvigTextButton
 import com.hedvig.android.core.designsystem.preview.HedvigMultiScreenPreview
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
 import com.hedvig.android.core.ui.preview.calculateForPreview
@@ -53,37 +53,43 @@ private fun ClaimSuccessScreen(
     windowSizeClass = windowSizeClass,
     navigateUp = navigateBack,
   ) { sideSpacingModifier ->
-    Spacer(Modifier.height(20.dp))
-    HedvigCard(
-      shape = RoundedCornerShape(12.dp),
-      elevation = HedvigCardElevation.Elevated(),
-      modifier = sideSpacingModifier
-        .fillMaxWidth(0.66f)
-        .wrapContentWidth(Alignment.Start),
+    Spacer(Modifier.height(16.dp))
+    Box(
+      contentAlignment = Alignment.Center,
+      modifier = sideSpacingModifier.weight(1f).fillMaxWidth(),
     ) {
-      Text(
-        text = stringResource(R.string.message_claims_record_ok),
-        modifier = Modifier.padding(vertical = 12.dp, horizontal = 16.dp),
-        style = MaterialTheme.typography.bodyLarge,
-      )
+      Column(Modifier.fillMaxWidth()) {
+        Text(
+          text = stringResource(hedvig.resources.R.string.CLAIMS_SUCCESS_TITLE),
+          style = MaterialTheme.typography.headlineMedium,
+          modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(Modifier.height(16.dp))
+        Text(
+          text = stringResource(hedvig.resources.R.string.CLAIMS_SUCCESS_LABEL),
+          style = MaterialTheme.typography.bodyLarge.copy(
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            textAlign = TextAlign.Center,
+            lineBreak = LineBreak.Heading,
+          ),
+          modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+        )
+      }
     }
-    Spacer(Modifier.weight(1f))
     Spacer(Modifier.height(16.dp))
-    LargeOutlinedButton(openChat, sideSpacingModifier) {
-      Text(stringResource(R.string.open_chat))
-    }
-    Spacer(Modifier.height(16.dp))
-    LargeContainedTextButton(
+    HedvigContainedButton(
+      text = stringResource(R.string.open_chat),
+      onClick = openChat,
+      modifier = sideSpacingModifier.fillMaxWidth(),
+    )
+    Spacer(Modifier.height(8.dp))
+    HedvigTextButton(
       onClick = navigateBack,
       text = stringResource(R.string.general_close_button),
-      modifier = sideSpacingModifier,
+      modifier = sideSpacingModifier.fillMaxWidth(),
     )
     Spacer(Modifier.height(16.dp))
-    Spacer(
-      Modifier.windowInsetsPadding(
-        WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom),
-      ),
-    )
+    Spacer(Modifier.windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom)))
   }
 }
 
