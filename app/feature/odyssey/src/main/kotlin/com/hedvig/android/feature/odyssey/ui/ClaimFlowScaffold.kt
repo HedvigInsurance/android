3c6a015b7129fff44d8b3e3b9ea8f77824457d9b
stylianosgakis <stylianos@hedvig.com>
2023-07-05 17:59:35 +0200

    Make ClaimFlowScaffold take a callback to finish the flow
    Should be used by the X button in most screens to quickly exit the flow
    instead.

--- app/feature/odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/ui/ClaimFlowScaffold.kt
+++ app/feature/odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/ui/ClaimFlowScaffold.kt
@@ -20,24 +20,32 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
-import com.hedvig.android.core.ui.appbar.m3.TopAppBarWithBack
+import com.hedvig.android.core.ui.appbar.m3.TopAppBarWithBackAndClose
+import com.hedvig.android.core.ui.dialog.HedvigAlertDialog
 import com.hedvig.android.core.ui.progress.HedvigFullScreenCenterAlignedProgress
 import com.hedvig.android.core.ui.snackbar.ErrorSnackbar
 import com.hedvig.android.core.ui.snackbar.ErrorSnackbarState
+import hedvig.resources.R
 
 /**
  * An opinionated scaffold to make it easier to make screens for the Claim flow.
- * Sets up a top app bar with a back button, scrollable content and error/loading visuals.
+ * Sets up a top app bar with a back button, the X button and scrollable content and error/loading visuals.
  */
 @Composable
 internal fun ClaimFlowScaffold(
   windowSizeClass: WindowSizeClass,
   navigateUp: () -> Unit,
+  closeClaimFlow: () -> Unit,
   modifier: Modifier = Modifier,
   topAppBarText: String? = null,
   isLoading: Boolean = false,
@@ -46,6 +54,16 @@ internal fun ClaimFlowScaffold(
   content: @Composable (ColumnScope.(sideSpacingModifier: Modifier) -> Unit),
 ) {
   HedvigTheme(useNewColorScheme = true) {
+    var showCloseClaimsFlowDialog by rememberSaveable { mutableStateOf(false) }
+    if (showCloseClaimsFlowDialog) {
+      HedvigAlertDialog(
+        title = stringResource(R.string.GENERAL_ARE_YOU_SURE),
+        text = stringResource(R.string.claims_alert_body),
+        onDismissRequest = { showCloseClaimsFlowDialog = false },
+        onConfirmClick = closeClaimFlow,
+      )
+    }
+
     Surface(
       color = MaterialTheme.colorScheme.background,
       modifier = modifier.fillMaxSize(),
@@ -53,8 +71,9 @@ internal fun ClaimFlowScaffold(
       Box {
         Column {
           val topAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
-          TopAppBarWithBack(
-            onClick = navigateUp,
+          TopAppBarWithBackAndClose(
+            onNavigateUp = navigateUp,
+            onClose = { showCloseClaimsFlowDialog = true },
             title = topAppBarText ?: "",
             scrollBehavior = topAppBarScrollBehavior,
           )
