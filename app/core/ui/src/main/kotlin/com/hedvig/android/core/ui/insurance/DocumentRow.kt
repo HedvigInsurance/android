78aa6f9b20b0debb3de0aba12db94a4661adfe4c
Hugo Linder <hugo@hedvig.com>
2023-07-11 16:44:44 +0200

    Add composables for documents

---
+++ app/core/ui/src/main/kotlin/com/hedvig/android/core/ui/insurance/DocumentRow.kt
@@ -0,0 +1,71 @@
+package com.hedvig.android.core.ui.insurance
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hedvig.android.core.designsystem.newtheme.SquircleShape
+import com.hedvig.android.core.designsystem.preview.HedvigPreview
+import com.hedvig.android.core.designsystem.theme.HedvigTheme
+import com.hedvig.android.core.icons.Hedvig
+import com.hedvig.android.core.icons.hedvig.small.hedvig.ArrowNorthEast
+
+@Composable
+fun DocumentRow(
+  modifier: Modifier = Modifier,
+  title: String,
+  subTitle: String,
+  onClick: () -> Unit,
+) {
+  Row(
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.SpaceBetween,
+    modifier = modifier
+      .background(
+        shape = SquircleShape,
+        color = Color(0xFFF0F0F0),
+      )
+      .fillMaxWidth()
+      .padding(horizontal = 16.dp, vertical = 12.dp)
+      .clickable { onClick() },
+  ) {
+    Column {
+      Row {
+        Text(title)
+        Spacer(modifier = Modifier.padding(1.dp))
+        Text("PDF", fontSize = 11.sp)
+      }
+      Text(subTitle, color = Color(0xFF727272))
+    }
+
+    Icon(
+      imageVector = Hedvig.ArrowNorthEast,
+      contentDescription = "link",
+      tint = Color(0xFF121212),
+    )
+  }
+}
+
+@Composable
+@HedvigPreview
+fun PreviewDocumentRow() {
+  HedvigTheme {
+    DocumentRow(
+      title = "Document 1",
+      subTitle = "Subtitle 1",
+      onClick = {},
+    )
+  }
+}
