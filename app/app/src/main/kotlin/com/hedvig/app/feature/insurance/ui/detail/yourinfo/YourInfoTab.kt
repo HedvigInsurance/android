9b18e6581e8b42fc17631ed9b7666259b9f8e4e0
stylianosgakis <stylianos@hedvig.com>
2023-07-13 17:29:08 +0200

    Small touches in the YourInfoTab
    Mostly making the spacings individual on each component, and not having
    their spacings leak into the outer layout.
    Also make the horizontal items play well if they grow really wide.
    And use ImmutableList

--- app/app/src/main/kotlin/com/hedvig/app/feature/insurance/ui/detail/yourinfo/YourInfoTab.kt
+++ app/app/src/main/kotlin/com/hedvig/app/feature/insurance/ui/detail/yourinfo/YourInfoTab.kt
@@ -9,93 +9,105 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.hedvig.android.core.designsystem.component.button.HedvigContainedButton
 import com.hedvig.android.core.designsystem.component.button.HedvigTextButton
-import com.hedvig.android.core.designsystem.component.button.LargeContainedButton
-import com.hedvig.android.core.designsystem.material3.squircle
 import com.hedvig.android.core.designsystem.preview.HedvigPreview
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
 import com.hedvig.android.core.ui.text.HorizontalItemsWithMaximumSpaceTaken
 import hedvig.resources.R
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 @Composable
-fun YourInfoTab(
-  modifier: Modifier = Modifier,
-  coverageRowItems: List<Pair<String, String>>,
+internal fun YourInfoTab(
+  coverageRowItems: ImmutableList<Pair<String, String>>,
   onEditInfoClick: () -> Unit,
   onCancelInsuranceClick: () -> Unit,
+  modifier: Modifier = Modifier,
 ) {
   Column(modifier) {
-    CoverageRows(coverageRowItems)
-    Spacer(modifier = Modifier.height(32.dp))
-    LargeContainedButton(
+    Spacer(Modifier.height(8.dp))
+    CoverageRows(coverageRowItems, Modifier.padding(horizontal = 16.dp))
+    Spacer(Modifier.height(16.dp))
+    HedvigContainedButton(
+      text = stringResource(R.string.CONTRACT_EDIT_INFO_LABEL),
       onClick = onEditInfoClick,
-      shape = MaterialTheme.shapes.squircle,
       colors = ButtonDefaults.buttonColors(
         containerColor = MaterialTheme.colorScheme.surfaceVariant,
-        contentColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
       ),
-    ) {
-      Text(text = stringResource(id = R.string.CONTRACT_EDIT_INFO_LABEL))
-    }
-    Spacer(modifier = Modifier.height(8.dp))
+      modifier = Modifier.padding(horizontal = 16.dp),
+    )
+    Spacer(Modifier.height(8.dp))
     HedvigTextButton(
-      text = stringResource(id = R.string.TERMINATION_BUTTON),
+      text = stringResource(R.string.TERMINATION_BUTTON),
       onClick = onCancelInsuranceClick,
       colors = ButtonDefaults.textButtonColors(
         contentColor = MaterialTheme.colorScheme.error,
       ),
+      modifier = Modifier.padding(horizontal = 16.dp),
     )
   }
 }
 
+@Suppress("UnusedReceiverParameter")
 @Composable
-private fun CoverageRows(coverageRowItems: List<Pair<String, String>>) {
-  coverageRowItems.forEachIndexed { index, (firstText, secondText) ->
-    HorizontalItemsWithMaximumSpaceTaken(
-      startSlot = {
-        Text(firstText)
-      },
-      endSlot = {
-        Row(
-          verticalAlignment = Alignment.CenterVertically,
-          horizontalArrangement = Arrangement.End,
-        ) {
-          CompositionLocalProvider(LocalContentColor.provides(MaterialTheme.colorScheme.onSurfaceVariant)) {
-            Text(secondText)
+private fun CoverageRows(
+  coverageRowItems: ImmutableList<Pair<String, String>>,
+  modifier: Modifier = Modifier,
+) {
+  Column(modifier = modifier) {
+    coverageRowItems.forEachIndexed { index, (firstText, secondText) ->
+      HorizontalItemsWithMaximumSpaceTaken(
+        startSlot = {
+          Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(vertical = 16.dp),
+          ) {
+            Text(firstText)
           }
-        }
-      },
-    )
-    if (index != coverageRowItems.lastIndex) {
-      Spacer(Modifier.height(16.dp))
-      Divider()
-      Spacer(Modifier.height(16.dp))
+        },
+        endSlot = {
+          Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.End,
+            modifier = Modifier.padding(vertical = 16.dp),
+          ) {
+            Text(
+              text = secondText,
+              color = MaterialTheme.colorScheme.onSurfaceVariant,
+              textAlign = TextAlign.End,
+            )
+          }
+        },
+      )
+      if (index != coverageRowItems.lastIndex) {
+        Divider()
+      }
     }
   }
 }
 
 @Composable
 @HedvigPreview
-fun PreviewYourInfoTab() {
+private fun PreviewYourInfoTab() {
   HedvigTheme(useNewColorScheme = true) {
-    Surface {
+    Surface(color = MaterialTheme.colorScheme.background) {
       YourInfoTab(
-        modifier = Modifier.padding(16.dp),
-        coverageRowItems = listOf(
-          "Address" to "Bellmansgatan 19A",
-          "Postal code" to "118 47",
+        coverageRowItems = persistentListOf(
+          "Address".repeat(4) to "Bellmansgatan 19A",
+          "Postal code" to "118 47".repeat(6),
           "Type" to "Homeowner",
           "Size" to "56 m2",
-          "Co-insured" to "You +1",
+          "Co-insured".repeat(4) to "You +1".repeat(5),
         ),
         onEditInfoClick = {},
         onCancelInsuranceClick = {},
@@ -103,5 +115,3 @@ fun PreviewYourInfoTab() {
     }
   }
 }
-
-
