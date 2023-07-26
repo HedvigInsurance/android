56bb292a895e4a6c99d1a953c97692f591e63a83
stylianosgakis <stylianos@hedvig.com>
2023-07-26 13:57:51 +0200

    Make FakeCrossSellCardNotificationBadgeService a better test fake

--- app/notification-badge-data/fake/src/main/kotlin/com/hedvig/android/notification/badge/data/crosssell/card/CrossSellCardNotificationBadgeService.kt
+++ app/notification-badge-data/fake/src/main/kotlin/com/hedvig/android/notification/badge/data/crosssell/card/CrossSellCardNotificationBadgeService.kt
@@ -1,19 +1,17 @@
 package com.hedvig.android.notification.badge.data.crosssell.card
 
+import app.cash.turbine.Turbine
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.receiveAsFlow
 
-class FakeCrossSellCardNotificationBadgeService(
-  initialNotificationState: Boolean = true,
-) : CrossSellCardNotificationBadgeService {
-  val showNotification = MutableStateFlow<Boolean>(initialNotificationState)
+class FakeCrossSellCardNotificationBadgeService : CrossSellCardNotificationBadgeService {
+  val showNotification = Turbine<Boolean>()
 
   override fun showNotification(): Flow<Boolean> {
-    return showNotification
+    return showNotification.asChannel().receiveAsFlow()
   }
 
   override suspend fun markAsSeen() {
-    showNotification.update { false }
+    showNotification.add(false)
   }
 }
