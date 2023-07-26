eecd7f52d03a8424d6a1152fb9cfcde81ebd4215
Hugo Linder <hugo@hedvig.com>
2023-07-26 10:02:23 +0200

    Refactor viewmodel

--- app/app/src/main/kotlin/com/hedvig/app/feature/profile/ui/tab/ProfileViewModel.kt
+++ app/app/src/main/kotlin/com/hedvig/app/feature/profile/ui/tab/ProfileViewModel.kt
@@ -1,20 +1,14 @@
 package com.hedvig.app.feature.profile.ui.tab
 
-import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.errorprone.annotations.Immutable
-import com.hedvig.android.core.common.RetryChannel
 import com.hedvig.android.hanalytics.featureflags.FeatureManager
 import com.hedvig.android.hanalytics.featureflags.flags.Feature
-import com.hedvig.android.market.MarketManager
 import com.hedvig.app.authenticate.LogoutUseCase
-import com.hedvig.app.feature.profile.data.ProfileRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import javax.money.MonetaryAmount
 
 internal class ProfileViewModel(
   private val getEuroBonusStatusUseCase: GetEurobonusStatusUseCase,
