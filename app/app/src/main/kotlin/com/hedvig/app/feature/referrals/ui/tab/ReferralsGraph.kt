bd0f11cc4cdd4bd54873bd8e6db56a2135c1952d
Hugo Linder <hugo@hedvig.com>
2023-07-27 14:01:23 +0200

    Tweak design

--- app/app/src/main/kotlin/com/hedvig/app/feature/referrals/ui/tab/ReferralsGraph.kt
+++ app/app/src/main/kotlin/com/hedvig/app/feature/referrals/ui/tab/ReferralsGraph.kt
@@ -6,12 +6,14 @@ import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
@@ -23,6 +25,7 @@ import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.pullrefresh.PullRefreshDefaults
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
@@ -39,13 +42,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -60,15 +59,12 @@ import com.hedvig.android.core.designsystem.component.button.HedvigContainedButt
 import com.hedvig.android.core.designsystem.component.button.HedvigTextButton
 import com.hedvig.android.core.designsystem.component.card.HedvigBigCard
 import com.hedvig.android.core.designsystem.component.error.HedvigErrorSection
-import com.hedvig.android.core.designsystem.material3.motion.MotionDefaults
-import com.hedvig.android.core.ui.appbar.m3.TopAppBarLayoutForActions
 import com.hedvig.android.core.designsystem.component.progress.HedvigFullScreenCenterAlignedProgress
 import com.hedvig.android.core.designsystem.component.textfield.HedvigTextField
-import com.hedvig.android.core.designsystem.component.progress.HedvigFullScreenCenterAlignedProgress
 import com.hedvig.android.core.designsystem.material3.motion.MotionDefaults
+import com.hedvig.android.core.icons.Hedvig
+import com.hedvig.android.core.icons.hedvig.normal.Copy
 import com.hedvig.android.core.ui.appbar.m3.TopAppBarLayoutForActions
-import com.hedvig.android.core.ui.genericinfo.GenericErrorScreen
-import com.hedvig.android.core.ui.getLocale
 import com.hedvig.android.language.LanguageService
 import com.hedvig.android.navigation.compose.typed.animatedComposable
 import com.hedvig.android.navigation.compose.typed.animatedNavigation
@@ -84,11 +80,11 @@ import com.hedvig.app.util.apollo.toWebLocaleTag
 import com.hedvig.app.util.extensions.copyToClipboard
 import com.hedvig.app.util.extensions.showShareSheet
 import com.kiwi.navigationcompose.typed.createRoutePattern
-import org.javamoney.moneta.Money
-import org.koin.androidx.compose.koinViewModel
-import java.util.Locale
+import java.util.*
 import javax.money.MonetaryAmount
 import kotlinx.coroutines.launch
+import org.javamoney.moneta.Money
+import org.koin.androidx.compose.koinViewModel
 
 internal fun NavGraphBuilder.referralsGraph(
   languageService: LanguageService,
@@ -382,6 +378,7 @@ fun ColumnScope.ReferralsContent(
   )
   Spacer(Modifier.height(24.dp))
   if (uiState.referrals.isEmpty() && uiState.incentive != null) {
+    Spacer(Modifier.height(32.dp))
     CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onSurfaceVariant) {
       Text(
         text = stringResource(
@@ -395,6 +392,7 @@ fun ColumnScope.ReferralsContent(
           .padding(horizontal = 16.dp),
       )
     }
+    Spacer(Modifier.height(16.dp))
   } else {
     Text(
       text = stringResource(id = hedvig.resources.R.string.FOREVER_TAB_MONTLY_COST_LABEL),
@@ -413,19 +411,40 @@ fun ColumnScope.ReferralsContent(
         modifier = Modifier.fillMaxWidth(),
       )
     }
+    Spacer(Modifier.height(82.dp))
   }
-  Spacer(Modifier.height(82.dp))
   HedvigBigCard(
     onClick = {
       uiState.campaignCode?.let {
         context.copyToClipboard(uiState.campaignCode)
       }
     },
-    hintText = stringResource(id = hedvig.resources.R.string.referrals_empty_code_headline),
-    inputText = uiState.campaignCode,
     enabled = true,
     modifier = Modifier
       .fillMaxWidth()
       .padding(horizontal = 16.dp),
-  )
+  ) {
+    Row(
+      modifier = Modifier
+        .heightIn(min = 72.dp)
+        .padding(horizontal = 16.dp, vertical = 10.dp),
+    ) {
+      Column {
+        Text(
+          text = stringResource(id = hedvig.resources.R.string.referrals_empty_code_headline),
+          style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.onSurfaceVariant),
+        )
+        Text(
+          text = uiState.campaignCode ?: "",
+          style = MaterialTheme.typography.headlineSmall,
+        )
+      }
+      Spacer(modifier = Modifier.weight(1f))
+      Icon(
+        imageVector = Icons.Hedvig.Copy,
+        contentDescription = "Copy",
+        modifier = Modifier.align(Alignment.Bottom).padding(bottom = 8.dp)
+      )
+    }
+  }
 }
