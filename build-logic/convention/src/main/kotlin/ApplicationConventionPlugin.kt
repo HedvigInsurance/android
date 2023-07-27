6b1e4ffc0ab278a3a2d2c7e2eabb588423dbab19
stylianosgakis <stylianos@hedvig.com>
2023-07-27 14:31:40 +0200

    Update application convention plugin with agp 8.1.0
    Context:
    https://github.com/android/nowinandroid/blob/6ab175c08c7414878b413712335fd0fad721b678/build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt#L27-L28

--- build-logic/convention/src/main/kotlin/ApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/ApplicationConventionPlugin.kt
@@ -1,9 +1,11 @@
+import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 import com.hedvig.android.configureKotlinAndroid
 import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.the
 
 class ApplicationConventionPlugin : Plugin<Project> {
@@ -17,7 +19,8 @@ class ApplicationConventionPlugin : Plugin<Project> {
       }
 
       extensions.configure<BaseAppModuleExtension> {
-        configureKotlinAndroid(this)
+        val extension = extensions.getByType<ApplicationExtension>()
+        configureKotlinAndroid(extension)
         defaultConfig.targetSdk = libs.versions.targetSdkVersion.get().toInt()
       }
     }
