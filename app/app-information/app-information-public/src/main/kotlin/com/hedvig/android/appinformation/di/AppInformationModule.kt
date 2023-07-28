6b39327219ad249fede0f429805b685eb77c63b2
stylianosgakis <stylianos@hedvig.com>
2023-07-28 14:58:59 +0200

    Hookup app information items to DI

--- app/app-information/app-information-public/src/main/kotlin/com/hedvig/android/appinformation/di/AppInformationModule.kt
+++ app/app-information/app-information-public/src/main/kotlin/com/hedvig/android/appinformation/di/AppInformationModule.kt
@@ -1,9 +1,27 @@
 package com.hedvig.android.appinformation.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.apollographql.apollo3.ApolloClient
+import com.hedvig.android.apollo.giraffe.di.giraffeClient
 import com.hedvig.android.appinformation.EnableNotificationsReminderManager
 import com.hedvig.android.appinformation.EnableNotificationsReminderManagerImpl
+import com.hedvig.android.appinformation.GetConnectPaymentReminderUseCase
+import com.hedvig.android.appinformation.GetConnectPaymentReminderUseCaseImpl
+import com.hedvig.android.appinformation.GetUpcomingRenewalRemindersUseCase
+import com.hedvig.android.appinformation.GetUpcomingRenewalRemindersUseCaseImpl
+import com.hedvig.android.hanalytics.featureflags.FeatureManager
+import kotlinx.datetime.Clock
 import org.koin.dsl.module
 
 val appInformationModule = module {
-  single<EnableNotificationsReminderManager> { EnableNotificationsReminderManagerImpl(get(), get()) }
+  single<EnableNotificationsReminderManager> {
+    EnableNotificationsReminderManagerImpl(get<DataStore<Preferences>>(), get<Clock>())
+  }
+  single<GetConnectPaymentReminderUseCase> {
+    GetConnectPaymentReminderUseCaseImpl(get<ApolloClient>(giraffeClient), get<FeatureManager>())
+  }
+  single<GetUpcomingRenewalRemindersUseCase> {
+    GetUpcomingRenewalRemindersUseCaseImpl(get<ApolloClient>(giraffeClient), get<Clock>())
+  }
 }
