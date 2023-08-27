cb94061027f52d1fdf3e78576369e40f8a17a27f
stylianosgakis <stylianos@hedvig.com>
2023-08-27 20:39:49 +0200

    Make the convention plugin use the lint.xml file

--- build-logic/convention/src/main/kotlin/HedvigLintConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/HedvigLintConventionPlugin.kt
@@ -6,28 +6,31 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.the
+import java.io.File
 
 class HedvigLintConventionPlugin : Plugin<Project> {
   override fun apply(target: Project) {
     with(target) {
       val libs = the<LibrariesForLibs>()
+      val lintXmlPath: File = rootProject.projectDir.resolve("hedvig-lint").resolve("lint.xml")
       when {
         pluginManager.hasPlugin(libs.plugins.androidApplication.get().pluginId) -> {
-          configure<ApplicationExtension> { lint(Lint::configure) }
+          configure<ApplicationExtension> { lint { configure(lintXmlPath) } }
         }
         pluginManager.hasPlugin(libs.plugins.androidLibrary.get().pluginId) -> {
-          configure<LibraryExtension> { lint(Lint::configure) }
+          configure<LibraryExtension> { lint { configure(lintXmlPath) } }
         }
         else -> {
           pluginManager.apply(libs.plugins.lintGradlePlugin.get().pluginId)
-          configure<Lint>(Lint::configure)
+          configure<Lint> { configure(lintXmlPath) }
         }
       }
     }
   }
 }
 
-private fun Lint.configure() {
+private fun Lint.configure(lintXmlFile: File) {
+  lintConfig = lintXmlFile
   xmlReport = true
   checkDependencies = true
 }
