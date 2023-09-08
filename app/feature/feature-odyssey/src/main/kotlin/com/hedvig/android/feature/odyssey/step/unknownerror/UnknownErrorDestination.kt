bdddfe091a14daac9899fe03835609516ee09045
Hugo Linder <hugo@hedvig.com>
2023-09-08 15:12:17 +0200

    Use HedvigErrorSection

--- app/feature/feature-odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/step/unknownerror/UnknownErrorDestination.kt
+++ app/feature/feature-odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/step/unknownerror/UnknownErrorDestination.kt
@@ -6,13 +6,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -21,12 +19,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.hedvig.android.core.designsystem.component.button.HedvigContainedButton
 import com.hedvig.android.core.designsystem.component.button.HedvigTextButton
-import com.hedvig.android.core.designsystem.component.information.AppStateInformation
-import com.hedvig.android.core.designsystem.component.information.AppStateInformationType
+import com.hedvig.android.core.designsystem.component.error.HedvigErrorSection
 import com.hedvig.android.core.designsystem.preview.HedvigPreview
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
 import hedvig.resources.R
@@ -61,23 +56,15 @@ private fun UnknownErrorScreen(
         Spacer(Modifier.height(16.dp))
         Box(
           contentAlignment = Alignment.Center,
-          modifier = Modifier.padding(horizontal = 16.dp).weight(1f),
+          modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .weight(1f),
         ) {
-          AppStateInformation(
-            type = AppStateInformationType.Failure,
-            title = stringResource(R.string.home_tab_error_title),
-            description = stringResource(R.string.home_tab_error_body),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally).fillMaxWidth(0.8f),
+          HedvigErrorSection(
+            buttonText = stringResource(R.string.open_chat),
+            retry = openChat,
           )
         }
-        Spacer(Modifier.height(16.dp))
-        HedvigContainedButton(
-          text = stringResource(R.string.open_chat),
-          onClick = openChat,
-          modifier = Modifier.padding(horizontal = 16.dp),
-        )
         Spacer(Modifier.height(16.dp))
         HedvigTextButton(
           stringResource(R.string.general_close_button),
