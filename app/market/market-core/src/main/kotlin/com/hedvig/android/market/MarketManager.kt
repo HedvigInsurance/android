7184c3cbc2b16ceddf5896b3ffdb09f8c2e095d9
stylianosgakis <stylianos@hedvig.com>
2023-09-28 15:43:12 +0200

    Make Market non-null and use non-blocking storage
    Market can always default to SE instead of being null, which does
    sometimes become a problem. This persists backwards compatibility with
    the market being stored in SharedPreferences, as it takes the last value
    from in there, and stores it inside DataStore going forward.
    
    We maintain the ability to get back a synchronous value of what the
    market currently is by exposing a StateFlow, and always keeping it hot,
    just like we've been doing with AuthTokenService and the
    `val authStatus: StateFlow<AuthStatus?>` it exposes. This also comes
    with the benefit that we no longer do this access on the main thread as
    we did with shared preferences.

--- app/market/market-core/src/main/kotlin/com/hedvig/android/market/MarketManager.kt
+++ app/market/market-core/src/main/kotlin/com/hedvig/android/market/MarketManager.kt
@@ -1,49 +1,63 @@
 package com.hedvig.android.market
 
-import android.annotation.SuppressLint
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.preference.PreferenceManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 interface MarketManager {
-  val enabledMarkets: List<Market>
-  var market: Market?
+  val market: StateFlow<Market>
+  suspend fun setMarket(market: Market)
 }
 
 internal class MarketManagerImpl(
+  private val datastore: DataStore<Preferences>,
   context: Context,
+  coroutineScope: CoroutineScope,
 ) : MarketManager {
-
   private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
-  override val enabledMarkets = listOfNotNull(
-    Market.SE,
-    Market.NO,
-    Market.DK,
-  )
-
-  override var market: Market?
-    get() = getMarketLocally()
-    set(value) {
-      value?.let(::setMarketLocally) ?: removeMarket()
+  override val market: StateFlow<Market> = datastore.data
+    .map { preferences ->
+      val storedMarket: Market? = preferences[marketPreferencesKey]?.let { Market.valueOf(it) }
+      if (storedMarket != null) {
+        return@map storedMarket
+      }
+      // Remove sharedPrefs code when we no longer need to migrate old users, and just fallback to SE instead
+      val sharedPrefsStoredMarket = sharedPreferences
+        .getString(Market.MARKET_SHARED_PREF, null)
+        ?.let { Market.valueOf(it) }
+      if (sharedPrefsStoredMarket == null) {
+        return@map Market.SE // Fallback to SE in case there was nothing in the datastore
+      }
+      datastore.edit {
+        it[marketPreferencesKey] = sharedPrefsStoredMarket.name
+      }
+      sharedPreferences.edit()
+        .remove(Market.MARKET_SHARED_PREF)
+        .apply()
+      sharedPrefsStoredMarket
     }
+    .stateIn(
+      coroutineScope,
+      SharingStarted.Eagerly,
+      Market.SE,
+    )
 
-  private fun getMarketLocally(): Market? {
-    return sharedPreferences
-      .getString(Market.MARKET_SHARED_PREF, null)
-      ?.let { Market.valueOf(it) }
+  override suspend fun setMarket(market: Market) {
+    datastore.edit {
+      it[marketPreferencesKey] = market.name
+    }
   }
 
-  @SuppressLint("ApplySharedPref") // We need to do this right away
-  private fun setMarketLocally(market: Market) {
-    sharedPreferences.edit()
-      .putString(Market.MARKET_SHARED_PREF, market.name)
-      .commit()
-  }
-
-  @SuppressLint("ApplySharedPref") // We need to do this right away
-  private fun removeMarket() {
-    sharedPreferences.edit()
-      .remove(Market.MARKET_SHARED_PREF)
-      .commit()
+  companion object {
+    val marketPreferencesKey = stringPreferencesKey("com.hedvig.android.market.Market")
   }
 }
