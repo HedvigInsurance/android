1aee8ea63ecfea7c877db963dcd21589345f97d3
stylianosgakis <stylianos@hedvig.com>
2023-09-28 15:46:14 +0200

    Remove the GEO query to determine market
    This will soon be deleted completely anyway, as the defaults from
    MarketManager should suffice for what we are doing here.

--- app/app/src/main/kotlin/com/hedvig/app/feature/marketing/data/GetInitialMarketPickerValuesUseCase.kt
+++ app/app/src/main/kotlin/com/hedvig/app/feature/marketing/data/GetInitialMarketPickerValuesUseCase.kt
@@ -1,36 +1,18 @@
 package com.hedvig.app.feature.marketing.data
 
-import arrow.core.merge
-import com.apollographql.apollo3.ApolloClient
-import com.hedvig.android.apollo.safeExecute
-import com.hedvig.android.apollo.toEither
 import com.hedvig.android.language.LanguageService
 import com.hedvig.android.market.Language
 import com.hedvig.android.market.Market
 import com.hedvig.android.market.MarketManager
-import giraffe.GeoQuery
 
+// TODO delete this class and use defaults from MarketManager for the new login screens
 class GetInitialMarketPickerValuesUseCase(
-  private val apolloClient: ApolloClient,
   private val marketManager: MarketManager,
   private val languageService: LanguageService,
 ) {
   suspend operator fun invoke(): Pair<Market, Language> {
-    val currentMarket = marketManager.market
-    if (currentMarket != null) {
-      val currentLanguage = languageService.getLanguage()
-      return currentMarket to currentLanguage
-    }
-
-    val marketOrDefault = apolloClient
-      .query(GeoQuery())
-      .safeExecute()
-      .toEither()
-      .map { runCatching { Market.valueOf(it.geo.countryISOCode) }.getOrNull() }
-      .mapLeft { null }
-      .merge()
-      ?: Market.SE
-
-    return marketOrDefault to marketOrDefault.defaultLanguage()
+    val currentMarket = marketManager.market.value
+    val currentLanguage = languageService.getLanguage()
+    return currentMarket to currentLanguage
   }
 }
