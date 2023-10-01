7e02323ce7341c80cb7907b114f11f005254cc2c
stylianosgakis <stylianos@hedvig.com>
2023-10-02 00:10:03 +0200

    Create LoginDestination with the loginGraph
    Ready to be hooked up into wherever it may be used.

---
+++ app/feature/feature-login/src/main/kotlin/com/hedvig/android/feature/login/navigation/LoginDestination.kt
@@ -0,0 +1,15 @@
+package com.hedvig.android.feature.login.navigation
+
+import com.kiwi.navigationcompose.typed.Destination
+import kotlinx.serialization.Serializable
+
+internal sealed interface LoginDestination : Destination {
+  @Serializable
+  object Marketing : LoginDestination
+
+  @Serializable
+  object SwedishLogin : LoginDestination
+
+  @Serializable
+  object OtpLogin : LoginDestination
+}
