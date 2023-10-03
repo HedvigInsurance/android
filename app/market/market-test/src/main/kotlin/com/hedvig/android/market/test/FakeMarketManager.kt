5d27da84f07be1a8deed0884590636042ed33985
stylianosgakis <stylianos@hedvig.com>
2023-10-03 11:29:22 +0200

    Run lintDebug

--- app/market/market-test/src/main/kotlin/com/hedvig/android/market/test/FakeMarketManager.kt
+++ app/market/market-test/src/main/kotlin/com/hedvig/android/market/test/FakeMarketManager.kt
@@ -5,15 +5,10 @@ import com.hedvig.android.market.MarketManager
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 
 class FakeMarketManager(
   private val initialMarket: Market,
 ) : MarketManager {
   val _market: MutableStateFlow<Market> = MutableStateFlow(initialMarket)
   override val market: StateFlow<Market> = _market.asStateFlow()
-
-  override suspend fun setMarket(market: Market) {
-    _market.update { market }
-  }
 }
