bbca00abf1c66ceb948d1e211038f3637a98162c
stylianosgakis <stylianos@hedvig.com>
2023-10-04 16:02:17 +0200

    Add release mode for design showcase micro-app

--- micro-apps/design-showcase/build.gradle.kts
+++ micro-apps/design-showcase/build.gradle.kts
@@ -24,6 +24,18 @@ android {
     val debug by getting {
       isDebuggable = true
     }
+    val release by getting {
+      signingConfig = debug.signingConfig // uncomment to run release build locally
+      applicationIdSuffix = ".app"
+      isMinifyEnabled = true
+      isShrinkResources = true
+      setProguardFiles(
+        listOf(
+          getDefaultProguardFile("proguard-android.txt"),
+          "proguard-rules.pro",
+        ),
+      )
+    }
   }
 }
 
@@ -36,8 +48,15 @@ dependencies {
   implementation(libs.androidx.compose.runtime)
   implementation(libs.androidx.graphicsShapes)
   implementation(libs.androidx.other.activityCompose)
+  implementation(libs.coil.coil)
+  implementation(libs.kotlinx.immutable.collections)
   implementation(libs.zoomable)
+  implementation(projects.audioPlayer)
   implementation(projects.coreDesignSystem)
   implementation(projects.coreIcons)
   implementation(projects.coreUi)
+  implementation(projects.loggingAndroid)
+  implementation(projects.memberRemindersPublic)
+  implementation(projects.memberRemindersUi)
+  implementation(projects.notificationPermission)
 }
