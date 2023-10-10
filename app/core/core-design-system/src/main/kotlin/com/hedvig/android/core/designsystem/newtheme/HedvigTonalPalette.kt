a5f753d1aa9b680a7013aa15395f145f4ecd4e4c
stylianosgakis <stylianos@hedvig.com>
2023-10-10 17:16:54 +0200

    Only keep the relevant colors

--- app/core/core-design-system/src/main/kotlin/com/hedvig/android/core/designsystem/newtheme/HedvigTonalPalette.kt
+++ app/core/core-design-system/src/main/kotlin/com/hedvig/android/core/designsystem/newtheme/HedvigTonalPalette.kt
@@ -16,93 +16,37 @@ internal interface HedvigTonalPalette {
   val greyscale900: Color
   val greyscale1000: Color
 
-  val green50: Color
   val green100: Color
-  val green200: Color
   val green300: Color
-  val green400: Color
   val green500: Color
   val green600: Color
   val green700: Color
   val green800: Color
   val green900: Color
 
-  val yellow50: Color
-  val yellow100: Color
-  val yellow200: Color
-  val yellow300: Color
-  val yellow400: Color
-  val yellow500: Color
-  val yellow600: Color
-  val yellow700: Color
-  val yellow800: Color
-  val yellow900: Color
-
-  val amber50: Color
   val amber100: Color
-  val amber200: Color
   val amber300: Color
-  val amber400: Color
   val amber500: Color
   val amber600: Color
   val amber700: Color
   val amber800: Color
   val amber900: Color
 
-  val red50: Color
   val red100: Color
-  val red200: Color
   val red300: Color
-  val red400: Color
   val red500: Color
   val red600: Color
   val red700: Color
   val red800: Color
   val red900: Color
 
-  val pink50: Color
-  val pink100: Color
-  val pink200: Color
-  val pink300: Color
-  val pink400: Color
-  val pink500: Color
-  val pink600: Color
-  val pink700: Color
-  val pink800: Color
-  val pink900: Color
-
-  val purple50: Color
-  val purple100: Color
-  val purple200: Color
-  val purple300: Color
-  val purple400: Color
-  val purple500: Color
-  val purple600: Color
-  val purple700: Color
-  val purple800: Color
-  val purple900: Color
-
-  val blue50: Color
   val blue100: Color
-  val blue200: Color
   val blue300: Color
-  val blue400: Color
   val blue500: Color
   val blue600: Color
   val blue700: Color
   val blue800: Color
   val blue900: Color
-
-  val teal50: Color
-  val teal100: Color
-  val teal200: Color
-  val teal300: Color
-  val teal400: Color
-  val teal500: Color
-  val teal600: Color
-  val teal700: Color
-  val teal800: Color
-  val teal900: Color
 
   val lavender300: Color
   val lavender400: Color
