0edbd552c6c7755a4a28fda4cf668ef648aaa062
stylianosgakis <stylianos@hedvig.com>
2023-10-16 16:38:57 +0200

    Add some skeleton for TrustlyDestination

--- app/feature/feature-connect-payment-trustly/build.gradle.kts
+++ app/feature/feature-connect-payment-trustly/build.gradle.kts
@@ -6,10 +6,16 @@ plugins {
 }
 
 dependencies {
+  implementation(libs.androidx.lifecycle.compose)
+  implementation(libs.androidx.lifecycle.viewModel)
   implementation(libs.androidx.navigation.common)
   implementation(libs.androidx.navigation.compose)
   implementation(libs.androidx.navigation.runtime)
   implementation(libs.kiwi.navigationCompose)
+  implementation(libs.koin.compose)
+  implementation(libs.koin.core)
   implementation(projects.composeWebview)
+  implementation(projects.moleculeAndroid)
+  implementation(projects.moleculePublic)
   implementation(projects.navigationCore)
 }
--- app/feature/feature-connect-payment-trustly/src/main/kotlin/com/hedvig/android/feature/connect/payment/trustly/ConnectTrustlyPaymentGraph.kt
+++ app/feature/feature-connect-payment-trustly/src/main/kotlin/com/hedvig/android/feature/connect/payment/trustly/ConnectTrustlyPaymentGraph.kt
@@ -1,10 +1,44 @@
 package com.hedvig.android.feature.connect.payment
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
+import com.hedvig.android.molecule.android.MoleculeViewModel
+import com.hedvig.android.molecule.public.MoleculePresenter
+import com.hedvig.android.molecule.public.MoleculePresenterScope
 import com.hedvig.android.navigation.core.AppDestination
 import com.kiwi.navigationcompose.typed.composable
+import org.koin.androidx.compose.koinViewModel
 
 fun NavGraphBuilder.connectTrustlyPaymentGraph() {
   composable<AppDestination.ConnectPaymentTrustly>() {
+    val viewModel: TrustlyViewModel = koinViewModel()
+    TrustlyDestination(viewModel)
   }
 }
+
+@Composable
+internal fun TrustlyDestination(viewModel: TrustlyViewModel) {
+  val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+  TrustlyScreen(uiState)
+}
+
+@Composable
+private fun TrustlyScreen(uiState: TrustlyUiState) {
+}
+
+internal class TrustlyViewModel : MoleculeViewModel<TrustlyEvent, TrustlyUiState>(
+  TrustlyUiState,
+  TrustlyPresenter(),
+)
+
+internal class TrustlyPresenter : MoleculePresenter<TrustlyEvent, TrustlyUiState> {
+  @Composable
+  override fun MoleculePresenterScope<TrustlyEvent>.present(lastState: TrustlyUiState): TrustlyUiState {
+    return TrustlyUiState
+  }
+}
+
+internal object TrustlyEvent
+internal object TrustlyUiState
