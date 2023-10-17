ff584249310965364d8aaa2250c68f61d1e04113
stylianosgakis <stylianos@hedvig.com>
2023-10-17 15:45:04 +0200

    Create TrustlyCallback
    To be passed to Trustly to get informed about success/failure

---
+++ app/feature/feature-connect-payment-trustly/src/main/kotlin/com/hedvig/android/feature/connect/payment/trustly/data/TrustlyCallback.kt
@@ -0,0 +1,20 @@
+package com.hedvig.android.feature.connect.payment.trustly.data
+
+import com.hedvig.android.code.buildoconstants.HedvigBuildConstants
+
+/**
+ * Trustly asks for two urls to redirect to when they are done processing the payment connection.
+ * We need to pass these when we start the process, and we can then listen in on them to know when the process is done
+ * and what the outcome of it was.
+ */
+internal interface TrustlyCallback {
+  val successUrl: String
+  val failureUrl: String
+}
+
+internal class TrustlyCallbackImpl(
+  hedvigBuildConstants: HedvigBuildConstants,
+) : TrustlyCallback {
+  override val successUrl: String = "${hedvigBuildConstants.urlBaseWeb}/payment-success"
+  override val failureUrl: String = "${hedvigBuildConstants.urlBaseWeb}/payment-failure"
+}
