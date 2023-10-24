fe309246c4fd51c38b7637d16d89901c1133d7fa
stylianosgakis <stylianos@hedvig.com>
2023-10-24 20:17:21 +0200

    Fix overly verbose error logs
    The real solution here is for `safeExecute` to report properly errors
    that are expected (like being unauthenticated) versus ones that are real
    problems and need to be logged with Error priority.
    This work is postponed till the safeExecute function is able to just
    return the correct Either directly, when no more callers rely on it
    being OperationResponse. That way we will then also remove all
    `toEither` calls at the same time.
    
    On top of this, the logging for the error cases can happen globally in
    there too, to get better logs, possibly along with the query name,
    parameters and so on without having to opt-in for each caller.
    Extra logs can then still happen at call-site when there's such a need,
    but not to get the basic error reporting.

--- app/data/data-travel-certificate/data-travel-certificate-public/src/main/kotlin/com/hedvig/android/data/travelcertificate/GetTravelCertificateSpecificationsUseCase.kt
+++ app/data/data-travel-certificate/data-travel-certificate-public/src/main/kotlin/com/hedvig/android/data/travelcertificate/GetTravelCertificateSpecificationsUseCase.kt
@@ -10,7 +10,6 @@ import com.hedvig.android.apollo.toEither
 import com.hedvig.android.core.common.ErrorMessage
 import com.hedvig.android.hanalytics.featureflags.FeatureManager
 import com.hedvig.android.hanalytics.featureflags.flags.Feature
-import com.hedvig.android.logger.LogPriority
 import com.hedvig.android.logger.logcat
 import kotlinx.datetime.LocalDate
 import octopus.TravelCertificateSpecificationsQuery
@@ -34,7 +33,11 @@ internal class GetTravelCertificateSpecificationsUseCaseImpl(
         .safeExecute()
         .toEither(::ErrorMessage)
         .mapLeft(TravelCertificateError::Error)
-        .onLeft { logcat(LogPriority.ERROR, it.throwable) { it.message ?: "Could not fetch travel certificate" } }
+        .onLeft {
+          logcat(throwable = it.throwable) {
+            "GetTravelCertificateSpecificationsUseCaseImpl: ${it.message ?: "Could not fetch travel certificate"}"
+          }
+        }
         .bind()
         .currentMember
 
