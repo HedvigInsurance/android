2b70c3b1832ad17a55f8eb3e30fc47c6b5fa02cb
stylianosgakis <stylianos@hedvig.com>
2023-12-15 15:39:09 +0100

    Remove unused DisabledByFeatureFlag state
    It was never used anymore ever since Demo implementation ChatRepository
    was added.

--- app/feature/feature-chat/src/main/kotlin/com/hedvig/android/feature/chat/ChatViewModel.kt
+++ app/feature/feature-chat/src/main/kotlin/com/hedvig/android/feature/chat/ChatViewModel.kt
@@ -2,19 +2,16 @@ package com.hedvig.android.feature.chat
 
 import com.hedvig.android.core.demomode.Provider
 import com.hedvig.android.feature.chat.data.ChatRepository
-import com.hedvig.android.hanalytics.featureflags.FeatureManager
 import com.hedvig.android.molecule.android.MoleculeViewModel
 import kotlinx.datetime.Clock
 
 internal class ChatViewModel(
   chatRepository: Provider<ChatRepository>,
-  featureManager: FeatureManager,
   clock: Clock,
 ) : MoleculeViewModel<ChatEvent, ChatUiState>(
     ChatUiState.Initializing,
     ChatPresenter(
       chatRepository = chatRepository,
-      featureManager = featureManager,
       clock = clock,
     ),
   )
