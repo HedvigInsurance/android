231bea2548504b1e563000bc66f3560d39c56fd9
stylianosgakis <stylianos@hedvig.com>
2023-12-19 18:17:20 +0100

    Add deep links for help-center destinations

--- app/feature/feature-help-center/src/main/kotlin/com/hedvig/android/feature/help/center/HelpCenterGraph.kt
+++ app/feature/feature-help-center/src/main/kotlin/com/hedvig/android/feature/help/center/HelpCenterGraph.kt
@@ -2,25 +2,38 @@ package com.hedvig.android.feature.help.center
 
 import androidx.compose.material3.Text
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.navDeepLink
 import com.hedvig.android.feature.help.center.navigation.HelpCenterDestination
 import com.hedvig.android.feature.help.center.navigation.HelpCenterDestinations
+import com.hedvig.android.navigation.core.HedvigDeepLinkContainer
 import com.hedvig.android.navigation.core.Navigator
 import com.kiwi.navigationcompose.typed.composable
 import com.kiwi.navigationcompose.typed.createRoutePattern
 import com.kiwi.navigationcompose.typed.navigation
 
-fun NavGraphBuilder.helpCenterGraph(navigator: Navigator) {
+fun NavGraphBuilder.helpCenterGraph(navigator: Navigator, hedvigDeepLinkContainer: HedvigDeepLinkContainer) {
   navigation<HelpCenterDestination>(
     startDestination = createRoutePattern<HelpCenterDestinations.HelpCenter>(),
+    deepLinks = listOf(
+      navDeepLink { uriPattern = hedvigDeepLinkContainer.helpCenter },
+    ),
   ) {
     composable<HelpCenterDestinations.HelpCenter> { backStackEntry ->
       Text("HelpCenterDestinations.HelpCenter")
     }
-    composable<HelpCenterDestinations.Topic> { backStackEntry ->
-      Text("HelpCenterDestinations.Topic")
+    composable<HelpCenterDestinations.Topic>(
+      deepLinks = listOf(
+        navDeepLink { uriPattern = hedvigDeepLinkContainer.helpCenterCommonTopic },
+      ),
+    ) { backStackEntry ->
+      Text("HelpCenterDestinations.Topic:$id")
     }
-    composable<HelpCenterDestinations.Question> { backStackEntry ->
-      Text("HelpCenterDestinations.Question")
+    composable<HelpCenterDestinations.Question>(
+      deepLinks = listOf(
+        navDeepLink { uriPattern = hedvigDeepLinkContainer.helpCenterQuestion },
+      ),
+    ) { backStackEntry ->
+      Text("HelpCenterDestinations.Question:$id")
     }
   }
 }
