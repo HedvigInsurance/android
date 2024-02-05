735e0724f23242dbfdfb80a11a5e20400eda9d75
stylianosgakis <stylianos@hedvig.com>
2024-02-05 16:46:36 +0100

    Create GetOnlyHasNonPayingContractsUseCase for prod and demo use cases
    Returns true for the cases where at least on active agreement exists and
    from them, all of them are of the contract type which is considered a
    non-paying member.

---
+++ app/data/data-paying-member/src/main/kotlin/com/hedvig/android/data/paying/member/GetOnlyHasNonPayingContractsUseCaseImpl.kt
@@ -0,0 +1,34 @@
+package com.hedvig.android.data.paying.member
+
+import arrow.core.Either
+import arrow.core.raise.either
+import com.apollographql.apollo3.ApolloClient
+import com.hedvig.android.apollo.safeExecute
+import com.hedvig.android.apollo.toEither
+import com.hedvig.android.core.common.ErrorMessage
+import com.hedvig.android.data.contract.ContractType
+import com.hedvig.android.data.contract.toContractType
+import octopus.ActiveInsuranceContractTypesQuery
+
+internal class GetOnlyHasNonPayingContractsUseCaseImpl(
+    private val apolloClient: ApolloClient,
+) : GetOnlyHasNonPayingContractsUseCase {
+  override suspend fun invoke(): Either<ErrorMessage, Boolean> {
+    return either {
+        val contractTypes: List<ContractType> =
+            apolloClient.query(ActiveInsuranceContractTypesQuery())
+                .safeExecute()
+                .toEither(::ErrorMessage)
+                .bind()
+                .currentMember
+                .activeContracts
+                .map { it.currentAgreement.productVariant.typeOfContract.toContractType() }
+
+        contractTypes.all { it.isNonPayingContractType() }
+    }
+  }
+}
+
+private fun ContractType.isNonPayingContractType(): Boolean {
+  return this == ContractType.SE_QASA_SHORT_TERM_RENTAL || this == ContractType.SE_QASA_LONG_TERM_RENTAL
+}
