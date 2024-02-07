9746e57a29c2e8a816d535006932ba31c23590c0
stylianosgakis <stylianos@hedvig.com>
2024-02-07 22:04:28 +0100

    Make linear progress handle small/big screen sizes better

--- app/core/core-design-system/src/main/kotlin/com/hedvig/android/core/designsystem/component/progress/HedvigFullScreenCenterAlignedLinearProgress.kt
+++ app/core/core-design-system/src/main/kotlin/com/hedvig/android/core/designsystem/component/progress/HedvigFullScreenCenterAlignedLinearProgress.kt
@@ -3,27 +3,30 @@ package com.hedvig.android.core.designsystem.component.progress
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.requiredWidthIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.hedvig.android.core.designsystem.preview.HedvigPreview
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
 import kotlinx.coroutines.delay
 
@@ -36,11 +39,9 @@ fun HedvigFullScreenCenterAlignedLinearProgressDebounced(
   show: Boolean = true,
   debounceMillis: Long = DEBOUNCE_MILLIS,
 ) {
-  var debounce by remember { mutableStateOf(false) }
-
-  LaunchedEffect(Unit) {
+  val debounce by produceState(false) {
     delay(debounceMillis)
-    debounce = true
+    value = true
   }
 
   HedvigFullScreenCenterAlignedLinearProgress(
@@ -68,7 +69,10 @@ fun HedvigFullScreenCenterAlignedLinearProgress(title: String, modifier: Modifie
           Spacer(Modifier.height(16.dp))
           LinearProgressIndicator(
             strokeCap = StrokeCap.Round,
-            modifier = Modifier.defaultMinSize(247.dp),
+            modifier = Modifier
+              .fillMaxWidth(2 / 3f)
+              .requiredWidthIn(min = 64.dp)
+              .widthIn(max = 240.dp)
           )
         }
       }
@@ -76,14 +80,35 @@ fun HedvigFullScreenCenterAlignedLinearProgress(title: String, modifier: Modifie
   }
 }
 
-@HedvigPreview
+@Preview(device = "spec:width=1100px,height=300px,dpi=440")
+@Preview(device = "spec:width=700px,height=300px,dpi=440")
+@Preview(device = "spec:width=500px,height=300px,dpi=440")
+@Preview(device = "spec:width=350px,height=300px,dpi=440")
+@Preview(device = "spec:width=200px,height=300px,dpi=440")
+@Preview(device = "spec:width=100px,height=300px,dpi=440")
 @Composable
 private fun PreviewHedvigFullScreenCenterAlignedLoadingIndicator() {
   HedvigTheme {
     Surface(color = MaterialTheme.colorScheme.background) {
-      HedvigFullScreenCenterAlignedLinearProgress(
-        title = "Terminating...",
-      )
+      Box {
+        HedvigFullScreenCenterAlignedLinearProgress(title = "Terminating...")
+        Box(
+          Modifier
+            .height(2.dp)
+            .width(240.dp)
+            .offset(y = 12.dp)
+            .background(Color.Red)
+            .align(Alignment.Center),
+        )
+        Box(
+          Modifier
+            .height(2.dp)
+            .width(64.dp)
+            .offset(y = 6.dp)
+            .background(Color.Red)
+            .align(Alignment.Center),
+        )
+      }
     }
   }
 }
