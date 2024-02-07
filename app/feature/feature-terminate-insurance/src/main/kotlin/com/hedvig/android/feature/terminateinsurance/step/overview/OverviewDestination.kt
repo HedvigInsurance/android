df84b2164a3b491d05cdee619a94d7027b14c44c
stylianosgakis <stylianos@hedvig.com>
2024-02-08 00:41:52 +0100

    Create TerminationOverviewScreenScaffold as a common screen
    Most termination related screens can just use this one composable to
    render their information

--- app/feature/feature-terminate-insurance/src/main/kotlin/com/hedvig/android/feature/terminateinsurance/step/overview/OverviewDestination.kt
+++ app/feature/feature-terminate-insurance/src/main/kotlin/com/hedvig/android/feature/terminateinsurance/step/overview/OverviewDestination.kt
@@ -1,41 +1,25 @@
 package com.hedvig.android.feature.terminateinsurance.step.overview
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.ImageLoader
-import com.hedvig.android.core.designsystem.component.button.HedvigContainedButton
-import com.hedvig.android.core.designsystem.component.button.HedvigTextButton
 import com.hedvig.android.core.designsystem.component.progress.HedvigFullScreenCenterAlignedLinearProgress
 import com.hedvig.android.core.designsystem.preview.HedvigPreview
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
 import com.hedvig.android.core.ui.preview.BooleanCollectionPreviewParameterProvider
 import com.hedvig.android.core.ui.preview.rememberPreviewImageLoader
-import com.hedvig.android.core.ui.scaffold.HedvigScaffold
 import com.hedvig.android.data.contract.ContractGroup
 import com.hedvig.android.data.contract.android.toDrawableRes
 import com.hedvig.android.feature.terminateinsurance.data.TerminateInsuranceStep
-import com.hedvig.android.feature.terminateinsurance.ui.TerminationSummary
+import com.hedvig.android.feature.terminateinsurance.ui.TerminationOverviewScreenContent
+import com.hedvig.android.feature.terminateinsurance.ui.TerminationOverviewScreenScaffold
 import hedvig.resources.R
 import kotlinx.datetime.LocalDate
 
@@ -55,7 +39,7 @@ internal fun OverviewDestination(
     navigateToNextStep(nextStep)
   }
 
-  OverViewScreen(
+  OverviewScreen(
     uiState = uiState,
     onContinue = onContinue,
     navigateUp = navigateUp,
@@ -65,58 +49,35 @@ internal fun OverviewDestination(
 }
 
 @Composable
-private fun OverViewScreen(
+private fun OverviewScreen(
   uiState: OverviewUiState,
   onContinue: () -> Unit,
   navigateUp: () -> Unit,
   navigateBack: () -> Unit,
   imageLoader: ImageLoader,
 ) {
-  val topAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
-  Column(
-    Modifier
-      .fillMaxSize()
-      .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom)),
-  ) {
-    val isSubmittingTerminationOrNavigatingForward = uiState.isSubmittingContractTermination || uiState.nextStep != null
-    if (isSubmittingTerminationOrNavigatingForward) {
-      HedvigFullScreenCenterAlignedLinearProgress(
-        title = stringResource(id = R.string.TERMINATE_CONTRACT_TERMINATING_PROGRESS),
+  val isSubmittingTerminationOrNavigatingForward = uiState.isSubmittingContractTermination || uiState.nextStep != null
+  if (isSubmittingTerminationOrNavigatingForward) {
+    HedvigFullScreenCenterAlignedLinearProgress(
+      title = stringResource(id = R.string.TERMINATE_CONTRACT_TERMINATING_PROGRESS),
+    )
+  } else {
+    TerminationOverviewScreenScaffold(
+      navigateUp = navigateUp,
+      topAppBarText = stringResource(R.string.TERMINATION_CONFIRM_BUTTON),
+    ) {
+      TerminationOverviewScreenContent(
+        terminationDate = uiState.terminationDate,
+        insuranceDisplayName = uiState.insuranceDisplayName,
+        exposureName = uiState.exposureName,
+        insuranceCardPainter = painterResource(uiState.contractGroup.toDrawableRes()),
+        imageLoader = imageLoader,
+        infoText = null,
+        continueButtonText = stringResource(R.string.TERMINATION_CONFIRM_BUTTON),
+        onContinueButtonClick = onContinue,
+        cancelButtonText = stringResource(R.string.general_cancel_button),
+        onCancelButtonClick = navigateBack,
       )
-    } else {
-      HedvigScaffold(
-        navigateUp = navigateUp,
-        topAppBarScrollBehavior = topAppBarScrollBehavior,
-        topAppBarText = stringResource(R.string.TERMINATION_CONFIRM_BUTTON),
-      ) {
-        Spacer(Modifier.height(8.dp))
-        TerminationSummary(
-          selectedDate = uiState.terminationDate,
-          insuranceDisplayName = uiState.insuranceDisplayName,
-          exposureName = uiState.exposureName,
-          insuranceCardPainter = painterResource(uiState.contractGroup.toDrawableRes()),
-          imageLoader = imageLoader,
-          modifier = Modifier.padding(horizontal = 16.dp),
-        )
-        Spacer(Modifier.height(32.dp))
-        Spacer(Modifier.weight(1f))
-        HedvigContainedButton(
-          text = stringResource(id = R.string.TERMINATION_CONFIRM_BUTTON),
-          colors = ButtonDefaults.buttonColors(
-            containerColor = MaterialTheme.colorScheme.error,
-            contentColor = MaterialTheme.colorScheme.onError,
-          ),
-          onClick = onContinue,
-          modifier = Modifier.padding(horizontal = 16.dp),
-        )
-        Spacer(Modifier.height(8.dp))
-        HedvigTextButton(
-          text = stringResource(id = R.string.general_cancel_button),
-          onClick = navigateBack,
-          modifier = Modifier.padding(horizontal = 16.dp),
-        )
-        Spacer(Modifier.height(16.dp))
-      }
     }
   }
 }
@@ -128,7 +89,7 @@ private fun OverviewScreenPreview(
 ) {
   HedvigTheme {
     Surface(color = MaterialTheme.colorScheme.background) {
-      OverViewScreen(
+      OverviewScreen(
         uiState = OverviewUiState(
           terminationDate = LocalDate.fromEpochDays(300),
           insuranceDisplayName = "Test insurance",
