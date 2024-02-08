c7a9e33fb6cc61bd040a282670897728109a7cba
stylianosgakis <stylianos@hedvig.com>
2024-02-08 14:45:59 +0100

    Make TerminationSuccessDestination use TerminationOverviewScreenScaffold

--- app/feature/feature-terminate-insurance/src/main/kotlin/com/hedvig/android/feature/terminateinsurance/step/terminationsuccess/TerminationSuccessDestination.kt
+++ app/feature/feature-terminate-insurance/src/main/kotlin/com/hedvig/android/feature/terminateinsurance/step/terminationsuccess/TerminationSuccessDestination.kt
@@ -1,112 +1,70 @@
 package com.hedvig.android.feature.terminateinsurance.step.terminationsuccess
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import coil.ImageLoader
-import com.hedvig.android.core.designsystem.component.button.HedvigTextButton
 import com.hedvig.android.core.designsystem.preview.HedvigPreview
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
-import com.hedvig.android.core.ui.appbar.m3.TopAppBarWithClose
+import com.hedvig.android.core.ui.preview.BooleanCollectionPreviewParameterProvider
 import com.hedvig.android.core.ui.preview.rememberPreviewImageLoader
-import com.hedvig.android.feature.terminateinsurance.ui.TerminationSummary
+import com.hedvig.android.core.ui.rememberHedvigDateTimeFormatter
+import com.hedvig.android.feature.terminateinsurance.ui.TerminationOverviewScreenContent
+import com.hedvig.android.feature.terminateinsurance.ui.TerminationOverviewScreenScaffold
 import hedvig.resources.R
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.toJavaLocalDate
 
 @Composable
 internal fun TerminationSuccessDestination(
-  selectedDate: LocalDate?,
+  terminationDate: LocalDate?,
   insuranceDisplayName: String,
   exposureName: String,
-  finish: () -> Unit,
+  navigateUp: () -> Unit,
   imageLoader: ImageLoader,
   onSurveyClicked: () -> Unit,
 ) {
-  val topAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
-
-  Column(
-    Modifier
-      .fillMaxSize()
-      .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom)),
+  TerminationOverviewScreenScaffold(
+    navigateUp = navigateUp,
+    topAppBarText = stringResource(R.string.TERMINATE_CONTRACT_CONFIRMATION_TITLE),
   ) {
-    TopAppBarWithClose(
-      onClick = finish,
-      title = stringResource(R.string.TERMINATE_CONTRACT_CONFIRMATION_TITLE),
-      scrollBehavior = topAppBarScrollBehavior,
+    TerminationOverviewScreenContent(
+      terminationDate = terminationDate,
+      insuranceDisplayName = insuranceDisplayName,
+      exposureName = exposureName,
+      insuranceCardPainter = ColorPainter(MaterialTheme.colorScheme.onSurfaceVariant),
+      imageLoader = imageLoader,
+      infoText = terminationDate?.let {
+        stringResource(
+          R.string.TERMINATE_CONTRACT_CONFIRMATION_INFO_TEXT,
+          rememberHedvigDateTimeFormatter().format(terminationDate.toJavaLocalDate()),
+        )
+      },
+      containedButtonText = null,
+      onContainedButtonClick = null,
+      textButtonText = stringResource(R.string.TERMINATION_OPEN_SURVEY_LABEL),
+      onTextButtonClick = onSurveyClicked,
     )
-    Column(
-      Modifier
-        .fillMaxSize()
-        .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
-        .verticalScroll(rememberScrollState())
-        .padding(horizontal = 16.dp)
-        .padding(top = 8.dp, bottom = 32.dp)
-        .windowInsetsPadding(WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal)),
-    ) {
-      TerminationSummary(
-        selectedDate = selectedDate,
-        insuranceDisplayName = insuranceDisplayName,
-        exposureName = exposureName,
-        insuranceCardPainter = ColorPainter(Color.Black.copy(alpha = 0.7f)),
-        imageLoader = imageLoader,
-      )
-
-      Spacer(Modifier.height(32.dp))
-      Spacer(Modifier.weight(1f))
-      HedvigTextButton(
-        text = stringResource(id = R.string.TERMINATION_OPEN_SURVEY_LABEL),
-        onClick = onSurveyClicked,
-      )
-    }
   }
 }
 
 @HedvigPreview
 @Composable
-private fun PreviewTerminationSuccessScreen() {
+private fun PreviewTerminationSuccessScreen(
+  @PreviewParameter(BooleanCollectionPreviewParameterProvider::class) withTerminationDate: Boolean,
+) {
   HedvigTheme {
     Surface(color = MaterialTheme.colorScheme.background) {
       TerminationSuccessDestination(
-        selectedDate = LocalDate.fromEpochDays(300),
+        terminationDate = if (withTerminationDate) {
+          LocalDate.fromEpochDays(300)
+        } else null,
         insuranceDisplayName = "Test",
         exposureName = "123",
-        finish = {},
-        imageLoader = rememberPreviewImageLoader(),
-        onSurveyClicked = {},
-      )
-    }
-  }
-}
-
-@HedvigPreview
-@Composable
-private fun PreviewTerminationSuccessScreenWithoutTerminationDate() {
-  HedvigTheme {
-    Surface(color = MaterialTheme.colorScheme.background) {
-      TerminationSuccessDestination(
-        selectedDate = LocalDate.fromEpochDays(300),
-        insuranceDisplayName = "Test",
-        exposureName = "123",
-        finish = {},
+        navigateUp = {},
         imageLoader = rememberPreviewImageLoader(),
         onSurveyClicked = {},
       )
