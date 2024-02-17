9ef3448e342d7907c420bb4cb395ed9855e3803d
stylianosgakis <stylianos@hedvig.com>
2024-02-17 19:15:20 +0100

    Create delete account feature module

---
+++ app/feature/feature-delete-account/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+  id("hedvig.android.ktlint")
+  id("hedvig.android.library")
+  id("hedvig.android.library.compose")
+  alias(libs.plugins.apollo)
+  alias(libs.plugins.serialization)
+  alias(libs.plugins.squareSortDependencies)
+}
+
+dependencies {
+  apolloMetadata(projects.apolloOctopusPublic)
+
+  implementation(libs.androidx.datastore.core)
+  implementation(libs.androidx.datastore.preferencesCore)
+  implementation(libs.androidx.lifecycle.compose)
+  implementation(libs.androidx.navigation.common)
+  implementation(libs.androidx.navigation.compose)
+  implementation(libs.androidx.navigation.runtime)
+  implementation(libs.apollo.normalizedCache)
+  implementation(libs.arrow.core)
+  implementation(libs.coroutines.core)
+  implementation(libs.kiwi.navigationCompose)
+  implementation(libs.koin.compose)
+  implementation(libs.koin.core)
+  implementation(libs.kotlinx.serialization.core)
+  implementation(projects.apolloCore)
+  implementation(projects.apolloOctopusPublic)
+  implementation(projects.authCorePublic)
+  implementation(projects.coreCommonPublic)
+  implementation(projects.coreDesignSystem)
+  implementation(projects.coreResources)
+  implementation(projects.coreUi)
+  implementation(projects.moleculeAndroid)
+  implementation(projects.moleculePublic)
+  implementation(projects.navigationComposeTyped)
+  implementation(projects.navigationCore)
+}
+
+apollo {
+  service("octopus") {
+    packageName.set("octopus")
+    generateDataBuilders.set(true)
+  }
+}
