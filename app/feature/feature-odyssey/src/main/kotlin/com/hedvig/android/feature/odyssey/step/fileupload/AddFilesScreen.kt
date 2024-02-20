f1fa307658662b5c85112b6cbcc66e68365a0c01
stylianosgakis <stylianos@hedvig.com>
2024-02-20 16:35:40 +0100

    Adjust claim flow's AddFilesScreen to the new FilesLazyVerticalGrid
    Makes it work inside the already scrollable ClaimScaffold by making it
    take `weight(1f)`.

--- app/feature/feature-odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/step/fileupload/AddFilesScreen.kt
+++ app/feature/feature-odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/step/fileupload/AddFilesScreen.kt
@@ -11,16 +11,22 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.ImageLoader
+import com.hedvig.android.core.designsystem.component.button.HedvigContainedButton
+import com.hedvig.android.core.designsystem.component.button.HedvigSecondaryContainedButton
+import com.hedvig.android.core.designsystem.preview.HedvigMultiScreenPreview
 import com.hedvig.android.core.designsystem.preview.HedvigPreview
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
-import com.hedvig.android.core.ui.FilesGridScreen
+import com.hedvig.android.core.ui.FilesLazyVerticalGrid
 import com.hedvig.android.core.ui.preview.calculateForPreview
 import com.hedvig.android.core.ui.preview.rememberPreviewImageLoader
 import com.hedvig.android.core.ui.snackbar.ErrorSnackbarState
 import com.hedvig.android.core.uidata.UiFile
 import com.hedvig.android.feature.odyssey.ui.ClaimFlowScaffold
+import hedvig.resources.R
 
 @Composable
 internal fun AddFilesScreen(
@@ -38,19 +44,42 @@ internal fun AddFilesScreen(
     windowSizeClass = windowSizeClass,
     navigateUp = navigateUp,
     closeClaimFlow = closeClaimFlow,
+    topAppBarText = stringResource(R.string.claim_status_detail_uploaded_files_info_title),
     errorSnackbarState = ErrorSnackbarState(
       error = uiState.errorMessage != null,
       showedError = showedError,
     ),
-    scrollable = false,
   ) { sideSpacingModifier ->
-    Spacer(Modifier.height(16.dp))
-    FilesGridScreen(
+    FilesLazyVerticalGrid(
       files = (uiState.uploadedFiles + uiState.localFiles),
-      onContinue = onContinue,
-      onAddMoreFiles = onAddMoreFiles,
       onRemoveFile = onRemoveFile,
       imageLoader = imageLoader,
+      modifier = sideSpacingModifier
+        .weight(1f)
+        .layout { measurable, constraints ->
+          // Add 8.dp to the min and max width to account for the internal padding coming from FilesLazyVerticalGrid
+          // This is necessary as sideSpacingModifier does not really let us go outside of its bounds easily otherwise
+          val placeable = measurable.measure(
+            constraints.copy(
+              minWidth = constraints.minWidth.plus(16.dp.roundToPx()),
+              maxWidth = constraints.maxWidth.plus(16.dp.roundToPx()),
+            ),
+          )
+          layout(placeable.width, placeable.height) {
+            placeable.place(0, 0)
+          }
+        },
+    )
+    Spacer(Modifier.height(8.dp))
+    HedvigSecondaryContainedButton(
+      text = stringResource(R.string.claim_status_detail_add_more_files),
+      onClick = onAddMoreFiles,
+      modifier = sideSpacingModifier,
+    )
+    Spacer(Modifier.height(8.dp))
+    HedvigContainedButton(
+      text = stringResource(R.string.general_continue_button),
+      onClick = onContinue,
       isLoading = uiState.isLoading,
       modifier = sideSpacingModifier,
     )
@@ -59,39 +88,21 @@ internal fun AddFilesScreen(
   }
 }
 
-@HedvigPreview
+@HedvigMultiScreenPreview
 @Composable
 private fun AddFilesScreenPreview() {
   HedvigTheme {
     Surface {
       AddFilesScreen(
         uiState = FileUploadUiState(
-          localFiles = listOf(
+          localFiles = List(25) {
             UiFile(
-              "123",
+              "$it",
               "",
               "",
-              "123123123123123123123",
-            ),
-            UiFile(
-              "123",
-              "",
-              "",
-              "1234",
-            ),
-            UiFile(
-              "123",
-              "",
-              "",
-              "1232",
-            ),
-            UiFile(
-              "123",
-              "",
-              "",
-              "1123",
-            ),
-          ),
+              "$it",
+            )
+          },
         ),
         windowSizeClass = WindowSizeClass.calculateForPreview(),
         onContinue = {},
