e9c420c2b5e30e08101d0ce037e89ce8b26f121e
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-02-29 16:35:36 +0100

    add destinations

---
+++ app/feature/feature-odyssey/src/main/kotlin/com/hedvig/android/feature/odyssey/step/informdeflect/DeflectCarOtherDamageDestination.kt
@@ -0,0 +1,8 @@
+package com.hedvig.android.feature.odyssey.step.informdeflect
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun DeflectCarOtherDamageDestination() {
+  //TODO
+}
