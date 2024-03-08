363d8ea15be222d688d1fb018339434f479931cb
stylianosgakis <stylianos@hedvig.com>
2024-03-08 15:52:01 +0100

    Run ktlint

--- app/core/core-app-review/src/main/kotlin/com/hedvig/android/core/appreview/SelfServiceCompletedEventManager.kt
+++ app/core/core-app-review/src/main/kotlin/com/hedvig/android/core/appreview/SelfServiceCompletedEventManager.kt
@@ -2,13 +2,13 @@ package com.hedvig.android.core.appreview
 
 interface SelfServiceCompletedEventManager {
   suspend fun completedSelfServiceSuccessfully()
+
   suspend fun resetSelfServiceCompletions()
 }
 
 internal class SelfServiceCompletedEventManagerImpl(
   private val dataStore: SelfServiceCompletedEventStore,
 ) : SelfServiceCompletedEventManager {
-
   override suspend fun completedSelfServiceSuccessfully() {
     dataStore.onSelfServiceCompleted()
   }
