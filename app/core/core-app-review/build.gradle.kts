f9d7287b1f8b475fa56078bd8303430bb97342e2
stylianosgakis <stylianos@hedvig.com>
2024-03-08 15:36:01 +0100

    Simplify core-app-review module to only be a JVM library

--- app/core/core-app-review/build.gradle.kts
+++ app/core/core-app-review/build.gradle.kts
@@ -1,14 +1,11 @@
 plugins {
   id("hedvig.android.ktlint")
-  id("hedvig.android.library")
+  id("hedvig.kotlin.library")
   alias(libs.plugins.squareSortDependencies)
 }
 dependencies {
-  implementation(libs.androidx.other.constraintLayout)
   implementation(libs.androidx.datastore.core)
-  implementation(libs.koin.core)
-  implementation(libs.koin.android)
   implementation(libs.androidx.datastore.preferencesCore)
-  implementation(libs.androidx.lifecycle.viewModel)
-  implementation(libs.playReview)
+  implementation(libs.coroutines.core)
+  implementation(libs.koin.core)
 }
