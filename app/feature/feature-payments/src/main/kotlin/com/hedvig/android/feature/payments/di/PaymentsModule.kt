22e2125ff25cf5674006323735fc198edd0d91f6
stylianosgakis <stylianos@hedvig.com>
2024-03-14 16:53:29 +0100

    Add a todo for co-insured no data error
    Co-authored-by: mariiapanasetskaia <mariia.panasetskaia@hedvig.com>

--- app/feature/feature-payments/src/main/kotlin/com/hedvig/android/feature/payments/di/PaymentsModule.kt
+++ app/feature/feature-payments/src/main/kotlin/com/hedvig/android/feature/payments/di/PaymentsModule.kt
@@ -2,30 +2,70 @@ package com.hedvig.android.feature.payments.di
 
 import com.apollographql.apollo3.ApolloClient
 import com.hedvig.android.apollo.NetworkCacheManager
-import com.hedvig.android.feature.payments.data.AddDiscountUseCase
-import com.hedvig.android.feature.payments.data.AddDiscountUseCaseImpl
-import com.hedvig.android.feature.payments.data.GetUpcomingPaymentUseCase
-import com.hedvig.android.feature.payments.data.GetUpcomingPaymentUseCaseImpl
+import com.hedvig.android.core.demomode.DemoManager
+import com.hedvig.android.data.paying.member.GetOnlyHasNonPayingContractsUseCaseProvider
 import com.hedvig.android.feature.payments.overview.PaymentOverviewViewModel
+import com.hedvig.android.feature.payments.overview.data.AddDiscountUseCase
+import com.hedvig.android.feature.payments.overview.data.AddDiscountUseCaseImpl
+import com.hedvig.android.feature.payments.overview.data.GetForeverInformationUseCase
+import com.hedvig.android.feature.payments.overview.data.GetForeverInformationUseCaseImpl
+import com.hedvig.android.feature.payments.overview.data.GetPaymentOverviewDataUseCaseDemo
+import com.hedvig.android.feature.payments.overview.data.GetPaymentOverviewDataUseCaseImpl
+import com.hedvig.android.feature.payments.overview.data.GetPaymentOverviewDataUseCaseProvider
+import com.hedvig.android.feature.payments.overview.data.GetUpcomingPaymentUseCase
+import com.hedvig.android.feature.payments.overview.data.GetUpcomingPaymentUseCaseImpl
+import kotlinx.datetime.Clock
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val paymentsModule = module {
-  single<GetUpcomingPaymentUseCase> {
-    GetUpcomingPaymentUseCaseImpl(get<ApolloClient>())
-  }
-
   single<AddDiscountUseCase> {
     AddDiscountUseCaseImpl(
       get<ApolloClient>(),
       get<NetworkCacheManager>(),
     )
   }
+  single<GetForeverInformationUseCase> {
+    GetForeverInformationUseCaseImpl(
+      get<ApolloClient>(),
+    )
+  }
+  single<GetUpcomingPaymentUseCase> {
+    GetUpcomingPaymentUseCaseImpl(
+      get<ApolloClient>(),
+      get<Clock>(),
+    )
+  }
+
+  provideGetPaymentOverviewDataUseCase()
 
   viewModel<PaymentOverviewViewModel> {
     PaymentOverviewViewModel(
-      get<GetUpcomingPaymentUseCase>(),
+      get<GetPaymentOverviewDataUseCaseProvider>(),
       get<AddDiscountUseCase>(),
+    )
+  }
+}
+
+private fun Module.provideGetPaymentOverviewDataUseCase() {
+  single<GetPaymentOverviewDataUseCaseImpl> {
+    GetPaymentOverviewDataUseCaseImpl(
+      getUpcomingPaymentUseCase = get<GetUpcomingPaymentUseCase>(),
+      getForeverInformationUseCase = get<GetForeverInformationUseCase>(),
+      getOnlyHasNonPayingContractsUseCase = get<GetOnlyHasNonPayingContractsUseCaseProvider>().prodImpl,
+    )
+  }
+  single<GetPaymentOverviewDataUseCaseDemo> {
+    GetPaymentOverviewDataUseCaseDemo(
+      clock = get<Clock>(),
+    )
+  }
+  single<GetPaymentOverviewDataUseCaseProvider> {
+    GetPaymentOverviewDataUseCaseProvider(
+      demoManager = get<DemoManager>(),
+      demoImpl = get<GetPaymentOverviewDataUseCaseDemo>(),
+      prodImpl = get<GetPaymentOverviewDataUseCaseImpl>(),
     )
   }
 }
