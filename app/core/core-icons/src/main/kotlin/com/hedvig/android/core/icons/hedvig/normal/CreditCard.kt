f6185e729b20ced1c0ec24f73f30c28918d1906a
stylianosgakis <stylianos@hedvig.com>
2024-03-20 22:36:28 +0100

    Add back the old payments icon as CreditCard

---
+++ app/core/core-icons/src/main/kotlin/com/hedvig/android/core/icons/hedvig/normal/CreditCard.kt
@@ -0,0 +1,74 @@
+package com.hedvig.android.core.icons.hedvig.normal
+
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.materialPath
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.hedvig.android.core.icons.HedvigIcons
+
+@Suppress("UnusedReceiverParameter")
+public val HedvigIcons.CreditCard: ImageVector
+  get() {
+    if (_creditCard != null) {
+      return _creditCard!!
+    }
+    _creditCard = materialIcon(name = "Credit Card") {
+      materialPath(pathFillType = PathFillType.EvenOdd) {
+        moveTo(1.25f, 15.052f)
+        verticalLineTo(8.948f)
+        curveTo(1.2499f, 8.0495f, 1.2499f, 7.3003f, 1.3299f, 6.7055f)
+        curveTo(1.4143f, 6.0777f, 1.6f, 5.5109f, 2.0554f, 5.0554f)
+        curveTo(2.5109f, 4.6f, 3.0777f, 4.4143f, 3.7055f, 4.3299f)
+        curveTo(4.3003f, 4.2499f, 5.0495f, 4.25f, 5.948f, 4.25f)
+        horizontalLineTo(18.0515f)
+        curveTo(18.95f, 4.25f, 19.6992f, 4.2499f, 20.294f, 4.3299f)
+        curveTo(20.9218f, 4.4143f, 21.4886f, 4.6f, 21.944f, 5.0554f)
+        curveTo(22.3995f, 5.5109f, 22.5852f, 6.0777f, 22.6696f, 6.7055f)
+        curveTo(22.7495f, 7.3003f, 22.7495f, 8.0495f, 22.7495f, 8.948f)
+        verticalLineTo(15.052f)
+        curveTo(22.7495f, 15.9505f, 22.7495f, 16.6997f, 22.6696f, 17.2945f)
+        curveTo(22.5852f, 17.9223f, 22.3995f, 18.4891f, 21.944f, 18.9445f)
+        curveTo(21.4886f, 19.4f, 20.9218f, 19.5857f, 20.294f, 19.6701f)
+        curveTo(19.6992f, 19.7501f, 18.95f, 19.75f, 18.0515f, 19.75f)
+        horizontalLineTo(5.948f)
+        curveTo(5.0495f, 19.75f, 4.3003f, 19.7501f, 3.7055f, 19.6701f)
+        curveTo(3.0777f, 19.5857f, 2.5109f, 19.4f, 2.0554f, 18.9445f)
+        curveTo(1.6f, 18.4891f, 1.4143f, 17.9223f, 1.3299f, 17.2945f)
+        curveTo(1.2499f, 16.6997f, 1.2499f, 15.9505f, 1.25f, 15.052f)
+        close()
+        moveTo(2.8165f, 17.0946f)
+        curveTo(2.8786f, 17.5561f, 2.9858f, 17.7536f, 3.1161f, 17.8839f)
+        curveTo(3.2464f, 18.0142f, 3.4439f, 18.1214f, 3.9054f, 18.1835f)
+        curveTo(4.3884f, 18.2484f, 5.036f, 18.25f, 6.0f, 18.25f)
+        horizontalLineTo(17.9995f)
+        curveTo(18.9635f, 18.25f, 19.6111f, 18.2484f, 20.0941f, 18.1835f)
+        curveTo(20.5556f, 18.1214f, 20.7531f, 18.0142f, 20.8834f, 17.8839f)
+        curveTo(21.0137f, 17.7536f, 21.1209f, 17.5561f, 21.183f, 17.0946f)
+        curveTo(21.2479f, 16.6116f, 21.2495f, 15.964f, 21.2495f, 15.0f)
+        verticalLineTo(9.0f)
+        curveTo(21.2495f, 8.036f, 21.2479f, 7.3884f, 21.183f, 6.9054f)
+        curveTo(21.1209f, 6.4439f, 21.0137f, 6.2464f, 20.8834f, 6.1161f)
+        curveTo(20.7531f, 5.9858f, 20.5556f, 5.8786f, 20.0941f, 5.8165f)
+        curveTo(19.6111f, 5.7516f, 18.9635f, 5.75f, 17.9995f, 5.75f)
+        horizontalLineTo(6.0f)
+        curveTo(5.036f, 5.75f, 4.3884f, 5.7516f, 3.9054f, 5.8165f)
+        curveTo(3.4439f, 5.8786f, 3.2464f, 5.9858f, 3.1161f, 6.1161f)
+        curveTo(2.9858f, 6.2464f, 2.8786f, 6.4439f, 2.8165f, 6.9054f)
+        curveTo(2.7516f, 7.3884f, 2.75f, 8.036f, 2.75f, 9.0f)
+        verticalLineTo(15.0f)
+        curveTo(2.75f, 15.964f, 2.7516f, 16.6116f, 2.8165f, 17.0946f)
+        close()
+      }
+      materialPath(pathFillType = PathFillType.NonZero) {
+        moveTo(2.0005f, 7.6376f)
+        lineTo(22.0f, 7.6376f)
+        verticalLineTo(10.9094f)
+        lineTo(2.0005f, 10.9094f)
+        lineTo(2.0005f, 7.6376f)
+        close()
+      }
+    }
+    return _creditCard!!
+  }
+
+private var _creditCard: ImageVector? = null
