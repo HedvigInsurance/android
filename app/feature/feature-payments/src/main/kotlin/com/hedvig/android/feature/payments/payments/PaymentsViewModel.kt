adafda93456b993ab7cbbc4ae391a63ff27191b5
stylianosgakis <stylianos@hedvig.com>
2024-03-21 17:33:27 +0100

    Remove forgotten 0.seconds timeout on ViewModel

--- app/feature/feature-payments/src/main/kotlin/com/hedvig/android/feature/payments/payments/PaymentsViewModel.kt
+++ app/feature/feature-payments/src/main/kotlin/com/hedvig/android/feature/payments/payments/PaymentsViewModel.kt
@@ -3,9 +3,6 @@ package com.hedvig.android.feature.payments.payments
 import com.hedvig.android.core.demomode.Provider
 import com.hedvig.android.feature.payments.overview.data.GetPaymentOverviewDataUseCase
 import com.hedvig.android.molecule.android.MoleculeViewModel
-import kotlin.time.Duration.Companion.seconds
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.WhileSubscribed
 
 internal class PaymentsViewModel(
   getPaymentOverviewDataUseCase: Provider<GetPaymentOverviewDataUseCase>,
@@ -14,5 +11,4 @@ internal class PaymentsViewModel(
     PaymentsPresenter(
       getPaymentOverviewDataUseCase = getPaymentOverviewDataUseCase,
     ),
-    SharingStarted.WhileSubscribed(0.seconds),
   )
