7bdb6441e2f76a0ef02b2e6bca82f8b2dd32f483
stylianosgakis <stylianos@hedvig.com>
2024-03-21 10:29:59 +0100

    Make Payments screen continue rendering the existing data on back
    Fix clearing the data on refresh when the presenter starts presenting
    Remove unnecessary events and types that were only used in other screens
    We still keep the `contentForOtherScreens` because of the way these
    screens are structured for now. But in the future each screen should
    fetch their own information instead.
    Map the raw PaymentOverviewData into a screen-specific model to make the
    code in the UI much simpler to go through rather than checking for all
    the deep parameters inside a big object making the UI code harder to
    read.

--- app/feature/feature-payments/src/main/kotlin/com/hedvig/android/feature/payments/overview/PaymentOverviewPresenter.kt
+++ app/feature/feature-payments/src/main/kotlin/com/hedvig/android/feature/payments/overview/PaymentOverviewPresenter.kt
@@ -8,116 +8,138 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.hedvig.android.core.demomode.Provider
+import com.hedvig.android.core.uidata.UiMoney
+import com.hedvig.android.feature.payments.data.Discount
+import com.hedvig.android.feature.payments.data.MemberCharge
+import com.hedvig.android.feature.payments.data.PaymentConnection
 import com.hedvig.android.feature.payments.data.PaymentOverview
-import com.hedvig.android.feature.payments.overview.data.AddDiscountUseCase
-import com.hedvig.android.feature.payments.overview.data.ForeverInformation
 import com.hedvig.android.feature.payments.overview.data.GetPaymentOverviewDataUseCase
 import com.hedvig.android.molecule.public.MoleculePresenter
 import com.hedvig.android.molecule.public.MoleculePresenterScope
+import kotlinx.datetime.LocalDate
 
 internal class PaymentOverviewPresenter(
   val getPaymentOverviewDataUseCase: Provider<GetPaymentOverviewDataUseCase>,
-  val addDiscountUseCase: AddDiscountUseCase,
-) : MoleculePresenter<PaymentEvent, PaymentOverviewUiState> {
+) : MoleculePresenter<PaymentsEvent, PaymentOverviewUiState> {
   @Composable
-  override fun MoleculePresenterScope<PaymentEvent>.present(lastState: PaymentOverviewUiState): PaymentOverviewUiState {
+  override fun MoleculePresenterScope<PaymentsEvent>.present(
+    lastState: PaymentOverviewUiState,
+  ): PaymentOverviewUiState {
     var paymentUiState: PaymentOverviewUiState by remember { mutableStateOf(lastState) }
     var loadIteration by remember { mutableIntStateOf(0) }
-    var addedDiscount by remember { mutableStateOf<String?>(null) }
 
     CollectEvents { event ->
       when (event) {
-        PaymentEvent.Retry -> loadIteration++
-        is PaymentEvent.OnSubmitDiscountCode -> addedDiscount = event.code
-        PaymentEvent.DismissBottomSheet -> {
-          addedDiscount = null
-          paymentUiState = paymentUiState.copy(
-            showAddDiscountBottomSheet = false,
-            discountError = null,
-          )
-        }
-
-        PaymentEvent.ShowBottomSheet -> {
-          paymentUiState = paymentUiState.copy(
-            showAddDiscountBottomSheet = true,
-          )
-        }
+        PaymentsEvent.Retry -> loadIteration++
       }
     }
 
     LaunchedEffect(loadIteration) {
-      paymentUiState = if (loadIteration != 0) {
-        paymentUiState.copy(isRetrying = true)
+      val currentPaymentUiState = paymentUiState
+      paymentUiState = if (currentPaymentUiState is PaymentOverviewUiState.Content) {
+        currentPaymentUiState.copy(isLoading = true)
       } else {
-        paymentUiState.copy(isLoadingPaymentOverView = true)
+        PaymentOverviewUiState.Loading
       }
       getPaymentOverviewDataUseCase.provide().invoke().fold(
         ifLeft = {
-          paymentUiState = paymentUiState.copy(
-            error = true,
-            isLoadingPaymentOverView = false,
-            isRetrying = false,
-          )
+          paymentUiState = PaymentOverviewUiState.Error
         },
         ifRight = { paymentOverviewData ->
-          paymentUiState = paymentUiState.copy(
-            paymentOverview = paymentOverviewData.paymentOverview,
-            foreverInformation = paymentOverviewData.foreverInformation,
-            error = null,
-            isLoadingPaymentOverView = false,
-            isRetrying = false,
+          paymentUiState = PaymentOverviewUiState.Content(
+            isLoading = false,
+            upcomingPayment = paymentOverviewData.paymentOverview.memberCharge?.let { memberCharge ->
+              PaymentOverviewUiState.Content.UpcomingPayment(
+                grossAmount = memberCharge.grossAmount,
+                dueDate = memberCharge.dueDate,
+              )
+            },
+            upcomingPaymentInfo = run {
+              val memberCharge = paymentOverviewData.paymentOverview.memberCharge
+              if (memberCharge?.status == MemberCharge.MemberChargeStatus.PENDING) {
+                return@run PaymentOverviewUiState.Content.UpcomingPaymentInfo.InProgress
+              }
+              memberCharge?.failedCharge?.let { failedCharge ->
+                return@run PaymentOverviewUiState.Content.UpcomingPaymentInfo.PaymentFailed(
+                  failedPaymentStartDate = failedCharge.fromDate,
+                  failedPaymentEndDate = failedCharge.toDate,
+                )
+              }
+            },
+            connectedPaymentInfo = run {
+              val paymentConnection = paymentOverviewData.paymentOverview.paymentConnection
+              when (paymentConnection) {
+                is PaymentConnection.Active -> PaymentOverviewUiState.Content.ConnectedPaymentInfo.Connected(
+                  displayName = paymentConnection.displayName,
+                  maskedAccountNumber = paymentConnection.displayValue,
+                )
+
+                PaymentConnection.Pending -> PaymentOverviewUiState.Content.ConnectedPaymentInfo.Pending
+                else -> PaymentOverviewUiState.Content.ConnectedPaymentInfo.NotConnected(
+                  paymentOverviewData.paymentOverview.memberCharge?.dueDate,
+                )
+              }
+            },
+            contentForOtherScreens = PaymentOverviewUiState.Content.ContentForOtherScreens(
+              paymentOverview = paymentOverviewData.paymentOverview,
+              memberCharge = paymentOverviewData.paymentOverview.memberCharge,
+              discountList = paymentOverviewData.paymentOverview.discounts,
+            ),
           )
         },
       )
     }
-
-    LaunchedEffect(addedDiscount) {
-      addedDiscount?.let {
-        paymentUiState = paymentUiState.copy(
-          discountError = null,
-          isAddingDiscount = true,
-        )
-
-        addDiscountUseCase.invoke(it).fold(
-          ifLeft = { errorMessage ->
-            addedDiscount = null
-            paymentUiState = paymentUiState.copy(
-              discountError = errorMessage.message,
-              isAddingDiscount = false,
-            )
-          },
-          ifRight = {
-            addedDiscount = null
-            paymentUiState = paymentUiState.copy(
-              showAddDiscountBottomSheet = false,
-              isAddingDiscount = false,
-            )
-          },
-        )
-      }
-    }
-
     return paymentUiState
   }
 }
 
-internal sealed interface PaymentEvent {
-  data object Retry : PaymentEvent
-
-  data object DismissBottomSheet : PaymentEvent
-
-  data object ShowBottomSheet : PaymentEvent
-
-  data class OnSubmitDiscountCode(val code: String) : PaymentEvent
+internal sealed interface PaymentsEvent {
+  data object Retry : PaymentsEvent
 }
 
-internal data class PaymentOverviewUiState(
-  val foreverInformation: ForeverInformation?,
-  val paymentOverview: PaymentOverview? = null,
-  val discountError: String? = null,
-  val error: Boolean? = null,
-  val showAddDiscountBottomSheet: Boolean = false,
-  val isLoadingPaymentOverView: Boolean = true,
-  val isRetrying: Boolean = false,
-  val isAddingDiscount: Boolean = false,
-)
+internal sealed interface PaymentOverviewUiState {
+  data object Loading : PaymentOverviewUiState
+
+  data object Error : PaymentOverviewUiState
+
+  data class Content(
+    val isLoading: Boolean,
+    val upcomingPayment: UpcomingPayment?,
+    val upcomingPaymentInfo: UpcomingPaymentInfo?,
+    val connectedPaymentInfo: ConnectedPaymentInfo,
+    val contentForOtherScreens: ContentForOtherScreens,
+  ) : PaymentOverviewUiState {
+    data class UpcomingPayment(
+      val grossAmount: UiMoney,
+      val dueDate: LocalDate,
+    )
+
+    sealed interface UpcomingPaymentInfo {
+      data object InProgress : UpcomingPaymentInfo
+
+      data class PaymentFailed(
+        val failedPaymentStartDate: LocalDate,
+        val failedPaymentEndDate: LocalDate,
+      ) : UpcomingPaymentInfo
+    }
+
+    sealed interface ConnectedPaymentInfo {
+      data class NotConnected(
+        val dueDateToConnect: LocalDate?,
+      ) : ConnectedPaymentInfo
+
+      data object Pending : ConnectedPaymentInfo
+
+      data class Connected(
+        val displayName: String,
+        val maskedAccountNumber: String,
+      ) : ConnectedPaymentInfo
+    }
+
+    data class ContentForOtherScreens(
+      val paymentOverview: PaymentOverview,
+      val memberCharge: MemberCharge?,
+      val discountList: List<Discount>,
+    )
+  }
+}
