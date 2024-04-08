ac009e961ffea0e0d7381ae656ad3d92f0e52912
stylianosgakis <stylianos@hedvig.com>
2024-04-08 23:56:30 +0200

    Add getRouteFromBackStack to replace per-screen implementations

---
+++ app/navigation/navigation-compose-typed/src/main/kotlin/com/hedvig/android/navigation/compose/typed/getRouteFromBackStack.kt
@@ -0,0 +1,19 @@
+package com.hedvig.android.navigation.compose.typed
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import com.kiwi.navigationcompose.typed.Destination
+import com.kiwi.navigationcompose.typed.createRoutePattern
+import com.kiwi.navigationcompose.typed.decodeArguments
+import kotlinx.serialization.serializer
+
+@Composable
+inline fun <reified T : Destination> NavController.getRouteFromBackStack(
+  backStackEntry: NavBackStackEntry,
+): T {
+  return remember(this, backStackEntry) {
+    decodeArguments(serializer<T>(), getBackStackEntry(createRoutePattern<T>()))
+  }
+}
