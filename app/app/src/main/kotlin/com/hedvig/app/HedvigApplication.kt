99abd56209edf8b883a76c3f06eaacb5ee24eee6
stylianosgakis <stylianos@hedvig.com>
2024-04-12 16:20:18 +0200

    Move the language migration from Application to LIActivity
    These sources
    https://developer.android.com/guide/topics/resources/app-languages#androidx-impl
    https://cs.android.com/androidx/platform/frameworks/support/+/336a1f14a9e8e6729e833b3022ad3ffa8a3f0433:appcompat/appcompat/src/main/java/androidx/appcompat/app/AppCompatDelegate.java;l=733-734
    
    Talk a bit more about this.
    I suspect us doing it in the Application is the reason it simply didn't
    properly select the language for people who did not go ahead and do a
    language selection manually which has confused me for a very long time.

--- app/app/src/main/kotlin/com/hedvig/app/HedvigApplication.kt
+++ app/app/src/main/kotlin/com/hedvig/app/HedvigApplication.kt
@@ -3,14 +3,10 @@ package com.hedvig.app
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.hedvig.android.app.AppInitializers
-import com.hedvig.android.language.LanguageAndMarketLaunchCheckUseCase
 import com.hedvig.app.feature.tracking.ActivityChangeTracker
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 open class HedvigApplication : Application() {
-  private val languageAndMarketLaunchCheckUseCase: LanguageAndMarketLaunchCheckUseCase by inject()
   private val appInitializers: AppInitializers by inject()
 
   override fun onCreate() {
@@ -18,8 +14,5 @@ open class HedvigApplication : Application() {
     registerActivityLifecycleCallbacks(ActivityChangeTracker())
     AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
     appInitializers.initialize()
-    MainScope().launch {
-      languageAndMarketLaunchCheckUseCase.invoke()
-    }
   }
 }
