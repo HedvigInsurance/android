c9ec8749362b261383a7a1bb040775d1e42a2a82
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-04-16 13:10:21 +0200

    fix click ripple shape

--- app/feature/feature-payments/src/main/kotlin/com/hedvig/android/feature/payments/discounts/DiscountsDestination.kt
+++ app/feature/feature-payments/src/main/kotlin/com/hedvig/android/feature/payments/discounts/DiscountsDestination.kt
@@ -225,11 +225,13 @@ private fun ForeverSection(
             containerColor = MaterialTheme.colorScheme.secondaryContainedButtonContainer,
             contentColor = MaterialTheme.colorScheme.onSecondaryContainedButtonContainer,
           ),
-          modifier = Modifier.wrapContentSize(Alignment.TopStart).clip(MaterialTheme.shapes.squircleSmall).clickable {
+          shape = MaterialTheme.shapes.squircleSmall,
+          onClick = {
             context.getSystemService<ClipboardManager>()?.setPrimaryClip(
               ClipData.newPlainText(null, foreverInformation.foreverCode),
             )
           },
+          modifier = Modifier.wrapContentSize(Alignment.TopStart)
         ) {
           Text(
             text = foreverInformation.foreverCode,
