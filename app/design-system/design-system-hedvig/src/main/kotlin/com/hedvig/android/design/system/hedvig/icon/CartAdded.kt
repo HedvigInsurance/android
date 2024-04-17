95a15db4fa9289fef402b97b8eec013b73fd6bd8
stylianosgakis <stylianos@hedvig.com>
2024-04-17 17:38:57 +0200

    Run ktlintformat

--- app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/icon/CartAdded.kt
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/icon/CartAdded.kt
@@ -101,8 +101,8 @@ private fun IconPreview() {
         imageVector = HedvigIcons.CartAdded,
         contentDescription = null,
         modifier = Modifier
-            .width((24.0).dp)
-            .height((24.0).dp),
+          .width((24.0).dp)
+          .height((24.0).dp),
       )
     }
   }
