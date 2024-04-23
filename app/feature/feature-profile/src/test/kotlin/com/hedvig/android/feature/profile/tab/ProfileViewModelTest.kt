315b386bd338aa061af35f432e53b8fbf11ae224
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-04-23 18:38:04 +0200

    fix tests

--- app/feature/feature-profile/src/test/kotlin/com/hedvig/android/feature/profile/tab/ProfileViewModelTest.kt
+++ app/feature/feature-profile/src/test/kotlin/com/hedvig/android/feature/profile/tab/ProfileViewModelTest.kt
@@ -285,7 +285,8 @@ class ProfileViewModelTest {
           showPaymentScreen = true,
         ),
       )
-      getMemberRemindersUseCase.memberReminders.add(MemberReminders(connectPayment = MemberReminder.ConnectPayment()))
+      getMemberRemindersUseCase.memberReminders.add(MemberReminders(
+        connectPayment = MemberReminder.PaymentReminder.ConnectPayment()))
       runCurrent()
       val connectPayment = (viewModel.data.value as ProfileUiState.Success).memberReminders.connectPayment
       assertThat(connectPayment).isNotNull()
@@ -348,7 +349,7 @@ class ProfileViewModelTest {
 
       getMemberRemindersUseCase.memberReminders.add(
         MemberReminders(
-          connectPayment = MemberReminder.ConnectPayment(),
+          connectPayment = MemberReminder.PaymentReminder.ConnectPayment(),
           enableNotifications = MemberReminder.EnableNotifications(),
         ),
       )
@@ -404,7 +405,7 @@ class ProfileViewModelTest {
       travelCertificateAvailabilityUseCase.apply { turbine.add(Unit.right()) }
       featureManager.featureTurbine.add(Feature.PAYMENT_SCREEN to true)
       getMemberRemindersUseCase.memberReminders.add(
-        MemberReminders(connectPayment = MemberReminder.ConnectPayment(id = testId)),
+        MemberReminders(connectPayment = MemberReminder.PaymentReminder.ConnectPayment(id = testId)),
       )
       runCurrent()
       assertThat(viewModel.data.value).isEqualTo(
@@ -412,7 +413,7 @@ class ProfileViewModelTest {
           euroBonus = EuroBonus("abc"),
           travelCertificateAvailable = true,
           showPaymentScreen = true,
-          memberReminders = MemberReminders(connectPayment = MemberReminder.ConnectPayment(id = testId)),
+          memberReminders = MemberReminders(connectPayment = MemberReminder.PaymentReminder.ConnectPayment(id = testId)),
         ),
       )
       cancelAndIgnoreRemainingEvents()
