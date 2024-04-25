c3274c3095565aa476870068c9a17b9dc5b61927
stylianosgakis <stylianos@hedvig.com>
2024-04-25 12:43:21 +0200

    Add typography tokens

--- micro-apps/design-showcase/build.gradle.kts
+++ micro-apps/design-showcase/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
   implementation(libs.androidx.graphicsShapes)
   implementation(libs.coil.coil)
   implementation(libs.datadog.sdk.core)
+  implementation(libs.timber)
   implementation(libs.zoomable)
   implementation(projects.composeUi)
   implementation(projects.designSystemHedvig)
