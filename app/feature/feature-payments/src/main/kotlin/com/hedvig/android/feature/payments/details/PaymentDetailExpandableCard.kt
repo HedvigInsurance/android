02d0000cd03f22a235d3f59b03acc693d0c7716b
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-04-26 12:09:59 +0200

    remove the ripple

--- app/feature/feature-payments/src/main/kotlin/com/hedvig/android/feature/payments/details/PaymentDetailExpandableCard.kt
+++ app/feature/feature-payments/src/main/kotlin/com/hedvig/android/feature/payments/details/PaymentDetailExpandableCard.kt
@@ -3,6 +3,8 @@ package com.hedvig.android.feature.payments.details
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -20,6 +22,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -54,13 +57,18 @@ internal fun PaymentDetailExpandableCard(
 ) {
   val dateTimeFormatter = rememberHedvigMonthDateTimeFormatter()
   HedvigCard(
-    onClick = onClick,
+    onClick = null,
     modifier = modifier,
   ) {
     Column(
       modifier = Modifier
         .padding(horizontal = 16.dp, vertical = 12.dp)
-        .fillMaxWidth(),
+        .fillMaxWidth()
+        .clickable(
+          interactionSource = remember { MutableInteractionSource() },
+          indication = null,
+          onClick = onClick,
+        ),
     ) {
       HorizontalItemsWithMaximumSpaceTaken(
         startSlot = { Text(displayName) },
