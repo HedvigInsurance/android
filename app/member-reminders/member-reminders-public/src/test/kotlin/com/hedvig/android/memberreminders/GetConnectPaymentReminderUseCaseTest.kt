e924980be0b84f9291ba3ff778fce6bac9c0d6a9
stylianosgakis <stylianos@hedvig.com>
2024-04-30 16:39:40 +0200

    Ensure that non-paying members do not get connect payment reminder

--- app/member-reminders/member-reminders-public/src/test/kotlin/com/hedvig/android/memberreminders/GetConnectPaymentReminderUseCaseTest.kt
+++ app/member-reminders/member-reminders-public/src/test/kotlin/com/hedvig/android/memberreminders/GetConnectPaymentReminderUseCaseTest.kt
@@ -1,5 +1,7 @@
 package com.hedvig.android.memberreminders
 
+import arrow.core.Either
+import arrow.core.right
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isInstanceOf
@@ -8,10 +10,12 @@ import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.annotations.ApolloExperimental
 import com.apollographql.apollo3.testing.enqueueTestNetworkError
 import com.apollographql.apollo3.testing.enqueueTestResponse
-import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import com.hedvig.android.apollo.octopus.test.OctopusFakeResolver
 import com.hedvig.android.apollo.test.TestApolloClientRule
+import com.hedvig.android.core.common.ErrorMessage
 import com.hedvig.android.core.common.test.isLeft
+import com.hedvig.android.core.demomode.Provider
+import com.hedvig.android.data.paying.member.GetOnlyHasNonPayingContractsUseCase
 import com.hedvig.android.logger.TestLogcatLoggingRule
 import kotlinx.coroutines.test.runTest
 import octopus.GetPayinMethodStatusQuery
@@ -20,10 +24,8 @@ import octopus.type.buildMember
 import octopus.type.buildMemberPaymentInformation
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 @OptIn(ApolloExperimental::class)
-@RunWith(TestParameterInjector::class)
 class GetConnectPaymentReminderUseCaseTest {
   @get:Rule
   val testLogcatLogger = TestLogcatLoggingRule()
@@ -37,6 +39,7 @@ class GetConnectPaymentReminderUseCaseTest {
   fun `with the feature flag on but payment already connected, don't get a 'ShowReminder' response`() = runTest {
     val getConnectPaymentReminderUseCase = GetConnectPaymentReminderUseCaseImpl(
       apolloClient,
+      doesHavePayingContractsUseCaseProvider,
     )
     apolloClient.enqueueTestResponse(
       GetPayinMethodStatusQuery(),
@@ -58,6 +61,7 @@ class GetConnectPaymentReminderUseCaseTest {
   fun `with the feature flag on but network failure, don't get a 'ShowReminder' response`() = runTest {
     val getConnectPaymentReminderUseCase = GetConnectPaymentReminderUseCaseImpl(
       apolloClient,
+      doesHavePayingContractsUseCaseProvider,
     )
     apolloClient.enqueueTestNetworkError()
 
@@ -68,5 +72,36 @@ class GetConnectPaymentReminderUseCaseTest {
       .isInstanceOf<ConnectPaymentReminderError.NetworkError>()
       .prop(ConnectPaymentReminderError.NetworkError::message)
       .isEqualTo("Network error queued in QueueTestNetworkTransport")
+  }
+
+  @Test
+  fun `for a non paying member, don't remind them to connect payment`() = runTest {
+    val getConnectPaymentReminderUseCase = GetConnectPaymentReminderUseCaseImpl(
+      apolloClient,
+      onlyHasNonPayingContractsUseCaseProvider,
+    )
+    apolloClient.enqueueTestNetworkError()
+
+    val result = getConnectPaymentReminderUseCase.invoke()
+
+    assertThat(result)
+      .isLeft()
+      .isInstanceOf<ConnectPaymentReminderError.NonPayingMember>()
+  }
+
+  private val doesHavePayingContractsUseCaseProvider: Provider<GetOnlyHasNonPayingContractsUseCase> = Provider {
+    object : GetOnlyHasNonPayingContractsUseCase {
+      override suspend fun invoke(): Either<ErrorMessage, Boolean> {
+        return false.right()
+      }
+    }
+  }
+
+  private val onlyHasNonPayingContractsUseCaseProvider: Provider<GetOnlyHasNonPayingContractsUseCase> = Provider {
+    object : GetOnlyHasNonPayingContractsUseCase {
+      override suspend fun invoke(): Either<ErrorMessage, Boolean> {
+        return true.right()
+      }
+    }
   }
 }
