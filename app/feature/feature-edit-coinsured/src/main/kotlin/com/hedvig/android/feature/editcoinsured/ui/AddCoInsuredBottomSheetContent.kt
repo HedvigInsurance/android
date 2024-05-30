156776e9cfb658885738f48db71bcec1dbd414fe
stylianosgakis <stylianos@hedvig.com>
2024-05-30 18:19:21 +0200

    Make switch align properly in bigger screens

--- app/feature/feature-edit-coinsured/src/main/kotlin/com/hedvig/android/feature/editcoinsured/ui/AddCoInsuredBottomSheetContent.kt
+++ app/feature/feature-edit-coinsured/src/main/kotlin/com/hedvig/android/feature/editcoinsured/ui/AddCoInsuredBottomSheetContent.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.DatePickerDialog
@@ -138,6 +139,7 @@ internal fun AddCoInsuredBottomSheetContent(
             Switch(
               checked = bottomSheetState.showManualInput,
               onCheckedChange = onManualInputSwitchChanged,
+              modifier = Modifier.wrapContentSize(Alignment.CenterEnd),
             )
           },
           spaceBetween = 4.dp,
