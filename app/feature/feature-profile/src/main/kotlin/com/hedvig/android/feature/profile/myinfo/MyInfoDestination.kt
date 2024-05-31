84cefd81c49346ce4037aceb2e94f4a7f2f7f6e3
stylianosgakis <stylianos@hedvig.com>
2024-05-31 12:31:09 +0200

    Extract SuccessState to make the code a bit easier to read

--- app/feature/feature-profile/src/main/kotlin/com/hedvig/android/feature/profile/myinfo/MyInfoDestination.kt
+++ app/feature/feature-profile/src/main/kotlin/com/hedvig/android/feature/profile/myinfo/MyInfoDestination.kt
@@ -3,10 +3,9 @@ package com.hedvig.android.feature.profile.myinfo
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,11 +19,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -39,6 +39,7 @@ import com.hedvig.android.core.designsystem.component.textfield.HedvigTextField
 import com.hedvig.android.core.designsystem.preview.HedvigPreview
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
 import com.hedvig.android.core.ui.ValidatedInput
+import com.hedvig.android.core.ui.clearFocusOnTap
 import com.hedvig.android.core.ui.scaffold.HedvigScaffold
 import hedvig.resources.R
 
@@ -80,12 +81,7 @@ private fun MyInfoScreen(
     HedvigScaffold(
       topAppBarText = stringResource(R.string.PROFILE_MY_INFO_ROW_TITLE),
       navigateUp = navigateUp,
-      modifier = Modifier.clickable(
-        indication = null,
-        interactionSource = remember { MutableInteractionSource() },
-      ) {
-        focusManager.clearFocus()
-      },
+      modifier = Modifier.clearFocusOnTap(),
     ) {
       FadeAnimatedContent(
         targetState = uiState,
@@ -103,88 +99,90 @@ private fun MyInfoScreen(
               }
             }
             is MyInfoUiState.Success -> {
-              var emailInput by remember {
-                mutableStateOf(animatedUiState.member.email.input ?: "")
-              }
-              var phoneInput by remember {
-                mutableStateOf(animatedUiState.member.phoneNumber.input ?: "")
-              }
-              Spacer(Modifier.height(16.dp))
-              val errorText = animatedUiState.member.phoneNumber.errorMessageRes?.let { stringResource(id = it) }
-              HedvigTextField(
-                value = phoneInput,
-                onValueChange = { newInput ->
-                  if (newInput.indices.all {
-                      newInput[it].isWhitespace().not()
-                    }
-                  ) {
-                    phoneNumberChanged(newInput)
-                    phoneInput = newInput
-                  }
-                },
-                label = { Text(stringResource(R.string.PHONE_NUMBER_ROW_TITLE)) },
-                errorText = errorText,
-                keyboardOptions = KeyboardOptions(
-                  keyboardType = KeyboardType.Phone,
-                  imeAction = ImeAction.Next,
-                ),
-                withNewDesign = true,
-                modifier = Modifier
-                  .fillMaxWidth()
-                  .padding(horizontal = 16.dp),
-              )
-              AnimatedVisibility(visible = errorText != null) {
-                Spacer(Modifier.height(4.dp))
-              }
-              Spacer(Modifier.height(4.dp))
-              HedvigTextField(
-                value = emailInput,
-                onValueChange = { newInput ->
-                  if (newInput.indices.all { newInput[it].isWhitespace().not() }) {
-                    emailChanged(newInput)
-                    emailInput = newInput
-                  }
-                },
-                label = { Text(stringResource(R.string.PROFILE_MY_INFO_EMAIL_LABEL)) },
-                errorText = animatedUiState.member.email.errorMessageRes?.let { stringResource(id = it) },
-                keyboardOptions = KeyboardOptions(
-                  keyboardType = KeyboardType.Email,
-                  imeAction = ImeAction.Done,
-                ),
-                keyboardActions = KeyboardActions(
-                  onDone = {
-                    updateEmailAndPhoneNumber()
-                    focusManager.clearFocus()
-                  },
-                ),
-                withNewDesign = true,
-                modifier = Modifier
-                  .fillMaxWidth()
-                  .padding(horizontal = 16.dp),
-              )
-              Spacer(Modifier.height(16.dp))
-              AnimatedVisibility(
-                visible = animatedUiState.canSubmit || animatedUiState.isSubmitting,
-                enter = fadeIn(),
-                exit = fadeOut(),
-              ) {
-                HedvigContainedButton(
-                  text = stringResource(R.string.general_save_button),
-                  enabled = animatedUiState.canSubmit,
-                  onClick = {
-                    focusManager.clearFocus()
-                    updateEmailAndPhoneNumber()
-                  },
-                  isLoading = animatedUiState.isSubmitting,
-                  modifier = Modifier.padding(horizontal = 16.dp),
-                )
-                Spacer(Modifier.height(16.dp))
-              }
+              SuccessState(animatedUiState, phoneNumberChanged, emailChanged, updateEmailAndPhoneNumber, focusManager)
             }
           }
         }
       }
     }
+  }
+}
+
+@Composable
+private fun ColumnScope.SuccessState(
+  uiState: MyInfoUiState.Success,
+  phoneNumberChanged: (String) -> Unit,
+  emailChanged: (String) -> Unit,
+  updateEmailAndPhoneNumber: () -> Unit,
+  focusManager: FocusManager,
+) {
+  var emailInput by rememberSaveable { mutableStateOf(uiState.member.email.input ?: "") }
+  var phoneInput by rememberSaveable { mutableStateOf(uiState.member.phoneNumber.input ?: "") }
+  Spacer(Modifier.height(16.dp))
+  val errorText = uiState.member.phoneNumber.errorMessageRes?.let { stringResource(id = it) }
+  HedvigTextField(
+    value = phoneInput,
+    onValueChange = onValueChange@{ newInput ->
+      if (newInput.any { it.isWhitespace() }) return@onValueChange
+      phoneNumberChanged(newInput)
+      phoneInput = newInput
+    },
+    label = { Text(stringResource(R.string.PHONE_NUMBER_ROW_TITLE)) },
+    errorText = errorText,
+    keyboardOptions = KeyboardOptions(
+      keyboardType = KeyboardType.Phone,
+      imeAction = ImeAction.Next,
+    ),
+    withNewDesign = true,
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(horizontal = 16.dp),
+  )
+  AnimatedVisibility(visible = errorText != null) {
+    Spacer(Modifier.height(4.dp))
+  }
+  Spacer(Modifier.height(4.dp))
+  HedvigTextField(
+    value = emailInput,
+    onValueChange = onValueChange@{ newInput ->
+      if (newInput.any { it.isWhitespace() }) return@onValueChange
+      emailChanged(newInput)
+      emailInput = newInput
+    },
+    label = { Text(stringResource(R.string.PROFILE_MY_INFO_EMAIL_LABEL)) },
+    errorText = uiState.member.email.errorMessageRes?.let { stringResource(id = it) },
+    keyboardOptions = KeyboardOptions(
+      keyboardType = KeyboardType.Email,
+      imeAction = ImeAction.Done,
+    ),
+    keyboardActions = KeyboardActions(
+      onDone = {
+        updateEmailAndPhoneNumber()
+        focusManager.clearFocus()
+      },
+    ),
+    withNewDesign = true,
+    modifier = Modifier
+      .fillMaxWidth()
+      .padding(horizontal = 16.dp),
+  )
+  Spacer(Modifier.height(16.dp))
+  AnimatedVisibility(
+    visible = uiState.canSubmit || uiState.isSubmitting,
+    enter = fadeIn(),
+    exit = fadeOut(),
+  ) {
+    HedvigContainedButton(
+      text = stringResource(R.string.general_save_button),
+      enabled = uiState.canSubmit,
+      onClick = {
+        focusManager.clearFocus()
+        updateEmailAndPhoneNumber()
+      },
+      isLoading = uiState.isSubmitting,
+      modifier = Modifier.padding(horizontal = 16.dp),
+    )
+    Spacer(Modifier.height(16.dp))
   }
 }
 
