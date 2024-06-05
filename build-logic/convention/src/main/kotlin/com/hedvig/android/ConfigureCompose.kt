1216772eb9b0985642ff91d13f98f4e7812efa50
stylianosgakis <stylianos@hedvig.com>
2024-06-05 23:49:19 +0200

    Move the compose metrics code
    Reference:
    https://github.com/android/nowinandroid/blob/main/build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/AndroidCompose.kt

--- build-logic/convention/src/main/kotlin/com/hedvig/android/ConfigureCompose.kt
+++ build-logic/convention/src/main/kotlin/com/hedvig/android/ConfigureCompose.kt
@@ -1,15 +1,28 @@
 package com.hedvig.android
 
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.assign
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 
 internal fun ComposeCompilerGradlePluginExtension.configureComposeCompiler(project: Project) {
   enableStrongSkippingMode = true
 
-  // Get compose metrics with `./gradlew :app:assembleRelease -Pcom.hedvig.app.enableComposeCompilerReports=true`
-  if (project.findProperty("com.hedvig.app.enableComposeCompilerReports") == "true") {
-    metricsDestination.set(project.layout.buildDirectory.dir("/compose_metrics"))
-    reportsDestination.set(project.layout.buildDirectory.dir("/compose_reports"))
+  with(project) {
+    fun Provider<String>.onlyIfTrue() = flatMap { provider { it.takeIf(String::toBoolean) } }
+
+    fun Provider<*>.relativeToRootProject(dir: String) = flatMap {
+      rootProject.layout.buildDirectory.dir(projectDir.toRelativeString(rootDir))
+    }.map { it.dir(dir) }
+
+    // Get compose metrics with `./gradlew :app:assembleRelease -Pcom.hedvig.app.enableComposeCompilerReports=true`
+    project.providers.gradleProperty("com.hedvig.app.enableComposeCompilerReports")
+      .onlyIfTrue()
+      .relativeToRootProject("compose-metrics")
+      .let(metricsDestination::set)
+
+    project.providers.gradleProperty("com.hedvig.app.enableComposeCompilerReports").onlyIfTrue()
+      .relativeToRootProject("compose-reports")
+      .let(reportsDestination::set)
   }
 }
