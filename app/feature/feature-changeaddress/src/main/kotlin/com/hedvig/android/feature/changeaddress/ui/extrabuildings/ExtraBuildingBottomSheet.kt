c5e219712595a93f27a4a998c8ea1dd30c9a4f09
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-06-14 13:17:16 +0200

    android 12 keyboard/Sheet going up and dismissing work good. dismiss problem remains for 13 and 14

--- app/feature/feature-changeaddress/src/main/kotlin/com/hedvig/android/feature/changeaddress/ui/extrabuildings/ExtraBuildingBottomSheet.kt
+++ app/feature/feature-changeaddress/src/main/kotlin/com/hedvig/android/feature/changeaddress/ui/extrabuildings/ExtraBuildingBottomSheet.kt
@@ -1,19 +1,23 @@
 package com.hedvig.android.feature.changeaddress.ui.extrabuildings
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
@@ -35,12 +39,14 @@ import com.hedvig.android.core.designsystem.component.button.HedvigTextButton
 import com.hedvig.android.core.designsystem.material3.squircleLargeTop
 import com.hedvig.android.core.designsystem.preview.HedvigPreview
 import com.hedvig.android.core.designsystem.theme.HedvigTheme
+import com.hedvig.android.core.ui.clearFocusOnTap
 import com.hedvig.android.feature.changeaddress.data.ExtraBuilding
 import com.hedvig.android.feature.changeaddress.data.ExtraBuildingType
 import com.hedvig.android.feature.changeaddress.ui.ChangeAddressSwitch
 import com.hedvig.android.feature.changeaddress.ui.InputTextField
 import hedvig.resources.R
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 internal fun ExtraBuildingBottomSheet(
   extraBuildingTypes: List<ExtraBuildingType>,
@@ -54,6 +60,17 @@ internal fun ExtraBuildingBottomSheet(
   var selectedType by rememberSaveable { mutableStateOf(extraBuilding?.type) }
   var sizeErrorText by rememberSaveable { mutableStateOf<Int?>(null) }
 
+  val isImeVisible = WindowInsets.isImeVisible
+  val windowInsets = BottomSheetDefaults.windowInsets.only(WindowInsetsSides.Top)
+  val navigationBottomHeight = WindowInsets.systemBars.asPaddingValues().calculateBottomPadding()
+  val customModifier = if (isImeVisible) {
+    Modifier.imePadding()
+  } else {
+    Modifier.padding(
+      WindowInsets.safeDrawing
+        .only(WindowInsetsSides.Bottom).asPaddingValues(),
+    )
+  }
   ModalBottomSheet(
     containerColor = MaterialTheme.colorScheme.background,
     onDismissRequest = {
@@ -62,11 +79,11 @@ internal fun ExtraBuildingBottomSheet(
     shape = MaterialTheme.shapes.squircleLargeTop,
     sheetState = sheetState,
     tonalElevation = 0.dp,
-    windowInsets = WindowInsets.ime,
-    modifier = Modifier,
+    windowInsets = windowInsets,
+    modifier = Modifier.padding(bottom = navigationBottomHeight),
   ) {
     Column(
-      modifier = Modifier.verticalScroll(rememberScrollState()),
+      modifier = Modifier.verticalScroll(rememberScrollState()).clearFocusOnTap(),
     ) {
       Text(
         text = stringResource(id = R.string.CHANGE_ADDRESS_EXTRA_BUILDINGS_BOTTOM_SHEET_TITLE),
@@ -132,10 +149,11 @@ internal fun ExtraBuildingBottomSheet(
         modifier = Modifier.padding(horizontal = 16.dp),
       )
       Spacer(
-        Modifier.padding(
-          WindowInsets.safeDrawing
-            .only(WindowInsetsSides.Bottom).asPaddingValues(),
-        ),
+//        Modifier.padding(
+//          WindowInsets.safeDrawing
+//            .only(WindowInsetsSides.Bottom).asPaddingValues(),
+//        ),
+        customModifier,
       )
     }
   }
