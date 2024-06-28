eab1baf6dfef9b462e9e64e640c75e6072bd66a5
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-06-28 13:48:54 +0200

    HedvigBottomSheet

--- app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/HedvigBottomSheet.kt
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/HedvigBottomSheet.kt
@@ -1,58 +1,158 @@
 package com.hedvig.android.design.system.hedvig
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.isImeVisible
-import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import com.hedvig.android.design.system.hedvig.HedvigBottomSheetType.FullScreenWithManyTextInputs
+import com.hedvig.android.design.system.hedvig.HedvigBottomSheetType.SimpleHalfScreenSheet
 import com.hedvig.android.design.system.hedvig.tokens.BottomSheetTokens
-import com.hedvig.android.design.system.hedvig.tokens.LargeSizeButtonTokens
-import com.hedvig.android.logger.logcat
-import eu.wewox.modalsheet.ExperimentalSheetApi
-import eu.wewox.modalsheet.ModalSheet
+import com.skydoves.flexible.bottomsheet.material.FlexibleBottomSheet
+import com.skydoves.flexible.core.FlexibleSheetSize
+import com.skydoves.flexible.core.FlexibleSheetValue.FullyExpanded
+import com.skydoves.flexible.core.FlexibleSheetValue.IntermediatelyExpanded
+import com.skydoves.flexible.core.rememberFlexibleBottomSheetState
 
-@OptIn(ExperimentalSheetApi::class, ExperimentalLayoutApi::class)
 @Composable
 fun HedvigBottomSheet(
-  isVisible: Boolean,
   onVisibleChange: (Boolean) -> Unit,
+  allowNestedScroll: Boolean,
+  sheetType: HedvigBottomSheetType,
+  onDismissRequest: () -> Unit = { onVisibleChange(false) },
   content: @Composable ColumnScope.() -> Unit,
-  onSystemBack: (() -> Unit)? = null,
-  sheetPadding: PaddingValues? = null,
-  cancelable: Boolean = true,
 ) {
-  val isImeVisible = WindowInsets.isImeVisible
-  val defaultPadding = if (isImeVisible) {
-    WindowInsets.ime.asPaddingValues()
-  } else {
-    WindowInsets.safeDrawing
-      .only(WindowInsetsSides.Bottom).asPaddingValues()
+  when (sheetType) {
+    FullScreenWithManyTextInputs -> FullScreenWithManyTextInputsBottomSheet(
+      onVisibleChange,
+      allowNestedScroll,
+      onDismissRequest,
+      content,
+    )
+
+    SimpleHalfScreenSheet -> SimpleHalfScreenSheetBottomSheet(
+      onVisibleChange,
+      allowNestedScroll,
+      onDismissRequest,
+      content,
+    )
   }
-  val finalSheetPadding = sheetPadding ?: defaultPadding
-  val finalOnSystemBack = onSystemBack ?: { onVisibleChange(false) }
-  logcat { "mariia: finalOnSystemBack is onSystemBack: ${finalOnSystemBack == onSystemBack}" }
-  ModalSheet(
-    visible = isVisible,
-    onVisibleChange = onVisibleChange,
-    cancelable = cancelable,
-    content = content,
-    scrimColor = bottomSheetColors.scrimColor,
-    backgroundColor = bottomSheetColors.bottomSheetBackgroundColor,
-    contentColor = bottomSheetColors.contentColor,
-    sheetPadding = finalSheetPadding,
-    onSystemBack = finalOnSystemBack,
-    shape = bottomSheetShape.shape,
+}
+
+@Composable
+private fun FullScreenWithManyTextInputsBottomSheet(
+  onVisibleChange: (Boolean) -> Unit,
+  allowNestedScroll: Boolean,
+  onDismissRequest: (() -> Unit) = { onVisibleChange(false) },
+  content: @Composable ColumnScope.() -> Unit,
+) {
+  var currentSheetTarget by remember {
+    mutableStateOf(FullyExpanded)
+  }
+  val sheetState = rememberFlexibleBottomSheetState(
+    skipSlightlyExpanded = true,
+    skipIntermediatelyExpanded = true,
+    isModal = HedvigBottomSheetDefaults.ISMODAL,
+    containSystemBars = HedvigBottomSheetDefaults.containSystemBars,
+    allowNestedScroll = allowNestedScroll,
+    flexibleSheetSize = HedvigBottomSheetDefaults.flexibleSheetSize,
   )
+  FlexibleBottomSheet(
+    sheetState = sheetState,
+    scrimColor = HedvigBottomSheetDefaults.bottomSheetColors.scrimColor,
+    containerColor = HedvigBottomSheetDefaults.bottomSheetColors.bottomSheetBackgroundColor,
+    contentColor = HedvigBottomSheetDefaults.bottomSheetColors.contentColor,
+    windowInsets = HedvigBottomSheetDefaults.defaultInsets,
+    modifier = HedvigBottomSheetDefaults.modifier,
+    onDismissRequest = onDismissRequest,
+    shape = HedvigBottomSheetDefaults.bottomSheetShape.shape,
+    onTargetChanges = { sheetValue ->
+      currentSheetTarget = sheetValue
+    },
+  ) {
+    Column {
+      content()
+    }
+  }
+}
+
+@Composable
+private fun SimpleHalfScreenSheetBottomSheet(
+  onVisibleChange: (Boolean) -> Unit,
+  allowNestedScroll: Boolean,
+  onDismissRequest: (() -> Unit) = { onVisibleChange(false) },
+  content: @Composable ColumnScope.() -> Unit,
+) {
+  var currentSheetTarget by remember {
+    mutableStateOf(IntermediatelyExpanded)
+  }
+  val sheetState = rememberFlexibleBottomSheetState(
+    skipSlightlyExpanded = true,
+    skipIntermediatelyExpanded = false,
+    isModal = HedvigBottomSheetDefaults.ISMODAL,
+    containSystemBars = HedvigBottomSheetDefaults.containSystemBars,
+    allowNestedScroll = allowNestedScroll,
+    flexibleSheetSize = HedvigBottomSheetDefaults.flexibleSheetSize,
+  )
+  FlexibleBottomSheet(
+    sheetState = sheetState,
+    scrimColor = HedvigBottomSheetDefaults.bottomSheetColors.scrimColor,
+    containerColor = HedvigBottomSheetDefaults.bottomSheetColors.bottomSheetBackgroundColor,
+    contentColor = HedvigBottomSheetDefaults.bottomSheetColors.contentColor,
+    windowInsets = HedvigBottomSheetDefaults.defaultInsets,
+    modifier = HedvigBottomSheetDefaults.modifier,
+    onDismissRequest = onDismissRequest,
+    shape = HedvigBottomSheetDefaults.bottomSheetShape.shape,
+    onTargetChanges = { sheetValue ->
+      currentSheetTarget = sheetValue
+    },
+  ) {
+    Column {
+      content()
+    }
+  }
+}
+
+private object HedvigBottomSheetDefaults {
+  val bottomSheetColors: BottomSheetColors
+    @Composable
+    get() = with(HedvigTheme.colorScheme) {
+      remember(this) {
+        BottomSheetColors(
+          scrimColor = fromToken(BottomSheetTokens.ScrimColor),
+          bottomSheetBackgroundColor = fromToken(BottomSheetTokens.BottomSheetBackgroundColor),
+          contentColor = fromToken(BottomSheetTokens.ContentColor),
+        )
+      }
+    }
+
+  val bottomSheetShape: BottomSheetShape
+    @Composable
+    get() = BottomSheetShape(BottomSheetTokens.ContainerShape.value)
+
+  val modifier: Modifier
+    @Composable
+    get() = Modifier.positionAwareImePadding()
+
+  const val ISMODAL = true
+
+  val containSystemBars = true
+
+  val flexibleSheetSize: FlexibleSheetSize
+    get() = FlexibleSheetSize(
+      fullyExpanded = 1.0f,
+      intermediatelyExpanded = 0.6f,
+      slightlyExpanded = 0.15f,
+    )
+
+  val defaultInsets: WindowInsets = WindowInsets(0, 0, 0, 0)
 }
 
 @Immutable
@@ -62,23 +162,18 @@ internal data class BottomSheetColors(
   val contentColor: Color,
 )
 
-internal val bottomSheetColors: BottomSheetColors
-  @Composable
-  get() = with(HedvigTheme.colorScheme) {
-    remember(this) {
-      BottomSheetColors(
-        scrimColor = fromToken(BottomSheetTokens.ScrimColor),
-        bottomSheetBackgroundColor = fromToken(BottomSheetTokens.BottomSheetBackgroundColor),
-        contentColor = fromToken(BottomSheetTokens.ContentColor),
-      )
-    }
-  }
-
 @Immutable
 internal data class BottomSheetShape(
   val shape: Shape,
 )
 
-internal val bottomSheetShape: BottomSheetShape
-  @Composable
-  get() = BottomSheetShape(LargeSizeButtonTokens.ContainerShape.value)
+enum class HedvigBottomSheetType {
+  FullScreenWithManyTextInputs,
+
+  /** should use this one if there many TextFields with input (ime works better), or a long content */
+
+  SimpleHalfScreenSheet,
+  /** should use this one with short content */
+
+  // tb added if needed: dynamic content depending on sheetSize; slightly expanded and always visible etc
+}
--- micro-apps/design-showcase/src/main/kotlin/com/hedvig/android/sample/design/showcase/bottomSheet/BottomSheetShowcase.kt
+++ micro-apps/design-showcase/src/main/kotlin/com/hedvig/android/sample/design/showcase/bottomSheet/BottomSheetShowcase.kt
@@ -1,108 +1,221 @@
 package com.hedvig.android.sample.design.showcase.bottomSheet
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.hedvig.android.design.system.hedvig.HedvigBottomSheet
+import com.hedvig.android.design.system.hedvig.HedvigBottomSheetType.FullScreenWithManyTextInputs
+import com.hedvig.android.design.system.hedvig.HedvigBottomSheetType.SimpleHalfScreenSheet
 import com.hedvig.android.design.system.hedvig.HedvigButton
 import com.hedvig.android.design.system.hedvig.HedvigText
+import com.hedvig.android.design.system.hedvig.HedvigTextField
+import com.hedvig.android.design.system.hedvig.HedvigTextFieldDefaults.TextFieldSize.Medium
 import com.hedvig.android.design.system.hedvig.HedvigTheme
 import com.hedvig.android.design.system.hedvig.Surface
-import com.hedvig.android.logger.logcat
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun ShowcaseBottomSheet() {
   HedvigTheme {
-    Surface(color = HedvigTheme.colorScheme.backgroundPrimary) {
+    Surface(
+      color = HedvigTheme.colorScheme.backgroundWhite,
+      modifier = Modifier.fillMaxSize(),
+    ) {
       Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        val isSomeStuffVisible = remember { mutableStateOf(false) }
-//        BackHandler {
-//          isSomeStuffVisible.value = true
-//        }
-
         val isBottomSheetVisible = remember { mutableStateOf(false) }
-
-        val textFieldValue = remember { mutableStateOf("enter your name") }
+        val isBottomSheetVisible2 = remember { mutableStateOf(false) }
+        val isBottomSheetVisible3 = remember { mutableStateOf(false) }
+        val textFieldValue = remember { mutableStateOf("") }
+        val textFieldValue2 = remember { mutableStateOf("") }
         Spacer(Modifier.height(40.dp))
         HedvigText(
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
           Modifier.padding(horizontal = 16.dp),
         )
         Spacer(Modifier.height(40.dp))
-        if (isSomeStuffVisible.value) {
-          HedvigText("Some stuff is visible!")
-        }
-        Spacer(Modifier.height(40.dp))
-        HedvigButton(enabled = true, onClick = { isBottomSheetVisible.value = true }) { HedvigText("Open sheet") }
-        Spacer(Modifier.height(40.dp))
-        HedvigBottomSheet(
-          isVisible = isBottomSheetVisible.value,
-          onVisibleChange = { bool ->
-            isBottomSheetVisible.value = bool
-          },
-          onSystemBack = {
-            isSomeStuffVisible.value = true
-            logcat {
-              "mariia: onSystemBack is called, setting isSomeStuffVisible.value to true. isSomeStuffVisible = ${isSomeStuffVisible.value}"
-            }
-          },
-          content = {
-            Column(
-              Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .verticalScroll(rememberScrollState()),
-              horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-              HedvigText(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-              )
-              Spacer(Modifier.height(40.dp))
-              BasicTextField(
-                value = textFieldValue.value,
-                modifier = Modifier.background(Color.Red),
-                onValueChange = {
-                  textFieldValue.value = it
-                },
-              )
-              Spacer(Modifier.height(40.dp))
-              if (isSomeStuffVisible.value) {
-                HedvigText("Some stuff is visible!")
-              }
-              Spacer(Modifier.height(40.dp))
-              HedvigButton(
-                enabled = textFieldValue.value != "",
-                onClick = {
-                  isBottomSheetVisible.value = false
-                },
-              ) { HedvigText("Save") }
-              Spacer(Modifier.height(40.dp))
-              HedvigButton(
-                enabled = true,
-                onClick = {
-                  isBottomSheetVisible.value = false
-                  textFieldValue.value = ""
-                },
-              ) { HedvigText("Cancel") }
-            }
-          },
+        HedvigText(
+          "Name: ${textFieldValue.value} \nphone: ${textFieldValue2.value}",
+          modifier = Modifier.padding(16.dp),
         )
+        Spacer(Modifier.height(40.dp))
+        Column(Modifier.padding(horizontal = 16.dp)) {
+          HedvigButton(
+            enabled = true,
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { isBottomSheetVisible.value = true },
+          ) { HedvigText("Open fullscreen sheet with scroll and text input", textAlign = TextAlign.Center) }
+          Spacer(Modifier.height(16.dp))
+          HedvigButton(
+            enabled = true,
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { isBottomSheetVisible2.value = true },
+          ) { HedvigText("Open simple halfscreen sheet", textAlign = TextAlign.Center) }
+          Spacer(Modifier.height(16.dp))
+          HedvigButton(
+            enabled = true,
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { isBottomSheetVisible3.value = true },
+          ) { HedvigText("Open halfscreen sheet with text input", textAlign = TextAlign.Center) }
+          Spacer(Modifier.height(40.dp))
+        }
+        if (isBottomSheetVisible3.value) {
+          HalfScreenBottomSheetWithInput(
+            onVisibleChange = { bool ->
+              isBottomSheetVisible3.value = bool
+            },
+          )
+        }
+        if (isBottomSheetVisible2.value) {
+          SimpleBottomSheet(
+            onVisibleChange = { bool ->
+              isBottomSheetVisible2.value = bool
+            },
+          )
+        }
+        if (isBottomSheetVisible.value) {
+          LongBottomSheetWithManyTextInputs(
+            textFieldValue = textFieldValue.value,
+            textFieldValue2 = textFieldValue2.value,
+            onVisibleChange = { bool ->
+              isBottomSheetVisible.value = bool
+            },
+            onValueChange1 = {
+              textFieldValue.value = it
+            },
+            onValueChange2 = {
+              textFieldValue2.value = it
+            },
+            onCancelButtonCLick = {
+              isBottomSheetVisible.value = false
+              textFieldValue.value = ""
+            },
+            onSaveButtonCLick = {
+              isBottomSheetVisible.value = false
+            },
+          )
+        }
       }
+    }
+  }
+}
+
+@Composable
+private fun LongBottomSheetWithManyTextInputs(
+  textFieldValue: String,
+  textFieldValue2: String,
+  onVisibleChange: (Boolean) -> Unit,
+  onValueChange1: (String) -> Unit,
+  onValueChange2: (String) -> Unit,
+  onSaveButtonCLick: () -> Unit,
+  onCancelButtonCLick: () -> Unit,
+) {
+  HedvigBottomSheet(
+    onVisibleChange = onVisibleChange,
+    allowNestedScroll = false,
+    sheetType = FullScreenWithManyTextInputs,
+    content = {
+      Column(
+        Modifier
+          .fillMaxSize()
+          .padding(horizontal = 16.dp)
+          .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+      ) {
+        HedvigText(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        )
+        Spacer(Modifier.height(40.dp))
+        HedvigTextField(
+          text = textFieldValue,
+          textFieldSize = Medium,
+          labelText = "Name",
+          onValueChange = onValueChange1,
+        )
+        Spacer(Modifier.height(40.dp))
+        HedvigText(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        )
+        Spacer(Modifier.height(40.dp))
+        HedvigTextField(
+          text = textFieldValue2,
+          textFieldSize = Medium,
+          labelText = "Phone",
+          onValueChange = onValueChange2,
+        )
+        Spacer(Modifier.height(40.dp))
+        HedvigText(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        )
+        Spacer(Modifier.height(40.dp))
+        HedvigButton(
+          enabled = textFieldValue != "",
+          onClick = onSaveButtonCLick,
+        ) { HedvigText("Save") }
+        Spacer(Modifier.height(40.dp))
+        HedvigButton(
+          enabled = true,
+          onClick = onCancelButtonCLick,
+        ) { HedvigText("Remove value") }
+        Spacer(Modifier.height(16.dp))
+      }
+    },
+  )
+}
+
+@Composable
+private fun SimpleBottomSheet(onVisibleChange: (Boolean) -> Unit) {
+  HedvigBottomSheet(
+    onVisibleChange = onVisibleChange,
+    allowNestedScroll = true,
+    sheetType = SimpleHalfScreenSheet,
+  ) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+      HedvigText(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      )
+      Spacer(Modifier.height(16.dp))
+      HedvigButton(onClick = {}, enabled = true, content = { HedvigText("Button") })
+    }
+  }
+}
+
+@Composable
+private fun HalfScreenBottomSheetWithInput(onVisibleChange: (Boolean) -> Unit) {
+  HedvigBottomSheet(
+    onVisibleChange = onVisibleChange,
+    allowNestedScroll = true,
+    sheetType = SimpleHalfScreenSheet,
+  ) {
+    var textFieldValue by remember { mutableStateOf("placeholder") }
+    Column {
+      HedvigText(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      )
+      Spacer(Modifier.height(16.dp))
+      HedvigTextField(
+        text = textFieldValue,
+        textFieldSize = Medium,
+        labelText = "Phone",
+        modifier = Modifier.padding(horizontal = 16.dp),
+        onValueChange = { textFieldValue = it },
+      )
     }
   }
 }
