478c2c28d0146459cff83f4e24b2056d6b839d7d
Hugo Linder <hugo@hedvig.com>
2024-07-15 13:41:44 +0200

    Show button text for important messages dynamically. Hide button if no link present.

--- app/feature/feature-home/src/main/kotlin/com/hedvig/android/feature/home/home/ui/HomeDestination.kt
+++ app/feature/feature-home/src/main/kotlin/com/hedvig/android/feature/home/home/ui/HomeDestination.kt
@@ -509,7 +509,7 @@ private fun VeryImportantMessageCard(
         if (veryImportantMessage.link != null) {
           Spacer(modifier = Modifier.width(8.dp))
           InfoCardTextButton(
-            text = stringResource(R.string.important_message_read_more),
+            text = veryImportantMessage.buttonText ?: stringResource(R.string.important_message_read_more),
             onClick = { openUrl(veryImportantMessage.link) },
             modifier = Modifier.weight(1f),
           )
@@ -606,6 +606,7 @@ private fun PreviewHomeScreen(
             HomeData.VeryImportantMessage(
               "id",
               "Beware of the earthquake",
+              "Read more",
               "",
             ),
           ),
