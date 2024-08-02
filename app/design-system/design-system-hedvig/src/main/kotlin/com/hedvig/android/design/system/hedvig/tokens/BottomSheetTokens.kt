1722d39d65c3551472cea7b4e36eaff58f9973e4
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-08-02 12:36:33 +0200

    spacers etc. long scroll not working (bottom padding)

--- app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/tokens/BottomSheetTokens.kt
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/tokens/BottomSheetTokens.kt
@@ -1,8 +1,12 @@
 package com.hedvig.android.design.system.hedvig.tokens
 
+import androidx.compose.ui.unit.dp
+
 internal object BottomSheetTokens {
   val ScrimColor = ColorSchemeKeyTokens.SurfacePrimary
+  val UpperChipColor = ColorSchemeKeyTokens.SurfaceSecondary
   val BottomSheetBackgroundColor = ColorSchemeKeyTokens.BackgroundPrimary
   val ContentColor = ColorSchemeKeyTokens.TextPrimary
   val ContainerShape = ShapeKeyTokens.CornerTopOnlyXLarge
+  val ContentPadding = 16.dp
 }
--- micro-apps/design-showcase/src/main/kotlin/com/hedvig/android/sample/design/showcase/bottomSheet/BottomSheetShowcase.kt
+++ micro-apps/design-showcase/src/main/kotlin/com/hedvig/android/sample/design/showcase/bottomSheet/BottomSheetShowcase.kt
@@ -46,16 +46,16 @@ fun ShowcaseBottomSheet() {
           onVisibleChange = { bool ->
             isBottomSheetVisible.value = bool
           },
+          bottomButtonText = "Close",
         ) {
           Column(
             Modifier
               .fillMaxWidth()
-              .padding(16.dp)
               .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
           ) {
             HedvigText(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             )
             Spacer(Modifier.height(40.dp))
             BasicTextField(
@@ -66,21 +66,14 @@ fun ShowcaseBottomSheet() {
               },
             )
             Spacer(Modifier.height(40.dp))
-            Spacer(Modifier.height(40.dp))
             HedvigButton(
               enabled = textFieldValue.value != "",
+              modifier = Modifier.fillMaxWidth(),
               onClick = {
                 isBottomSheetVisible.value = false
               },
             ) { HedvigText("Save") }
-            Spacer(Modifier.height(40.dp))
-            HedvigButton(
-              enabled = true,
-              onClick = {
-                isBottomSheetVisible.value = false
-                textFieldValue.value = ""
-              },
-            ) { HedvigText("Cancel") }
+            Spacer(Modifier.height(8.dp))
           }
         }
       }
