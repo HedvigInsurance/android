f47e64cfbe35c69679c8744df72d02f734e10fcf
stylianosgakis <stylianos@hedvig.com>
2024-08-02 16:32:55 +0200

    Also add sharedBounds overload

--- app/compose/compose-ui/src/main/kotlin/com/hedvig/android/compose/ui/SharedElement.kt
+++ app/compose/compose-ui/src/main/kotlin/com/hedvig/android/compose/ui/SharedElement.kt
@@ -4,22 +4,30 @@ package com.hedvig.android.compose.ui
 
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.BoundsTransform
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.animation.SharedTransitionScope.OverlayClip
 import androidx.compose.animation.SharedTransitionScope.PlaceHolderSize
 import androidx.compose.animation.SharedTransitionScope.PlaceHolderSize.Companion.contentSize
+import androidx.compose.animation.SharedTransitionScope.ResizeMode
+import androidx.compose.animation.SharedTransitionScope.ResizeMode.Companion.ScaleToBounds
 import androidx.compose.animation.SharedTransitionScope.SharedContentState
 import androidx.compose.animation.core.Spring.StiffnessMediumLow
 import androidx.compose.animation.core.SpringSpec
 import androidx.compose.animation.core.VisibilityThreshold
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 
@@ -32,9 +40,8 @@ val LocalSharedTransitionScope: ProvidableCompositionLocal<SharedTransitionScope
 }
 
 @Composable
-fun rememberSharedContentState(key: Any): SharedContentState {
-  return LocalSharedTransitionScope.current.rememberSharedContentState(key)
-}
+fun rememberSharedContentState(key: Any): SharedContentState =
+  LocalSharedTransitionScope.current.rememberSharedContentState(key)
 
 fun Modifier.sharedElement(
   sharedTransitionScope: SharedTransitionScope,
@@ -57,6 +64,33 @@ fun Modifier.sharedElement(
   )
 }
 
+fun Modifier.sharedBounds(
+  sharedTransitionScope: SharedTransitionScope,
+  animatedVisibilityScope: AnimatedVisibilityScope,
+  state: SharedContentState,
+  enter: EnterTransition = fadeIn(),
+  exit: ExitTransition = fadeOut(),
+  boundsTransform: BoundsTransform = DefaultBoundsTransform,
+  resizeMode: ResizeMode = ScaleToBounds(ContentScale.FillWidth, Center),
+  placeHolderSize: PlaceHolderSize = contentSize,
+  renderInOverlayDuringTransition: Boolean = true,
+  zIndexInOverlay: Float = 0f,
+  clipInOverlayDuringTransition: OverlayClip = ParentClip,
+): Modifier = with(sharedTransitionScope) {
+  this@sharedBounds.sharedBounds(
+    sharedContentState = state,
+    animatedVisibilityScope = animatedVisibilityScope,
+    enter = enter,
+    exit = exit,
+    boundsTransform = boundsTransform,
+    resizeMode = resizeMode,
+    placeHolderSize = placeHolderSize,
+    renderInOverlayDuringTransition = renderInOverlayDuringTransition,
+    zIndexInOverlay = zIndexInOverlay,
+    clipInOverlayDuringTransition = clipInOverlayDuringTransition,
+  )
+}
+
 private val DefaultBoundsTransform: BoundsTransform = BoundsTransform { _, _ -> DefaultSpring }
 
 private val DefaultSpring: SpringSpec<Rect> = spring(
@@ -70,7 +104,5 @@ private val ParentClip: OverlayClip = object : OverlayClip {
     bounds: Rect,
     layoutDirection: LayoutDirection,
     density: Density,
-  ): Path? {
-    return state.parentSharedContentState?.clipPathInOverlay
-  }
+  ): Path? = state.parentSharedContentState?.clipPathInOverlay
 }
