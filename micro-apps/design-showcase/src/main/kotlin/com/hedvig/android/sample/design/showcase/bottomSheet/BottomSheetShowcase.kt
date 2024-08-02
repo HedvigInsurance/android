d9b20afd126c0355a0ac9fa2a30b7fbd6d9350c7
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-08-02 15:15:29 +0200

    change showcase

--- micro-apps/design-showcase/src/main/kotlin/com/hedvig/android/sample/design/showcase/bottomSheet/BottomSheetShowcase.kt
+++ micro-apps/design-showcase/src/main/kotlin/com/hedvig/android/sample/design/showcase/bottomSheet/BottomSheetShowcase.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -25,12 +26,12 @@ import com.hedvig.android.design.system.hedvig.Surface
 @Composable
 fun ShowcaseBottomSheet() {
   HedvigTheme {
-    Surface(color = HedvigTheme.colorScheme.backgroundPrimary) {
+    Surface(color = HedvigTheme.colorScheme.backgroundPrimary, modifier = Modifier.fillMaxSize()) {
       Column(horizontalAlignment = Alignment.CenterHorizontally) {
         val isBottomSheetVisible = remember { mutableStateOf(false) }
 
         val textFieldValue = remember { mutableStateOf("enter your name") }
-        Spacer(Modifier.height(40.dp))
+        Spacer(Modifier.height(120.dp))
         HedvigText(
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..",
           Modifier.padding(horizontal = 16.dp),
