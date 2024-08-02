86f1a6cb6e1a779f03c7bfc2d08bdafaf7809584
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-08-02 12:36:33 +0200

    spacers etc. long scroll not working (bottom padding)

--- app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/HedvigBottomSheet.kt
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/HedvigBottomSheet.kt
@@ -1,22 +1,34 @@
 package com.hedvig.android.design.system.hedvig
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.hedvig.android.design.system.hedvig.ButtonDefaults.ButtonStyle.Ghost
 import com.hedvig.android.design.system.hedvig.tokens.BottomSheetTokens
-import com.hedvig.android.design.system.hedvig.tokens.LargeSizeButtonTokens
 import eu.wewox.modalsheet.ExperimentalSheetApi
 import eu.wewox.modalsheet.ModalSheet
 
@@ -25,7 +37,9 @@ import eu.wewox.modalsheet.ModalSheet
 fun HedvigBottomSheet(
   isVisible: Boolean,
   onVisibleChange: (Boolean) -> Unit,
+  bottomButtonText: String,
   onSystemBack: (() -> Unit)? = { onVisibleChange(false) },
+  onBottomButtonClick: () -> Unit = { onVisibleChange(false) },
   sheetPadding: PaddingValues? = null,
   cancelable: Boolean = true,
   content: @Composable ColumnScope.() -> Unit,
@@ -42,14 +56,40 @@ fun HedvigBottomSheet(
     visible = isVisible,
     onVisibleChange = onVisibleChange,
     cancelable = cancelable,
-    content = content,
     scrimColor = bottomSheetColors.scrimColor,
     backgroundColor = bottomSheetColors.bottomSheetBackgroundColor,
     contentColor = bottomSheetColors.contentColor,
     sheetPadding = finalSheetPadding,
     onSystemBack = onSystemBack,
     shape = bottomSheetShape.shape,
-  )
+  ) {
+    Column(
+      modifier = Modifier.padding(horizontal = bottomSheetShape.contentHorizontalPadding),
+    ) {
+      Spacer(modifier = Modifier.height(8.dp))
+      Surface(
+        modifier = Modifier
+          .width(40.dp) // todo: put to tokens
+          .height(4.dp) // todo: put to tokens
+          .align(Alignment.CenterHorizontally)
+          .background(
+            shape = ShapeDefaults.CornerSmall,
+            color = bottomSheetColors.chipColor,
+          ).clip(ShapeDefaults.CornerSmall),
+      ) {
+      }
+      Spacer(modifier = Modifier.height(20.dp))
+      content()
+      HedvigButton(
+        onClick = onBottomButtonClick,
+        text = bottomButtonText,
+        enabled = true,
+        buttonStyle = Ghost,
+        modifier = Modifier.fillMaxWidth(),
+      )
+      Spacer(modifier = Modifier.height(32.dp))
+    }
+  }
 }
 
 @Immutable
@@ -57,6 +97,7 @@ internal data class BottomSheetColors(
   val scrimColor: Color,
   val bottomSheetBackgroundColor: Color,
   val contentColor: Color,
+  val chipColor: Color,
 )
 
 internal val bottomSheetColors: BottomSheetColors
@@ -67,6 +108,7 @@ internal val bottomSheetColors: BottomSheetColors
         scrimColor = fromToken(BottomSheetTokens.ScrimColor),
         bottomSheetBackgroundColor = fromToken(BottomSheetTokens.BottomSheetBackgroundColor),
         contentColor = fromToken(BottomSheetTokens.ContentColor),
+        chipColor = fromToken(BottomSheetTokens.UpperChipColor),
       )
     }
   }
@@ -74,8 +116,12 @@ internal val bottomSheetColors: BottomSheetColors
 @Immutable
 internal data class BottomSheetShape(
   val shape: Shape,
+  val contentHorizontalPadding: Dp,
 )
 
 internal val bottomSheetShape: BottomSheetShape
   @Composable
-  get() = BottomSheetShape(LargeSizeButtonTokens.ContainerShape.value)
+  get() = BottomSheetShape(
+    shape = BottomSheetTokens.ContainerShape.value,
+    contentHorizontalPadding = BottomSheetTokens.ContentPadding,
+  )
