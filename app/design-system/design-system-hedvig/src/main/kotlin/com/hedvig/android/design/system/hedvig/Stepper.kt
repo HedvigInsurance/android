df6c899bbe09cfd53bd3c8c204ec543d9f0316ce
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-08-07 17:12:03 +0200

    in prog

---
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/Stepper.kt
@@ -0,0 +1,78 @@
+package com.hedvig.android.design.system.hedvig
+
+import androidx.compose.ui.unit.dp
+import com.hedvig.android.design.system.hedvig.tokens.ShapeKeyTokens
+import com.hedvig.android.design.system.hedvig.tokens.TypographyKeyTokens
+
+internal object StepperColorTokens {
+  //todo
+//  val ContainerColor = ColorSchemeKeyTokens.SurfacePrimary
+//  val LabelColor = ColorSchemeKeyTokens.TextPrimary
+//  val DescriptionColor = ColorSchemeKeyTokens.TextSecondary
+//  val PulsatingContainerColor = ColorSchemeKeyTokens.SignalGreenFill
+//  val PulsatingLabelColor = ColorSchemeKeyTokens.TextBlack
+//  val PulsatingDescriptionColor = ColorSchemeKeyTokens.SignalGreenText
+//  val ToggleTopColor = ColorSchemeKeyTokens.FillWhite
+//  val ToggleBackgroundOnColor = ColorSchemeKeyTokens.SignalGreenElement
+//  val ToggleBackgroundOffColor = ColorSchemeKeyTokens.FillDisabled
+}
+
+internal object LargeSizeDefaultStepperTokens {
+//  val HorizontalPadding = 16.dp
+//  val TopPadding = 16.dp
+//  val BottomPadding = 18.dp
+//  val StepperTopPadding = 7.dp
+//  val StepperBottomPadding = 5.dp
+//  val ContainerShape = ShapeKeyTokens.CornerLarge
+//  val LabelTextFont = TypographyKeyTokens.BodyMedium
+}
+
+internal object MediumSizeDefaultStepperTokens {
+//  val HorizontalPadding = 16.dp
+//  val TopPadding = 16.dp
+//  val BottomPadding = 18.dp
+//  val StepperTopPadding = 7.dp
+//  val StepperBottomPadding = 5.dp
+//  val ContainerShape = ShapeKeyTokens.CornerLarge
+//  val LabelTextFont = TypographyKeyTokens.BodySmall
+}
+
+internal object SmallSizeDefaultStepperTokens {
+//  val HorizontalPadding = 14.dp
+//  val TopPadding = 15.dp
+//  val BottomPadding = 17.dp
+//  val StepperTopPadding = 0.dp
+//  val StepperBottomPadding = 0.dp
+//  val ContainerShape = ShapeKeyTokens.CornerLarge
+//  val LabelTextFont = TypographyKeyTokens.BodySmall
+}
+
+internal object LargeSizeLabeledStepperTokens {
+//  val HorizontalPadding = 16.dp
+//  val TopPadding = 12.dp
+//  val BottomPadding = 14.dp
+//  val SpacerHeight = 8.dp
+//  val ContainerShape = ShapeKeyTokens.CornerLarge
+//  val LabelTextFont = TypographyKeyTokens.BodySmall
+//  val DescriptionTextFont = TypographyKeyTokens.Label
+}
+
+internal object MediumSizeLabeledStepperTokens {
+//  val HorizontalPadding = 16.dp
+//  val TopPadding = 12.dp
+//  val BottomPadding = 14.dp
+//  val SpacerHeight = 8.dp
+//  val ContainerShape = ShapeKeyTokens.CornerLarge
+//  val LabelTextFont = TypographyKeyTokens.Label
+//  val DescriptionTextFont = TypographyKeyTokens.Label
+}
+
+internal object SmallSizeLabeledStepperTokens {
+//  val HorizontalPadding = 16.dp
+//  val TopPadding = 12.dp
+//  val BottomPadding = 14.dp
+//  val SpacerHeight = 8.dp
+//  val ContainerShape = ShapeKeyTokens.CornerLarge
+//  val LabelTextFont = TypographyKeyTokens.Label
+//  val DescriptionTextFont = TypographyKeyTokens.Label
+}
