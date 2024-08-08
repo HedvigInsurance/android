feb420568a2c74c600f19693a141f27ec2acc624
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-08-08 09:19:20 +0200

    change contrast

--- app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/tokens/BottomSheetTokens.kt
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/tokens/BottomSheetTokens.kt
@@ -5,8 +5,8 @@ import androidx.compose.ui.unit.dp
 internal object BottomSheetTokens {
   val ScrimColor = ColorSchemeKeyTokens.SurfacePrimary
   val UpperChipColor = ColorSchemeKeyTokens.SurfaceSecondary
-  val ArrowColor = ColorSchemeKeyTokens.FillBlack
-  val ArrowColorBackground = ColorSchemeKeyTokens.FillWhite
+  val ArrowColor = ColorSchemeKeyTokens.FillNegative
+  val ArrowColorBackground = ColorSchemeKeyTokens.FillPrimary
   val BottomSheetBackgroundColor = ColorSchemeKeyTokens.BackgroundPrimary
   val ContentColor = ColorSchemeKeyTokens.TextPrimary
   val ContainerShape = ShapeKeyTokens.CornerTopOnlyXLarge
