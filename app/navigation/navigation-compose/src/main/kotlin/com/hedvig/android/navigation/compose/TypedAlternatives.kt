e1d0afa9886aa68a06362c72b3d1dad4398a0dd9
stylianosgakis <stylianos@hedvig.com>
2024-08-08 18:45:59 +0200

    Add typed version for toRoute

--- app/navigation/navigation-compose/src/main/kotlin/com/hedvig/android/navigation/compose/TypedAlternatives.kt
+++ app/navigation/navigation-compose/src/main/kotlin/com/hedvig/android/navigation/compose/TypedAlternatives.kt
@@ -1,10 +1,14 @@
 package com.hedvig.android.navigation.compose
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.NavType
 import androidx.navigation.PopUpToBuilder
+import androidx.navigation.toRoute
 import kotlin.reflect.KClass
+import kotlin.reflect.KType
 
 public inline fun <reified T : Any> NavOptionsBuilder.typedPopUpTo(
   noinline popUpToBuilder: PopUpToBuilder.() -> Unit = {},
@@ -13,3 +17,7 @@ public inline fun <reified T : Any> NavOptionsBuilder.typedPopUpTo(
 inline fun <reified T : Destination> NavDestination.typedHasRoute() = hasRoute<T>()
 
 fun <T : Destination> NavDestination.typedHasRoute(route: KClass<T>) = hasRoute(route)
+
+inline fun <reified T : Any> SavedStateHandle.typedToRoute(
+  typeMap: Map<KType, @JvmSuppressWildcards NavType<*>> = emptyMap(),
+): T = toRoute<T>(typeMap)
