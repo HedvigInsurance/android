bd7887bd0c7cf0102dc0e4b58492a98abc9e880c
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-08-09 10:20:46 +0200

    fx color animating

--- app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/Stepper.kt
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/Stepper.kt
@@ -2,23 +2,37 @@ package com.hedvig.android.design.system.hedvig
 
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.hedvig.android.design.system.hedvig.StepperDefaults.StepperSize.Large
+import com.hedvig.android.design.system.hedvig.StepperDefaults.StepperSize.Medium
+import com.hedvig.android.design.system.hedvig.StepperDefaults.StepperSize.Small
 import com.hedvig.android.design.system.hedvig.StepperDefaults.StepperStyle
 import com.hedvig.android.design.system.hedvig.StepperDefaults.StepperStyle.Default
 import com.hedvig.android.design.system.hedvig.StepperDefaults.StepperStyle.Labeled
+import com.hedvig.android.design.system.hedvig.icon.HedvigIcons
+import com.hedvig.android.design.system.hedvig.icon.Minus
+import com.hedvig.android.design.system.hedvig.icon.Plus
 import com.hedvig.android.design.system.hedvig.tokens.AnimationTokens
 import com.hedvig.android.design.system.hedvig.tokens.LargeSizeDefaultStepperTokens
 import com.hedvig.android.design.system.hedvig.tokens.LargeSizeLabeledStepperTokens
@@ -37,12 +51,11 @@ fun HedvigStepper(
   isPlusEnabled: Boolean,
   isMinusEnabled: Boolean,
   modifier: Modifier = Modifier,
-  stepperStyle: StepperDefaults.StepperStyle = StepperDefaults.stepperStyle,
+  stepperStyle: StepperStyle = StepperDefaults.stepperStyle,
   stepperSize: StepperDefaults.StepperSize = StepperDefaults.stepperSize,
   showError: Boolean = false,
   errorText: String? = null,
 ) {
-  val enabled by remember(showError) { mutableStateOf(showError) }
   val initialContainerColor = stepperColors.containerColor(false)
   val pulsatingContainerColor = stepperColors.containerColor(true)
   val containerColor = remember { Animatable(initialContainerColor) }
@@ -52,15 +65,45 @@ fun HedvigStepper(
   val initialTextColor = stepperColors.textColor(false)
   val pulsatingTextColor = stepperColors.textColor(true)
   val textColor = remember { Animatable(initialTextColor) }
-  val initialMinusColor = if (isMinusEnabled) stepperColors.enabledSymbolColor(false) else stepperColors.disabledSymbolColor(false)
-  val initialPlusColor = if (isPlusEnabled) stepperColors.enabledSymbolColor(false) else stepperColors.disabledSymbolColor(false)
+  val disabledSymbolColor = stepperColors.disabledSymbolColor(false)
+  val enabledSymbolColor = stepperColors.enabledSymbolColor(false)
+  val initialMinusColor =
+    if (isMinusEnabled) enabledSymbolColor else disabledSymbolColor
+  val initialPlusColor =
+    if (isPlusEnabled) enabledSymbolColor else disabledSymbolColor
   val pulsatingSymbolColor = stepperColors.enabledSymbolColor(true)
   val plusColor = remember { Animatable(initialPlusColor) }
   val minusColor = remember { Animatable(initialMinusColor) }
   val enterAnimationSpec = tween<Color>(AnimationTokens().pulsatingAnimationDuration)
   val exitAnimationSpec = tween<Color>(AnimationTokens().pulsatingAnimationDurationExit)
-  LaunchedEffect(enabled) {
-    if (enabled) {
+  LaunchedEffect(isPlusEnabled, showError) {
+    if (!isPlusEnabled && !showError) {
+      plusColor.animateTo(disabledSymbolColor, animationSpec = exitAnimationSpec)
+    } else if (!isPlusEnabled) {
+      plusColor.animateTo(pulsatingSymbolColor, animationSpec = enterAnimationSpec)
+      plusColor.animateTo(disabledSymbolColor, animationSpec = exitAnimationSpec)
+    } else if (showError) {
+      plusColor.animateTo(pulsatingSymbolColor, animationSpec = enterAnimationSpec)
+      plusColor.animateTo(enabledSymbolColor, animationSpec = exitAnimationSpec)
+    } else {
+      plusColor.animateTo(enabledSymbolColor, animationSpec = exitAnimationSpec)
+    }
+  }
+  LaunchedEffect(isMinusEnabled, showError) {
+    if (!isMinusEnabled && !showError) {
+      minusColor.animateTo(disabledSymbolColor, animationSpec = exitAnimationSpec)
+    } else if (!isMinusEnabled) {
+      minusColor.animateTo(pulsatingSymbolColor, animationSpec = enterAnimationSpec)
+      minusColor.animateTo(disabledSymbolColor, animationSpec = exitAnimationSpec)
+    } else if (showError) {
+      minusColor.animateTo(pulsatingSymbolColor, animationSpec = enterAnimationSpec)
+      minusColor.animateTo(enabledSymbolColor, animationSpec = exitAnimationSpec)
+    } else {
+      minusColor.animateTo(enabledSymbolColor, animationSpec = exitAnimationSpec)
+    }
+  }
+  LaunchedEffect(showError) {
+    if (showError) {
       launch {
         containerColor.animateTo(pulsatingContainerColor, animationSpec = enterAnimationSpec)
         containerColor.animateTo(initialContainerColor, animationSpec = exitAnimationSpec)
@@ -73,20 +116,10 @@ fun HedvigStepper(
         textColor.animateTo(pulsatingTextColor, animationSpec = enterAnimationSpec)
         textColor.animateTo(initialTextColor, animationSpec = exitAnimationSpec)
       }
-      launch {
-        plusColor.animateTo(pulsatingSymbolColor, animationSpec = enterAnimationSpec)
-        plusColor.animateTo(initialPlusColor, animationSpec = exitAnimationSpec)
-      }
-      launch {
-        minusColor.animateTo(pulsatingSymbolColor, animationSpec = enterAnimationSpec)
-        minusColor.animateTo(initialMinusColor, animationSpec = exitAnimationSpec)
-      }
     } else {
       launch { containerColor.animateTo(initialContainerColor, animationSpec = enterAnimationSpec) }
       launch { labelColor.animateTo(initialLabelColor, animationSpec = enterAnimationSpec) }
       launch { textColor.animateTo(initialTextColor, animationSpec = enterAnimationSpec) }
-      launch { plusColor.animateTo(initialPlusColor, animationSpec = enterAnimationSpec) }
-      launch { minusColor.animateTo(initialMinusColor, animationSpec = enterAnimationSpec) }
     }
   }
   when (stepperStyle) {
@@ -99,10 +132,11 @@ fun HedvigStepper(
       textColor = textColor.value,
       plusColor = plusColor.value,
       minusColor = minusColor.value,
-      showError = showError,
       errorText = errorText,
-      modifier = modifier
+      showError = showError,
+      modifier = modifier,
     )
+
     is Labeled -> LabeledStepper(
       stepperSize = stepperSize,
       text = text,
@@ -114,9 +148,9 @@ fun HedvigStepper(
       labelColor = labelColor.value,
       plusColor = plusColor.value,
       minusColor = minusColor.value,
-      showError = showError,
       errorText = errorText,
-      modifier = modifier
+      showError = showError,
+      modifier = modifier,
     )
   }
 }
@@ -135,7 +169,59 @@ private fun DefaultStepper(
   errorText: String?,
   modifier: Modifier = Modifier,
 ) {
-  //todo
+  Column(modifier) {
+    Surface(
+      shape = stepperSize.size.shape,
+      color = containerColor,
+      modifier = Modifier.defaultMinSize(minHeight = stepperSize.size.minHeight(Default)),
+    ) {
+      Row(
+        Modifier.padding(stepperSize.size.contentPadding(Default)),
+        verticalAlignment = Alignment.CenterVertically,
+      ) {
+        HedvigText(
+          text = text,
+          style = stepperSize.size.textStyle(Default),
+          color = textColor,
+        )
+        Spacer(Modifier.weight(1f))
+        Spacer(Modifier.width(4.dp))
+        StepperSymbols(
+          onMinusClick,
+          onPlusClick,
+          plusColor,
+          minusColor,
+          modifier = Modifier.padding(stepperSize.size.symbolsPadding),
+        )
+      }
+    }
+    if (showError && errorText != null) {
+      HedvigText(
+        text = errorText,
+        color = stepperColors.errorTextColor,
+        style = stepperSize.size.labelTextStyle,
+        modifier = Modifier.padding(stepperSize.size.errorTextPadding(Default)),
+      )
+    }
+  }
+}
+
+@Composable
+private fun StepperSymbols(
+  onMinusClick: () -> Unit,
+  onPlusClick: () -> Unit,
+  plusColor: Color,
+  minusColor: Color,
+  modifier: Modifier = Modifier,
+) {
+  Row(modifier) {
+    IconButton(onClick = onMinusClick) {
+      Icon(HedvigIcons.Minus, null, tint = minusColor)
+    }
+    IconButton(onClick = onPlusClick) {
+      Icon(HedvigIcons.Plus, null, tint = plusColor)
+    }
+  }
 }
 
 @Composable
@@ -150,13 +236,49 @@ private fun LabeledStepper(
   labelColor: Color,
   plusColor: Color,
   minusColor: Color,
-  showError: Boolean,
   errorText: String?,
+  showError: Boolean,
   modifier: Modifier = Modifier,
 ) {
-  //todo
+  Column(modifier) {
+    Surface(
+      shape = stepperSize.size.shape,
+      color = containerColor,
+      modifier = Modifier.defaultMinSize(minHeight = stepperSize.size.minHeight(Labeled(labelText))),
+    ) {
+      Row(
+        Modifier.padding(stepperSize.size.contentPadding(Labeled(labelText))),
+        verticalAlignment = Alignment.CenterVertically,
+      ) {
+        Column {
+          HedvigText(
+            text = labelText,
+            style = stepperSize.size.labelTextStyle,
+            color = labelColor,
+          )
+          HedvigText(
+            text = text,
+            style = stepperSize.size.textStyle(Labeled(labelText)),
+            color = textColor,
+          )
+        }
+        Spacer(Modifier.weight(1f))
+        Spacer(Modifier.width(4.dp))
+        StepperSymbols(onMinusClick, onPlusClick, plusColor, minusColor)
+      }
+    }
+    if (showError && errorText != null) {
+      HedvigText(
+        text = errorText,
+        color = stepperColors.errorTextColor,
+        style = stepperSize.size.labelTextStyle,
+        modifier = Modifier.padding(
+          stepperSize.size.errorTextPadding(Labeled(labelText)),
+        ),
+      )
+    }
+  }
 }
-
 
 object StepperDefaults {
   internal val stepperStyle: StepperStyle = Default
@@ -185,6 +307,11 @@ private val StepperDefaults.StepperSize.size: StepperSize
 private sealed class StepperSize {
   protected abstract val defaultContentPadding: PaddingValues
   protected abstract val labelContentPadding: PaddingValues
+  protected abstract val labelMinHeight: Dp
+  protected abstract val defaultMinHeight: Dp
+  abstract val symbolsPadding: PaddingValues
+  protected abstract val labelStyleErrorTextPadding: PaddingValues
+  protected abstract val defaultStyleErrorTextPadding: PaddingValues
 
   @get:Composable
   protected abstract val defaultCommonTextStyle: TextStyle
@@ -197,6 +324,20 @@ private sealed class StepperSize {
 
   @get:Composable
   abstract val shape: Shape
+
+  fun minHeight(stepperStyle: StepperStyle): Dp {
+    return when (stepperStyle) {
+      Default -> defaultMinHeight
+      is Labeled -> labelMinHeight
+    }
+  }
+
+  fun errorTextPadding(stepperStyle: StepperStyle): PaddingValues {
+    return when (stepperStyle) {
+      Default -> defaultStyleErrorTextPadding
+      is Labeled -> labelStyleErrorTextPadding
+    }
+  }
 
   fun contentPadding(stepperStyle: StepperStyle): PaddingValues {
     return when (stepperStyle) {
@@ -228,6 +369,25 @@ private sealed class StepperSize {
         start = LargeSizeLabeledStepperTokens.HorizontalPadding,
         end = LargeSizeLabeledStepperTokens.HorizontalPadding,
       )
+    override val symbolsPadding: PaddingValues
+      get() = PaddingValues(
+        top = LargeSizeDefaultStepperTokens.StepperTopPadding,
+        bottom = LargeSizeDefaultStepperTokens.StepperBottomPadding,
+      )
+    override val labelStyleErrorTextPadding: PaddingValues
+      get() = PaddingValues(
+        start = LargeSizeLabeledStepperTokens.ErrorTextStartPadding,
+        end = LargeSizeLabeledStepperTokens.ErrorTextEndPadding,
+      )
+    override val defaultStyleErrorTextPadding: PaddingValues
+      get() = PaddingValues(
+        start = LargeSizeDefaultStepperTokens.ErrorTextStartPadding,
+        end = LargeSizeDefaultStepperTokens.ErrorTextEndPadding,
+      )
+    override val defaultMinHeight: Dp
+      get() = LargeSizeDefaultStepperTokens.MinHeight
+    override val labelMinHeight: Dp
+      get() = LargeSizeLabeledStepperTokens.MinHeight
     override val defaultCommonTextStyle: TextStyle
       @Composable
       @ReadOnlyComposable
@@ -261,6 +421,25 @@ private sealed class StepperSize {
         start = MediumSizeLabeledStepperTokens.HorizontalPadding,
         end = MediumSizeLabeledStepperTokens.HorizontalPadding,
       )
+    override val symbolsPadding: PaddingValues
+      get() = PaddingValues(
+        top = MediumSizeDefaultStepperTokens.StepperTopPadding,
+        bottom = MediumSizeDefaultStepperTokens.StepperBottomPadding,
+      )
+    override val labelStyleErrorTextPadding: PaddingValues
+      get() = PaddingValues(
+        start = MediumSizeLabeledStepperTokens.ErrorTextStartPadding,
+        end = MediumSizeLabeledStepperTokens.ErrorTextEndPadding,
+      )
+    override val defaultStyleErrorTextPadding: PaddingValues
+      get() = PaddingValues(
+        start = MediumSizeDefaultStepperTokens.ErrorTextStartPadding,
+        end = MediumSizeDefaultStepperTokens.ErrorTextEndPadding,
+      )
+    override val defaultMinHeight: Dp
+      get() = MediumSizeDefaultStepperTokens.MinHeight
+    override val labelMinHeight: Dp
+      get() = MediumSizeLabeledStepperTokens.MinHeight
     override val defaultCommonTextStyle: TextStyle
       @Composable
       @ReadOnlyComposable
@@ -294,6 +473,25 @@ private sealed class StepperSize {
         start = SmallSizeLabeledStepperTokens.HorizontalPadding,
         end = SmallSizeLabeledStepperTokens.HorizontalPadding,
       )
+    override val symbolsPadding: PaddingValues
+      get() = PaddingValues(
+        top = SmallSizeDefaultStepperTokens.StepperTopPadding,
+        bottom = SmallSizeDefaultStepperTokens.StepperBottomPadding,
+      )
+    override val labelStyleErrorTextPadding: PaddingValues
+      get() = PaddingValues(
+        start = SmallSizeLabeledStepperTokens.ErrorTextStartPadding,
+        end = SmallSizeLabeledStepperTokens.ErrorTextEndPadding,
+      )
+    override val defaultStyleErrorTextPadding: PaddingValues
+      get() = PaddingValues(
+        start = SmallSizeDefaultStepperTokens.ErrorTextStartPadding,
+        end = SmallSizeDefaultStepperTokens.ErrorTextEndPadding,
+      )
+    override val defaultMinHeight: Dp
+      get() = SmallSizeDefaultStepperTokens.MinHeight
+    override val labelMinHeight: Dp
+      get() = SmallSizeLabeledStepperTokens.MinHeight
     override val defaultCommonTextStyle: TextStyle
       @Composable
       @ReadOnlyComposable
@@ -322,6 +520,7 @@ private data class StepperColors(
   private val disabledSymbolColor: Color,
   private val pulsatingContainerColor: Color,
   private val pulsatingContentColor: Color,
+  val errorTextColor: Color,
 ) {
   @Stable
   fun containerColor(pulsating: Boolean): Color = when {
@@ -366,6 +565,116 @@ private val stepperColors: StepperColors
         disabledSymbolColor = fromToken(StepperColorTokens.DisabledSymbolColor),
         enabledSymbolColor = fromToken(StepperColorTokens.EnabledSymbolColor),
         pulsatingContentColor = fromToken(StepperColorTokens.PulsatingContentColor),
+        errorTextColor = fromToken(StepperColorTokens.LabelColor),
       )
     }
   }
+
+@Preview
+@Composable
+private fun StepperPreview() {
+  HedvigTheme {
+    Surface(color = Color.White) {
+      Column(Modifier.padding(horizontal = 16.dp)) {
+        Spacer(Modifier.height(8.dp))
+        StepperPreviewWithParameters(
+          text = "Large",
+          stepperStyle = Default,
+          size = Large,
+          quantity = 1,
+          showError = false,
+          onPlusClick = { },
+          onMinusClick = { },
+          isPlusEnabled = true,
+          isMinusEnabled = true,
+        )
+        Spacer(Modifier.height(8.dp))
+        StepperPreviewWithParameters(
+          text = "Medium",
+          stepperStyle = Default,
+          size = Medium,
+          quantity = 6,
+          showError = true,
+          onPlusClick = { },
+          onMinusClick = { },
+          isPlusEnabled = false,
+          isMinusEnabled = true,
+        )
+        Spacer(Modifier.height(8.dp))
+        StepperPreviewWithParameters(
+          text = "Small",
+          stepperStyle = Default,
+          size = Small,
+          quantity = 0,
+          showError = false,
+          onPlusClick = { },
+          onMinusClick = { },
+          isPlusEnabled = true,
+          isMinusEnabled = false,
+        )
+        Spacer(Modifier.height(8.dp))
+        StepperPreviewWithParameters(
+          text = "Large",
+          stepperStyle = Labeled("Label"),
+          size = Large,
+          quantity = 1,
+          showError = false,
+          onPlusClick = { },
+          onMinusClick = { },
+          isPlusEnabled = true,
+          isMinusEnabled = true,
+        )
+        Spacer(Modifier.height(8.dp))
+        StepperPreviewWithParameters(
+          text = "Medium",
+          stepperStyle = Labeled("Label"),
+          size = Medium,
+          quantity = 1,
+          showError = false,
+          onPlusClick = { },
+          onMinusClick = { },
+          isPlusEnabled = true,
+          isMinusEnabled = true,
+        )
+        Spacer(Modifier.height(8.dp))
+        StepperPreviewWithParameters(
+          text = "Small",
+          stepperStyle = Labeled("Label"),
+          size = Small,
+          quantity = 1,
+          showError = false,
+          onPlusClick = { },
+          onMinusClick = { },
+          isPlusEnabled = true,
+          isMinusEnabled = true,
+        )
+        Spacer(Modifier.height(8.dp))
+      }
+    }
+  }
+}
+
+@Composable
+private fun StepperPreviewWithParameters(
+  stepperStyle: StepperStyle,
+  size: StepperDefaults.StepperSize,
+  text: String,
+  quantity: Int,
+  onMinusClick: () -> Unit,
+  onPlusClick: () -> Unit,
+  showError: Boolean,
+  isPlusEnabled: Boolean,
+  isMinusEnabled: Boolean,
+) {
+  HedvigStepper(
+    showError = showError,
+    onPlusClick = onPlusClick,
+    onMinusClick = onMinusClick,
+    text = "$text, quantity: $quantity",
+    stepperStyle = stepperStyle,
+    stepperSize = size,
+    isPlusEnabled = isPlusEnabled,
+    isMinusEnabled = isMinusEnabled,
+    errorText = "That would be too much or too little",
+  )
+}
