481bcc558c583e48d410d82abbe5145a8e4b7c1d
stylianosgakis <stylianos@hedvig.com>
2024-08-15 11:15:17 +0200

    Parse the response into E<AOE, Data> and add public API
    Also provide a convenient ErrorMessage() contructor-like function to
    allow us to continue using ::ErrorMessage to ease up migration

--- app/apollo/apollo-core/src/main/kotlin/com/hedvig/android/apollo/ApolloCallExt.kt
+++ app/apollo/apollo-core/src/main/kotlin/com/hedvig/android/apollo/ApolloCallExt.kt
@@ -1,83 +1,106 @@
 package com.hedvig.android.apollo
 
 import arrow.core.Either
+import arrow.core.raise.Raise
+import arrow.core.raise.either
 import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.api.ApolloResponse
 import com.apollographql.apollo.api.Operation
 import com.apollographql.apollo.exception.ApolloException
+import com.apollographql.apollo.exception.CacheMissException
+import com.hedvig.android.apollo.ApolloOperationError.CacheMiss
 import com.hedvig.android.apollo.ApolloOperationError.OperationError
 import com.hedvig.android.apollo.ApolloOperationError.OperationException
 import com.hedvig.android.core.common.ErrorMessage
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 
-suspend fun <D : Operation.Data> ApolloCall<D>.safeExecute(): Either<ApolloOperationError, D> {
+sealed interface ApolloOperationError {
+  data class CacheMiss(val exception: CacheMissException) : ApolloOperationError
+
+  data class OperationException(val exception: ApolloException) : ApolloOperationError
+
+  data class OperationError(val message: String) : ApolloOperationError
 }
 
-suspend fun <D : Operation.Data, E> ApolloCall<D>.safeExecute(mapError: (ApolloOperationError) -> E): Either<E, D> {
+suspend fun <D : Operation.Data> ApolloCall<D>.safeExecute(): Either<ApolloOperationError, D> {
+  return either {
+    parseResponse(execute())
+  }
+}
+
+suspend fun <D : Operation.Data, ErrorType> ApolloCall<D>.safeExecute(
+  mapError: (ApolloOperationError) -> ErrorType,
+): Either<ErrorType, D> {
+  return safeExecute().mapLeft(mapError)
+}
+
+fun <D : Operation.Data> ApolloCall<D>.safeFlow(): Flow<Either<ApolloOperationError, D>> {
+  return toFlow().map {
+    either {
+      parseResponse(it)
+    }
+  }
+}
+
+fun <D : Operation.Data, ErrorType> ApolloCall<D>.safeFlow(
+  mapError: (ApolloOperationError) -> ErrorType,
+): Flow<Either<ErrorType, D>> {
+  return safeFlow().map { it.mapLeft(mapError) }
 }
 
 fun ErrorMessage(apolloOperationError: ApolloOperationError): ErrorMessage = object : ErrorMessage {
-  override val message = (apolloOperationError as? OperationError)?.error?.toString()
-  override val throwable = (apolloOperationError as? OperationException)?.exception
+  override val message = when (apolloOperationError) {
+    is CacheMiss -> "Cache miss"
+    is OperationError -> apolloOperationError.message
+    is OperationException -> apolloOperationError.exception.message
+  }
+  override val throwable = when (apolloOperationError) {
+    is CacheMiss -> apolloOperationError.exception
+    is OperationError -> null
+    is OperationException -> apolloOperationError.exception
+  }
 
   override fun toString(): String {
     return "ErrorMessage(message=$message, throwable=$throwable)"
   }
 }
 
-sealed interface ApolloOperationError {
-  data class OperationError(val error: GraphqlError) : ApolloOperationError
-
-  data class OperationException(val exception: ApolloException) : ApolloOperationError
-}
-
-suspend fun <D : Operation.Data> ApolloCall<D>.safeExecute(): OperationResult<D> {
-  return try {
-    executeV3().toOperationResult()
-  } catch (apolloException: ApolloException) {
-    OperationResult.Error.NetworkError(apolloException)
-  } catch (throwable: Throwable) {
-    if (throwable is CancellationException) {
-      throw throwable
+// https://www.apollographql.com/docs/kotlin/essentials/errors/#truth-table
+private fun <D : Operation.Data> Raise<ApolloOperationError>.parseResponse(response: ApolloResponse<D>): D {
+  val exception = response.exception
+  val data = response.data
+  val errors = response.errors
+  if (exception == null && data == null && errors == null) {
+    error("Non compliant server")
+  }
+  if (exception != null && errors != null) {
+    error("Impossible. Exceptions and errors can't exist at the same time")
+  }
+  if (exception != null && data != null) {
+    error("Impossible. Exceptions and data can't exist at the same time")
+  }
+  if (exception != null) {
+    if (exception is CacheMissException) {
+      raise(ApolloOperationError.CacheMiss(exception))
     }
-    OperationResult.Error.GeneralError(throwable)
+    raise(ApolloOperationError.OperationException(exception))
+  }
+  if (errors != null) {
+    raise(
+      ApolloOperationError.OperationError(
+        message = errors.map { error ->
+          buildString {
+            append(error.message)
+            if (error.extensions != null) {
+              append(error.extensions!!.toList().joinToString(prefix = " ext: [", postfix = "]", separator = ", "))
+            }
+          }
+        }.joinToString(separator = " | "),
+      ),
+    )
+  }
+  return requireNotNull(data) {
+    "Data must be null after checking all other possible scenarios"
   }
 }
-
-// region asd
-fun <D : Operation.Data, ErrorType> ApolloCall<D>.safeFlow(
-  ifEmpty: (message: String?, throwable: Throwable?) -> ErrorType,
-): Flow<Either<ErrorType, D>> {
-  return toFlowV3()
-    .map(ApolloResponse<D>::toOperationResult)
-    .map { it.toEither(ifEmpty) }
-    .catch { throwable ->
-      if (throwable is ApolloException) {
-        OperationResult.Error.NetworkError(throwable)
-      } else {
-        OperationResult.Error.GeneralError(throwable)
-      }.also {
-        emit(it.toEither(ifEmpty))
-      }
-    }
-}
-
-private fun <D : Operation.Data> ApolloResponse<D>.toOperationResult(): OperationResult<D> {
-  val data = data
-  return when {
-    hasErrors() -> {
-      val exception = errors?.first()?.extensions?.get("exception")
-      val body = (exception as? Map<*, *>)?.get("body")
-      val message = (body as? Map<*, *>)?.get("message") as? String
-
-      OperationResult.Error.OperationError(message ?: errors?.first()?.message)
-    }
-
-    data != null -> OperationResult.Success(data)
-    else -> OperationResult.Error.NoDataError("No data")
-  }
-}
-// endregion
