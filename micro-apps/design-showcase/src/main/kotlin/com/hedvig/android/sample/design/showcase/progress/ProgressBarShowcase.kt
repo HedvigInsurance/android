1609373fe40023d164aae38cdc0494fc5a537d99
stylianosgakis <stylianos@hedvig.com>
2024-08-27 16:29:39 +0200

    Simplify the showcase composable

--- micro-apps/design-showcase/src/main/kotlin/com/hedvig/android/sample/design/showcase/progress/ProgressBarShowcase.kt
+++ micro-apps/design-showcase/src/main/kotlin/com/hedvig/android/sample/design/showcase/progress/ProgressBarShowcase.kt
@@ -2,9 +2,7 @@ package com.hedvig.android.sample.design.showcase.progress
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,13 +15,11 @@ import com.hedvig.android.design.system.hedvig.HedvigThreeDotsProgressIndicator
 fun ProgressBarShowcase() {
   Column(
     horizontalAlignment = Alignment.CenterHorizontally,
-    verticalArrangement = Arrangement.Center,
+    verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.CenterVertically),
     modifier = Modifier.fillMaxSize(),
   ) {
     HedvigLinearProgressIndicator()
-    Spacer(Modifier.height(32.dp))
     HedvigThreeDotsProgressIndicator()
-    Spacer(Modifier.height(32.dp))
     HedvigCircularProgressIndicator()
   }
 }
