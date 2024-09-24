522fd198dce6995b2957fc1a6f653d9edd2b32aa
stylianosgakis <stylianos@hedvig.com>
2024-09-24 09:45:13 +0200

    Make progress indicator fit in the text field

--- app/feature/feature-login/src/main/kotlin/com/hedvig/android/feature/login/genericauth/EmailInputScreen.kt
+++ app/feature/feature-login/src/main/kotlin/com/hedvig/android/feature/login/genericauth/EmailInputScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -89,7 +90,7 @@ private fun EmailTextField(
     textFieldSize = HedvigTextFieldDefaults.TextFieldSize.Large,
     labelText = stringResource(hedvig.resources.R.string.login_text_input_email_address),
     suffix = if (loading) {
-      { HedvigCircularProgressIndicator() }
+      { HedvigCircularProgressIndicator(Modifier.size(24.dp)) }
     } else {
       null
     },
