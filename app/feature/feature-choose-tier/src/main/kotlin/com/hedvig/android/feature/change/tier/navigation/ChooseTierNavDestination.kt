18ffea7ebd0bcd59bc7788cb650dfb798a52ffdb
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-10-08 19:35:00 +0200

    summary view model

--- app/feature/feature-choose-tier/src/main/kotlin/com/hedvig/android/feature/change/tier/navigation/ChooseTierNavDestination.kt
+++ app/feature/feature-choose-tier/src/main/kotlin/com/hedvig/android/feature/change/tier/navigation/ChooseTierNavDestination.kt
@@ -58,15 +58,20 @@ internal sealed interface ChooseTierDestination {
   }
 
   @Serializable
-  data class Summary(
-    val quoteIdToSubmit: String,
-    // todo: also activation date, maybe???
-  ) : ChooseTierDestination, Destination {
+  data class Summary(val params: SummaryParameters) : ChooseTierDestination, Destination {
     companion object : DestinationNavTypeAware {
-      override val typeList: List<KType> = listOf(typeOf<String>())
+      override val typeList: List<KType> = listOf(typeOf<SummaryParameters>())
     }
   }
 }
+
+@Serializable
+@Parcelize
+data class SummaryParameters(
+  val quoteIdToSubmit: String,
+  val insuranceId: String,
+  val activationDateEpochDays: Int,
+) : Parcelable
 
 @Serializable
 @Parcelize
--- app/feature/feature-choose-tier/src/main/kotlin/com/hedvig/android/feature/change/tier/ui/stepsummary/SummaryViewModel.kt
+++ app/feature/feature-choose-tier/src/main/kotlin/com/hedvig/android/feature/change/tier/ui/stepsummary/SummaryViewModel.kt
@@ -1,51 +1,122 @@
 package com.hedvig.android.feature.change.tier.ui.stepsummary
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.hedvig.android.data.changetier.data.ChangeTierRepository
 import com.hedvig.android.data.changetier.data.TierDeductibleQuote
+import com.hedvig.android.feature.change.tier.data.GetCurrentContractDataUseCase
+import com.hedvig.android.feature.change.tier.navigation.SummaryParameters
+import com.hedvig.android.feature.change.tier.ui.stepcustomize.ContractData
+import com.hedvig.android.feature.change.tier.ui.stepsummary.SummaryEvent.ClearNavigation
 import com.hedvig.android.feature.change.tier.ui.stepsummary.SummaryEvent.ExpandCard
 import com.hedvig.android.feature.change.tier.ui.stepsummary.SummaryEvent.Reload
 import com.hedvig.android.feature.change.tier.ui.stepsummary.SummaryEvent.ScrollToDetails
 import com.hedvig.android.feature.change.tier.ui.stepsummary.SummaryEvent.SubmitQuote
+import com.hedvig.android.feature.change.tier.ui.stepsummary.SummaryState.Failure
 import com.hedvig.android.feature.change.tier.ui.stepsummary.SummaryState.Loading
+import com.hedvig.android.feature.change.tier.ui.stepsummary.SummaryState.MakingChanges
+import com.hedvig.android.feature.change.tier.ui.stepsummary.SummaryState.Success
 import com.hedvig.android.molecule.android.MoleculeViewModel
 import com.hedvig.android.molecule.public.MoleculePresenter
 import com.hedvig.android.molecule.public.MoleculePresenterScope
 
 internal class SummaryViewModel(
-  quoteId: String,
+  params: SummaryParameters,
   tierRepository: ChangeTierRepository,
+  getCurrentContractDataUseCase: GetCurrentContractDataUseCase,
 ) : MoleculeViewModel<SummaryEvent, SummaryState>(
     initialState = Loading,
     presenter = SummaryPresenter(
-      quoteId = quoteId,
+      params = params,
       tierRepository = tierRepository,
+      getCurrentContractDataUseCase = getCurrentContractDataUseCase,
     ),
   )
 
 private class SummaryPresenter(
-  private val quoteId: String,
+  private val params: SummaryParameters,
   private val tierRepository: ChangeTierRepository,
+  private val getCurrentContractDataUseCase: GetCurrentContractDataUseCase,
 ) : MoleculePresenter<SummaryEvent, SummaryState> {
   @Composable
   override fun MoleculePresenterScope<SummaryEvent>.present(lastState: SummaryState): SummaryState {
+    var submitIteration by remember { mutableIntStateOf(0) }
+    var loadDataIteration by remember { mutableIntStateOf(0) }
+    var currentState by remember { mutableStateOf(lastState) }
+
     CollectEvents { event ->
       when (event) {
         ExpandCard -> TODO()
-        Reload -> TODO()
+        Reload -> loadDataIteration++
         ScrollToDetails -> TODO()
-        SubmitQuote -> TODO()
+        SubmitQuote -> submitIteration++
+        ClearNavigation -> {
+          if (currentState !is Success) return@CollectEvents
+          currentState = (currentState as Success).copy(navigateToSuccessActivationDate = null, navigateToFail = false)
+        }
       }
     }
-    TODO("Not yet implemented")
+
+    LaunchedEffect(submitIteration) {
+      if (submitIteration > 0) {
+        val previousState = currentState
+        currentState = MakingChanges
+        tierRepository.submitChangeTierQuote(params.quoteIdToSubmit).fold(
+          ifLeft = {
+            currentState =
+              (previousState as Success).copy(navigateToSuccessActivationDate = null, navigateToFail = true)
+          },
+          ifRight = {
+            currentState = (previousState as Success).copy(
+              navigateToSuccessActivationDate = params.activationDateEpochDays,
+              navigateToFail = false,
+            )
+          },
+        )
+      }
+    }
+
+    LaunchedEffect(loadDataIteration) {
+      currentState = Loading
+      getCurrentContractDataUseCase.invoke(params.insuranceId).fold(
+        ifLeft = {
+          currentState = Failure
+        },
+        ifRight = { currentContractData ->
+          val quote = tierRepository.getQuoteById(params.quoteIdToSubmit)
+          val currentContract = ContractData(
+            contractGroup = currentContractData.productVariant.contractGroup,
+            activeDisplayPremium = currentContractData.currentDisplayPremium.toString(),
+            contractDisplayName = currentContractData.productVariant.displayName,
+            contractDisplaySubtitle = currentContractData.currentExposureName,
+          )
+          currentState = Success(
+            quote = quote,
+            currentContractData = currentContract,
+          )
+        },
+      )
+    }
+
+    return currentState
   }
 }
 
 internal sealed interface SummaryState {
   data object Loading : SummaryState
 
+  data object MakingChanges : SummaryState
+
   data class Success(
     val quote: TierDeductibleQuote,
+    val currentContractData: ContractData,
+    val navigateToSuccessActivationDate: Int? = null,
+    val navigateToFail: Boolean = false,
   ) : SummaryState
 
   data object Failure : SummaryState
@@ -59,4 +130,6 @@ internal sealed interface SummaryEvent {
   data object ExpandCard : SummaryEvent
 
   data object Reload : SummaryEvent
+
+  data object ClearNavigation : SummaryEvent
 }
