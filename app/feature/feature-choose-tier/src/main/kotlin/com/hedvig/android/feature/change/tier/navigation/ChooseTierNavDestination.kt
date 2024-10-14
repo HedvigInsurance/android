e226bb7fab992f3300a717fa22d52ff8a4ae7410
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-10-14 16:16:11 +0200

    remove navtype not needed

--- app/feature/feature-choose-tier/src/main/kotlin/com/hedvig/android/feature/change/tier/navigation/ChooseTierNavDestination.kt
+++ app/feature/feature-choose-tier/src/main/kotlin/com/hedvig/android/feature/change/tier/navigation/ChooseTierNavDestination.kt
@@ -12,11 +12,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class StartTierFlowDestination(
   val insuranceId: String,
-) : Destination {
-  companion object : DestinationNavTypeAware {
-    override val typeList: List<KType> = listOf(typeOf<String>())
-  }
-}
+) : Destination
 
 /**
  * The start of the flow, where we have can choose insurance to change its tier
