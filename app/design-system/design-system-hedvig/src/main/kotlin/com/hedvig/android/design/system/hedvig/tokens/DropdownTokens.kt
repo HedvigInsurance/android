635bb4e7b1fbe4ee1dcad5eb763961f2c2f5d5f2
stylianosgakis <stylianos@hedvig.com>
2024-10-22 22:24:49 +0200

    Fix dropdown color for surface on surface scenarios

--- app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/tokens/DropdownTokens.kt
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/tokens/DropdownTokens.kt
@@ -3,7 +3,10 @@ package com.hedvig.android.design.system.hedvig.tokens
 import androidx.compose.ui.unit.dp
 
 internal object DropdownTokens {
-  val ContainerColor = ColorSchemeKeyTokens.SurfacePrimary
+//  val ContainerColor = ColorSchemeKeyTokens.SurfacePrimary
+  // todo: Temp while this is still figured out and we get a new translucent or better contrast surface color
+  //  https://hedviginsurance.slack.com/archives/C03U9C6Q7TP/p1729261859881029?thread_ts=1729253107.272419&cid=C03U9C6Q7TP
+  val ContainerColor = ColorSchemeKeyTokens.ButtonSecondaryResting
   val EnabledChevronColor = ColorSchemeKeyTokens.FillPrimary
   val DisabledChevronColor = ColorSchemeKeyTokens.FillDisabled
   val TextColor = ColorSchemeKeyTokens.TextPrimary
