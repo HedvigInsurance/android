c836854a2d519a7a62b39572128f5f90526d04aa
stylianosgakis <stylianos@hedvig.com>
2024-10-22 17:46:22 +0200

    Add real UI impl for EnterNewAddressDestination

--- app/feature/feature-movingflow/src/main/kotlin/com/hedvig/android/feature/movingflow/ui/enternewaddress/EnterNewAddressDestination.kt
+++ app/feature/feature-movingflow/src/main/kotlin/com/hedvig/android/feature/movingflow/ui/enternewaddress/EnterNewAddressDestination.kt
@@ -1,16 +1,27 @@
 package com.hedvig.android.feature.movingflow.ui.enternewaddress
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -18,17 +29,30 @@ import com.hedvig.android.design.system.hedvig.HedvigButton
 import com.hedvig.android.design.system.hedvig.HedvigDialogError
 import com.hedvig.android.design.system.hedvig.HedvigErrorSection
 import com.hedvig.android.design.system.hedvig.HedvigFullScreenCenterAlignedProgress
+import com.hedvig.android.design.system.hedvig.HedvigMultiScreenPreview
+import com.hedvig.android.design.system.hedvig.HedvigNotificationCard
 import com.hedvig.android.design.system.hedvig.HedvigScaffold
 import com.hedvig.android.design.system.hedvig.HedvigStepper
 import com.hedvig.android.design.system.hedvig.HedvigText
 import com.hedvig.android.design.system.hedvig.HedvigTextField
 import com.hedvig.android.design.system.hedvig.HedvigTextFieldDefaults
+import com.hedvig.android.design.system.hedvig.HedvigTheme
+import com.hedvig.android.design.system.hedvig.HedvigToggle
+import com.hedvig.android.design.system.hedvig.NotificationDefaults.NotificationPriority
 import com.hedvig.android.design.system.hedvig.StepperDefaults.StepperSize.Medium
 import com.hedvig.android.design.system.hedvig.StepperDefaults.StepperStyle.Labeled
+import com.hedvig.android.design.system.hedvig.ToggleDefaults.ToggleDefaultStyleSize
+import com.hedvig.android.design.system.hedvig.ToggleDefaults.ToggleStyle
+import com.hedvig.android.design.system.hedvig.clearFocusOnTap
+import com.hedvig.android.design.system.hedvig.datepicker.HedvigDatePicker
+import com.hedvig.android.design.system.hedvig.datepicker.HedvigDatePickerState
+import com.hedvig.android.design.system.hedvig.datepicker.HedvigDateTimeFormatterDefaults
+import com.hedvig.android.design.system.hedvig.datepicker.getLocale
 import com.hedvig.android.feature.movingflow.compose.NoopValidator
 import com.hedvig.android.feature.movingflow.compose.ValidatedInput
 import com.hedvig.android.feature.movingflow.ui.enternewaddress.EnterNewAddressUiState.Content
 import com.hedvig.android.feature.movingflow.ui.enternewaddress.EnterNewAddressUiState.Content.PropertyType
+import com.hedvig.android.feature.movingflow.ui.enternewaddress.EnterNewAddressUiState.Content.PropertyType.Apartment.WithStudentOption
 import com.hedvig.android.feature.movingflow.ui.enternewaddress.EnterNewAddressUiState.Content.SubmittingInfoFailure.NetworkFailure
 import com.hedvig.android.feature.movingflow.ui.enternewaddress.EnterNewAddressUiState.Content.SubmittingInfoFailure.UserError
 import com.hedvig.android.feature.movingflow.ui.enternewaddress.EnterNewAddressUiState.Loading
@@ -42,7 +66,11 @@ import com.hedvig.android.feature.movingflow.ui.enternewaddress.EnterNewAddressV
 import com.hedvig.android.feature.movingflow.ui.enternewaddress.EnterNewAddressValidationError.InvalidSquareMeters
 import hedvig.resources.R
 import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.toJavaLocalDate
 import kotlinx.datetime.toLocalDateTime
 
 @Composable
@@ -115,82 +143,177 @@ private fun EnterNewAddressScreen(
       onDismissRequest = dismissSubmissionError,
     )
   }
-  Column(modifier) {
-    HedvigText("START Select a housing type")
-    Spacer(Modifier.weight(1f))
-    Spacer(Modifier.height(16.dp))
-    Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-      HedvigTextField(
-        text = uiState.address.value ?: "",
-        onValueChange = {
-          uiState.address.updateValue(it)
-        },
-        labelText = stringResource(R.string.CHANGE_ADDRESS_NEW_ADDRESS_LABEL),
-        textFieldSize = HedvigTextFieldDefaults.TextFieldSize.Medium,
-        errorState = when (val validationError = uiState.address.validationError) {
-          null -> HedvigTextFieldDefaults.ErrorState.NoError
-          else -> HedvigTextFieldDefaults.ErrorState.Error.WithMessage(validationError.string())
-        },
-        enabled = !uiState.shouldDisableInput,
-      )
-      HedvigTextField(
-        text = uiState.postalCode.value ?: "",
-        onValueChange = onValueChange@{
-          if (it.isEmpty()) {
-            uiState.postalCode.updateValue(null)
-          } else if (it.isDigitsOnly()) {
-            uiState.postalCode.updateValue(it)
-          }
-        },
-        labelText = stringResource(R.string.CHANGE_ADDRESS_NEW_POSTAL_CODE_LABEL),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        textFieldSize = HedvigTextFieldDefaults.TextFieldSize.Medium,
-        errorState = when (val validationError = uiState.postalCode.validationError) {
-          null -> HedvigTextFieldDefaults.ErrorState.NoError
-          else -> HedvigTextFieldDefaults.ErrorState.Error.WithMessage(validationError.string())
-        },
-        enabled = !uiState.shouldDisableInput,
-      )
-      HedvigTextField(
-        text = uiState.squareMeters.value?.toString() ?: "",
-        onValueChange = onValueChange@{
-          if (it.isEmpty()) {
-            uiState.squareMeters.updateValue(null)
-          } else {
-            val number = it.toIntOrNull()
-            if (number != null) {
-              uiState.squareMeters.updateValue(number)
-            }
-          }
-        },
-        labelText = stringResource(R.string.CHANGE_ADDRESS_NEW_LIVING_SPACE_LABEL),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        textFieldSize = HedvigTextFieldDefaults.TextFieldSize.Medium,
-        errorState = when (val validationError = uiState.squareMeters.validationError) {
-          null -> HedvigTextFieldDefaults.ErrorState.NoError
-          else -> HedvigTextFieldDefaults.ErrorState.Error.WithMessage(validationError.string())
-        },
-        enabled = !uiState.shouldDisableInput,
-      )
-    }
-    HedvigStepper(
-      text = when (val numberCoInsured = uiState.numberCoInsured.value) {
-        0 -> stringResource(R.string.CHANGE_ADDRESS_ONLY_YOU)
-        else -> stringResource(R.string.CHANGE_ADDRESS_YOU_PLUS, numberCoInsured)
-      },
-      stepperSize = Medium,
-      stepperStyle = Labeled(stringResource(R.string.CHANGE_ADDRESS_CO_INSURED_LABEL)),
-      onMinusClick = { uiState.numberCoInsured.updateValue(uiState.numberCoInsured.value - 1) },
-      onPlusClick = { uiState.numberCoInsured.updateValue(uiState.numberCoInsured.value + 1) },
-      isPlusEnabled = !uiState.isLoadingNextStep,
-      isMinusEnabled = !uiState.isLoadingNextStep,
-      errorText = uiState.numberCoInsured.validationError?.string(),
+  Column(
+    modifier
+      .clearFocusOnTap()
+      .padding(horizontal = 16.dp),
+  ) {
+    HedvigText(
+      text = stringResource(R.string.insurance_details_change_address_button),
+      style = HedvigTheme.typography.bodyMedium,
     )
-    HedvigButton(
-      onClick = submitInput,
-      text = stringResource(R.string.general_continue_button),
-      enabled = !uiState.shouldDisableInput,
-      isLoading = uiState.isLoadingNextStep,
+    HedvigText(
+      text = stringResource(R.string.CHANGE_ADDRESS_ENTER_NEW_ADDRESS_TITLE),
+      style = HedvigTheme.typography.bodyMedium,
+      color = HedvigTheme.colorScheme.textSecondary,
+    )
+    Spacer(Modifier.weight(1f))
+    Spacer(Modifier.height(8.dp))
+    Column(Modifier.verticalScroll(rememberScrollState())) {
+      Spacer(Modifier.height(8.dp))
+      Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+        HedvigTextField(
+          text = uiState.address.value ?: "",
+          onValueChange = {
+            uiState.address.updateValue(it)
+          },
+          labelText = stringResource(R.string.CHANGE_ADDRESS_NEW_ADDRESS_LABEL),
+          textFieldSize = HedvigTextFieldDefaults.TextFieldSize.Medium,
+          errorState = when (val validationError = uiState.address.validationError) {
+            null -> HedvigTextFieldDefaults.ErrorState.NoError
+            else -> HedvigTextFieldDefaults.ErrorState.Error.WithMessage(validationError.string())
+          },
+          enabled = !uiState.shouldDisableInput,
+        )
+        HedvigTextField(
+          text = uiState.postalCode.value ?: "",
+          onValueChange = onValueChange@{
+            if (it.isEmpty()) {
+              uiState.postalCode.updateValue(null)
+            } else if (it.isDigitsOnly()) {
+              uiState.postalCode.updateValue(it)
+            }
+          },
+          labelText = stringResource(R.string.CHANGE_ADDRESS_NEW_POSTAL_CODE_LABEL),
+          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+          textFieldSize = HedvigTextFieldDefaults.TextFieldSize.Medium,
+          errorState = when (val validationError = uiState.postalCode.validationError) {
+            null -> HedvigTextFieldDefaults.ErrorState.NoError
+            else -> HedvigTextFieldDefaults.ErrorState.Error.WithMessage(validationError.string())
+          },
+          enabled = !uiState.shouldDisableInput,
+        )
+        HedvigTextField(
+          text = uiState.squareMeters.value?.toString() ?: "",
+          onValueChange = onValueChange@{
+            if (it.isEmpty()) {
+              uiState.squareMeters.updateValue(null)
+            } else {
+              val number = it.toIntOrNull()
+              if (number != null) {
+                uiState.squareMeters.updateValue(number)
+              }
+            }
+          },
+          labelText = stringResource(R.string.CHANGE_ADDRESS_NEW_LIVING_SPACE_LABEL),
+          keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+          textFieldSize = HedvigTextFieldDefaults.TextFieldSize.Medium,
+          errorState = when (val validationError = uiState.squareMeters.validationError) {
+            null -> HedvigTextFieldDefaults.ErrorState.NoError
+            else -> HedvigTextFieldDefaults.ErrorState.Error.WithMessage(validationError.string())
+          },
+          suffix = {
+            HedvigText(
+              text = stringResource(R.string.CHANGE_ADDRESS_SIZE_SUFFIX),
+              color = HedvigTheme.colorScheme.textSecondary,
+            )
+          },
+          enabled = !uiState.shouldDisableInput,
+        )
+        HedvigStepper(
+          text = when (val numberCoInsured = uiState.numberCoInsured.value) {
+            0 -> stringResource(R.string.CHANGE_ADDRESS_ONLY_YOU)
+            else -> stringResource(R.string.CHANGE_ADDRESS_YOU_PLUS, numberCoInsured)
+          },
+          stepperSize = Medium,
+          stepperStyle = Labeled(stringResource(R.string.CHANGE_ADDRESS_CO_INSURED_LABEL)),
+          onMinusClick = { uiState.numberCoInsured.updateValue(uiState.numberCoInsured.value - 1) },
+          onPlusClick = { uiState.numberCoInsured.updateValue(uiState.numberCoInsured.value + 1) },
+          isPlusEnabled = !uiState.isLoadingNextStep,
+          isMinusEnabled = !uiState.isLoadingNextStep,
+          errorText = uiState.numberCoInsured.validationError?.string(),
+        )
+        DatePickerField(uiState.movingDate, uiState.allowedMovingDateRange)
+        if (uiState.propertyType is WithStudentOption) {
+          HedvigToggle(
+            labelText = stringResource(R.string.CHANGE_ADDRESS_STUDENT_LABEL),
+            turnedOn = uiState.propertyType.isStudentSelected,
+            onClick = { uiState.propertyType.selectedIsStudent.updateValue(it) },
+            toggleStyle = ToggleStyle.Default(ToggleDefaultStyleSize.Medium),
+          )
+        }
+      }
+      Spacer(Modifier.height(16.dp))
+      HedvigNotificationCard(stringResource(R.string.CHANGE_ADDRESS_COVERAGE_INFO_TEXT), NotificationPriority.Info)
+      Spacer(Modifier.height(16.dp))
+      HedvigButton(
+        onClick = submitInput,
+        text = stringResource(R.string.general_continue_button),
+        enabled = !uiState.shouldDisableInput,
+        isLoading = uiState.isLoadingNextStep,
+        modifier = Modifier.fillMaxWidth(),
+      )
+      Spacer(Modifier.height(16.dp))
+    }
+  }
+}
+
+@Composable
+private fun DatePickerField(
+  input: ValidatedInput<LocalDate?, LocalDate, EnterNewAddressValidationError>,
+  allowedMovingDateRange: ClosedRange<LocalDate>,
+  modifier: Modifier = Modifier,
+) {
+  var showDatePicker by rememberSaveable { mutableStateOf(false) }
+  val selectedDateMillis = input.value?.atStartOfDayIn(TimeZone.UTC)?.toEpochMilliseconds()
+  HedvigDatePicker(
+    isVisible = showDatePicker,
+    datePickerState = HedvigDatePickerState(
+      selectedDateMillis = selectedDateMillis,
+      displayedMonthMillis = selectedDateMillis,
+      yearRange = IntRange(
+        start = allowedMovingDateRange.start.year,
+        endInclusive = allowedMovingDateRange.endInclusive.year,
+      ),
+      minDateInMillis = allowedMovingDateRange.start.atStartOfDayIn(TimeZone.UTC)
+        .toEpochMilliseconds(),
+      maxDateInMillis = allowedMovingDateRange.endInclusive.atStartOfDayIn(TimeZone.UTC)
+        .toEpochMilliseconds(),
+      locale = getLocale(),
+    ),
+    onDismissRequest = { showDatePicker = false },
+    onConfirmRequest = { showDatePicker = false },
+    onSelectedDateChanged = {
+      if (it != null) {
+        input.updateValue(Instant.fromEpochMilliseconds(it).toLocalDateTime(TimeZone.UTC).date)
+      }
+    },
+  )
+  val locale = getLocale()
+  val rememberHedvigDateTimeFormatter = remember(locale) {
+    HedvigDateTimeFormatterDefaults.dateMonthAndYear(locale)
+  }
+  val text = input.value?.let { localDate ->
+    rememberHedvigDateTimeFormatter.format(localDate.toJavaLocalDate())
+  }
+  // Workaround to get the layout of the hedvigTextField, without the functionality of it. Perhaps room for another
+  //  component here
+  Box(modifier) {
+    HedvigTextField(
+      text = text ?: "",
+      onValueChange = {},
+      readOnly = true,
+      labelText = stringResource(R.string.CHANGE_ADDRESS_MOVING_DATE_LABEL),
+      textFieldSize = HedvigTextFieldDefaults.TextFieldSize.Medium,
+      trailingContent = {},
+    )
+    Box(
+      Modifier
+        .matchParentSize()
+        .clip(HedvigTheme.shapes.cornerLarge)
+        .clickable {
+          showDatePicker = true
+        },
     )
   }
 }
@@ -211,14 +334,17 @@ private fun EnterNewAddressValidationError.string(): String {
   }
 }
 
-@Preview
+@HedvigMultiScreenPreview
 @Composable
 fun PreviewEnterNewAddressScreen() {
   EnterNewAddressScreen(
     uiState = EnterNewAddressUiState.Content(
       moveFromAddressId = "moveFromAddressId",
-      movingDate =
-        ValidatedInput(Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date, NoopValidator()),
+      movingDate = ValidatedInput(
+        Clock.System.now().toLocalDateTime(TimeZone.UTC).date,
+        NoopValidator(),
+      ),
+      allowedMovingDateRange = LocalDate(2023, 1, 1)..LocalDate(2100, 1, 1),
       address = ValidatedInput<String?, String, EnterNewAddressValidationError>("address", NoopValidator()),
       postalCode = ValidatedInput("postalCode", NoopValidator()),
       squareMeters = ValidatedInput(0, NoopValidator()),
