e2956198510dad0cff63959674d1fb058a56cb7a
stylianosgakis <stylianos@hedvig.com>
2024-10-25 17:21:27 +0200

    Add styled dialog pickers for tier and deductible

--- app/feature/feature-movingflow/src/main/kotlin/com/hedvig/android/feature/movingflow/ui/chosecoveragelevelanddeductible/ChoseCoverageLevelAndDeductibleDestination.kt
+++ app/feature/feature-movingflow/src/main/kotlin/com/hedvig/android/feature/movingflow/ui/chosecoveragelevelanddeductible/ChoseCoverageLevelAndDeductibleDestination.kt
@@ -10,10 +10,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -22,20 +27,37 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.compose.dropUnlessResumed
+import com.hedvig.android.core.uidata.UiCurrencyCode.SEK
+import com.hedvig.android.core.uidata.UiMoney
+import com.hedvig.android.data.contract.ContractGroup.ACCIDENT
+import com.hedvig.android.data.contract.ContractType.SE_APARTMENT_BRF
 import com.hedvig.android.data.contract.android.toPillow
+import com.hedvig.android.data.productvariant.ProductVariant
+import com.hedvig.android.design.system.hedvig.ButtonDefaults.ButtonSize.Large
+import com.hedvig.android.design.system.hedvig.ChosenState.Chosen
+import com.hedvig.android.design.system.hedvig.ChosenState.NotChosen
 import com.hedvig.android.design.system.hedvig.DropdownDefaults.DropdownSize.Small
 import com.hedvig.android.design.system.hedvig.DropdownDefaults.DropdownStyle.Label
 import com.hedvig.android.design.system.hedvig.DropdownItem.SimpleDropdownItem
 import com.hedvig.android.design.system.hedvig.DropdownWithDialog
 import com.hedvig.android.design.system.hedvig.HedvigButton
 import com.hedvig.android.design.system.hedvig.HedvigCard
+import com.hedvig.android.design.system.hedvig.HedvigPreview
 import com.hedvig.android.design.system.hedvig.HedvigScaffold
 import com.hedvig.android.design.system.hedvig.HedvigText
 import com.hedvig.android.design.system.hedvig.HedvigTextButton
 import com.hedvig.android.design.system.hedvig.HedvigTextField
 import com.hedvig.android.design.system.hedvig.HedvigTextFieldDefaults.TextFieldSize
 import com.hedvig.android.design.system.hedvig.HedvigTheme
+import com.hedvig.android.design.system.hedvig.HighlightLabel
+import com.hedvig.android.design.system.hedvig.HighlightLabelDefaults.HighLightSize
+import com.hedvig.android.design.system.hedvig.HighlightLabelDefaults.HighlightColor
+import com.hedvig.android.design.system.hedvig.HighlightLabelDefaults.HighlightShade.MEDIUM
 import com.hedvig.android.design.system.hedvig.HorizontalItemsWithMaximumSpaceTaken
+import com.hedvig.android.design.system.hedvig.RadioOption
+import com.hedvig.android.feature.movingflow.data.MovingFlowQuotes.DisplayItem
+import com.hedvig.android.feature.movingflow.data.MovingFlowQuotes.MoveHomeQuote
+import com.hedvig.android.feature.movingflow.data.MovingFlowQuotes.MoveHomeQuote.Deductible
 import com.hedvig.android.feature.movingflow.ui.chosecoveragelevelanddeductible.ChoseCoverageLevelAndDeductibleUiState.Content
 import com.hedvig.android.feature.movingflow.ui.chosecoveragelevelanddeductible.ChoseCoverageLevelAndDeductibleUiState.Loading
 import com.hedvig.android.feature.movingflow.ui.chosecoveragelevelanddeductible.ChoseCoverageLevelAndDeductibleUiState.MissingOngoingMovingFlow
@@ -43,6 +65,7 @@ import com.hedvig.android.feature.movingflow.ui.chosecoveragelevelanddeductible.
 import com.hedvig.android.feature.movingflow.ui.chosecoveragelevelanddeductible.DeductibleOptions.NoOptions
 import com.hedvig.android.feature.movingflow.ui.chosecoveragelevelanddeductible.DeductibleOptions.OneOption
 import hedvig.resources.R
+import kotlinx.datetime.LocalDate
 
 @Composable
 internal fun ChoseCoverageLevelAndDeductibleDestination(
@@ -172,6 +195,11 @@ private fun CoverageCard(
         }
       }
       Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+        val chosenCoverageItemIndex = tiersInfo.coverageOptions.indexOfFirst {
+          it.moveHomeQuoteId == tiersInfo.selectedCoverage.id
+        }.let {
+          if (it == -1) null else it
+        }
         DropdownWithDialog(
           style = Label(
             items = tiersInfo.coverageOptions.map { coverageInfo ->
@@ -181,19 +209,26 @@ private fun CoverageCard(
           ),
           size = Small,
           hintText = tiersInfo.selectedCoverage.tierName,
-          onItemChosen = {
-            onSelectCoverageOption(tiersInfo.coverageOptions[it].moveHomeQuoteId)
-          },
-          chosenItemIndex = tiersInfo.coverageOptions.indexOfFirst {
-            it.moveHomeQuoteId == tiersInfo.selectedCoverage.id
-          }.let {
-            if (it == -1) null else it
-          },
-          onSelectorClick = {},
-        )
+          chosenItemIndex = chosenCoverageItemIndex,
+          applyDefaultDialogPadding = false,
+        ) { onDismissRequest ->
+          CoverageChoiceDialogContent(
+            coverageOptions = tiersInfo.coverageOptions,
+            initiallyChosenItemIndex = chosenCoverageItemIndex,
+            onDismissRequest = onDismissRequest,
+            onItemSelected = {
+              onSelectCoverageOption(tiersInfo.coverageOptions[it].moveHomeQuoteId)
+            },
+          )
+        }
         when (val deductibleOptions = tiersInfo.deductibleOptions) {
           NoOptions -> {}
           is MutlipleOptions -> {
+            val chosenDeductibleItemIndex = deductibleOptions.deductibleOptions.indexOfFirst {
+              it.homeQuoteId == tiersInfo.selectedDeductible?.id
+            }.let {
+              if (it == -1) null else it
+            }
             DropdownWithDialog(
               style = Label(
                 items = deductibleOptions.deductibleOptions.map { coverageInfo ->
@@ -204,16 +239,18 @@ private fun CoverageCard(
               size = Small,
               hintText = tiersInfo.selectedDeductible?.tierName
                 ?: stringResource(R.string.TIER_FLOW_DEDUCTIBLE_PLACEHOLDER),
-              onItemChosen = {
-                onSelectDeductibleOption(deductibleOptions.deductibleOptions[it].homeQuoteId)
-              },
-              chosenItemIndex = deductibleOptions.deductibleOptions.indexOfFirst {
-                it.homeQuoteId == tiersInfo.selectedDeductible?.id
-              }.let {
-                if (it == -1) null else it
-              },
-              onSelectorClick = {},
-            )
+              chosenItemIndex = chosenDeductibleItemIndex,
+              applyDefaultDialogPadding = false,
+            ) { onDismissRequest ->
+              DeductibleChoiceDialogContent(
+                deductibleOptions = deductibleOptions.deductibleOptions,
+                initiallyChosenItemIndex = chosenDeductibleItemIndex,
+                onDismissRequest = onDismissRequest,
+                onItemSelected = {
+                  onSelectDeductibleOption(deductibleOptions.deductibleOptions[it].homeQuoteId)
+                },
+              )
+            }
           }
 
           is OneOption -> {
@@ -243,4 +280,218 @@ private fun CoverageCard(
       )
     }
   }
+}
+
+@Composable
+private fun CoverageChoiceDialogContent(
+  coverageOptions: List<CoverageInfo>,
+  initiallyChosenItemIndex: Int?,
+  onItemSelected: (Int) -> Unit,
+  onDismissRequest: () -> Unit,
+) {
+  CommonChoiceDialogContent(
+    firstText = stringResource(R.string.TIER_FLOW_SELECT_DEDUCTIBLE_TITLE),
+    secondText = stringResource(R.string.TIER_FLOW_SELECT_DEDUCTIBLE_SUBTITLE),
+    radioOptions = coverageOptions.map { Triple(it.tierName, it.premium.toString(), it.tierDescription) },
+    initiallyChosenItemIndex = initiallyChosenItemIndex,
+    onItemSelected = onItemSelected,
+    onDismissRequest = onDismissRequest,
+  )
+}
+
+@Composable
+private fun DeductibleChoiceDialogContent(
+  deductibleOptions: List<DeductibleOption>,
+  initiallyChosenItemIndex: Int?,
+  onItemSelected: (Int) -> Unit,
+  onDismissRequest: () -> Unit,
+) {
+  CommonChoiceDialogContent(
+    firstText = stringResource(R.string.TIER_FLOW_SELECT_DEDUCTIBLE_TITLE),
+    secondText = stringResource(R.string.TIER_FLOW_SELECT_DEDUCTIBLE_SUBTITLE),
+    radioOptions = deductibleOptions.map { Triple(it.deductible.amount.toString(), it.deductible.displayText, null) },
+    initiallyChosenItemIndex = initiallyChosenItemIndex,
+    onItemSelected = onItemSelected,
+    onDismissRequest = onDismissRequest,
+  )
+}
+
+@Composable
+private fun CommonChoiceDialogContent(
+  firstText: String,
+  secondText: String,
+  radioOptions: List<Triple<String, String, String?>>,
+  initiallyChosenItemIndex: Int?,
+  onItemSelected: (Int) -> Unit,
+  onDismissRequest: () -> Unit,
+) {
+  var chosenItemIndex by remember { mutableStateOf(initiallyChosenItemIndex) }
+  Column(Modifier.padding(horizontal = 16.dp)) {
+    Spacer(Modifier.height(16.dp))
+    HedvigText(
+      text = firstText,
+      style = HedvigTheme.typography.bodyMedium,
+    )
+    HedvigText(
+      text = secondText,
+      style = HedvigTheme.typography.bodyMedium,
+      color = HedvigTheme.colorScheme.textSecondary,
+    )
+    Spacer(Modifier.height(12.dp))
+    Column(
+      verticalArrangement = Arrangement.spacedBy(4.dp),
+      modifier = Modifier.verticalScroll(rememberScrollState()),
+    ) {
+      Spacer(Modifier.height(12.dp))
+      radioOptions.forEachIndexed { index, option ->
+        RadioOption(
+          chosenState = if (chosenItemIndex == index) Chosen else NotChosen,
+          onClick = {
+            chosenItemIndex = index
+          },
+          optionContent = {
+            Column {
+              HorizontalItemsWithMaximumSpaceTaken(
+                startSlot = {
+                  HedvigText(
+                    text = option.first,
+                    modifier = Modifier.wrapContentSize(Alignment.CenterStart),
+                  )
+                },
+                endSlot = {
+                  HighlightLabel(
+                    labelText = option.second,
+                    size = HighLightSize.Small,
+                    color = HighlightColor.Grey(MEDIUM),
+                    modifier = Modifier.wrapContentSize(Alignment.TopEnd),
+                  )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                spaceBetween = 8.dp,
+              )
+              if (option.third != null) {
+                HedvigText(
+                  text = option.third!!,
+                  color = HedvigTheme.colorScheme.textSecondary,
+                  style = HedvigTheme.typography.label,
+                )
+              }
+            }
+          },
+        )
+      }
+    }
+    Spacer(Modifier.height(16.dp))
+    HedvigButton(
+      text = stringResource(R.string.GENERAL_CONFIRM),
+      onClick = {
+        if (chosenItemIndex != null) {
+          onItemSelected(chosenItemIndex!!)
+          onDismissRequest()
+        }
+      },
+      buttonSize = Large,
+      enabled = chosenItemIndex != null,
+      modifier = Modifier.fillMaxWidth(),
+    )
+    Spacer(Modifier.height(8.dp))
+    HedvigTextButton(
+      text = stringResource(R.string.general_cancel_button),
+      onClick = onDismissRequest,
+      buttonSize = Large,
+      modifier = Modifier.fillMaxWidth(),
+    )
+    Spacer(Modifier.height(16.dp))
+  }
+}
+
+@HedvigPreview
+@Composable
+fun PreviewCoverageChoiceDialogContent() {
+  CoverageChoiceDialogContent(
+    coverageOptions = List(3) {
+      CoverageInfo(
+        tierName = "tierName#$it",
+        moveHomeQuoteId = "moveHomeQuoteId#$it",
+        tierDescription = "tierDescription#$it",
+        premium = UiMoney(
+          amount = it.toDouble(),
+          currencyCode = SEK,
+        ),
+      )
+    },
+    initiallyChosenItemIndex = null,
+    onItemSelected = {},
+    onDismissRequest = {},
+  )
+}
+
+@HedvigPreview
+@Composable
+fun PreviewDeductibleChoiceDialogContent() {
+  DeductibleChoiceDialogContent(
+    deductibleOptions = List(3) {
+      DeductibleOption(
+        homeQuoteId = it.toString(),
+        deductible = Deductible(UiMoney(it.toDouble(), SEK), null, "Display text#$it"),
+      )
+    },
+    initiallyChosenItemIndex = null,
+    onItemSelected = {},
+    onDismissRequest = {},
+  )
+}
+
+@HedvigPreview
+@Composable
+fun PreviewChoseCoverageLevelAndDeductibleScreen() {
+  val allOptions: List<MoveHomeQuote> = List(3) {
+    MoveHomeQuote(
+      id = it.toString(),
+      premium = UiMoney(
+        amount = (it + 1) * 100.0,
+        currencyCode = SEK,
+      ),
+      startDate = LocalDate.parse("2025-01-01"),
+      displayItems = List(2) {
+        DisplayItem(
+          title = "title#$it",
+          subtitle = "subtitle#$it",
+          value = "value#$it",
+        )
+      },
+      exposureName = "exposureName",
+      productVariant = ProductVariant(
+        displayName = "displayName",
+        contractGroup = ACCIDENT,
+        contractType = SE_APARTMENT_BRF,
+        partner = null,
+        perils = listOf(),
+        insurableLimits = listOf(),
+        documents = listOf(),
+        displayTierName = null,
+        tierDescription = null,
+      ),
+      tierName = "Pat Vance",
+      tierLevel = 1299,
+      deductible = null,
+      defaultChoice = false,
+    )
+  }
+  ChoseCoverageLevelAndDeductibleScreen(
+    uiState = ChoseCoverageLevelAndDeductibleUiState.Content(
+      tiersInfo = TiersInfo(
+        allOptions = allOptions,
+        coverageOptions = List(2) {
+          CoverageInfo(it.toString(), "tierName#$it", "tierDescription#$it", UiMoney(it.toDouble(), SEK))
+        },
+        selectedCoverage = allOptions[0],
+        selectedDeductible = allOptions[0],
+      ),
+    ),
+    navigateUp = {},
+    onSubmit = {},
+    onSelectCoverageOption = {},
+    onSelectDeductibleOption = {},
+  )
 }
