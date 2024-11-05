212bedb117609bac1ce3b5662920760448c9eccd
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-11-05 09:37:33 +0100

    termsVersion lil fix

--- app/ui/ui-tiers-and-addons/src/main/kotlin/com/hedvig/android/tiersandaddons/QuoteCard.kt
+++ app/ui/ui-tiers-and-addons/src/main/kotlin/com/hedvig/android/tiersandaddons/QuoteCard.kt
@@ -293,6 +293,7 @@ private fun PreviewQuoteCard(
           },
           displayTierName = "displayTierName",
           tierDescription = "tierDescription",
+          termsVersion = "termsVersion"
         ),
         subtitle = "subtitle",
         premium = "premium",
