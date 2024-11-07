35f2712e7963c6396ed0bfeaba03acb281f1e8b3
stylianosgakis <stylianos@hedvig.com>
2024-11-07 21:29:03 +0100

    Ensure that the click indicator fills up the entire fixed column width

--- app/shared/tier-comparison/src/main/kotlin/com/hedvig/android/shared/tier/comparison/ui/ComparisonDestination.kt
+++ app/shared/tier-comparison/src/main/kotlin/com/hedvig/android/shared/tier/comparison/ui/ComparisonDestination.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
@@ -261,7 +262,7 @@ private fun FixSizedComparisonDataColumn(
       Surface(
         onClick = { selectComparisonRow(comparisonRow) },
         color = HedvigTheme.colorScheme.backgroundPrimary,
-        modifier = Modifier,
+        modifier = Modifier.fillMaxWidth(),
       ) {
         HedvigText(
           text = comparisonRow.title,
