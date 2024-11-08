f2c997671455577bcdfedf41b08f7cc9865b1273
stylianosgakis <stylianos@hedvig.com>
2024-11-09 00:33:00 +0100

    Improvements on LayoutWithoutPlacement
    Ensure that the `content` child gets the minimum constraints setup to be
    at least as big as the `sizeAdjustingContent` was, but also allow itself
    to adjust the parent's size, sometihng which `matchParentSize` does not
    let you do.
    This as a consequence removes the `BoxScope` from the API, which
    requires a couple of call-site changes, but nothing major.

--- app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/Button.kt
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/Button.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.ReadOnlyComposable
@@ -77,7 +78,7 @@ fun HedvigButton(
           ThreeDotsLoading(
             stableColor = buttonColors.activeLoadingIndicatorColor,
             temporaryColor = buttonColors.inactiveLoadingIndicatorColor,
-            modifier = Modifier.align(Alignment.Center),
+            modifier = Modifier.wrapContentSize(Alignment.Center),
           )
         }
       } else {
