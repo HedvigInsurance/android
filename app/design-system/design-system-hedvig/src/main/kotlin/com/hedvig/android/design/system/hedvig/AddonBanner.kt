fb6b56a9c6c2950db1fd7c4aeddf3d85bb861686
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2024-12-06 13:13:23 +0100

    add lint baseline

--- app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/AddonBanner.kt
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/AddonBanner.kt
@@ -2,6 +2,7 @@ package com.hedvig.android.design.system.hedvig
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -10,7 +11,9 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun FeatureAddonBanner(
-  modifier: Modifier = Modifier
+  buttonText: String,
+  onButtonClick: () -> Unit,
+  modifier: Modifier = Modifier,
 ) {
   val containerColor = HedvigTheme.colorScheme.fillNegative
   val borderColor = HedvigTheme.colorScheme.borderPrimary
@@ -18,24 +21,19 @@ fun FeatureAddonBanner(
     modifier = modifier,
     shape = HedvigTheme.shapes.cornerLarge,
     color = containerColor,
-    border = borderColor) {
-
-      Row(Modifier.padding()) {
-
-              Spacer(Modifier.height(8.dp))
-              HedvigTheme(darkTheme = buttonDarkTheme) {
-                HedvigButton(
-                  text = ,
-                  enabled = true,
-                  onClick = style.onButtonClick,
-                  buttonStyle = priority.buttonStyle,
-                  buttonSize = Small,
-                  modifier = Modifier.fillMaxWidth(),
-                )
-              }
-            }
-          }
-        }
-      }
+    border = borderColor,
+  ) {
+    Row(Modifier.padding()) {
+      Spacer(Modifier.height(8.dp))
+      HedvigButton(
+        text = buttonText,
+        enabled = true,
+        onClick = onButtonClick,
+        buttonStyle = ButtonDefaults.ButtonStyle.Secondary,
+        buttonSize = ButtonDefaults.ButtonSize.Small,
+        modifier = Modifier.fillMaxWidth(),
+      )
     }
   }
+}
+
