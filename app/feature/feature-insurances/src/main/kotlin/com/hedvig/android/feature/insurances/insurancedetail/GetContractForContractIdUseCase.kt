a5fbbef0f74e785b43967fb022afa8c6edf422ef
Hugo Linder <hugo@hedvig.com>
2025-02-14 12:02:09 +0100

    Force network update when viewing contract detail in order to refresh new co insured after completing co insured flow.

--- app/feature/feature-insurances/src/main/kotlin/com/hedvig/android/feature/insurances/insurancedetail/GetContractForContractIdUseCase.kt
+++ app/feature/feature-insurances/src/main/kotlin/com/hedvig/android/feature/insurances/insurancedetail/GetContractForContractIdUseCase.kt
@@ -10,38 +10,34 @@ import com.hedvig.android.feature.insurances.data.InsuranceContract
 import com.hedvig.android.logger.LogPriority
 import com.hedvig.android.logger.logcat
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 
 internal interface GetContractForContractIdUseCase {
-  fun invoke(contractId: String): Flow<Either<GetContractForContractIdError, InsuranceContract>>
+  suspend fun invoke(contractId: String): Flow<Either<GetContractForContractIdError, InsuranceContract>>
 }
 
 internal class GetContractForContractIdUseCaseImpl(
   private val getInsuranceContractsUseCaseProvider: Provider<GetInsuranceContractsUseCase>,
 ) : GetContractForContractIdUseCase {
-  override fun invoke(contractId: String): Flow<Either<GetContractForContractIdError, InsuranceContract>> {
-    return flow {
-      getInsuranceContractsUseCaseProvider
-        .provide()
-        .invoke(forceNetworkFetch = false)
-        .map { insuranceContractResult ->
-          either {
-            val contract = insuranceContractResult
-              .mapLeft { GetContractForContractIdError.GenericError(it) }
-              .bind()
-              .firstOrNull { it.id == contractId }
-            ensureNotNull(contract) {
-              GetContractForContractIdError.NoContractFound(
-                ErrorMessage("No contract found with id: $contractId").also {
-                  logcat(LogPriority.ERROR) { it.message.toString() }
-                },
-              )
-            }
+  override suspend fun invoke(contractId: String): Flow<Either<GetContractForContractIdError, InsuranceContract>> {
+    return getInsuranceContractsUseCaseProvider
+      .provide()
+      .invoke(forceNetworkFetch = true)
+      .map { insuranceContractResult ->
+        either {
+          val contract = insuranceContractResult
+            .mapLeft { GetContractForContractIdError.GenericError(it) }
+            .bind()
+            .firstOrNull { it.id == contractId }
+          ensureNotNull(contract) {
+            GetContractForContractIdError.NoContractFound(
+              ErrorMessage("No contract found with id: $contractId").also {
+                logcat(LogPriority.ERROR) { it.message.toString() }
+              },
+            )
           }
         }
-        .collect(this)
-    }
+      }
   }
 }
 
