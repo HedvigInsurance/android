09083db870d575dece3ffa6ac621c5d709cd1315
stylianosgakis <stylianos@hedvig.com>
2025-02-26 13:32:21 +0100

    Remove unused and never sent notification type

--- app/app/src/main/kotlin/com/hedvig/android/app/notification/senders/ReferralsNotificationSender.kt
+++ app/app/src/main/kotlin/com/hedvig/android/app/notification/senders/ReferralsNotificationSender.kt
@@ -6,13 +6,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.PendingIntentCompat
-import androidx.core.app.TaskStackBuilder
 import androidx.core.net.toUri
 import com.google.firebase.messaging.RemoteMessage
-import com.hedvig.android.app.MainActivity
-import com.hedvig.android.app.notification.DATA_MESSAGE_BODY
-import com.hedvig.android.app.notification.DATA_MESSAGE_TITLE
-import com.hedvig.android.app.notification.getImmutablePendingIntentFlags
 import com.hedvig.android.navigation.core.HedvigDeepLinkContainer
 import com.hedvig.android.notification.core.HedvigNotificationChannel
 import com.hedvig.android.notification.core.NotificationSender
@@ -27,16 +22,11 @@ class ReferralsNotificationSender(
   override suspend fun sendNotification(type: String, remoteMessage: RemoteMessage) {
     when (type) {
       NOTIFICATION_TYPE_REFERRAL_SUCCESS -> sendReferralSuccessfulNotification(remoteMessage)
-      NOTIFICATION_TYPE_REFERRALS_CAMPAIGN -> sendReferralCampaignNotification(remoteMessage)
     }
   }
 
-  override fun handlesNotificationType(notificationType: String) = when (notificationType) {
-    NOTIFICATION_TYPE_REFERRAL_SUCCESS,
-    NOTIFICATION_TYPE_REFERRALS_CAMPAIGN,
-    -> true
-    else -> false
-  }
+  override fun handlesNotificationType(notificationType: String) =
+    notificationType == NOTIFICATION_TYPE_REFERRAL_SUCCESS
 
   private fun sendReferralSuccessfulNotification(remoteMessage: RemoteMessage) {
     val foreverIntent = Intent(Intent.ACTION_VIEW, hedvigDeepLinkContainer.forever.first().toUri())
@@ -67,28 +57,6 @@ class ReferralsNotificationSender(
     sendNotificationInner(REFERRAL_NOTIFICATION_ID, notificationBuilder.build())
   }
 
-  private fun sendReferralCampaignNotification(remoteMessage: RemoteMessage) {
-    val pendingIntent: PendingIntent? = TaskStackBuilder
-      .create(context)
-      .addNextIntent(
-        // todo consider opening Forever directly instead here
-        MainActivity.newInstance(context),
-      )
-      .getPendingIntent(0, getImmutablePendingIntentFlags())
-
-    val title = remoteMessage.data[DATA_MESSAGE_TITLE]
-    val message = remoteMessage.data[DATA_MESSAGE_BODY]
-
-    val notificationBuilder = createNotificationBuilder(
-      context = context,
-      title = title,
-      body = message,
-      pendingIntent = pendingIntent,
-    )
-
-    sendNotificationInner(REFERRALS_CAMPAIGN_ID, notificationBuilder.build())
-  }
-
   private fun sendNotificationInner(id: Int, notification: Notification) {
     sendHedvigNotification(
       context = context,
@@ -117,11 +85,8 @@ class ReferralsNotificationSender(
   companion object {
     private const val REFERRAL_NOTIFICATION_ID = 2
 
-    private const val REFERRALS_CAMPAIGN_ID = 12
-
     internal const val DATA_MESSAGE_REFERRED_SUCCESS_NAME = "DATA_MESSAGE_REFERRED_SUCCESS_NAME"
 
     private const val NOTIFICATION_TYPE_REFERRAL_SUCCESS = "REFERRAL_SUCCESS"
-    private const val NOTIFICATION_TYPE_REFERRALS_CAMPAIGN = "REFERRALS_CAMPAIGN"
   }
 }
