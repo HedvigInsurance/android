0db236d80825fc342cb17881ada10c2b65a75fff
stylianosgakis <stylianos@hedvig.com>
2025-03-06 18:29:09 +0100

    Map ComparisonData model to the new UI state

--- app/shared/tier-comparison/src/main/kotlin/com/hedvig/android/shared/tier/comparison/ui/ComparisonViewModel.kt
+++ app/shared/tier-comparison/src/main/kotlin/com/hedvig/android/shared/tier/comparison/ui/ComparisonViewModel.kt
@@ -7,18 +7,21 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import arrow.core.Either.Left
-import arrow.core.Either.Right
 import com.hedvig.android.molecule.android.MoleculeViewModel
 import com.hedvig.android.molecule.public.MoleculePresenter
 import com.hedvig.android.molecule.public.MoleculePresenterScope
-import com.hedvig.android.shared.tier.comparison.data.ComparisonData
+import com.hedvig.android.shared.tier.comparison.data.ComparisonData.CoverageLevel.CoveredItem.CoveredStatus.Covered
+import com.hedvig.android.shared.tier.comparison.data.ComparisonData.CoverageLevel.CoveredItem.CoveredStatus.CoveredWithDescription
+import com.hedvig.android.shared.tier.comparison.data.ComparisonData.CoverageLevel.CoveredItem.CoveredStatus.NotCovered
 import com.hedvig.android.shared.tier.comparison.data.GetCoverageComparisonUseCase
 import com.hedvig.android.shared.tier.comparison.navigation.ComparisonParameters
 import com.hedvig.android.shared.tier.comparison.ui.ComparisonEvent.Reload
 import com.hedvig.android.shared.tier.comparison.ui.ComparisonState.Failure
 import com.hedvig.android.shared.tier.comparison.ui.ComparisonState.Loading
 import com.hedvig.android.shared.tier.comparison.ui.ComparisonState.Success
+import com.hedvig.android.shared.tier.comparison.ui.ComparisonState.Success.CoverageLevel
+import com.hedvig.android.shared.tier.comparison.ui.ComparisonState.Success.CoverageLevel.ComparisonItem.CoveredStatus.Checkmark
+import com.hedvig.android.shared.tier.comparison.ui.ComparisonState.Success.CoverageLevel.ComparisonItem.CoveredStatus.Description
 
 class ComparisonViewModel(
   comparisonParameters: ComparisonParameters,
@@ -49,20 +52,38 @@ private class ComparisonPresenter(
     }
 
     LaunchedEffect(loadIteration) {
-      val result = getCoverageComparisonUseCase.invoke(termsIds)
-      currentState = when (result) {
-        is Left -> {
-          Failure
-        }
-
-        is Right -> {
-          val selectedColumn = result.value.columns.firstOrNull { it.termsVersion == selectedTermVersion }
-          val selectedIndex = selectedColumn?.let {
-            result.value.columns.indexOf(it)
-          }
-          Success(result.value, selectedIndex)
-        }
+      if (currentState == Failure) {
+        currentState = Loading
       }
+      val result = getCoverageComparisonUseCase.invoke(termsIds)
+      currentState = result.fold(
+        ifLeft = { Failure },
+        ifRight = { comparisonData ->
+          val initialTabIndex = comparisonData.coverageLevels.indexOfFirst { it.termsVersion == selectedTermVersion }
+          Success(
+            coverageLevels = comparisonData.coverageLevels.map { coverageLevel ->
+              CoverageLevel(
+                title = coverageLevel.displayNameTier,
+                items = coverageLevel
+                  .coveredItems
+                  .filterNot { it.coveredStatus == NotCovered }
+                  .map { coveredItem ->
+                    Success.CoverageLevel.ComparisonItem(
+                      coveredItem.title,
+                      coveredItem.description,
+                      when (coveredItem.coveredStatus) {
+                        Covered -> Checkmark
+                        is CoveredWithDescription -> Description(coveredItem.coveredStatus.coverageText)
+                        NotCovered -> error("Must be filtered out from the UI completely")
+                      },
+                    )
+                  },
+              )
+            },
+            initialTabIndex = initialTabIndex,
+          )
+        },
+      )
     }
 
     return currentState
@@ -75,9 +96,26 @@ sealed interface ComparisonState {
   data object Failure : ComparisonState
 
   data class Success(
-    val comparisonData: ComparisonData,
-    val selectedColumnIndex: Int?,
-  ) : ComparisonState
+    val coverageLevels: List<CoverageLevel>,
+    val initialTabIndex: Int,
+  ) : ComparisonState {
+    data class CoverageLevel(
+      val title: String,
+      val items: List<ComparisonItem>,
+    ) {
+      data class ComparisonItem(
+        val title: String,
+        val description: String,
+        val coveredStatus: CoveredStatus,
+      ) {
+        sealed interface CoveredStatus {
+          data object Checkmark : CoveredStatus
+
+          data class Description(val description: String) : CoveredStatus
+        }
+      }
+    }
+  }
 }
 
 sealed interface ComparisonEvent {
