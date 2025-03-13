42c04d246562ac752f666722d007c6729703831c
stylianosgakis <stylianos@hedvig.com>
2025-03-13 21:41:36 +0100

    Rename NetworkCacheManagerImpl to a more descriptive name

--- app/apollo/apollo-network-cache-manager/src/main/kotlin/com/hedvig/android/apollo/di/NetworkCacheManagerModule.kt
+++ app/apollo/apollo-network-cache-manager/src/main/kotlin/com/hedvig/android/apollo/di/NetworkCacheManagerModule.kt
@@ -1,10 +1,10 @@
 package com.hedvig.android.apollo.di
 
 import com.apollographql.apollo.ApolloClient
+import com.hedvig.android.apollo.ApolloNetworkCacheManager
 import com.hedvig.android.apollo.NetworkCacheManager
-import com.hedvig.android.apollo.NetworkCacheManagerImpl
 import org.koin.dsl.module
 
 val networkCacheManagerModule = module {
-  single<NetworkCacheManager> { NetworkCacheManagerImpl(get<ApolloClient>()) }
+  single<NetworkCacheManager> { ApolloNetworkCacheManager(get<ApolloClient>()) }
 }
