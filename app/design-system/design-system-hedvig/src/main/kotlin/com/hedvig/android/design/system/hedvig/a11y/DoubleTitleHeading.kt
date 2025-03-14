be6b192fcec4d09172ff08c67a9578ff2f485f42
stylianosgakis <stylianos@hedvig.com>
2025-03-14 14:44:16 +0100

    Add DoubleTitleHeading
    Should allow for a one-pass reading of the content through a11y services
    by merging the two headers as one semantic heading

---
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/a11y/DoubleTitleHeading.kt
@@ -0,0 +1,33 @@
+package com.hedvig.android.design.system.hedvig.a11y
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.LineBreak
+import com.hedvig.android.design.system.hedvig.HedvigText
+import com.hedvig.android.design.system.hedvig.HedvigTheme
+
+@Composable
+fun DoubleTitleHeading(title: String, description: String, modifier: Modifier = Modifier) {
+  Column(
+    modifier = modifier.semantics(mergeDescendants = true) {
+      heading()
+    },
+  ) {
+    HedvigText(
+      text = title,
+      style = HedvigTheme.typography.headlineMedium.copy(
+        lineBreak = LineBreak.Heading,
+      ),
+    )
+    HedvigText(
+      text = description,
+      style = HedvigTheme.typography.headlineMedium.copy(
+        lineBreak = LineBreak.Heading,
+        color = HedvigTheme.colorScheme.textSecondary,
+      ),
+    )
+  }
+}
