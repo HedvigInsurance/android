63bb4f80d88862ec9c6a319d609f34d6e4cc60e3
stylianosgakis <stylianos@hedvig.com>
2025-03-26 14:30:43 +0100

    Consolidate pre-build script in one
    Make setting up the lokalise properties file, the authlib auth and the
    downloading of apollo/lokalise things

--- scripts/ci-prebuild.sh
+++ scripts/ci-prebuild.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 set +uex
 
+echo "" >> ~/.gradle/gradle.properties
+echo "authlibUsername=${HEDVIG_GITHUB_PACKAGES_USER}" >> ~/.gradle/gradle.properties
+echo "authlibPassword=${HEDVIG_GITHUB_PACKAGES_TOKEN}" >> ~/.gradle/gradle.properties
+
 cat <<EOT > lokalise.properties
 id=${LOKALISE_ID}
 token=${LOKALISE_TOKEN}
