aa91ce9e28f0c06cf81dab46442c7f6f1c1ebc71
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2025-04-30 11:31:56 +0200

    flow instead of execute

--- app/member-reminders/member-reminders-public/src/main/kotlin/com/hedvig/android/memberreminders/GetNeedsCoInsuredInfoRemindersUseCase.kt
+++ app/member-reminders/member-reminders-public/src/main/kotlin/com/hedvig/android/memberreminders/GetNeedsCoInsuredInfoRemindersUseCase.kt
@@ -2,6 +2,7 @@ package com.hedvig.android.memberreminders
 
 import arrow.core.Either
 import arrow.core.NonEmptyList
+import arrow.core.left
 import arrow.core.raise.either
 import arrow.core.raise.ensureNotNull
 import arrow.core.toNonEmptyListOrNull
@@ -9,12 +10,13 @@ import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.cache.normalized.FetchPolicy
 import com.apollographql.apollo.cache.normalized.fetchPolicy
 import com.hedvig.android.apollo.ErrorMessage
-import com.hedvig.android.apollo.safeExecute
+import com.hedvig.android.apollo.safeFlow
 import com.hedvig.android.core.common.ErrorMessage
 import com.hedvig.android.featureflags.FeatureManager
 import com.hedvig.android.featureflags.flags.Feature
-import com.hedvig.android.logger.logcat
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.mapLatest
 import octopus.NeedsCoInsuredInfoReminderQuery
 
@@ -27,27 +29,32 @@ internal class GetNeedsCoInsuredInfoRemindersUseCaseImpl(
   private val featureManager: FeatureManager,
 ) : GetNeedsCoInsuredInfoRemindersUseCase {
   override fun invoke(): Flow<Either<CoInsuredInfoReminderError, NonEmptyList<MemberReminder.CoInsuredInfo>>> {
-    return featureManager.isFeatureEnabled(Feature.EDIT_COINSURED).mapLatest { isEditCoInsuredFeatureEnabled ->
-      either {
-        if (!isEditCoInsuredFeatureEnabled) {
-          raise(CoInsuredInfoReminderError.CoInsuredReminderNotEnabled)
+    return featureManager.isFeatureEnabled(Feature.EDIT_COINSURED).flatMapLatest { isEditCoInsuredFeatureEnabled ->
+      if (!isEditCoInsuredFeatureEnabled) {
+        flow {
+          emit(CoInsuredInfoReminderError.CoInsuredReminderNotEnabled.left())
         }
-        val contracts = apolloClient.query(NeedsCoInsuredInfoReminderQuery())
-          .fetchPolicy(FetchPolicy.NetworkOnly)
-          .safeExecute(::ErrorMessage)
-          .mapLeft(CoInsuredInfoReminderError::NetworkError)
-          .bind()
-          .currentMember
-          .activeContracts
+      } else {
+        apolloClient.query(NeedsCoInsuredInfoReminderQuery())
+          .fetchPolicy(FetchPolicy.CacheAndNetwork)
+          .safeFlow(::ErrorMessage)
+          .mapLatest { result: Either<ErrorMessage, NeedsCoInsuredInfoReminderQuery.Data> ->
+            either {
+              val contracts = result.mapLeft(CoInsuredInfoReminderError::NetworkError)
+                .bind()
+                .currentMember
+                .activeContracts
 
-        val coInsuredReminderInfoList = contracts
-          .filter { it.hasMissingInfoAndIsNotTerminating() }
-          .map { MemberReminder.CoInsuredInfo(it.id) }
-          .toNonEmptyListOrNull()
+              val coInsuredReminderInfoList = contracts
+                .filter { it.hasMissingInfoAndIsNotTerminating() }
+                .map { MemberReminder.CoInsuredInfo(it.id) }
+                .toNonEmptyListOrNull()
 
-        ensureNotNull(coInsuredReminderInfoList) {
-          CoInsuredInfoReminderError.NoCoInsuredReminders
-        }
+              ensureNotNull(coInsuredReminderInfoList) {
+                CoInsuredInfoReminderError.NoCoInsuredReminders
+              }
+            }
+          }
       }
     }
   }
