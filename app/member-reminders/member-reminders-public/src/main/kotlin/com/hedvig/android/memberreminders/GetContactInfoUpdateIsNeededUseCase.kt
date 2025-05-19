72b13eaab25327a8265ea1073857d547e4fec671
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2025-05-19 14:27:05 +0200

    add schema

--- app/member-reminders/member-reminders-public/src/main/kotlin/com/hedvig/android/memberreminders/GetContactInfoUpdateIsNeededUseCase.kt
+++ app/member-reminders/member-reminders-public/src/main/kotlin/com/hedvig/android/memberreminders/GetContactInfoUpdateIsNeededUseCase.kt
@@ -3,9 +3,14 @@ package com.hedvig.android.memberreminders
 import arrow.core.Either
 import arrow.core.raise.either
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.cache.normalized.FetchPolicy
+import com.apollographql.apollo.cache.normalized.fetchPolicy
+import com.hedvig.android.apollo.ErrorMessage
+import com.hedvig.android.apollo.safeFlow
 import com.hedvig.android.core.common.ErrorMessage
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.mapLatest
+import octopus.NeedsContactInfoUpdateReminderQuery
 
 interface GetContactInfoUpdateIsNeededUseCase {
   fun invoke(): Flow<Either<ErrorMessage, MemberReminder.ContactInfoUpdateNeeded?>>
@@ -15,25 +20,20 @@ internal class GetContactInfoUpdateIsNeededUseCaseImpl(
   private val apolloClient: ApolloClient,
 ) : GetContactInfoUpdateIsNeededUseCase {
   override fun invoke(): Flow<Either<ErrorMessage, MemberReminder.ContactInfoUpdateNeeded?>> {
-//    return apolloClient.query(
-//      NeedsContactInfoUpdateReminderQuery()
-//    )
-//      .fetchPolicy(FetchPolicy.CacheAndNetwork)
-//      .safeFlow(::ErrorMessage)
-//      .mapLatest { result: Either<ErrorMessage, NeedsContactInfoUpdateReminderQuery.Data> ->
-//        either {
-//          val isContactInfoUpdateNeeded = result.
-//            .bind()
-//            .currentMember
-//            .memberActions
-//            .isContactInfoUpdateNeeded
-//          if (isContactInfoUpdateNeeded) Unit else null
-//        }
-    return flowOf(
-      either {
-        null
-        // todo: implement instead the commented out part when BE will be ready
-      },
+    return apolloClient.query(
+      NeedsContactInfoUpdateReminderQuery(),
     )
+      .fetchPolicy(FetchPolicy.CacheAndNetwork)
+      .safeFlow(::ErrorMessage)
+      .mapLatest { result: Either<ErrorMessage, NeedsContactInfoUpdateReminderQuery.Data> ->
+        either {
+          val isContactInfoUpdateNeeded = result
+            .bind()
+            .currentMember
+            .memberActions
+            ?.isContactInfoUpdateNeeded
+          if (isContactInfoUpdateNeeded == true) MemberReminder.ContactInfoUpdateNeeded() else null
+        }
+      }
   }
 }
