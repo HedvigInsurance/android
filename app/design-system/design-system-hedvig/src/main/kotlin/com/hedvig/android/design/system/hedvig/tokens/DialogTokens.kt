a9b8a928d678d780c6bfb9f97e9ece7e12b88697
stylianosgakis <stylianos@hedvig.com>
2025-06-12 11:41:15 +0200

    Add support for a dialog with a title and a single button
    The DS does not normally have such a case, but we have a need for this
    Multi-select dialogs need a way to exit the dialog in a a11y-friendly
    way, since selecting an option does not auto-dismiss the dialog as it
    happens for the non-multi-select alternative.
    
    Make use of the existing CoreSelectDialog styling to add the buttons the
    same way they are added for the other dialog types

--- app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/tokens/DialogTokens.kt
+++ app/design-system/design-system-hedvig/src/main/kotlin/com/hedvig/android/design/system/hedvig/tokens/DialogTokens.kt
@@ -8,6 +8,9 @@ internal object DialogTokens {
   val ContainerShape = ShapeKeyTokens.CornerXLarge
   val ShadowElevation = 2.dp
   val NoButtonsPadding = PaddingValues(16.dp)
+  val TitlePlusButtonPadding = PaddingValues(top = 20.dp, bottom = 16.dp, start = 16.dp, end = 16.dp)
   val BigButtonsPadding = PaddingValues(top = 48.dp, bottom = 16.dp, start = 16.dp, end = 16.dp)
   val SmallButtonsPadding = PaddingValues(top = 48.dp, bottom = 24.dp, start = 24.dp, end = 24.dp)
+  val ContentToButtonsPaddingHeight = 40.dp
+  val ContentToTitlePlusButtonPaddingHeight = 24.dp
 }
