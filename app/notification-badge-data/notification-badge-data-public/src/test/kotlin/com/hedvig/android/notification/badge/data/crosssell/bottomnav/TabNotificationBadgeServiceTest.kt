8d8f5c1a57ae711294281d4266dc4db2e29ca4fe
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2025-06-24 10:34:44 +0200

    remove red dot on insurances bottom nav

--- app/notification-badge-data/notification-badge-data-public/src/test/kotlin/com/hedvig/android/notification/badge/data/crosssell/bottomnav/TabNotificationBadgeServiceTest.kt
+++ app/notification-badge-data/notification-badge-data-public/src/test/kotlin/com/hedvig/android/notification/badge/data/crosssell/bottomnav/TabNotificationBadgeServiceTest.kt
@@ -3,32 +3,19 @@ package com.hedvig.android.notification.badge.data.crosssell.bottomnav
 import app.cash.turbine.test
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import com.hedvig.android.notification.badge.data.crosssell.CrossSellIdentifier
-import com.hedvig.android.notification.badge.data.crosssell.CrossSellNotificationBadgeService
 import com.hedvig.android.notification.badge.data.crosssell.FakeNotificationBadgeStorage
-import com.hedvig.android.notification.badge.data.crosssell.GetCrossSellRecommendationIdUseCase
-import com.hedvig.android.notification.badge.data.crosssell.card.FakeGetCrossSellRecommendationIdUseCase
 import com.hedvig.android.notification.badge.data.referrals.ReferralsNotificationBadgeService
-import com.hedvig.android.notification.badge.data.storage.NotificationBadge
 import com.hedvig.android.notification.badge.data.storage.NotificationBadgeStorage
 import com.hedvig.android.notification.badge.data.tab.BottomNavTab
 import com.hedvig.android.notification.badge.data.tab.TabNotificationBadgeService
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class TabNotificationBadgeServiceTest {
   private fun tabNotificationBadgeService(
     notificationBadgeStorage: NotificationBadgeStorage,
-    getCrossSellRecommendationIdUseCase: GetCrossSellRecommendationIdUseCase,
   ): TabNotificationBadgeService {
     return TabNotificationBadgeService(
-      CrossSellBottomNavNotificationBadgeService(
-        CrossSellNotificationBadgeService(
-          getCrossSellRecommendationIdUseCase,
-          notificationBadgeStorage,
-        ),
-      ),
       ReferralsNotificationBadgeService(
         notificationBadgeStorage,
       ),
@@ -36,92 +23,12 @@ class TabNotificationBadgeServiceTest {
   }
 
   @Test
-  fun `When backend returns no cross sell recommendation, show no badge`() = runTest {
-    val notificationBadgeService = FakeNotificationBadgeStorage(backgroundScope)
-    val getCrossSellsContractTypeIdentifiersUseCase = FakeGetCrossSellRecommendationIdUseCase()
-    val service = tabNotificationBadgeService(
-      notificationBadgeStorage = notificationBadgeService,
-      getCrossSellRecommendationIdUseCase = getCrossSellsContractTypeIdentifiersUseCase,
-    )
-
-    val unseenBadges = service.unseenTabNotificationBadges().first()
-
-    assertThat(unseenBadges).isEqualTo(setOf(BottomNavTab.FOREVER))
-  }
-
-  @Test
-  fun `When backend returns a cross sell recommendation and it's not seen, show insurance badge`() = runTest {
-    val seAccident = CrossSellIdentifier("1111")
-    val notificationBadgeService = FakeNotificationBadgeStorage(backgroundScope)
-    val getCrossSellsContractTypesUseCase = FakeGetCrossSellRecommendationIdUseCase {
-      seAccident
-    }
-    val service = tabNotificationBadgeService(
-      notificationBadgeStorage = notificationBadgeService,
-      getCrossSellRecommendationIdUseCase = getCrossSellsContractTypesUseCase,
-    )
-
-    val unseenBadges = service.unseenTabNotificationBadges().first()
-
-    assertThat(unseenBadges).isEqualTo(setOf(BottomNavTab.INSURANCE, BottomNavTab.FOREVER))
-  }
-
-  @Test
-  fun `When backend returns a cross sell recommendation but it's seen, show no badge`() = runTest {
-    val seAccident = CrossSellIdentifier("SE_ACCIDENT")
-    val notificationBadgeService = FakeNotificationBadgeStorage(backgroundScope).apply {
-      setValue(
-        NotificationBadge.BottomNav.CrossSellOnInsuranceScreen,
-        setOf(seAccident.rawValue),
-      )
-    }
-    val getCrossSellsContractTypesUseCase = FakeGetCrossSellRecommendationIdUseCase {
-      seAccident
-    }
-    val service = tabNotificationBadgeService(
-      notificationBadgeStorage = notificationBadgeService,
-      getCrossSellRecommendationIdUseCase = getCrossSellsContractTypesUseCase,
-    )
-
-    val unseenBadges = service.unseenTabNotificationBadges().first()
-
-    assertThat(unseenBadges).isEqualTo(setOf(BottomNavTab.FOREVER))
-  }
-
-  @Test
   fun `When a notification is shown, when it is marked as seen it no longer shows`() = runTest {
-    val seAccident = CrossSellIdentifier("SE_ACCIDENT")
     val notificationBadgeService = FakeNotificationBadgeStorage(backgroundScope)
-    val getCrossSellsContractTypesUseCase = FakeGetCrossSellRecommendationIdUseCase {
-      seAccident
-    }
     val service = tabNotificationBadgeService(
       notificationBadgeStorage = notificationBadgeService,
-      getCrossSellRecommendationIdUseCase = getCrossSellsContractTypesUseCase,
     )
     service.unseenTabNotificationBadges().test {
-      assertThat(awaitItem()).isEqualTo(setOf(BottomNavTab.INSURANCE, BottomNavTab.FOREVER))
-      service.visitTab(BottomNavTab.INSURANCE)
-      assertThat(awaitItem()).isEqualTo(setOf(BottomNavTab.FOREVER))
-      ensureAllEventsConsumed()
-    }
-  }
-
-  @Test
-  fun `When two notifications are shown, they get cleared one by one when visiting the tabs`() = runTest {
-    val seAccident = CrossSellIdentifier("SE_ACCIDENT")
-    val notificationBadgeService = FakeNotificationBadgeStorage(backgroundScope)
-    val getCrossSellsContractTypesUseCase = FakeGetCrossSellRecommendationIdUseCase {
-      seAccident
-    }
-    val service = tabNotificationBadgeService(
-      notificationBadgeStorage = notificationBadgeService,
-      getCrossSellRecommendationIdUseCase = getCrossSellsContractTypesUseCase,
-    )
-
-    service.unseenTabNotificationBadges().test {
-      assertThat(awaitItem()).isEqualTo(setOf(BottomNavTab.INSURANCE, BottomNavTab.FOREVER))
-      service.visitTab(BottomNavTab.INSURANCE)
       assertThat(awaitItem()).isEqualTo(setOf(BottomNavTab.FOREVER))
       service.visitTab(BottomNavTab.FOREVER)
       assertThat(awaitItem()).isEqualTo(emptySet())
