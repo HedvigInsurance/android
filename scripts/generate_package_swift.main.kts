e78ec121e4d70fefb40875e7d07526c247d87b32
stylianosgakis <stylianos@hedvig.com>
2025-07-04 12:11:57 +0200

    Test run CI

---
+++ scripts/generate_package_swift.main.kts
@@ -0,0 +1,65 @@
+#!/usr/bin/env kotlin
+
+import kotlin.Exception
+
+fun runCommand(vararg args: String): String {
+  val builder = ProcessBuilder(*args)
+    .redirectError(ProcessBuilder.Redirect.INHERIT)
+  val process = builder.start()
+  val output = process.inputStream.bufferedReader().readText().trim()
+  val ret = process.waitFor()
+  if (ret != 0) {
+    throw Exception("command ${args.joinToString(" ")} failed:\n$output")
+  }
+  return output
+}
+
+fun generatePackageSwiftFileContents(versionCode: String, checksum: String): String = """
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "umbrella",
+    platforms: [
+        .iOS(.v14),
+    ],
+    products: [
+        .library(
+            name: "umbrella",
+            targets: ["umbrella"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "umbrella",
+            url: "https://github.com/HedvigInsurance/umbrella/releases/download/v$versionCode/umbrella.xcframework.zip",
+            checksum: "$checksum"
+        )
+    ]
+)
+"""
+
+fun generateChecksum(): String {
+  return runCommand(
+    "swift",
+    "package",
+    "compute-checksum",
+    "../app/umbrella/build/XCFrameworks/release/umbrella.xcframework.zip",
+  )
+}
+
+fun main() {
+  // testing
+//  runCommand("echo", "VERSION_CODE=0.0.4", ">>", "\$GITHUB_ENV")
+  runCommand("echo", "0.0.4", ">>", "\$VERSION_CODE")
+  //
+  val version = runCommand("echo", "\$VERSION_CODE")
+  println("version: $version")
+//  val checksum = generateChecksum()
+//  println(checksum)
+//  val fileContents = generatePackageSwiftFileContents(version, checksum)
+//  println(fileContents)
+}
+
+main()
