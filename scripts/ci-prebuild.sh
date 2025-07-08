969a2143756c2b9c981708fb3eceb3c03190e252
stylianosgakis <stylianos@hedvig.com>
2025-07-08 12:20:56 +0200

    Remove old authlib related CI steps

--- scripts/ci-prebuild.sh
+++ scripts/ci-prebuild.sh
@@ -2,8 +2,6 @@
 set +uex
 
 echo "" >> ~/.gradle/gradle.properties
-echo "authlibUsername=${HEDVIG_GITHUB_PACKAGES_USER}" >> ~/.gradle/gradle.properties
-echo "authlibPassword=${HEDVIG_GITHUB_PACKAGES_TOKEN}" >> ~/.gradle/gradle.properties
 echo "DATADOG_API_KEY=${DATADOG_API_KEY}" >> ~/.gradle/gradle.properties
 
 cat <<EOT > lokalise.properties
