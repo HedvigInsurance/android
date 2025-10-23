207096ace045708ae069408ca9874548436462cd
stylianosgakis <stylianos.gakis98@gmail.com>
2025-10-23 15:31:28 +0200

    Update API usage to crossSellV2

--- app/feature/feature-insurances/src/main/kotlin/com/hedvig/android/feature/insurances/data/GetCrossSellsUseCaseDemo.kt
+++ app/feature/feature-insurances/src/main/kotlin/com/hedvig/android/feature/insurances/data/GetCrossSellsUseCaseDemo.kt
@@ -3,39 +3,40 @@ package com.hedvig.android.feature.insurances.data
 import arrow.core.Either
 import arrow.core.raise.either
 import com.hedvig.android.core.common.ErrorMessage
-import octopus.CrossSellsQuery
+import com.hedvig.android.data.contract.CrossSell
+import com.hedvig.android.data.contract.ImageAsset
 
 internal class GetCrossSellsUseCaseDemo : GetCrossSellsUseCase {
-  override suspend fun invoke(): Either<ErrorMessage, List<CrossSellsQuery.Data.CurrentMember.CrossSell>> {
+  override suspend fun invoke(): Either<ErrorMessage, List<CrossSell>> {
     return either {
       listOf(
-        CrossSellsQuery.Data.CurrentMember.CrossSell(
+        CrossSell(
           "1",
           "Home Insurance",
           "For you, your family and your home",
           "",
-          octopus.CrossSellsQuery.Data.CurrentMember.CrossSell.PillowImageLarge("", "", ""),
+          ImageAsset("", "", ""),
         ),
-        CrossSellsQuery.Data.CurrentMember.CrossSell(
+        CrossSell(
           "2",
           "Pet Insurance",
           "For your dog or cat",
           "",
-          octopus.CrossSellsQuery.Data.CurrentMember.CrossSell.PillowImageLarge("", "", ""),
+          ImageAsset("", "", ""),
         ),
-        CrossSellsQuery.Data.CurrentMember.CrossSell(
+        CrossSell(
           "3",
           "Car Insurance",
           "For your and your car",
           "",
-          octopus.CrossSellsQuery.Data.CurrentMember.CrossSell.PillowImageLarge("", "", ""),
+          ImageAsset("", "", ""),
         ),
-        CrossSellsQuery.Data.CurrentMember.CrossSell(
+        CrossSell(
           "4",
           "Accident Insurance",
           "No loopholes on our part. No worries on your part.",
           "",
-          octopus.CrossSellsQuery.Data.CurrentMember.CrossSell.PillowImageLarge("", "", ""),
+          ImageAsset("", "", ""),
         ),
       )
     }
