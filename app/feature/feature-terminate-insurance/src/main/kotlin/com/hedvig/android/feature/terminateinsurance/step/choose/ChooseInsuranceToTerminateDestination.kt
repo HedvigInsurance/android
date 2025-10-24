d9645da9fba5161fa8bdf9813d0006d4c0a86663
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2025-10-24 13:40:40 +0200

    paparazzi and ChooseInsuranceToTerminateScreen UI snapshot tests

--- app/feature/feature-terminate-insurance/src/main/kotlin/com/hedvig/android/feature/terminateinsurance/step/choose/ChooseInsuranceToTerminateDestination.kt
+++ app/feature/feature-terminate-insurance/src/main/kotlin/com/hedvig/android/feature/terminateinsurance/step/choose/ChooseInsuranceToTerminateDestination.kt
@@ -69,7 +69,7 @@ internal fun ChooseInsuranceToTerminateDestination(
 }
 
 @Composable
-private fun ChooseInsuranceToTerminateScreen(
+internal fun ChooseInsuranceToTerminateScreen(
   uiState: ChooseInsuranceToTerminateStepUiState,
   navigateUp: () -> Unit,
   reload: () -> Unit,
