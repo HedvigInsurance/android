ef61c84bf4c1e00f1b65bcba74b2240111a15c0f
stylianosgakis <stylianos.gakis98@gmail.com>
2025-10-29 15:44:59 +0100

    Make the marketing screen bottom sheet have a heading

--- app/feature/feature-login/src/main/kotlin/com/hedvig/android/feature/login/marketing/MarketingDestination.kt
+++ app/feature/feature-login/src/main/kotlin/com/hedvig/android/feature/login/marketing/MarketingDestination.kt
@@ -24,6 +24,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
@@ -196,6 +198,7 @@ private fun ColumnScope.PreferencesSheetContent(
     text = stringResource(R.string.SETTINGS_LANGUAGE_TITLE),
     textAlign = TextAlign.Center,
     modifier = Modifier
+      .semantics { heading() }
       .fillMaxWidth()
       .padding(horizontal = 16.dp),
   )
