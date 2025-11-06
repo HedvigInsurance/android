19de28d52fe8400a54ac3bc929b5e10c1038a734
Hugo Linder <hugo@hedvig.com>
2025-11-06 14:07:53 +0100

    Implement UploadFileUseCase

--- app/feature/feature-claim-chat/src/androidMain/kotlin/com/hedvig/feature/claim/chat/data/file/AudioFileReference.kt
+++ app/feature/feature-claim-chat/src/androidMain/kotlin/com/hedvig/feature/claim/chat/data/file/AudioFileReference.kt
@@ -1,10 +1,12 @@
-package com.hedvig.feature.claim.chat.file
+package com.hedvig.feature.claim.chat.data.file
 
 import java.io.File
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-actual class AudioFileReference(override val pathOrUri: String) {
+actual class AudioFileReference(
+  actual val pathOrUri: String
+) {
   actual suspend fun readBytes(): ByteArray {
     return withContext(Dispatchers.IO) {
       File(pathOrUri).readBytes()
