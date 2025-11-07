eceabe4af0c1099d428f6bb68fd02415347a205c
stylianosgakis <stylianos.gakis98@gmail.com>
2025-11-07 15:26:14 +0100

    Fix actualy for AudioFileReference

--- app/feature/feature-claim-chat/src/nativeMain/kotlin/com/hedvig/feature/claim/chat/data/file/AudioFileReference.native.kt
+++ app/feature/feature-claim-chat/src/nativeMain/kotlin/com/hedvig/feature/claim/chat/data/file/AudioFileReference.native.kt
@@ -1,8 +1,6 @@
 package com.hedvig.feature.claim.chat.data.file
 
-actual class AudioFileReference {
-    actual val pathOrUri: String = ""
-
+actual class AudioFileReference actual constructor(val pathOrUri: String) {
     actual suspend fun readBytes(): ByteArray {
         return ByteArray(0)
     }
