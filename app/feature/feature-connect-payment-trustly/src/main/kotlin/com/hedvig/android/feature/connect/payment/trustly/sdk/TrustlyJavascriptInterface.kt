bb3a30a4074e453f1e7daec6da2925aa5c73b0a2
stylianosgakis <stylianos.gakis98@gmail.com>
2025-11-13 14:44:08 +0100

    Replace TrustlyJavascriptInterface with the copy from the SDK

--- app/feature/feature-connect-payment-trustly/src/main/kotlin/com/hedvig/android/feature/connect/payment/trustly/sdk/TrustlyJavascriptInterface.kt
+++ app/feature/feature-connect-payment-trustly/src/main/kotlin/com/hedvig/android/feature/connect/payment/trustly/sdk/TrustlyJavascriptInterface.kt
@@ -2,16 +2,16 @@ package com.hedvig.android.feature.connect.payment.trustly.sdk
 
 import android.app.Activity
 import android.content.Intent
-import android.net.Uri
 import android.webkit.JavascriptInterface
+import androidx.core.net.toUri
 import com.hedvig.android.logger.LogPriority
 import com.hedvig.android.logger.logcat
-
 
 class TrustlyJavascriptInterface(
   private val activity: Activity,
   private val webViewHandler: TrustlyWebView,
 ) {
+  @Suppress("unused")
   @JavascriptInterface
   fun handleTrustlyEvent(typeLabel: String?, url: String?, packageName: String?) {
     val eventType = TrustlyEventType.valueForEventTypeLabel(typeLabel)
@@ -31,7 +31,7 @@ class TrustlyJavascriptInterface(
   private fun handleRedirect(urlString: String?) {
     try {
       val intent = Intent(Intent.ACTION_VIEW).apply {
-        data = Uri.parse(urlString)
+        data = urlString?.toUri()
       }
       activity.startActivityForResult(intent, 0)
     } catch (e: Error) {
