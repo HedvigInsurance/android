2c1d365099bc27ab2ac427acb4a8df0cb24fe6ba
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2025-11-18 11:50:52 +0100

    impl AssistantMessageBubble

--- app/feature/feature-claim-chat/src/androidMain/kotlin/com/hedvig/feature/claim/chat/ui/ClaimChatUiComponents.kt
+++ app/feature/feature-claim-chat/src/androidMain/kotlin/com/hedvig/feature/claim/chat/ui/ClaimChatUiComponents.kt
@@ -1,16 +1,21 @@
 package com.hedvig.feature.claim.chat.ui
 
+import android.graphics.Paint
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
@@ -21,8 +26,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -47,6 +56,7 @@ import com.hedvig.android.design.system.hedvig.RadioOption
 import com.hedvig.android.design.system.hedvig.RadioOptionId
 import com.hedvig.android.design.system.hedvig.SingleSelectDialog
 import com.hedvig.android.design.system.hedvig.Surface
+import com.hedvig.android.design.system.hedvig.ThreeDotsLoading
 import com.hedvig.android.design.system.hedvig.datepicker.getLocale
 import com.hedvig.android.design.system.hedvig.icon.Close
 import com.hedvig.android.design.system.hedvig.icon.HedvigIcons
@@ -134,10 +144,39 @@ internal fun AssistantMessageBubble(
   text: String,
   comment: String?,
   isLoading: Boolean,
-  isCurrentStep: Boolean,
   modifier: Modifier = Modifier,
 ) {
-
+  Column(modifier) {
+    HedvigText(text)
+    if (comment != null) {
+      HedvigText(
+        comment,
+        style = HedvigTheme.typography.label,
+        color = HedvigTheme.colorScheme.textSecondaryTranslucent,
+      )
+    }
+    if (isLoading) {
+      ThreeDotsLoading(Modifier.padding(vertical = 16.dp))
+    } else {
+      Spacer(Modifier.height(8.dp))
+      Row(
+        verticalAlignment = Alignment.CenterVertically
+      ) {
+        val color = HedvigTheme.colorScheme.signalGreenElement
+        Spacer(
+          Modifier
+            .wrapContentSize(Alignment.Center)
+            .size(20.dp)
+            .padding(1.dp)
+            .background(color, CircleShape),
+        )
+        Spacer(Modifier.width(8.dp))
+        HedvigText("Hedvig AI Assistant", //todo
+          style = HedvigTheme.typography.label,
+          color = HedvigTheme.colorScheme.textSecondaryTranslucent,)
+      }
+    }
+  }
 }
 
 @Composable
@@ -162,8 +201,10 @@ internal fun YesNoBubble(
     onSkip = onSkip,
     selectedAnswer = answerSelected,
     content = {
-      Column(Modifier.padding(16.dp),
-      horizontalAlignment = Alignment.End) {
+      Column(
+        Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.End,
+      ) {
         HedvigText(
           questionLabel,
           style = HedvigTheme.typography.label,
@@ -257,6 +298,55 @@ internal fun SingleSelectBubbleWithDialog(
       )
     },
   )
+}
+
+@Composable
+internal fun MultiSelectBubbleWithDialog(
+  questionLabel: String,
+  options: List<RadioOption>,
+  selectedOptionIds: List<RadioOptionId?>,
+  isPrefilled: Boolean,
+  isCurrentStep: Boolean,
+  canBeChanged: Boolean,
+  canSkip: Boolean,
+  onSkip: () -> Unit,
+  onSelect: (RadioOptionId) -> Unit,
+  onSubmit: (RadioOptionId) -> Unit,
+  modifier: Modifier = Modifier,
+) {
+  var showDialog by rememberSaveable { mutableStateOf(false) }
+  if (showDialog) {
+//    SingleSelectDialog(
+//      title = questionLabel,
+//      options = options,
+//      selectedOption = selectedOptionId,
+//      onRadioOptionSelected = onSelect,
+//      onDismissRequest = {
+//        showDialog = false
+//      },
+//    )
+    //todo
+  }
+//  StandardBubble(
+//    isPrefilledByAI = isPrefilled,
+//    isCurrentStep = isCurrentStep,
+//    canSkip = canSkip,
+//    onSubmit = onSubmit,
+//    modifier = modifier,
+//    onSkip = onSkip,
+//    selectedAnswer = selectedOptionId,
+//    content = {
+//      HedvigBigCard(
+//        onClick = { showDialog = true },
+//        labelText = questionLabel,
+//        inputText = options.firstOrNull {
+//          it.id == selectedOptionId
+//        }?.text,
+//        modifier = modifier,
+//        enabled = canBeChanged,
+//      )
+//    },
+//  )
 }
 
 @Composable
@@ -394,77 +484,110 @@ internal fun <T> StandardBubble(
   modifier: Modifier = Modifier,
   content: @Composable () -> Unit,
 ) {
-  Column(
-    modifier,
-    horizontalAlignment = Alignment.End,
+  Row(
+    modifier.fillMaxWidth(),
+    horizontalArrangement = Arrangement.End,
   ) {
-    Box {
-      HedvigCard(
-        Modifier.padding(start = 8.dp, top = 8.dp),
-      ) {
-        content()
+    Column {
+      Box {
+        HedvigCard(
+          Modifier.padding(start = 8.dp, top = 8.dp),
+        ) {
+          content()
+        }
+        if (isPrefilledByAI) {
+          Icon(
+            imageVector = HedvigIcons.HelipadFilled,
+            tint = HedvigTheme.colorScheme.signalAmberElement,
+            modifier = Modifier.align(Alignment.TopStart),
+            contentDescription = null, //todo
+          )
+        }
       }
-      if (isPrefilledByAI) {
-        Icon(
-          imageVector = HedvigIcons.HelipadFilled,
-          tint = HedvigTheme.colorScheme.signalAmberElement,
-          modifier = Modifier.align(Alignment.TopStart),
-          contentDescription = null, //todo
-        )
-      }
-    }
-    if (isCurrentStep) {
-      Spacer(Modifier.height(8.dp))
-      Row(
-        horizontalArrangement = Arrangement.End,
-      ) {
-        if (canSkip) {
-          HedvigTextButton(
-            stringResource(R.string.claims_skip_button),
-            onClick = onSkip,
+      if (isCurrentStep) {
+        Spacer(Modifier.height(16.dp))
+        Row(
+          Modifier.fillMaxWidth(),
+          horizontalArrangement = Arrangement.End,
+        ) {
+          if (canSkip) {
+            HedvigTextButton(
+              stringResource(R.string.claims_skip_button),
+              onClick = onSkip,
+              buttonSize = ButtonDefaults.ButtonSize.Medium,
+            )
+          }
+          Spacer(Modifier.width(16.dp))
+          HedvigButton(
+            text = stringResource(R.string.CHAT_UPLOAD_PRESS_SEND_LABEL),
+            enabled = selectedAnswer != null,
+            onClick = {
+              if (selectedAnswer != null) onSubmit(selectedAnswer)
+            },
             buttonSize = ButtonDefaults.ButtonSize.Medium,
           )
         }
-        Spacer(Modifier.width(16.dp))
-        HedvigButton(
-          text = stringResource(R.string.CHAT_UPLOAD_PRESS_SEND_LABEL),
-          enabled = selectedAnswer != null,
-          onClick = {
-            if (selectedAnswer != null) onSubmit(selectedAnswer)
-          },
-          buttonSize = ButtonDefaults.ButtonSize.Medium,
-        )
       }
     }
   }
+
 }
 
 @HedvigPreview
 @Composable
-private fun PreviewAssistantBubble() {
+private fun PreviewWithAssistantBubble() {
   HedvigTheme {
-    Surface {
+    Surface(
+      color = HedvigTheme.colorScheme.backgroundPrimary,
+    ) {
       Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = Modifier.padding(horizontal = 16.dp),
+        modifier = Modifier.padding(16.dp),
       ) {
         AssistantMessageBubble(
-          text = "Specify when and where it happened",
+          text = "Tell us where it happened.",
           comment = null,
           isLoading = false,
+        )
+        SingleSelectBubbleWithDialog(
+          questionLabel = "Location",
+          options = listOf(
+            RadioOption(RadioOptionId("01"), "At home"),
+            RadioOption(RadioOptionId("02"), "Outside home"),
+          ),
+          selectedOptionId = RadioOptionId("01"),
+          isPrefilled = true,
           isCurrentStep = false,
+          canBeChanged = true,
+          canSkip = false,
+          onSubmit = {},
+          onSkip = {},
+          onSelect = {},
         )
         AssistantMessageBubble(
-          text = "Processing audio information",
-          comment = null,
-          isLoading = true,
-          isCurrentStep = false,
-        )
-        AssistantMessageBubble(
-          text = "Processing audio information",
+          text = "Processing information",
           comment = "Done!",
           isLoading = false,
+        )
+        AssistantMessageBubble(
+          text = "Specify when it happened.",
+          comment = null,
+          isLoading = false,
+        )
+        DateSelectBubble(
+          questionLabel = "Date of occurence",
+          date = LocalDate(2025, 11, 10),
+          isPrefilled = true,
           isCurrentStep = false,
+          canSkip = false,
+          canBeChanged = true,
+          onSubmit = {},
+          onSkip = {},
+        )
+        AssistantMessageBubble(
+          text = "Processing information",
+          comment = null,
+          isLoading = true,
         )
       }
     }
@@ -536,6 +659,8 @@ private fun PreviewClaimChatComponents() {
           onSelect = {},
         )
         Spacer(Modifier.height(16.dp))
+//        MultiSelectBubbleWithDialog()
+//        Spacer(Modifier.height(16.dp))
         DateSelectBubble(
           questionLabel = "Date of occurence",
           date = LocalDate(2025, 11, 10),
