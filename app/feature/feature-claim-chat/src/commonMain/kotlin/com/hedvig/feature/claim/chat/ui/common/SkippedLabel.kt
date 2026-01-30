1b86ca26e4030aa69fef197b4ed57ccbbdd05ea9
stylianosgakis <stylianos.gakis98@gmail.com>
2026-01-30 10:47:29 +0100

    Remove redundant Row

--- app/feature/feature-claim-chat/src/commonMain/kotlin/com/hedvig/feature/claim/chat/ui/common/SkippedLabel.kt
+++ app/feature/feature-claim-chat/src/commonMain/kotlin/com/hedvig/feature/claim/chat/ui/common/SkippedLabel.kt
@@ -3,7 +3,9 @@ package com.hedvig.feature.claim.chat.ui.common
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.hedvig.android.design.system.hedvig.HedvigText
 import com.hedvig.android.design.system.hedvig.HedvigTheme
@@ -13,18 +15,14 @@ import org.jetbrains.compose.resources.stringResource
 
 @Composable
 internal fun SkippedLabel() {
-  Row(
-    Modifier.fillMaxWidth(),
-    horizontalArrangement = Arrangement.End,
+  val skippedLabelText = stringResource(Res.string.CLAIM_CHAT_SKIPPED_STEP)
+  RoundCornersPill(
+    onClick = null,
+    modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.End),
   ) {
-    val skippedLabelText = stringResource(Res.string.CLAIM_CHAT_SKIPPED_STEP)
-    RoundCornersPill(
-      onClick = null,
-    ) {
-      HedvigText(
-        skippedLabelText,
-        color = HedvigTheme.colorScheme.textSecondaryTranslucent,
-      )
-    }
+    HedvigText(
+      skippedLabelText,
+      color = HedvigTheme.colorScheme.textSecondaryTranslucent,
+    )
   }
 }
