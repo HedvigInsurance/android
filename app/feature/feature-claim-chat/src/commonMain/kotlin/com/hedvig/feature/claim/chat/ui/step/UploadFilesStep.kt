cc665b5142a47fa382a99a60f046fdf600497fb1
stylianosgakis <stylianos.gakis98@gmail.com>
2026-02-11 14:33:40 +0100

    Fix spacing problems in upload files step

--- app/feature/feature-claim-chat/src/commonMain/kotlin/com/hedvig/feature/claim/chat/ui/step/UploadFilesStep.kt
+++ app/feature/feature-claim-chat/src/commonMain/kotlin/com/hedvig/feature/claim/chat/ui/step/UploadFilesStep.kt
@@ -69,14 +69,13 @@ internal fun UploadFilesStep(
   continueButtonLoading: Boolean,
   skipButtonLoading: Boolean,
   imageLoader: ImageLoader,
-  localFiles: List<UiFile>,
   onEvent: (ClaimChatEvent) -> Unit,
   onNavigateToImageViewer: (imageUrl: String, cacheKey: String) -> Unit,
   modifier: Modifier = Modifier,
 ) {
   Box(modifier) {
     if (isCurrentStep) {
-      Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+      Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
         UploadFilesBubble(
           addLocalFile = { uri ->
             onEvent(
@@ -95,45 +94,43 @@ internal fun UploadFilesStep(
             )
           },
           appPackageId = appPackageId,
-          localFiles = localFiles,
+          localFiles = stepContent.localFiles,
           imageLoader = imageLoader,
           onNavigateToImageViewer = onNavigateToImageViewer,
         )
-        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-          if (stepContent.localFiles.isNotEmpty()) {
-            HedvigButton(
-              text = stringResource(Res.string.CLAIM_CHAT_FILE_UPLOAD_SEND_BUTTON),
-              enabled = !continueButtonLoading,
-              onClick = {
-                onEvent(
-                  ClaimChatEvent.SubmitFile(
-                    itemId,
-                  ),
-                )
-              },
-              isLoading = continueButtonLoading,
-              modifier = Modifier.fillMaxWidth(),
-            )
-          }
-          if (stepContent.isSkippable && stepContent.localFiles.isEmpty()) {
-            HedvigButton(
-              text = stringResource(Res.string.claims_skip_button),
-              enabled = !skipButtonLoading,
-              onClick = {
-                onEvent(ClaimChatEvent.Skip(itemId))
-              },
-              isLoading = skipButtonLoading,
-              modifier = Modifier.fillMaxWidth(),
-              buttonStyle = ButtonDefaults.ButtonStyle.Secondary,
-            )
-          }
+        if (stepContent.localFiles.isNotEmpty()) {
+          HedvigButton(
+            text = stringResource(Res.string.CLAIM_CHAT_FILE_UPLOAD_SEND_BUTTON),
+            enabled = !continueButtonLoading,
+            onClick = {
+              onEvent(
+                ClaimChatEvent.SubmitFile(
+                  itemId,
+                ),
+              )
+            },
+            isLoading = continueButtonLoading,
+            modifier = Modifier.fillMaxWidth(),
+          )
+        }
+        if (stepContent.isSkippable && stepContent.localFiles.isEmpty()) {
+          HedvigButton(
+            text = stringResource(Res.string.claims_skip_button),
+            enabled = !skipButtonLoading,
+            onClick = {
+              onEvent(ClaimChatEvent.Skip(itemId))
+            },
+            isLoading = skipButtonLoading,
+            modifier = Modifier.fillMaxWidth(),
+            buttonStyle = ButtonDefaults.ButtonStyle.Secondary,
+          )
         }
       }
     } else {
       Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-        if (localFiles.isNotEmpty()) {
+        if (stepContent.localFiles.isNotEmpty()) {
           FilesRow(
-            uiFiles = localFiles,
+            uiFiles = stepContent.localFiles,
             onRemoveFile = null,
             imageLoader = imageLoader,
             onNavigateToImageViewer = onNavigateToImageViewer,
@@ -229,7 +226,7 @@ private fun UploadFilesBubbleContent(
   onNavigateToImageViewer: (imageUrl: String, cacheKey: String) -> Unit,
   modifier: Modifier = Modifier,
 ) {
-  Column(modifier) {
+  Column(modifier, verticalArrangement = Arrangement.spacedBy(16.dp)) {
     if (localFiles.isNotEmpty()) {
       FilesRow(
         uiFiles = localFiles,
@@ -249,7 +246,6 @@ private fun UploadFilesBubbleContent(
         },
       )
     }
-    Spacer(Modifier.height(16.dp))
     HedvigButton(
       buttonStyle = if (localFiles.isNotEmpty()) {
         ButtonDefaults.ButtonStyle.Secondary
@@ -344,38 +340,34 @@ private fun FilePickerBottomSheetContent(
 
 @HedvigPreview
 @Composable
-private fun PreviewUploadFilesBubbleContent(
+private fun PreviewUploadFilesStep(
   @PreviewParameter(BooleanCollectionPreviewParameterProvider::class) hasFiles: Boolean,
 ) {
   HedvigTheme {
-    Surface(
-      color = HedvigTheme.colorScheme.backgroundPrimary,
-    ) {
-      Column(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = Modifier.padding(horizontal = 16.dp),
-      ) {
-        UploadFilesBubbleContent(
-          modifier = Modifier.height(400.dp),
-          onRemoveFile = {},
-          localFiles = if (hasFiles) {
-            listOf(
-              UiFile(
-                name = "file",
-                localPath = "path",
-                mimeType = "image/jpg",
-                url = null,
-                id = "1",
-              ),
-            )
-          } else {
-            emptyList()
-          },
-          imageLoader = rememberPreviewImageLoader(),
-          onNavigateToImageViewer = { _, _ -> },
-          onAddFilesButtonClick = {},
-        )
-      }
+    Surface(color = HedvigTheme.colorScheme.backgroundPrimary) {
+      UploadFilesStep(
+        itemId = StepId(""),
+        stepContent = StepContent.FileUpload(
+          "", true,
+          listOf(
+            UiFile(
+              name = "file",
+              localPath = "path",
+              mimeType = "image/jpg",
+              url = null,
+              id = "1",
+            ),
+          ).takeIf { hasFiles }.orEmpty(),
+        ),
+        appPackageId = "",
+        isCurrentStep = true,
+        canEdit = true,
+        continueButtonLoading = false,
+        skipButtonLoading = false,
+        imageLoader = rememberPreviewImageLoader(),
+        onEvent = {},
+        onNavigateToImageViewer = { _, _ -> },
+      )
     }
   }
 }
