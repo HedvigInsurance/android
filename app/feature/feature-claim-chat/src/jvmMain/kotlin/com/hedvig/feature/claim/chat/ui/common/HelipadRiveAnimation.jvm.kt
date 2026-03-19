f69d7cc4ae098ec34dabb6e9d5d93f72d5927de1
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2026-03-19 10:29:40 +0100

    add missing actuals for lint

---
+++ app/feature/feature-claim-chat/src/jvmMain/kotlin/com/hedvig/feature/claim/chat/ui/common/HelipadRiveAnimation.jvm.kt
@@ -0,0 +1,11 @@
+package com.hedvig.feature.claim.chat.ui.common
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal actual fun HelipadRiveAnimation(
+    modifier: Modifier,
+    bottomAnimationFinished: Boolean
+) {
+}
