948a948edbb10cc6a121358d785fe8032818f287
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2026-03-30 11:27:38 +0200

    bump version to 14.0.10

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     applicationId = "com.hedvig"
 
     versionCode = 43
-    versionName = "14.0.9"
+    versionName = "14.0.10"
 
     resourceConfigurations.addAll(listOf("en", "sv-rSE"))
   }
