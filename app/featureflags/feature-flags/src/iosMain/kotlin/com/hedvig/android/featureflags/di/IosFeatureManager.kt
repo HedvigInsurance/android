2371d348ade733f082856ed3bc8243c318ee9f52
stylianosgakis <stylianos.gakis98@gmail.com>
2026-04-13 16:50:46 +0200

    Try to call Swift-provided lambda on the main thread
    This should ensure that any Swift @MainActor-isolated implementation of
    the block will always be called on the correct thread, regardless of
    what dispatcher the surrounding Kotlin coroutine is using.

--- app/featureflags/feature-flags/src/iosMain/kotlin/com/hedvig/android/featureflags/di/IosFeatureManager.kt
+++ app/featureflags/feature-flags/src/iosMain/kotlin/com/hedvig/android/featureflags/di/IosFeatureManager.kt
@@ -2,11 +2,13 @@ package com.hedvig.android.featureflags.di
 
 import com.hedvig.android.featureflags.FeatureManager
 import com.hedvig.android.featureflags.flags.Feature
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 
 class IosFeatureManager(private val isFeatureEnabledBlock: (Feature) -> Boolean) : FeatureManager {
-  override fun isFeatureEnabled(feature: Feature): Flow<Boolean> {
-    return flowOf(isFeatureEnabledBlock(feature))
+  override fun isFeatureEnabled(feature: Feature): Flow<Boolean> = flow {
+    emit(withContext(Dispatchers.Main.immediate) { isFeatureEnabledBlock(feature) })
   }
 }
