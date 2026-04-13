cf326f16e48d2fcd7cdc0a7e7112251bde688f3d
stylianosgakis <stylianos.gakis98@gmail.com>
2026-04-13 19:16:22 +0200

    Update umbrella-consumer to the new APIs

--- micro-apps/umbrella-consumer/umbrella-consumer/ContentView.swift
+++ micro-apps/umbrella-consumer/umbrella-consumer/ContentView.swift
@@ -8,8 +8,26 @@ struct ContentView: View {
 }
 
 struct HelpCenterViewController: UIViewControllerRepresentable {
+    @Environment(\.dismiss) private var dismiss
+
     func makeUIViewController(context: Context) -> UIViewController {
-        return HelpCenterViewControllerKt.HelpCenterViewController()
+        return HelpCenterViewControllerKt.HelpCenterViewController(
+            onNavigateUp: { dismiss() },
+            onNavigateToInbox: {},
+            onNavigateToNewConversation: {},
+            onNavigateToQuickLink: { _ in },
+            openUrl: { url in
+                if let nsUrl = URL(string: url) {
+                    UIApplication.shared.open(nsUrl)
+                }
+            },
+            tryToDialPhone: { number in
+                let cleaned = number.components(separatedBy: .whitespaces).joined()
+                if let url = URL(string: "tel://\(cleaned)") {
+                    UIApplication.shared.open(url)
+                }
+            }
+        )
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
