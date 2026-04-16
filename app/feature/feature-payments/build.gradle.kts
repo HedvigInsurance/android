c84fe7673f2b917f265d36db1f9dc939217674d0
stylianosgakis <stylianos.gakis98@gmail.com>
2026-04-16 15:45:23 +0200

    Move payments feature to commonMain
    Remove unused dependencies which were blocking this move
    Turn apollo-network-cache-manager also KMP as it was ready already

--- app/feature/feature-payments/build.gradle.kts
+++ app/feature/feature-payments/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-  id("hedvig.android.library")
+  id("hedvig.multiplatform.library")
+  id("hedvig.multiplatform.library.android")
   id("hedvig.gradle.plugin")
 }
 
@@ -9,58 +10,41 @@ hedvig {
   compose()
 }
 
-android {
-  testOptions.unitTests.isReturnDefaultValues = true
-}
-
-dependencies {
-  implementation(libs.androidx.compose.foundation)
-  implementation(libs.apollo.normalizedCache)
-  implementation(libs.apollo.runtime)
-  implementation(libs.arrow.core)
-  implementation(libs.arrow.fx)
-  implementation(libs.jetbrains.compose.runtime)
-  implementation(libs.jetbrains.lifecycle.runtime.compose)
-  implementation(libs.jetbrains.navigation.compose)
-  implementation(libs.koin.composeViewModel)
-  implementation(libs.koin.core)
-  implementation(libs.kotlinx.serialization.core)
-  implementation(projects.apolloCore)
-  implementation(projects.apolloNetworkCacheManager)
-  implementation(projects.apolloOctopusPublic)
-  implementation(projects.authCorePublic)
-  implementation(projects.composeUi)
-  implementation(projects.coreBuildConstants)
-  implementation(projects.coreCommonPublic)
-  implementation(projects.coreDatastorePublic)
-  implementation(projects.coreDemoMode)
-  implementation(projects.coreResources)
-  implementation(projects.coreUiData)
-  implementation(projects.dataContract)
-  implementation(projects.dataPayingMember)
-  implementation(projects.dataSettingsDatastorePublic)
-  implementation(projects.designSystemHedvig)
-  implementation(projects.featureFlagsPublic)
-  implementation(projects.foreverUi)
-  implementation(projects.languageCore)
-  implementation(projects.languageData)
-  implementation(projects.memberRemindersPublic)
-  implementation(projects.memberRemindersUi)
-  implementation(projects.moleculePublic)
-  implementation(projects.navigationCommon)
-  implementation(projects.navigationCompose)
-  implementation(projects.navigationComposeTyped)
-  implementation(projects.navigationCore)
-  implementation(projects.notificationPermission)
-  implementation(projects.pullrefresh)
-  implementation(projects.theme)
-
-  testImplementation(libs.coroutines.test)
-  testImplementation(projects.coreCommonTest)
-  testImplementation(projects.coreDatastoreTest)
-  testImplementation(projects.dataSettingsDatastoreTest)
-  testImplementation(projects.featureFlagsTest)
-  testImplementation(projects.languageTest)
-  testImplementation(projects.memberRemindersTest)
-  testImplementation(projects.moleculeTest)
+kotlin {
+  sourceSets {
+    commonMain.dependencies {
+      implementation(libs.apollo.normalizedCache)
+      implementation(libs.apollo.runtime)
+      implementation(libs.arrow.core)
+      implementation(libs.arrow.fx)
+      implementation(libs.jetbrains.compose.foundation)
+      implementation(libs.jetbrains.compose.runtime)
+      implementation(libs.jetbrains.lifecycle.runtime.compose)
+      implementation(libs.jetbrains.navigation.compose)
+      implementation(libs.koin.composeViewModel)
+      implementation(libs.koin.core)
+      implementation(libs.kotlinx.serialization.core)
+      implementation(projects.apolloCore)
+      implementation(projects.apolloOctopusPublic)
+      implementation(projects.composeUi)
+      implementation(projects.coreBuildConstants)
+      implementation(projects.coreCommonPublic)
+      implementation(projects.coreDemoMode)
+      implementation(projects.coreResources)
+      implementation(projects.coreUiData)
+      implementation(projects.dataPayingMember)
+      implementation(projects.designSystemHedvig)
+      implementation(projects.foreverUi)
+      implementation(projects.languageCore)
+      implementation(projects.moleculePublic)
+      implementation(projects.navigationCommon)
+      implementation(projects.navigationCompose)
+      implementation(projects.navigationCore)
+      implementation(projects.pullrefresh)
+      implementation(projects.theme)
+    }
+    androidMain.dependencies {
+      implementation(libs.bundles.kmpPreviewBugWorkaround)
+    }
+  }
 }
