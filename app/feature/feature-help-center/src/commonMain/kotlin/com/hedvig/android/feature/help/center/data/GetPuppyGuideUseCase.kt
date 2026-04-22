2096f2d780d3a83d863a952ea41aa6b65ae98b91
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2026-04-22 09:20:24 +0200

    remove log

--- app/feature/feature-help-center/src/commonMain/kotlin/com/hedvig/android/feature/help/center/data/GetPuppyGuideUseCase.kt
+++ app/feature/feature-help-center/src/commonMain/kotlin/com/hedvig/android/feature/help/center/data/GetPuppyGuideUseCase.kt
@@ -29,10 +29,6 @@ internal class GetPuppyGuideUseCaseImpl(
         ?.currentMember
         ?.puppyGuideStories
         ?.map { story ->
-          // todo: remove log
-          if (story.name == "Ögonvård") {
-            logcat { "Mariia: story Ögonvård read or not: ${story.read} rating: ${story.rating} " }
-          }
           PuppyGuideStory(
             categories = story.categories,
             content = story.content,
