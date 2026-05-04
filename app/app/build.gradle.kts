da65f63e3c48abcf46da94c86a48e983c73f2695
stylianosgakis <stylianos.gakis98@gmail.com>
2026-05-04 19:45:12 +0200

    Bump version

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     applicationId = "com.hedvig"
 
     versionCode = 43
-    versionName = "14.1.3"
+    versionName = "14.2.0"
 
     resourceConfigurations.addAll(listOf("en", "sv-rSE"))
   }
