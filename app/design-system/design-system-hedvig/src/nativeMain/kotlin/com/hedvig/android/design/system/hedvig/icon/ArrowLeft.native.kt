f41fc8dddfd7968ea09bad8bc0a8661389d38d15
stylianosgakis <stylianos.gakis98@gmail.com>
2026-05-07 15:25:57 +0200

    Make ArrowLeft actual to ChevronLeft for native

--- app/design-system/design-system-hedvig/src/nativeMain/kotlin/com/hedvig/android/design/system/hedvig/icon/ArrowLeft.native.kt
+++ app/design-system/design-system-hedvig/src/nativeMain/kotlin/com/hedvig/android/design/system/hedvig/icon/ArrowLeft.native.kt
@@ -2,4 +2,5 @@ package com.hedvig.android.design.system.hedvig.icon
 
 import androidx.compose.ui.graphics.vector.ImageVector
 
-expect val HedvigIcons.ArrowLeft: ImageVector
+actual val HedvigIcons.ArrowLeft: ImageVector
+  get() = ChevronLeft
