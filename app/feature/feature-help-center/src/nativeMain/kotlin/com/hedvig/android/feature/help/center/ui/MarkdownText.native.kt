9ea21294c873dc4d7d557e80ee3694a4bcbaa657
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2026-05-15 16:16:17 +0200

    color changes

--- app/feature/feature-help-center/src/nativeMain/kotlin/com/hedvig/android/feature/help/center/ui/MarkdownText.native.kt
+++ app/feature/feature-help-center/src/nativeMain/kotlin/com/hedvig/android/feature/help/center/ui/MarkdownText.native.kt
@@ -5,7 +5,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import com.hedvig.android.design.system.hedvig.HedvigTheme
+import com.hedvig.android.design.system.hedvig.ProvideTextStyle
 import com.mikepenz.markdown.compose.Markdown
 import com.mikepenz.markdown.model.MarkdownColors
 import com.mikepenz.markdown.model.MarkdownTypography
@@ -19,21 +21,35 @@ actual fun MarkdownText(markdown: String, modifier: Modifier, withArticleStyle: 
     content = markdown,
     modifier = modifier,
     colors = object : MarkdownColors {
-      override val text: Color = colors.textPrimary
+      override val text: Color = colors.textSecondaryTranslucent
       override val codeBackground: Color = colors.surfaceSecondary
       override val inlineCodeBackground: Color = colors.surfaceSecondary
       override val dividerColor: Color = colors.borderPrimary
       override val tableBackground: Color = colors.surfaceSecondary
     },
     typography = object : MarkdownTypography {
-      override val h1: TextStyle = typography.headlineLarge
-      override val h2: TextStyle = typography.headlineMedium
-      override val h3: TextStyle = typography.headlineSmall
-      override val h4: TextStyle = typography.displaySmall
-      override val h5: TextStyle = typography.bodyLarge
-      override val h6: TextStyle = typography.bodyMedium
+      override val h1: TextStyle = typography.headlineLarge.copy(
+        color = HedvigTheme.colorScheme.textPrimary,
+        fontWeight = FontWeight.Normal)
+      override val h2: TextStyle = typography.headlineMedium.copy(
+        color = HedvigTheme.colorScheme.textPrimary,
+        fontWeight = FontWeight.Normal)
+      override val h3: TextStyle = typography.headlineSmall.copy(
+        color = HedvigTheme.colorScheme.textPrimary,
+        fontWeight = FontWeight.Normal)
+      override val h4: TextStyle = typography.displaySmall.copy(
+        color = HedvigTheme.colorScheme.textPrimary,
+        fontWeight = FontWeight.Normal)
+      override val h5: TextStyle = typography.bodyLarge.copy(
+        color = HedvigTheme.colorScheme.textPrimary,
+        fontWeight = FontWeight.Normal)
+      override val h6: TextStyle = typography.bodyMedium.copy(
+        color = HedvigTheme.colorScheme.textPrimary,
+        fontWeight = FontWeight.Normal)
       override val text: TextStyle = typography.bodySmall
-      override val paragraph: TextStyle = typography.bodySmall
+      override val paragraph: TextStyle = typography.bodySmall.copy(
+        color = HedvigTheme.colorScheme.textPrimary,
+        fontWeight = FontWeight.Normal)
       override val code: TextStyle = typography.label
       override val inlineCode: TextStyle = typography.label
       override val bullet: TextStyle = typography.bodySmall
