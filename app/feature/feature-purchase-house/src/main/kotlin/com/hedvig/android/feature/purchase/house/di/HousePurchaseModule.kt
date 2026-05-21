f8f345a112b62b13f7408989af6845abbe2a7974
Hugo Linder <hugo@hedvig.com>
2026-05-21 11:59:33 +0200

    feat: register HouseFormViewModel + SubmitHouseFormAndGetOffersUseCase in Koin

--- app/feature/feature-purchase-house/src/main/kotlin/com/hedvig/android/feature/purchase/house/di/HousePurchaseModule.kt
+++ app/feature/feature-purchase-house/src/main/kotlin/com/hedvig/android/feature/purchase/house/di/HousePurchaseModule.kt
@@ -2,8 +2,11 @@ package com.hedvig.android.feature.purchase.house.di
 
 import com.hedvig.android.feature.purchase.house.data.CreateHouseSessionAndPriceIntentUseCase
 import com.hedvig.android.feature.purchase.house.data.CreateHouseSessionAndPriceIntentUseCaseImpl
+import com.hedvig.android.feature.purchase.house.data.SubmitHouseFormAndGetOffersUseCase
+import com.hedvig.android.feature.purchase.house.data.SubmitHouseFormAndGetOffersUseCaseImpl
 import com.hedvig.android.feature.purchase.house.data.SubmitVacationHomeFormAndGetOffersUseCase
 import com.hedvig.android.feature.purchase.house.data.SubmitVacationHomeFormAndGetOffersUseCaseImpl
+import com.hedvig.android.feature.purchase.house.ui.house.HouseFormViewModel
 import com.hedvig.android.feature.purchase.house.ui.vacationhome.VacationHomeFormViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
@@ -15,12 +18,23 @@ val housePurchaseModule = module {
   single<SubmitVacationHomeFormAndGetOffersUseCase> {
     SubmitVacationHomeFormAndGetOffersUseCaseImpl(apolloClient = get())
   }
+  single<SubmitHouseFormAndGetOffersUseCase> {
+    SubmitHouseFormAndGetOffersUseCaseImpl(apolloClient = get())
+  }
 
   viewModel<VacationHomeFormViewModel> { params ->
     VacationHomeFormViewModel(
       productName = params.get(),
       createHouseSessionAndPriceIntentUseCase = get(),
       submitVacationHomeFormAndGetOffersUseCase = get(),
+    )
+  }
+
+  viewModel<HouseFormViewModel> { params ->
+    HouseFormViewModel(
+      productName = params.get(),
+      createHouseSessionAndPriceIntentUseCase = get(),
+      submitHouseFormAndGetOffersUseCase = get(),
     )
   }
 }
