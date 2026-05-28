d8771f671df7727ed9653f973f1f13090c9fadc8
stylianosgakis <stylianos.gakis98@gmail.com>
2026-05-28 11:00:42 +0200

    Adapt iOS API for observePuppyGuideAvailability with young dogs

--- app/feature/feature-help-center/src/nativeMain/kotlin/com/hedvig/android/feature/help/center/data/PuppyGuideAvailability.kt
+++ app/feature/feature-help-center/src/nativeMain/kotlin/com/hedvig/android/feature/help/center/data/PuppyGuideAvailability.kt
@@ -8,15 +8,29 @@ import kotlinx.coroutines.launch
 import org.koin.mp.KoinPlatform
 
 @Suppress("unused") // Used from iOS
-fun observePuppyGuideAvailability(onResult: (Boolean) -> Unit): PuppyGuideAvailabilityCancellable {
+fun observePuppyGuideAvailability(
+  onResult: (PuppyGuidePresentation?) -> Unit,
+): PuppyGuideAvailabilityCancellable {
   val useCase = KoinPlatform.getKoin().get<GetPuppyGuideUseCase>()
   val scope = CoroutineScope(Dispatchers.Main)
   val job: Job = scope.launch {
     useCase.invoke().collectLatest { either ->
-      onResult(either.getOrNull()?.stories?.isNotEmpty() == true)
+      val puppyGuide = either.getOrNull()
+      val presentation = when {
+        puppyGuide == null || puppyGuide.stories.isEmpty() -> null
+        puppyGuide.isForYoungDog == true -> PuppyGuidePresentation.FullCard
+        else -> PuppyGuidePresentation.QuickAction
+      }
+      onResult(presentation)
     }
   }
   return PuppyGuideAvailabilityCancellable { job.cancel() }
+}
+
+sealed class PuppyGuidePresentation {
+  object FullCard : PuppyGuidePresentation()
+
+  object QuickAction : PuppyGuidePresentation()
 }
 
 class PuppyGuideAvailabilityCancellable internal constructor(private val onCancel: () -> Unit) {
