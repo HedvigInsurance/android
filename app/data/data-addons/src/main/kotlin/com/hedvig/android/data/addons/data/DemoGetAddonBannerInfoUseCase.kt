a954bd163d4c2c27de20ccfc7d097787303591e0
stylianosgakis <stylianos.gakis98@gmail.com>
2026-06-01 01:48:08 +0300

    style: use imported Metro annotations in data-addons

--- app/data/data-addons/src/main/kotlin/com/hedvig/android/data/addons/data/DemoGetAddonBannerInfoUseCase.kt
+++ app/data/data-addons/src/main/kotlin/com/hedvig/android/data/addons/data/DemoGetAddonBannerInfoUseCase.kt
@@ -3,10 +3,11 @@ package com.hedvig.android.data.addons.data
 import arrow.core.Either
 import arrow.core.right
 import com.hedvig.android.core.common.ErrorMessage
+import dev.zacsweers.metro.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
-@dev.zacsweers.metro.Inject
+@Inject
 class DemoGetAddonBannerInfoUseCase : GetAddonBannerInfoUseCase {
   override fun invoke(source: AddonBannerSource): Flow<Either<ErrorMessage, List<AddonBannerInfo>>> {
     return flowOf(emptyList<AddonBannerInfo>().right())
