0835a912fd27d0c19def2b66e4aef3fac31223f4
stylianosgakis <stylianos.gakis98@gmail.com>
2026-06-02 13:30:33 +0300

    refactor(di): self-bind forever demo/prod provider via binding<Provider<T>>
    Annotate the ProdOrDemoProvider with @ContributesBinding(binding<Provider<T>>())
    so Metro generates the binding in-module. The provider and impls become internal
    while keeping the ProdOrDemoProvider forcing function and consumers'
    Provider<T>.provide() calls untouched. The ForeverRepository interface stays
    public because the public ForeverViewModel injects Provider<ForeverRepository>
    and is referenced cross-module by navigation. The @Provides factory module is no
    longer needed.

--- app/shared/forever-ui/src/main/kotlin/com/hedvig/android/shared/foreverui/ui/data/ForeverRepositoryProvider.kt
+++ app/shared/forever-ui/src/main/kotlin/com/hedvig/android/shared/foreverui/ui/data/ForeverRepositoryProvider.kt
@@ -1,10 +1,22 @@
 package com.hedvig.android.shared.foreverui.ui.data
 
+import com.apollographql.apollo.ApolloClient
+import com.hedvig.android.core.common.di.AppScope
 import com.hedvig.android.core.demomode.DemoManager
 import com.hedvig.android.core.demomode.ProdOrDemoProvider
+import com.hedvig.android.core.demomode.Provider
+import dev.zacsweers.metro.ContributesBinding
+import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.SingleIn
+import dev.zacsweers.metro.binding
 
+@Inject
+@SingleIn(AppScope::class)
+@ContributesBinding(AppScope::class, binding<Provider<ForeverRepository>>())
 internal class ForeverRepositoryProvider(
   override val demoManager: DemoManager,
-  override val demoImpl: ForeverRepository,
-  override val prodImpl: ForeverRepository,
-) : ProdOrDemoProvider<ForeverRepository>
+  apolloClient: ApolloClient,
+) : ProdOrDemoProvider<ForeverRepository> {
+  override val prodImpl: ForeverRepository = ForeverRepositoryImpl(apolloClient)
+  override val demoImpl: ForeverRepository = ForeverRepositoryDemo()
+}
