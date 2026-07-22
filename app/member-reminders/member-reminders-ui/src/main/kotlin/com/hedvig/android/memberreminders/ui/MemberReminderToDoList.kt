1c80368e56f6d24774c0a67caa520b07757dfe3a
stylianosgakis <stylianos.gakis98@gmail.com>
2026-07-22 11:01:31 +0200

    Fix a11y problem with text in dark mode

--- app/member-reminders/member-reminders-ui/src/main/kotlin/com/hedvig/android/memberreminders/ui/MemberReminderToDoList.kt
+++ app/member-reminders/member-reminders-ui/src/main/kotlin/com/hedvig/android/memberreminders/ui/MemberReminderToDoList.kt
@@ -196,7 +196,7 @@ private fun ToDoRow(icon: ImageVector, title: String, onClick: () -> Unit, modif
       HedvigText(
         text = "Requires action",
         style = HedvigTheme.typography.label,
-        color = HedvigTheme.colorScheme.signalRedText,
+        color = HedvigTheme.colorScheme.signalRedElement,
       )
     }
     Icon(
