ebf990107c5e1e3e0e3fb9de90052eb917fe87d0
stylianosgakis <stylianos.gakis98@gmail.com>
2026-08-11 14:29:02 +0200

    Log analytics consent + effective collection state for on-device QA
    Emit a DEBUG line (tag AnalyticsConsentDebug) whenever consent is applied, so the
    consent gate can be verified with adb logcat instead of Firebase DebugView.

--- app/tracking/tracking-firebase/src/main/kotlin/com/hedvig/android/tracking/firebase/ConsentAwareEventTrackingClient.kt
+++ app/tracking/tracking-firebase/src/main/kotlin/com/hedvig/android/tracking/firebase/ConsentAwareEventTrackingClient.kt
@@ -6,6 +6,7 @@ import com.hedvig.android.core.common.di.IoDispatcher
 import com.hedvig.android.core.tracking.EventTrackingClient
 import com.hedvig.android.data.settings.datastore.AnalyticsConsent
 import com.hedvig.android.data.settings.datastore.SettingsDataStore
+import com.hedvig.android.logger.LogPriority
 import com.hedvig.android.logger.logcat
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.Provides
@@ -83,7 +84,12 @@ internal class ConsentAwareEventTrackingClient(
   }
 
   private fun applyCollectionEnabled() {
-    val effective = synchronized(lock) { collectionRequestedEnabled && consent != AnalyticsConsent.DENIED }
+    val (effective, currentConsent) = synchronized(lock) {
+      (collectionRequestedEnabled && consent != AnalyticsConsent.DENIED) to consent
+    }
+    logcat(LogPriority.DEBUG, tag = ANALYTICS_CONSENT_DEBUG_TAG) {
+      "consent=$currentConsent -> firebase analytics collection enabled=$effective"
+    }
     delegate.setCollectionEnabled(effective)
   }
 
@@ -161,6 +167,9 @@ internal class ConsentAwareEventTrackingClient(
 
   companion object {
     private const val MAX_BUFFERED_CALLS = 200
+
+    // Greppable tag for verifying analytics consent on-device: `adb logcat -s AnalyticsConsentDebug`.
+    private const val ANALYTICS_CONSENT_DEBUG_TAG = "AnalyticsConsentDebug"
   }
 }
 
