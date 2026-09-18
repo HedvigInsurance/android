2cd93fd1ed0b6d2b86ff57451e1b419b48b1f3e6
mariiapanasetskaia <mariia.panasetskaia@hedvig.com>
2026-09-18 11:43:52 +0200

    synt parents for trustly

--- app/feature/feature-connect-payment-trustly-navigation/src/commonMain/kotlin/com/hedvig/android/feature/connect/payment/trustly/ui/TrustlyKey.kt
+++ app/feature/feature-connect-payment-trustly-navigation/src/commonMain/kotlin/com/hedvig/android/feature/connect/payment/trustly/ui/TrustlyKey.kt
@@ -1,14 +1,20 @@
 package com.hedvig.android.feature.connect.payment.trustly.ui
 
+import com.hedvig.android.feature.payin.account.navigation.PayinAccountKey
+import com.hedvig.android.navigation.common.DeepLinkAncestry
 import com.hedvig.android.navigation.common.HedvigNavKey
+import com.hedvig.android.navigation.common.TopLevelTab
 import kotlinx.serialization.Serializable
 
-@Serializable
 /**
  * @param showSuccessScreen whether connecting ends on a confirmation the member dismisses. A caller
  *   that confirms the connection itself, as the onboarding step does, sets this false and gets the
  *   member handed straight back instead. Deep links leave it at the default.
  */
+@Serializable
 data class TrustlyKey(
   val showSuccessScreen: Boolean = true,
-) : HedvigNavKey
+) : HedvigNavKey, DeepLinkAncestry {
+  override val owningTab = TopLevelTab.Payments
+  override val syntheticParents = listOf(PayinAccountKey)
+}
